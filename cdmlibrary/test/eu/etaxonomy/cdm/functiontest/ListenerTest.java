f610a321d34aed6f5fadc9feae1fe9d41522de14
m.doering <m.doering@localhost>
2007-10-02 14:58:16 +0000

    moved play test functions to test package

---
+++ cdmlibrary/test/eu/etaxonomy/cdm/functiontest/ListenerTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package eu.etaxonomy.cdm.functiontest;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author a.mueller
+ *
+ */
+public class ListenerTest implements PropertyChangeListener {
+	private static final Logger logger = Logger.getLogger(ListenerTest.class);
+	
+	/* (non-Javadoc)
+	 * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
+	 */
+	public void propertyChange(PropertyChangeEvent evt) {
+		String str = " Changed from " + evt.getOldValue() + " to " + evt.getNewValue();
+		logger.info("Jippie, changes found: " + str);
+
+	}
+
+}
---
+++ cdmlibrary/test/eu/etaxonomy/cdm/functiontest/SpringControl.java
@@ -0,0 +1,97 @@
+/* just for testing */
+
+
+package eu.etaxonomy.cdm.functiontest;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.*;
+
+import eu.etaxonomy.cdm.api.application.CdmApplicationController;
+import eu.etaxonomy.cdm.model.agent.Team;
+import eu.etaxonomy.cdm.model.name.*;
+import eu.etaxonomy.cdm.persistence.dao.*;
+
+
+import java.beans.PropertyChangeListener;
+import java.util.*;
+
+public class SpringControl {
+	static Logger logger = Logger.getLogger(SpringControl.class);
+	
+	public void testBeanFactory (){
+		String fileName = "editCdm.spring.cfg.xml";
+		ClassPathResource cpr = new ClassPathResource(fileName);
+		
+		XmlBeanFactory  bf = new XmlBeanFactory(cpr);
+		ITaxonNameDao tnDao = (ITaxonNameDao)bf.getBean("tnDao");
+		TaxonName tn = tnDao.findById(1);
+		List<TaxonName> tnList = tnDao.getAllNames();
+		
+		logger.warn(tn.getUuid());
+	}
+	
+	public void testAppContext(){
+		String fileName = "editCdm.spring.cfg.xml";
+		
+		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext(fileName);
+		appContext.registerShutdownHook();
+		
+		String[] o = appContext.getBeanDefinitionNames();
+		for (int i= 0; i<o.length;i++){
+			System.out.println(o[i]);
+		}
+		
+		ITaxonNameDao tnDao = (ITaxonNameDao) appContext.getBean( "tnDao" );
+		TaxonName tn = tnDao.findById(1);
+		List<TaxonName> tnList = tnDao.getNamesByName(tn.getName());
+		for (TaxonName tn2: tnList){
+			System.out.print(tn2.getUuid()+";");
+		}
+		appContext.close();
+	}
+	
+	public void testAppController(){
+		CdmApplicationController appCtr = new CdmApplicationController();
+		TaxonName tn = appCtr.getNameService().createTaxonName(Rank.SPECIES);
+		TaxonName tn3 = appCtr.getNameService().createTaxonName(Rank.SPECIES);
+		
+		PropertyChangeListener listener = new ListenerTest();
+		tn.addPropertyChangeListener(listener);
+		tn3.addPropertyChangeListener(listener);
+		tn.setGenus("tn1-Genus1");
+		tn3.setGenus("tn3-genus");
+		tn3.getGenus();
+		
+		Team team= new Team();
+		team.setShortName("AuthorTeam1");
+		tn.setAuthorTeam(team);
+		
+		appCtr.getAgentService().saveTeam(team);
+		appCtr.getNameService().saveTaxonName(tn);
+		
+		List<TaxonName> tnList = appCtr.getNameService().getNamesByNameString("Abies alba");
+		for (TaxonName tn2: tnList){
+			System.out.print(tn2.getUuid()+";");
+		}
+		appCtr.close();
+	}
+	
+	private void test(){
+		System.out.println("Start");
+		SpringControl sc = new SpringControl();
+    	//testBeanFactory();
+    	//testAppContext();
+    	testAppController();
+    	System.out.println("\nEnd");
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void  main(String[] args) {
+		SpringControl sc = new SpringControl();
+    	sc.test();
+	}
+
+}
