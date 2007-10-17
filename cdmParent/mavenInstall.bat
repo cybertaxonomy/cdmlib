5428aa6d46988535c2f918022a67b202a4c91f0f
Andreas Müller <a.mueller@bgbm.org>
2007-10-17 12:39:58 +0000

    cdmParent

---
+++ cdmParent/mavenInstall.bat
@@ -0,0 +1,6 @@
+
+@REM template for installing files to maven repository
+@REM mvn install:install-file -DgroupId=com.sun.jdmk -DartifactId=jmxtools -Dversion=1.2.1 -Dpackaging=jar -Dfile="C:\Dokumente und Einstellungen\a.mueller\Eigene Dateien\Downloads\jmx-1_2_1-bin\lib\jmxtools.jar"
+
+@REM mvn install:install-file -DgroupId=com.sun.jmx -DartifactId=jmxri -Dversion=1.2.1 -Dpackaging=jar -Dfile="C:\Dokumente und Einstellungen\a.mueller\Eigene Dateien\Downloads\jmx-1_2_1-bin\lib\jmxri.jar"
+mvn install:install-file -DgroupId=javax.jms -DartifactId=jms -Dversion=1.1 -Dpackaging=jar -Dfile="C:\Dokumente und Einstellungen\a.mueller\Eigene Dateien\Downloads\jms1.1\lib\javax.jms.jar"
