3e22c04de1b00a42bc6b3dee6dbe771de5374d13
Andreas Müller <a.mueller@bgbm.org>
2007-12-04 12:50:41 +0000

    resourceDirectory

--- cdmlibrary/src/main/java/eu/etaxonomy/cdm/common/CdmUtils.java
+++ cdmlibrary/src/main/java/eu/etaxonomy/cdm/common/CdmUtils.java
@@ -5,15 +5,42 @@ package eu.etaxonomy.cdm.common;
 
 import java.io.File;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author a.mueller
  *
  */
 public class CdmUtils {
+	private static final Logger logger = Logger.getLogger(CdmUtils.class);
+	
+	//directory of the resources (configfiles etc.)
+	static File fileResourceDir;
+	static final String MUST_EXIST_FILE = "applicationContext.xml";
+	
 	
 	public static File getResourceDir(){
-		String strPath = System.getProperty("user.dir") + File.separator + "target" +  File.separator + "classes";
-		return new File(strPath);
+		//compute only once
+		if (fileResourceDir == null){
+			//workaround to find out in which environment the library is executed
+			String strUserDir = System.getProperty("user.dir"); //OLD:  
+			File fileJar = new File(strUserDir + File.separator + MUST_EXIST_FILE);
+			File fileEclipse = new File(strUserDir + File.separator + "target" +  File.separator + "classes" + File.separator + MUST_EXIST_FILE);
+			
+			if (fileJar.exists()){
+				// jar environment
+				fileResourceDir = fileJar.getParentFile();
+			}else if (fileEclipse.exists()){
+				// eclipse project environment
+				fileResourceDir = fileEclipse.getParentFile();
+			}else{
+				// unknown environment
+				fileResourceDir = null;
+			}
+			logger.debug("Resource directory: " + fileResourceDir.getAbsolutePath());	
+			
+		}
+		return fileResourceDir;
 	}
 	
 }
