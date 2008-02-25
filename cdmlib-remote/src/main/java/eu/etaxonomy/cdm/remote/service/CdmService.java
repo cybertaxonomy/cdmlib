7e2ccc238b6eaa7af4f6cf37be4fa536405bcde5
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2008-02-25 16:15:42 +0000

    service method headers completed - so far as the php cdm_api is completed

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/service/CdmService.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/service/CdmService.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) 2007 EDIT
+ * European Distributed Institute of Taxonomy 
+ * http://www.e-taxonomy.eu
+ * 
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * See LICENSE.TXT at the top of this package for the full license terms.
+ */
+
 package eu.etaxonomy.cdm.remote.service;
 
 import java.util.List;
@@ -5,36 +14,118 @@ import java.util.Set;
 import java.util.UUID;
 
 import eu.etaxonomy.cdm.remote.dto.NameTO;
+import eu.etaxonomy.cdm.remote.dto.NameTypeDesignationSTO;
+import eu.etaxonomy.cdm.remote.dto.ReferenceSTO;
+import eu.etaxonomy.cdm.remote.dto.ReferenceTO;
+import eu.etaxonomy.cdm.remote.dto.ReferencedEntityBaseSTO;
 import eu.etaxonomy.cdm.remote.dto.ResultSetPageSTO;
+import eu.etaxonomy.cdm.remote.dto.SpecimenTypeDesignationSTO;
 import eu.etaxonomy.cdm.remote.dto.TaxonSTO;
 import eu.etaxonomy.cdm.remote.dto.TaxonTO;
 import eu.etaxonomy.cdm.remote.dto.TreeNode;
 
-/*
- * Methods adopted from ws_method as 
- * found in http://dev.e-taxonomy.eu/svn/trunk/drupal/modules/cdm_dataportal/cdm_api.module
+/**
+ * Methods adopted from ws_method as found in
+ * http://dev.e-taxonomy.eu/svn/trunk/drupal/modules/cdm_dataportal/cdm_api.module
+ * 
+ * @author m.doering
+ * @author a.kohlbecker
+ * @version 1.0
+ * @created 30.1.2008
  */
 public interface CdmService {
 
-	public Class whatis(UUID uuid); 
+	public Class whatis(UUID uuid);
 
-	public NameTO getName(UUID uuid);// throws BusinessLogicException;  
-	
-	public TaxonTO getTaxon(UUID uuid); 
-	
+	public NameTO getName(UUID uuid);// throws BusinessLogicException;
+
+	public TaxonTO getTaxon(UUID uuid);
+
+	public ReferenceTO getReference(UUID uuid);
+
 	/**
-	 * @param q : name querystring
-	 * @param sec
-	 * @param higherTaxa
-	 * @param matchAnywhere
-	 * @param onlyAccepted
-	 * @param pagesize
-	 * @param page
-	 * @return
+	 * @param uuid
+	 *            the name UUID
+	 * @return a Set of typer designation which are assigned to the name given
+	 *         as parameter. The Set may contain {@link NameTypeDesignationSTO}
+	 *         and {@link SpecimenTypeDesignationSTO}
 	 */
-	public ResultSetPageSTO<TaxonSTO> findTaxa(String q, UUID sec, Set<UUID> higherTaxa, boolean matchAnywhere, boolean onlyAccepted, int pagesize, int page);
-	
-	public ResultSetPageSTO<TreeNode> getParentTaxa(String beginsWith, boolean onlyAccepted);
-	
-	
+	public Set<ReferencedEntityBaseSTO> getTypes(UUID uuid);
+
+	/**
+	 * @return A Set of all available concept references
+	 */
+	public Set<ReferenceSTO> getAllSecReferences();
+
+	/**
+	 * @param uuid
+	 *            the UUID of a {@link eu.etaxonomy.model.taxon.Taxon}
+	 * @return the taxon which is the accepted synonym for the taxon given as
+	 *         parameter uuid. If the taxon specified by uuid is itself the
+	 *         accepted taxon, this one will be returned.
+	 */
+	public TaxonSTO getAcceptedTaxon(UUID uuid);
+
+	/**
+	 * Find taxa matching the query defined by the given parameters.
+	 * 
+	 * @param q
+	 *            name query string
+	 * @param sec
+	 *            the UUID of the concept reference
+	 * @param higherTaxa
+	 *            Set of taxon UUIDs, if higherTaxa are defined only taxa which
+	 *            are included in one of these taxa are taken in to account
+	 * @param matchAnywhere
+	 *            match beginning of a name or any inner substring.
+	 *            <code>false</code> should be default for a web service
+	 *            implementation.
+	 * @param onlyAccepted
+	 *            return only taxa which are accepted in the sence of the
+	 *            concept reference as given by parameter sec
+	 * @param pagesize
+	 *            maximum number of items per result page
+	 * @param page
+	 *            the number of the page in request
+	 * @return a ResultSetPageSTO<TaxonSTO> instance
+	 */
+	public ResultSetPageSTO<TaxonSTO> findTaxa(String q, UUID sec,
+			Set<UUID> higherTaxa, boolean matchAnywhere, boolean onlyAccepted,
+			int pagesize, int page);
+
+	/**
+	 * Searches the concept taxon tree for all parent taxa by walking the tree
+	 * from the taxon which is referenced by the parameter uuid down to its
+	 * root. The reference taxon will also be included into the returned set of
+	 * {@link TreeNode} instances.
+	 * 
+	 * @param uuid
+	 *            the UUID of the {@link eu.etaxonomy.model.taxon.Taxon}
+	 * @return A List of all parent taxa including the one referenced by the
+	 *         parameter uuid. The {@link TreeNode} elements are ordered in the
+	 *         direction from root up to the references taxon.
+	 */
+	public List<TreeNode> getParentTaxa(UUID uuid);
+
+	/**
+	 * Getter to retrieve the children of the taxon referenced by the parameter
+	 * uuid.
+	 * 
+	 * @param uuid
+	 *            the UUID of the {@link eu.etaxonomy.model.taxon.Taxon}
+	 * @return A List of all taxa which are children in of the referenced taxon
+	 *         in the concept tree.
+	 */
+	public List<TreeNode> getChildrenTaxa(UUID uuid);
+
+	/**
+	 * Gets the root nodes of the taxonomic concept tree for the concept
+	 * reference specified by the uuid parameter.
+	 * 
+	 * @param uuid
+	 *            the concept reference uuid
+	 * @return the List of root {@link TreeNode}s
+	 */
+	public List<TreeNode> getRootTaxa(UUID uuid);
+
 }
