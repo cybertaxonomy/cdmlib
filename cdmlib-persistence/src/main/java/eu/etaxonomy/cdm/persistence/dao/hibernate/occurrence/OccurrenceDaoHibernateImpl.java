8b794d2515c4f33a16e4b9be38f59ae0f2f1a681
a.babadshanjan <a.babadshanjan@localhost>
2008-09-01 13:02:40 +0000

    Implemented Occurrence Service. Marshalling / unmarshalling SpecimenOrObservationBase.

---
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/hibernate/occurrence/OccurrenceDaoHibernateImpl.java
@@ -0,0 +1,37 @@
+/**
+* Copyright (C) 2008 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+*/
+
+package eu.etaxonomy.cdm.persistence.dao.hibernate.occurrence;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.log4j.Logger;
+import org.hibernate.criterion.Criterion;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import eu.etaxonomy.cdm.model.occurrence.SpecimenOrObservationBase;
+import eu.etaxonomy.cdm.model.taxon.TaxonBase;
+import eu.etaxonomy.cdm.persistence.dao.hibernate.common.IdentifiableDaoBase;
+import eu.etaxonomy.cdm.persistence.dao.hibernate.taxon.TaxonDaoHibernateImpl;
+import eu.etaxonomy.cdm.persistence.dao.occurrence.IOccurrenceDao;
+
+/**
+ * @author a.babadshanjan
+ * @created 01.09.2008
+ */
+@Repository
+public class OccurrenceDaoHibernateImpl extends IdentifiableDaoBase<SpecimenOrObservationBase> implements
+		IOccurrenceDao {
+
+	static Logger logger = Logger.getLogger(TaxonDaoHibernateImpl.class);
+
+	public OccurrenceDaoHibernateImpl() {
+		super(SpecimenOrObservationBase.class);
+	}
+	
+}
---
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/occurrence/IOccurrenceDao.java
@@ -0,0 +1,18 @@
+/**
+* Copyright (C) 2008 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+*/
+
+package eu.etaxonomy.cdm.persistence.dao.occurrence;
+
+import eu.etaxonomy.cdm.model.occurrence.SpecimenOrObservationBase;
+import eu.etaxonomy.cdm.persistence.dao.common.IIdentifiableDao;
+
+/**
+ * @author a.babadshanjan
+ * @created 01.09.2008
+ */
+public interface IOccurrenceDao extends IIdentifiableDao<SpecimenOrObservationBase> {
+
+}
