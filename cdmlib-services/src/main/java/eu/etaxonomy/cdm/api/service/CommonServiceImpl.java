aae87e977a8f98faa034e7941f6eef771df6db02
a.babadshanjan <a.babadshanjan@localhost>
2008-09-23 09:46:17 +0000

    Compare tables that map to a type

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/CommonServiceImpl.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/CommonServiceImpl.java
@@ -9,52 +9,23 @@
 
 package eu.etaxonomy.cdm.api.service;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import eu.etaxonomy.cdm.model.common.CdmBase;
-import eu.etaxonomy.cdm.model.common.IIdentifiableEntitiy;
 import eu.etaxonomy.cdm.model.common.ISourceable;
 import eu.etaxonomy.cdm.model.common.IdentifiableEntity;
-import eu.etaxonomy.cdm.model.common.LanguageStringBase;
-import eu.etaxonomy.cdm.model.common.OriginalSource;
-import eu.etaxonomy.cdm.model.common.ReferencedEntityBase;
-import eu.etaxonomy.cdm.model.common.Representation;
-import eu.etaxonomy.cdm.model.common.VersionableEntity;
-import eu.etaxonomy.cdm.model.description.FeatureNode;
-import eu.etaxonomy.cdm.model.description.FeatureTree;
-import eu.etaxonomy.cdm.model.media.Media;
-import eu.etaxonomy.cdm.model.name.NomenclaturalStatus;
-import eu.etaxonomy.cdm.persistence.dao.common.ICdmEntityDao;
 import eu.etaxonomy.cdm.persistence.dao.common.IOriginalSourceDao;
-import eu.etaxonomy.cdm.persistence.dao.occurrence.IOccurrenceDao;
-import eu.etaxonomy.cdm.persistence.dao.common.IRepresentationDao;
-import eu.etaxonomy.cdm.persistence.dao.name.INomenclaturalStatusDao;
 
 
 @Service
 @Transactional(readOnly = true)
 public class CommonServiceImpl extends ServiceBase<CdmBase> implements ICommonService {
 	private static final Logger logger = Logger.getLogger(CommonServiceImpl.class);
-	
-//	@Autowired
-//	@Qualifier("cdmDao")
-//	protected void setDao(ICdmEntityDao dao) {
-//		this.dao = dao;
-//	}
-	
-//	@Autowired
-//	@Qualifier("cdmDao")
-//	ICdmEntityDao dao;
 	
 	@Autowired
 	IOriginalSourceDao originalSourceDao;
