c41dd9e86007bb1acc1192637eb273d32cd93883
p.kelbert <p.kelbert@localhost>
2008-11-11 08:32:41 +0000

    separation of ABCD specimen import & Synthesys specimen import conflict resolved

--- app-import/src/main/java/eu/etaxonomy/cdm/app/abcdImport/SpecimenImport.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/abcdImport/SpecimenImport.java
@@ -4,9 +4,9 @@ import org.apache.log4j.Logger;
 import eu.etaxonomy.cdm.app.common.CdmDestinations;
 import eu.etaxonomy.cdm.database.DbSchemaValidation;
 import eu.etaxonomy.cdm.database.ICdmDataSource;
-import eu.etaxonomy.cdm.io.abcd206.SpecimenImportConfigurator;
 import eu.etaxonomy.cdm.io.common.CdmDefaultImport;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator.CHECK;
+import eu.etaxonomy.cdm.io.synthesys.SpecimenImportConfigurator;
 
 public class SpecimenImport {
 private static Logger logger = Logger.getLogger(SpecimenImport.class);
