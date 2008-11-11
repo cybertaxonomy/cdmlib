8b87d32e83b5f1c0f9c38f5a724bcd3ce7c4581a
p.kelbert <p.kelbert@localhost>
2008-11-11 08:21:14 +0000

    separation of ABCD specimen import & Synthesys specimen import

--- app-import/src/main/java/eu/etaxonomy/cdm/app/abcdImport/SpecimenImport.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/abcdImport/SpecimenImport.java
@@ -15,11 +15,8 @@ private static Logger logger = Logger.getLogger(SpecimenImport.class);
 	//database validation status (create, update, validate ...)
 	static DbSchemaValidation hbm2dll = DbSchemaValidation.CREATE;
 	final static String xmlSource = "/home/patricia/Desktop/multiABCD.xml";
-//	final static String xmlSource = "C:\\localCopy\\eclipse\\cdmlib\\app-import\\src\\main\\resources\\specimenABCD\\multiABCD.xml";
+//	final static String xmlSource = "C:\\localCopy\\eclipse\\cdmlib\\app-import\\src\\main\\resources\\specimenABCD\\multiABCD.xml";	
 	
-	
-	final static String excelSource ="/home/patricia/Desktop/CDMtabular9c04a474e2_23_09_08.xls";
-//	final static String excelSource ="C:\\localCopy\\eclipse\\cdmlib\\app-import\\src\\main\\resources\\specimenABCD\\CDMtabular9c04a474e2_23_09_08.xls";
 	
 	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_test_patricia();
 	//check - import
@@ -29,18 +26,9 @@ private static Logger logger = Logger.getLogger(SpecimenImport.class);
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String source;
-		
-		try {
-			if (args[0] == "xml")
-				source =  xmlSource;
-			else
-				source = excelSource;
-		} catch (Exception e) {
-			source =  xmlSource;
-		}
+		String source = xmlSource;
 		System.out.println(source);
-		System.out.println("Start import from Specimen data("+ source.toString() + ") ...");
+		System.out.println("Start import from  ABCD Specimen data("+ source.toString() + ") ...");
 		
 		ICdmDataSource destination = cdmDestination;
 		SpecimenImportConfigurator specimenImportConfigurator = SpecimenImportConfigurator.NewInstance(source,  destination);
