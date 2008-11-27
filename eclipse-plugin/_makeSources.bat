45b439e1f0479462cc436e392722abea2066e5f2
Andreas Müller <a.mueller@bgbm.org>
2008-11-27 00:55:28 +0000

    plugin classpath

--- eclipse-plugin/_makeSources.bat
+++ eclipse-plugin/_makeSources.bat
@@ -1,30 +1,36 @@
 
-del cdmlib-commons-1.3src.zip
-del cdmlib-commons-1.3.jar
+del cdmlib-commons-2.0src.zip
+del cdmlib-commons-2.0.jar
 
-del cdmlib-model-1.3src.zip
-del cdmlib-model-1.3.jar
+del cdmlib-model-2.0src.zip
+del cdmlib-model-2.0.jar
 
-del cdmlib-persistence-1.3src.zip
-del cdmlib-persistence-1.3.jar
+del cdmlib-persistence-2.0src.zip
+del cdmlib-persistence-2.0.jar
 
-del cdmlib-services-1.3src.zip
-del cdmlib-services-1.3.jar
+del cdmlib-services-2.0src.zip
+del cdmlib-services-2.0.jar
 
-del cdmlib-io-1.3src.zip
-del cdmlib-io-1.3.jar
+del cdmlib-io-2.0src.zip
+del cdmlib-io-2.0.jar
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\1.3\cdmlib-commons-1.3.jar" "cdmlib-commons-1.3.jar"
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\1.3\cdmlib-commons-1.3-sources.jar" "cdmlib-commons-1.3src.zip"
+del cdmlib-ext-2.0src.zip
+del cdmlib-ext-2.0.jar
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\1.3\cdmlib-model-1.3.jar" cdmlib-model-1.3.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\1.3\cdmlib-model-1.3-sources.jar" cdmlib-model-1.3src.zip
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\2.0\cdmlib-commons-2.0.jar" "cdmlib-commons-2.0.jar"
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\2.0\cdmlib-commons-2.0-sources.jar" "cdmlib-commons-2.0src.zip"
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\1.3\cdmlib-persistence-1.3.jar" cdmlib-persistence-1.3.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\1.3\cdmlib-persistence-1.3-sources.jar" cdmlib-persistence-1.3src.zip
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\2.0\cdmlib-model-2.0.jar" cdmlib-model-2.0.jar
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\2.0\cdmlib-model-2.0-sources.jar" cdmlib-model-2.0src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\1.3\cdmlib-services-1.3.jar" cdmlib-services-1.3.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\1.3\cdmlib-services-1.3-sources.jar" cdmlib-services-1.3src.zip
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\2.0\cdmlib-persistence-2.0.jar" cdmlib-persistence-2.0.jar
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\2.0\cdmlib-persistence-2.0-sources.jar" cdmlib-persistence-2.0src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\1.3\cdmlib-io-1.3.jar" cdmlib-io-1.3.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\1.3\cdmlib-io-1.3-sources.jar" cdmlib-io-1.3src.zip
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\2.0\cdmlib-services-2.0.jar" cdmlib-services-2.0.jar
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\2.0\cdmlib-services-2.0-sources.jar" cdmlib-services-2.0src.zip
+
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0.jar" cdmlib-io-2.0.jar
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0-sources.jar" cdmlib-io-2.0src.zip
+
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0.jar" cdmlib-ext-2.0.jar
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0-sources.jar" cdmlib-ext-2.0src.zip
