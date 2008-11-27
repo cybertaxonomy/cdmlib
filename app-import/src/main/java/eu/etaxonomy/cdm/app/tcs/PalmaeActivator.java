1f8bfd79caad928379c5df0fefe8a065499e8952
Andreas Müller <a.mueller@bgbm.org>
2008-11-27 12:54:22 +0000

    Distribution list for palmae

--- app-import/src/main/java/eu/etaxonomy/cdm/app/tcs/PalmaeActivator.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/tcs/PalmaeActivator.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 
 import org.apache.log4j.Logger;
 
+import eu.etaxonomy.cdm.api.application.CdmApplicationController;
 import eu.etaxonomy.cdm.app.common.CdmDestinations;
 import eu.etaxonomy.cdm.database.DbSchemaValidation;
 import eu.etaxonomy.cdm.database.ICdmDataSource;
@@ -20,6 +21,9 @@ import eu.etaxonomy.cdm.io.common.CdmDefaultImport;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator.CHECK;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator.DO_REFERENCES;
 import eu.etaxonomy.cdm.io.tcs.TcsImportConfigurator;
+import eu.etaxonomy.cdm.model.description.Feature;
+import eu.etaxonomy.cdm.model.description.FeatureNode;
+import eu.etaxonomy.cdm.model.description.FeatureTree;
 
 /**
  * @author a.mueller
@@ -32,11 +36,11 @@ public class PalmaeActivator {
 	//database validation status (create, update, validate ...)
 	static DbSchemaValidation hbm2dll = DbSchemaValidation.CREATE;
 	static final String tcsSource = TcsSources.arecaceae_local();
-	static final ICdmDataSource cdmDestination = CdmDestinations.localH2();
+	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_v1_palmae();
 //	static final ICdmDataSource cdmDestination = CdmDestinations.localH2();
 
-//	static final ICdmDataSource cdmDestination = CdmDestinations.cdm_portal_test_localhost();
-	
+	static final UUID featureTreeUuid = UUID.fromString("72ccce05-7cc8-4dab-8e47-bf3f5fd848a0");
+		
 	static final UUID secUuid = UUID.fromString("5f32b8af-0c97-48ac-8d33-6099ed68c625");
 	static final String sourceSecId = "palm_pub_ed_999999";
 	//should the taxonX import run as well?
@@ -95,9 +99,53 @@ public class PalmaeActivator {
 		CdmDefaultImport<TcsImportConfigurator> tcsImport = new CdmDefaultImport<TcsImportConfigurator>();
 		//new Test().invoke(tcsImportConfigurator);
 		success &= tcsImport.invoke(tcsImportConfigurator);
+		
+		
+		//make feature tree
+		logger.info("Make feature tree");
+		CdmApplicationController app = tcsImportConfigurator.getCdmAppController();
+		
+
+		
+		FeatureTree tree = getFeatureTree();
+		app.getDescriptionService().saveFeatureTree(tree);
 		System.out.println("End import from TCS ("+ source.toString() + ")...");
 		
 		return success;
+	}
+	
+	
+	private FeatureTree getFeatureTree(){
+		
+		FeatureTree result = FeatureTree.NewInstance(featureTreeUuid);
+		FeatureNode root = result.getRoot();
+		
+		FeatureNode newNode = FeatureNode.NewInstance(Feature.DIAGNOSIS());
+		root.addChild(newNode);
+		newNode = FeatureNode.NewInstance(Feature.DESCRIPTION());
+		root.addChild(newNode);
+		newNode = FeatureNode.NewInstance(Feature.DISTRIBUTION());
+		root.addChild(newNode);
+		newNode = FeatureNode.NewInstance(Feature.BIOLOGY_ECOLOGY());
+		root.addChild(newNode);
+		newNode = FeatureNode.NewInstance(Feature.CONSERVATION());
+		root.addChild(newNode);
+		newNode = FeatureNode.NewInstance(Feature.ETYMOLOGY());
+		root.addChild(newNode);
+		newNode = FeatureNode.NewInstance(Feature.COMMON_NAME());
+		root.addChild(newNode);
+		newNode = FeatureNode.NewInstance(Feature.USES());
+		root.addChild(newNode);
+		newNode = FeatureNode.NewInstance(Feature.CULTIVATION());
+		root.addChild(newNode);
+		newNode = FeatureNode.NewInstance(Feature.DISCUSSION());
+		root.addChild(newNode);
+		newNode = FeatureNode.NewInstance(Feature.MATERIALS_EXAMINED());
+		root.addChild(newNode);
+
+		return result;
+		
+
 	}
 
 	/**
