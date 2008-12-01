92e0969f177cbdb973df35735dd6047f11c6fe82
a.babadshanjan <a.babadshanjan@localhost>
2008-12-01 15:14:17 +0000

    Plugin version 1.4.01.08

--- eclipse-plugin/_makeSources.bat
+++ eclipse-plugin/_makeSources.bat
@@ -14,17 +14,17 @@ del cdmlib-services-1.4.jar
 del cdmlib-io-1.4src.zip
 del cdmlib-io-1.4.jar
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\1.4\cdmlib-commons-1.4.jar" "cdmlib-commons-1.4.jar"
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\1.4\cdmlib-commons-1.4-sources.jar" "cdmlib-commons-1.4src.zip"
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-commons\1.4\cdmlib-commons-1.4.jar" "cdmlib-commons-1.4.jar"
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-commons\1.4\cdmlib-commons-1.4-sources.jar" "cdmlib-commons-1.4src.zip"
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\1.4\cdmlib-model-1.4.jar" cdmlib-model-1.4.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\1.4\cdmlib-model-1.4-sources.jar" cdmlib-model-1.4src.zip
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-model\1.4\cdmlib-model-1.4.jar" cdmlib-model-1.4.jar
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-model\1.4\cdmlib-model-1.4-sources.jar" cdmlib-model-1.4src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\1.4\cdmlib-persistence-1.4.jar" cdmlib-persistence-1.4.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\1.4\cdmlib-persistence-1.4-sources.jar" cdmlib-persistence-1.4src.zip
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-persistence\1.4\cdmlib-persistence-1.4.jar" cdmlib-persistence-1.4.jar
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-persistence\1.4\cdmlib-persistence-1.4-sources.jar" cdmlib-persistence-1.4src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\1.4\cdmlib-services-1.4.jar" cdmlib-services-1.4.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\1.4\cdmlib-services-1.4-sources.jar" cdmlib-services-1.4src.zip
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-services\1.4\cdmlib-services-1.4.jar" cdmlib-services-1.4.jar
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-services\1.4\cdmlib-services-1.4-sources.jar" cdmlib-services-1.4src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\1.4\cdmlib-io-1.4.jar" cdmlib-io-1.4.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\1.4\cdmlib-io-1.4-sources.jar" cdmlib-io-1.4src.zip
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-io\1.4\cdmlib-io-1.4.jar" cdmlib-io-1.4.jar
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-io\1.4\cdmlib-io-1.4-sources.jar" cdmlib-io-1.4src.zip
