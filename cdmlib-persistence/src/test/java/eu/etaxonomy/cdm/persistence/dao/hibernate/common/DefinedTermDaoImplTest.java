4b7a458b160e6f1c64f6bfab97e099c23c0ad4c8
ben.clark <ben.clark@localhost>
2008-12-12 11:19:55 +0000

    refactored DefinedTermDaoImplTest to use unitils / dbunit

--- cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/dao/hibernate/common/DefinedTermDaoImplTest.java
+++ cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/dao/hibernate/common/DefinedTermDaoImplTest.java
@@ -1,91 +1,94 @@
 package eu.etaxonomy.cdm.persistence.dao.hibernate.common;
 
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import java.util.List;
 import java.util.UUID;
 
-import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.unitils.dbunit.annotation.DataSet;
+import org.unitils.spring.annotation.SpringBeanByType;
 
 import eu.etaxonomy.cdm.model.common.DefinedTermBase;
-import eu.etaxonomy.cdm.model.common.Keyword;
 import eu.etaxonomy.cdm.model.common.Language;
-import eu.etaxonomy.cdm.model.common.TermVocabulary;
-import eu.etaxonomy.cdm.model.common.WrongTermTypeException;
 import eu.etaxonomy.cdm.persistence.dao.common.IDefinedTermDao;
-import eu.etaxonomy.cdm.test.unit.CdmUnitTestBase;
+import eu.etaxonomy.cdm.test.integration.CdmIntegrationTest;
 
-public class DefinedTermDaoImplTest extends CdmUnitTestBase{
-	static Logger logger = Logger.getLogger(DefinedTermDaoImplTest.class);
+@DataSet
+public class DefinedTermDaoImplTest extends CdmIntegrationTest {
 
-	@Autowired
+	@SpringBeanByType
 	private IDefinedTermDao dao;
-
-	@Test
-	public void save() throws WrongTermTypeException {
-		TermVocabulary<DefinedTermBase> vocabulary	= new TermVocabulary<DefinedTermBase>("Biological subdomains","biodomain",null, "http://myterms.org/biodomain");
-		String [] repres = {"genetics","physiology","systematics","taxonomy","nomenclature"};
-		for (String r : repres){
-			Keyword term = Keyword.NewInstance(r,r, null);
-			vocabulary.addTerm(term);			
-		}
-		for (DefinedTermBase dt : vocabulary){
-			dao.save(dt);
-		}
+	
+	private UUID uuid;
+	private UUID armUuid;
+	
+	@Before
+	public void setUp() {
+		uuid = UUID.fromString("c0a18ec0-c838-11dd-ad8b-0800200c9a66");
+		armUuid = UUID.fromString("7a0fde13-26e9-4382-a5c9-5640fc2b3334");
 	}
 
 	@Test
 	public void findByTitle() {
-		List<DefinedTermBase> terms = dao.findByTitle("En");
-		logger.debug("Results: "+terms.size());
-		for (DefinedTermBase dt:terms){
-			logger.debug(dt.toString());
-		}
-		//fail();
-		logger.warn("findByTitle - Not yet implemented");
-		//assertTrue(terms.contains(this.vocabulary));
+		List<DefinedTermBase> terms = dao.findByTitle("nomenclature");
+		assertNotNull("findByTitle should return a List", terms);
+		assertEquals("findByTitle should return one term ",terms.size(),1);
+		assertEquals("findByTitle should return a term with uuid " + uuid,terms.get(0).getUuid(),uuid);
 	}
 
+	/**
+	 * FIXME Should list() be tested in CdmEntityDaoBaseTest?
+	 * Also - how is this list sorted? Should we supply an enum that allows
+	 * the list to be sorted by different fields (titleCache? label? text? uri?)
+	 */
 	@Test
 	public void listOneTerm() {
-		logger.debug("TEST: List 1 defined term");
-		List<DefinedTermBase> terms = dao.list(3, 3);
-		for (DefinedTermBase dt:terms){
-			logger.debug("Loaded term: "+dt.toString());
-		}
-	}
-	
-	@Test
-	public void getTermByUUID() {
-		String strUuid = "eeaea868-c4c1-497f-b9fe-52c9fc4aca53";
-		logger.debug("TEST: getTermByUUID " + strUuid);
-		DefinedTermBase dt = dao.findByUuid(UUID.fromString(strUuid));
-		//logger.debug("Loaded term: "+dt.toString());
+		List<DefinedTermBase> terms = dao.list(1,489);
+		assertNotNull("list should return a list",terms);
+		assertEquals("list should return one term",1, terms.size());
+		assertEquals("list should return one term with uuid " + uuid, uuid,terms.get(0).getUuid());		
 	}
 	
 	@Test
 	public void listManyTerms() {
-		logger.debug("TEST: List 10 defined terms");
-		List<DefinedTermBase> terms = dao.list(10, 0);
-		for (DefinedTermBase dt:terms){
-			logger.debug("Loaded term: "+dt.toString());
-		}
+		List<DefinedTermBase> terms = dao.list(5,485);
+		assertNotNull("list should return a list",terms);
+		assertEquals("list should return five terms",5, terms.size());
+		assertEquals("list should return a term with uuid " + uuid + " at position 5", uuid,terms.get(4).getUuid());	
 	}
 	
 	@Test
-	public void getLanguageByIso() {
+	public void getTermByUUID() {
+		DefinedTermBase term = dao.findByUuid(uuid);
+		assertNotNull("findByUuid should return a term",term);
+	}
+
+	
+	@Test
+	public void getLanguageByIso2() {
 		Language lang = dao.getLanguageByIso("arm");
-		logger.debug("Result for 'arm': " + lang.toString());
-		assertEquals(lang.toString(), "arm");
-		lang = dao.getLanguageByIso("hy");
-		logger.debug("Result for 'hy': " + lang.toString());
-		assertEquals(lang.toString(), "arm");
-		lang = dao.getLanguageByIso("a");
-		assertNull(lang);
-		lang = dao.getLanguageByIso("abcd");
-		assertNull(lang);
+		assertEquals("getLanguageByIso should return the correct Language instance",lang.getUuid(), armUuid);
+	}
+	
+	@Test
+	public void getLanguageByIso1() {
+		Language lang = dao.getLanguageByIso("hy");
+		assertEquals("getLanguageByIso should return the correct Language instance",lang.getUuid(), armUuid);
+	}
+	
+	@Test
+	public void getLanguageByMalformedIso1() {
+		Language lang = dao.getLanguageByIso("a");
+		assertNull("getLanguageByIso should return null for this malformed Iso \'a\'",lang);
+	}
+	
+	@Test
+	public void getLanguageByMalformedIso2() {
+		Language lang = dao.getLanguageByIso("abcd");
+		assertNull("getLanguageByIso should return null for this malformed Iso \'abcd\'",lang);
 	}
 }
