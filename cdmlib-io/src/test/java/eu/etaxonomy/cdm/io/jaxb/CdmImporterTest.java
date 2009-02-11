e436e17c535d14fdb257d2a561a66c63c4037963
a.babadshanjan <a.babadshanjan@localhost>
2009-02-11 12:59:36 +0000

    Filtered table DB_VERSION from comparison

--- cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/jaxb/CdmImporterTest.java
+++ cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/jaxb/CdmImporterTest.java
@@ -54,13 +54,35 @@ public class CdmImporterTest extends CdmTransactionalIntegrationTest{
 	}
 
 	protected void testExpectedDataSet(InputStream dataSet) {
+		
+		final String dbVersionTable = "DB_VERSION";
 		try {
 			IDatabaseConnection databaseConnection = getConnection();
 			
 			IDataSet actualDataSet = databaseConnection.createDataSet();
 			IDataSet expectedDataSet = new FlatXmlDataSet(dataSet, this.getClass().getResourceAsStream("/eu/etaxonomy/cdm/io/dataset.dtd"));
+			
+//          Filter the table DB_VERSION
+			String[] actualTables = actualDataSet.getTableNames();
+			int nbrOfTables = actualTables.length - 1;
+			String[] filteredTables = new String[nbrOfTables];
+			for (int i = 0, j = 0; i < actualTables.length; i++) {
+				if (!actualTables[i].equals(dbVersionTable)) {
+					filteredTables[j] = actualTables[i];
+					j++;
+				}
+			}
+			IDataSet filteredActualDataSet = databaseConnection.createDataSet(filteredTables);
+			Assertion.assertEquals(expectedDataSet, filteredActualDataSet);
 
-			Assertion.assertEquals(expectedDataSet,actualDataSet);
+//          Instead of filtering the entire table DB_VERSION as above,
+//			it might be more elegant to filter the column VERSION_TIMESTAMP only (see commented code below).
+//          Need to find a way to add the filtered table back to the actual data set.
+			
+//			ITable actualDbVersionTable = actualDataSet.getTable(dbVersionTable);
+//			ITable filteredTable = 
+//				DefaultColumnFilter.excludedColumnsTable(actualDbVersionTable, new String[]{dbVersionTable});
+//			filteredTable.getTableMetaData();
 			
 		} catch (Exception e) {
 			System.out.println(e);
