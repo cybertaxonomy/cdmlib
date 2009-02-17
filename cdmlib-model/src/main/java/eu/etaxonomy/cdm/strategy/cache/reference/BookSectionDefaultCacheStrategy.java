e3efa09e65dfbbf2d8adcb8a3980d40fda8cd603
n.hoffmann <n.hoffmann@localhost>
2009-02-17 15:22:35 +0000

    bookSectionstrategy bugfix

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/cache/reference/BookSectionDefaultCacheStrategy.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/cache/reference/BookSectionDefaultCacheStrategy.java
@@ -8,6 +8,7 @@
 */
 package eu.etaxonomy.cdm.strategy.cache.reference;
 
+import java.lang.reflect.Field;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
@@ -17,6 +18,7 @@ import eu.etaxonomy.cdm.model.agent.TeamOrPersonBase;
 import eu.etaxonomy.cdm.model.reference.Book;
 import eu.etaxonomy.cdm.model.reference.BookSection;
 import eu.etaxonomy.cdm.model.reference.INomenclaturalReference;
+import eu.etaxonomy.cdm.model.reference.ReferenceBase;
 
 public class BookSectionDefaultCacheStrategy <T extends BookSection> extends NomRefDefaultCacheStrategyBase<T>  implements  INomenclaturalReferenceCacheStrategy<T> {
 	private static final Logger logger = Logger.getLogger(BookSectionDefaultCacheStrategy.class);
@@ -63,16 +65,39 @@ public class BookSectionDefaultCacheStrategy <T extends BookSection> extends Nom
 			return null;
 		}
 		//use booksection's publication date if it exists
-		Book inBook = bookSection.getInBook().clone();
-		if (bookSection.getDatePublished() != null){
-			inBook.setDatePublished(bookSection.getDatePublished());
-		}
+		//better solution but creates probelms with hibernate, when getFullTitleCache (TaxonNameBase) is called during session.flush. Problems occurred when opening TaxonPage in Taxonomic Editor with non-empty database
+		//Book inBook = bookSection.getInBook().clone();  
+		Book inBook = bookSection.getInBook();
+		
+	//	if (bookSection.getDatePublished() != null){
+	//		inBook.setDatePublished(bookSection.getDatePublished());
+//		}
 		String result = inBook.getNomenclaturalCitation(INomenclaturalReference.MICRO_REFERENCE_TOKEN);
 		result = getBookAuthorPart(bookSection.getInBook(), afterNomRefBookAuthor) + result;
 		//TODO beforeMicroReference should be the bookstrategy one's
 		result = result.replace(beforeMicroReference +  INomenclaturalReference.MICRO_REFERENCE_TOKEN, INomenclaturalReference.MICRO_REFERENCE_TOKEN);
 		result = "in " +  result;
+		
+		result = replaceBookYearByBookSectionYear(result, inBook, bookSection);
 		return result;
+	}
+	
+    //	replace book.datePublished by bookSection.datePublished if bookSection.datePublished != null
+	private String replaceBookYearByBookSectionYear(String result, Book inBook, T bookSection){
+		if (bookSection.getDatePublished() == null){
+			return result;
+		}
+		try {
+			Field cacheStrategyField = ReferenceBase.class.getDeclaredField("cacheStrategy");
+			cacheStrategyField.setAccessible(true);
+			NomRefDefaultCacheStrategyBase cacheStrategy = (NomRefDefaultCacheStrategyBase)cacheStrategyField.get(inBook);
+			String dateString = cacheStrategy.addYear("", inBook);
+			int lastIndexOfDateString = result.lastIndexOf(dateString);
+			result = result.replace(result, this.addYear("", bookSection));
+			return result;
+		} catch (Exception e) {
+			throw new RuntimeException("replaceBookYearByBookSectionYear has a problem", e);
+		}
 	}
 
 	/* (non-Javadoc)
