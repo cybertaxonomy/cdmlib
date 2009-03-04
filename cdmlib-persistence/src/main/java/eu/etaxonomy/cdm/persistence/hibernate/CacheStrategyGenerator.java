376a891c129268e231c6476ec6461e2bc865ea1b
Andreas Müller <a.mueller@bgbm.org>
2009-03-04 13:47:49 +0000

    Hibernate listener for caches

---
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/hibernate/CacheStrategyGenerator.java
@@ -0,0 +1,53 @@
+/**
+* Copyright (C) 2007 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+* 
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+
+package eu.etaxonomy.cdm.persistence.hibernate;
+
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.event.SaveOrUpdateEvent;
+import org.hibernate.event.SaveOrUpdateEventListener;
+
+import eu.etaxonomy.cdm.model.common.IdentifiableEntity;
+import eu.etaxonomy.cdm.model.name.NonViralName;
+
+/**
+ * @author a.mueller
+ * @created 04.03.2009
+ * @version 1.0
+ */
+public class CacheStrategyGenerator implements SaveOrUpdateEventListener {
+	private static final long serialVersionUID = -5511287200489449838L;
+	@SuppressWarnings("unused")
+	private static final Logger logger = Logger.getLogger(CacheStrategyGenerator.class);
+
+    public void onSaveOrUpdate(SaveOrUpdateEvent event) throws HibernateException {
+        Object entity = event.getObject();
+        if (entity != null){
+            Class entityClazz = entity.getClass();
+        	//title cache
+        	if(IdentifiableEntity.class.isAssignableFrom(entityClazz)) {
+        		IdentifiableEntity identifiableEntity = (IdentifiableEntity)entity;
+        		identifiableEntity.getTitleCache();
+            }
+        	
+        	//non-viral-name caches
+        	if(NonViralName.class.isAssignableFrom(entityClazz)) {
+        		NonViralName nonViralName = (NonViralName)entity;
+        		nonViralName.getFullTitleCache();
+        		nonViralName.getAuthorshipCache();
+        		nonViralName.getNameCache();
+            }
+        	
+        	
+        }
+        
+
+    }
+}
