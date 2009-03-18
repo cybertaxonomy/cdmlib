c26b25e99e1b70be718840b1cb758d327f3a9aee
a.babadshanjan <a.babadshanjan@localhost>
2009-03-18 10:46:17 +0000

    minor

--- eclipse-plugin/_makeSources.bat
+++ eclipse-plugin/_makeSources.bat
@@ -17,20 +17,20 @@ del cdmlib-io-2.0.jar
 del cdmlib-ext-2.0src.zip
 del cdmlib-ext-2.0.jar
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\2.0\cdmlib-commons-2.0.jar" "cdmlib-commons-2.0.jar"
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\2.0\cdmlib-commons-2.0-sources.jar" "cdmlib-commons-2.0src.zip"
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-commons\2.0\cdmlib-commons-2.0.jar" "cdmlib-commons-2.0.jar"
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-commons\2.0\cdmlib-commons-2.0-sources.jar" "cdmlib-commons-2.0src.zip"
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\2.0\cdmlib-model-2.0.jar" cdmlib-model-2.0.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\2.0\cdmlib-model-2.0-sources.jar" cdmlib-model-2.0src.zip
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-model\2.0\cdmlib-model-2.0.jar" cdmlib-model-2.0.jar
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-model\2.0\cdmlib-model-2.0-sources.jar" cdmlib-model-2.0src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\2.0\cdmlib-persistence-2.0.jar" cdmlib-persistence-2.0.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\2.0\cdmlib-persistence-2.0-sources.jar" cdmlib-persistence-2.0src.zip
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-persistence\2.0\cdmlib-persistence-2.0.jar" cdmlib-persistence-2.0.jar
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-persistence\2.0\cdmlib-persistence-2.0-sources.jar" cdmlib-persistence-2.0src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\2.0\cdmlib-services-2.0.jar" cdmlib-services-2.0.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\2.0\cdmlib-services-2.0-sources.jar" cdmlib-services-2.0src.zip
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-services\2.0\cdmlib-services-2.0.jar" cdmlib-services-2.0.jar
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-services\2.0\cdmlib-services-2.0-sources.jar" cdmlib-services-2.0src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0.jar" cdmlib-io-2.0.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0-sources.jar" cdmlib-io-2.0src.zip
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0.jar" cdmlib-io-2.0.jar
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0-sources.jar" cdmlib-io-2.0src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0.jar" cdmlib-ext-2.0.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0-sources.jar" cdmlib-ext-2.0src.zip
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0.jar" cdmlib-ext-2.0.jar
+copy "C:\Dokumente und Einstellungen\a.babadshanjan\.m2\repository\eu\etaxonomy\cdmlib-io\2.0\cdmlib-io-2.0-sources.jar" cdmlib-ext-2.0src.zip
