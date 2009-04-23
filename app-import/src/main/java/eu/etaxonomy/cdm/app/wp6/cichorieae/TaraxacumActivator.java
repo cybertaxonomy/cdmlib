fdf47648147312f51a296ae1e6568dc80a32870c
a.babadshanjan <a.babadshanjan@localhost>
2009-04-23 14:51:41 +0000

    New media location

--- app-import/src/main/java/eu/etaxonomy/cdm/app/wp6/cichorieae/TaraxacumActivator.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/wp6/cichorieae/TaraxacumActivator.java
@@ -63,7 +63,7 @@ public class TaraxacumActivator {
 	//Mac
 	//static final File mediaPath = new File("/Volumes/protolog/protolog/");
 	//Windows
-	static final File mediaPath = new File("\\\\Bgbm11\\Edit-WP6\\protolog");
+	static final File mediaPath = new File("\\\\media\\editwp6\\protolog");
 	// set to zero for unlimited nameFacts
 	static final int maximumNumberOfNameFacts = 0;
 	
