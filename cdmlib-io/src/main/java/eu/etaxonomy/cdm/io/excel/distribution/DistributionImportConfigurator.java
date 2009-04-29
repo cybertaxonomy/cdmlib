6c308cbe667a20a95fd0a9dca319e5b8634e65cd
a.babadshanjan <a.babadshanjan@localhost>
2009-04-29 08:50:51 +0000

    Updates for Cich distribution import

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/excel/distribution/DistributionImportConfigurator.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/excel/distribution/DistributionImportConfigurator.java
@@ -24,7 +24,7 @@ public class DistributionImportConfigurator extends ImportConfiguratorBase imple
 
 	private static final Logger logger = Logger.getLogger(DistributionImportConfigurator.class);
 	
-//	@SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	protected void makeIoClassList() {
 		ioClassList = new Class[] {
 				DistributionImporter.class
