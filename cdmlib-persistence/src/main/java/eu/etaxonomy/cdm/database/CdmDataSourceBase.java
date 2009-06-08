3afc9e19a9d208d3158a551f56f51ac7f6a608b1
a.babadshanjan <a.babadshanjan@localhost>
2009-06-08 15:15:29 +0000

    Berlin Model Facts Import: For Cichorieae DB, concatenate Systematics text data Taraxacum Import: Update sec_id of Taraxacum taxa

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/CdmDataSourceBase.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/CdmDataSourceBase.java
@@ -11,6 +11,9 @@ package eu.etaxonomy.cdm.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.apache.log4j.Logger;
 
@@ -47,4 +50,62 @@ abstract class CdmDataSourceBase implements ICdmDataSource {
 			return false;
 		}
 	}
+	
+	// TODO: Harmonize methods (DRY)
+	
+    /**
+     * Executes a query and returns the ResultSet.
+     * @return ResultSet for the query.
+     */
+    public ResultSet executeQuery (String query) {
+    	ResultSet rs;
+    	try {
+    		IDatabaseType dbType = getDatabaseType().getDatabaseType();
+    		String classString = dbType.getClassString();
+    		Class.forName(classString);
+
+    		String mUrl = dbType.getConnectionString(this);
+    		Connection mConn = DriverManager.getConnection(mUrl, getUsername(), getPassword());
+    		if (query == null){
+    			return null;
+    		}
+    		Statement mStmt = mConn.createStatement();
+    		rs = mStmt.executeQuery(query);
+    		return rs;
+    	} catch(SQLException e) {
+    		logger.error("Problems when creating Resultset for query \n  " + query + " \n" + "Exception: " + e);
+    		return null;
+    	} catch (ClassNotFoundException e) {
+    		logger.error("Database driver class could not be loaded\n" + "Exception: " + e.toString());
+    		return null;
+    	}
+    }
+	
+    /**
+     * Executes an update
+     * @return return code
+     */
+    public int executeUpdate (String sqlUpdate) {
+    	int result;
+    	try {
+    		IDatabaseType dbType = getDatabaseType().getDatabaseType();
+    		String classString = dbType.getClassString();
+    		Class.forName(classString);
+
+    		String mUrl = dbType.getConnectionString(this);
+    		Connection mConn = DriverManager.getConnection(mUrl, getUsername(), getPassword());
+    		if (sqlUpdate == null){
+    			return 0;
+    		}
+    		Statement mStmt = mConn.createStatement();
+    		result = mStmt.executeUpdate(sqlUpdate);
+    		return result;
+    	} catch(SQLException e) {
+    		logger.error("Problems when creating executing update\n  " + sqlUpdate + " \n" + "Exception: " + e);
+    		return 0;
+    	} catch (ClassNotFoundException e) {
+    		logger.error("Database driver class could not be loaded\n" + "Exception: " + e.toString());
+    		return 0;
+    	}
+    }
 }
