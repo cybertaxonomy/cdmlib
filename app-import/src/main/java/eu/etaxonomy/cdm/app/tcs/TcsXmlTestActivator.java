6ee2987ece7b4a96f0af92625253737435e43717
Andreas Müller <a.mueller@bgbm.org>
2009-06-09 14:40:48 +0000

    minor

--- app-import/src/main/java/eu/etaxonomy/cdm/app/tcs/TcsXmlTestActivator.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/tcs/TcsXmlTestActivator.java
@@ -20,7 +20,7 @@ import eu.etaxonomy.cdm.database.ICdmDataSource;
 import eu.etaxonomy.cdm.io.common.CdmDefaultImport;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator.CHECK;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator.DO_REFERENCES;
-import eu.etaxonomy.cdm.io.tcsxml.TcsXmlImportConfigurator;
+import eu.etaxonomy.cdm.io.tcsxml.in.TcsXmlImportConfigurator;
 import eu.etaxonomy.cdm.model.common.TimePeriod;
 import eu.etaxonomy.cdm.model.reference.Book;
 
