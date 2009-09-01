2a7677083c36a0c87d7ca9d491d4cf37133884b9
n.hoffmann <n.hoffmann@localhost>
2009-09-01 15:46:06 +0000

    deleted old libraries

--- eclipse-plugin/_makeSources.sh
+++ eclipse-plugin/_makeSources.sh
@@ -2,7 +2,7 @@
 
 rm ./cdmlib-commons-2.1src.zip ./cdmlib-commons-2.1.jar ./cdmlib-model-2.1src.zip \
  ./cdmlib-model-2.1.jar ./cdmlib-persistence-2.1src.zip ./cdmlib-persistence-2.1.jar \
- ./cdmlib-services-2.1src.zip ./cdmlib-persistence-2.1-tests.jar ./cdmlib-services-2.1.jar \
+ ./cdmlib-services-2.1src.zip ./cdmlib-services-2.1.jar \
  ./cdmlib-io-2.1src.zip ./cdmlib-io-2.1.jar ./cdmlib-ext-2.1src.zip ./cdmlib-ext-2.1.jar > /dev/null 2>&1 
 
 
@@ -15,7 +15,6 @@ cp $LIBDIR/cdmlib-model/2.1/cdmlib-model-2.1.jar .
 cp $LIBDIR/cdmlib-model/2.1/cdmlib-model-2.1-sources.jar ./cdmlib-model-2.1src.zip
 
 cp $LIBDIR/cdmlib-persistence/2.1/cdmlib-persistence-2.1.jar .
-cp $LIBDIR/cdmlib-persistence/2.1/cdmlib-persistence-2.1-tests.jar .
 cp $LIBDIR/cdmlib-persistence/2.1/cdmlib-persistence-2.1-sources.jar ./cdmlib-persistence-2.1src.zip
 
 cp $LIBDIR/cdmlib-services/2.1/cdmlib-services-2.1.jar .
