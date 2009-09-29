a775c0e326b8d50ab12a9e8baec846b881fb3732
Andreas Müller <a.mueller@bgbm.org>
2009-09-29 14:17:55 +0000

    changes to datasource

--- cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/test/function/TestDatabase.java
+++ cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/test/function/TestDatabase.java
@@ -56,7 +56,7 @@ public class TestDatabase {
 		
 		logger.info("Setting DB " + dbname);
 		String password = AccountStore.readOrStorePassword(dbname, server, username, null);
-		ICdmDataSource datasource = CdmDataSource.NewMySqlInstance(server, dbname, username, password, null);
+		ICdmDataSource datasource = CdmDataSource.NewMySqlInstance(server, dbname, username, password);
 		return datasource;
 	}
 	
