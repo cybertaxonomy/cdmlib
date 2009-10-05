21eca816d30bf0ba732b7a61e1f556fa6894d7e9
Andreas Müller <a.mueller@bgbm.org>
2009-10-05 10:51:29 +0000

    update eclipse-plugin for version 2.2 (scripts and MANIFEST.MF updated)

--- eclipse-plugin/_makeSources.bat
+++ eclipse-plugin/_makeSources.bat
@@ -1,36 +1,36 @@
 
-del cdmlib-commons-2.1src.zip
-del cdmlib-commons-2.1.jar
+del cdmlib-commons-2.2src.zip
+del cdmlib-commons-2.2.jar
 
-del cdmlib-model-2.1src.zip
-del cdmlib-model-2.1.jar
+del cdmlib-model-2.2src.zip
+del cdmlib-model-2.2.jar
 
-del cdmlib-persistence-2.1src.zip
-del cdmlib-persistence-2.1.jar
+del cdmlib-persistence-2.2src.zip
+del cdmlib-persistence-2.2.jar
 
-del cdmlib-services-2.1src.zip
-del cdmlib-services-2.1.jar
+del cdmlib-services-2.2src.zip
+del cdmlib-services-2.2.jar
 
-del cdmlib-io-2.1src.zip
-del cdmlib-io-2.1.jar
+del cdmlib-io-2.2src.zip
+del cdmlib-io-2.2.jar
 
-del cdmlib-ext-2.1src.zip
-del cdmlib-ext-2.1.jar
+del cdmlib-ext-2.2src.zip
+del cdmlib-ext-2.2.jar
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\2.1\cdmlib-commons-2.1.jar" "cdmlib-commons-2.1.jar"
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\2.1\cdmlib-commons-2.1-sources.jar" "cdmlib-commons-2.1src.zip"
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\2.2\cdmlib-commons-2.2.jar" "cdmlib-commons-2.2.jar"
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-commons\2.2\cdmlib-commons-2.2-sources.jar" "cdmlib-commons-2.2src.zip"
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\2.1\cdmlib-model-2.1.jar" cdmlib-model-2.1.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\2.1\cdmlib-model-2.1-sources.jar" cdmlib-model-2.1src.zip
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\2.2\cdmlib-model-2.2.jar" cdmlib-model-2.2.jar
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-model\2.2\cdmlib-model-2.2-sources.jar" cdmlib-model-2.2src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\2.1\cdmlib-persistence-2.1.jar" cdmlib-persistence-2.1.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\2.1\cdmlib-persistence-2.1-sources.jar" cdmlib-persistence-2.1src.zip
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\2.2\cdmlib-persistence-2.2.jar" cdmlib-persistence-2.2.jar
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-persistence\2.2\cdmlib-persistence-2.2-sources.jar" cdmlib-persistence-2.2src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\2.1\cdmlib-services-2.1.jar" cdmlib-services-2.1.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\2.1\cdmlib-services-2.1-sources.jar" cdmlib-services-2.1src.zip
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\2.2\cdmlib-services-2.2.jar" cdmlib-services-2.2.jar
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-services\2.2\cdmlib-services-2.2-sources.jar" cdmlib-services-2.2src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.1\cdmlib-io-2.1.jar" cdmlib-io-2.1.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.1\cdmlib-io-2.1-sources.jar" cdmlib-io-2.1src.zip
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.2\cdmlib-io-2.2.jar" cdmlib-io-2.2.jar
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-io\2.2\cdmlib-io-2.2-sources.jar" cdmlib-io-2.2src.zip
 
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-ext\2.1\cdmlib-ext-2.1.jar" cdmlib-ext-2.1.jar
-copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-ext\2.1\cdmlib-ext-2.1-sources.jar" cdmlib-ext-2.1src.zip
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-ext\2.2\cdmlib-ext-2.2.jar" cdmlib-ext-2.2.jar
+copy "C:\Dokumente und Einstellungen\a.mueller\.m2\repository\eu\etaxonomy\cdmlib-ext\2.2\cdmlib-ext-2.2-sources.jar" cdmlib-ext-2.2src.zip
--- eclipse-plugin/_makeSources.sh
+++ eclipse-plugin/_makeSources.sh
@@ -1,27 +1,27 @@
 #! /bin/sh
 
-rm ./cdmlib-commons-2.1src.zip ./cdmlib-commons-2.1.jar ./cdmlib-model-2.1src.zip \
- ./cdmlib-model-2.1.jar ./cdmlib-persistence-2.1src.zip ./cdmlib-persistence-2.1.jar \
- ./cdmlib-services-2.1src.zip ./cdmlib-services-2.1.jar \
- ./cdmlib-io-2.1src.zip ./cdmlib-io-2.1.jar ./cdmlib-ext-2.1src.zip ./cdmlib-ext-2.1.jar > /dev/null 2>&1 
+rm ./cdmlib-commons-2.2src.zip ./cdmlib-commons-2.2.jar ./cdmlib-model-2.2src.zip \
+ ./cdmlib-model-2.2.jar ./cdmlib-persistence-2.2src.zip ./cdmlib-persistence-2.2.jar \
+ ./cdmlib-services-2.2src.zip ./cdmlib-services-2.2.jar \
+ ./cdmlib-io-2.2src.zip ./cdmlib-io-2.2.jar ./cdmlib-ext-2.2src.zip ./cdmlib-ext-2.2.jar > /dev/null 2>&2 
 
 
 LIBDIR=~/.m2/repository/eu/etaxonomy
 
-cp $LIBDIR/cdmlib-commons/2.1/cdmlib-commons-2.1.jar .
-cp $LIBDIR/cdmlib-commons/2.1/cdmlib-commons-2.1-sources.jar ./cdmlib-commons-2.1src.zip
+cp $LIBDIR/cdmlib-commons/2.2/cdmlib-commons-2.2.jar .
+cp $LIBDIR/cdmlib-commons/2.2/cdmlib-commons-2.2-sources.jar ./cdmlib-commons-2.2src.zip
 
-cp $LIBDIR/cdmlib-model/2.1/cdmlib-model-2.1.jar .
-cp $LIBDIR/cdmlib-model/2.1/cdmlib-model-2.1-sources.jar ./cdmlib-model-2.1src.zip
+cp $LIBDIR/cdmlib-model/2.2/cdmlib-model-2.2.jar .
+cp $LIBDIR/cdmlib-model/2.2/cdmlib-model-2.2-sources.jar ./cdmlib-model-2.2src.zip
 
-cp $LIBDIR/cdmlib-persistence/2.1/cdmlib-persistence-2.1.jar .
-cp $LIBDIR/cdmlib-persistence/2.1/cdmlib-persistence-2.1-sources.jar ./cdmlib-persistence-2.1src.zip
+cp $LIBDIR/cdmlib-persistence/2.2/cdmlib-persistence-2.2.jar .
+cp $LIBDIR/cdmlib-persistence/2.2/cdmlib-persistence-2.2-sources.jar ./cdmlib-persistence-2.2src.zip
 
-cp $LIBDIR/cdmlib-services/2.1/cdmlib-services-2.1.jar .
-cp $LIBDIR/cdmlib-services/2.1/cdmlib-services-2.1-sources.jar ./cdmlib-services-2.1src.zip
+cp $LIBDIR/cdmlib-services/2.2/cdmlib-services-2.2.jar .
+cp $LIBDIR/cdmlib-services/2.2/cdmlib-services-2.2-sources.jar ./cdmlib-services-2.2src.zip
 
-cp $LIBDIR/cdmlib-io/2.1/cdmlib-io-2.1.jar .
-cp $LIBDIR/cdmlib-io/2.1/cdmlib-io-2.1-sources.jar ./cdmlib-io-2.1src.zip
+cp $LIBDIR/cdmlib-io/2.2/cdmlib-io-2.2.jar .
+cp $LIBDIR/cdmlib-io/2.2/cdmlib-io-2.2-sources.jar ./cdmlib-io-2.2src.zip
 
-cp $LIBDIR/cdmlib-ext/2.1/cdmlib-ext-2.1.jar .
-cp $LIBDIR/cdmlib-ext/2.1/cdmlib-ext-2.1-sources.jar ./cdmlib-ext-2.1src.zip
+cp $LIBDIR/cdmlib-ext/2.2/cdmlib-ext-2.2.jar .
+cp $LIBDIR/cdmlib-ext/2.2/cdmlib-ext-2.2-sources.jar ./cdmlib-ext-2.2src.zip
