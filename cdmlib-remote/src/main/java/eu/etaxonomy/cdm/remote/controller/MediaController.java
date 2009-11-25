72d3ab031e4495c4608630b122beeaa7e643036a
Katja Luther <k.luther@bgbm.org>
2009-11-25 15:42:24 +0000

    restservice for mediaMetaData

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/MediaController.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/MediaController.java
@@ -10,12 +10,24 @@
 
 package eu.etaxonomy.cdm.remote.controller;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import eu.etaxonomy.cdm.api.service.IMediaService;
+import eu.etaxonomy.cdm.common.mediaMetaData.MediaMetaData;
+import eu.etaxonomy.cdm.model.common.Representation;
 import eu.etaxonomy.cdm.model.media.Media;
+import eu.etaxonomy.cdm.model.media.MediaRepresentation;
 
 /**
  * TODO write controller documentation
@@ -25,7 +37,7 @@ import eu.etaxonomy.cdm.model.media.Media;
  */
 
 @Controller
-@RequestMapping(value = {"/*/media/*","/*/media/*/annotation"})
+@RequestMapping(value = {"/*/media/*","/*/media/*/annotation", "/*/media/*/metadata"})
 public class MediaController extends AnnotatableController<Media, IMediaService>
 {
 
@@ -41,6 +53,27 @@ public class MediaController extends AnnotatableController<Media, IMediaService>
 	@Override
 	public void setService(IMediaService service) {
 		this.service = service;
+	}
+	
+	@RequestMapping(value = {"/*/media/*/metadata"})
+	public Map<String, String> doGetMediaMetaData(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		Map<String, String> result;
+		Media media = getCdmBase(request, response, null, Media.class);
+		
+		Set<MediaRepresentation> representations = media.getRepresentations();
+		//hole die erste Representation und davon die Metadaten 
+
+		MediaRepresentation[] repArray = (MediaRepresentation[]) representations.toArray();
+		
+		try {
+			result = service.getImageMetaData(new URI(repArray[0].getParts().get(0).getUri()));
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		return result;
+		
 	}
 
 }
