a9b7d84130576760b89f0757aff51972cdf433a2
ben.clark <ben.clark@localhost>
2009-11-26 10:45:48 +0000

    First pass implementation of an OAI-PMH Controller for identifiable objects

---
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/oaipmh/TaxonOaiPmhController.java
@@ -0,0 +1,50 @@
+package eu.etaxonomy.cdm.remote.controller.oaipmh;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.ModelAndView;
+
+import eu.etaxonomy.cdm.api.service.ITaxonService;
+import eu.etaxonomy.cdm.model.taxon.TaxonBase;
+import eu.etaxonomy.cdm.remote.controller.AbstractOaiPmhController;
+import eu.etaxonomy.cdm.remote.dto.oaipmh.SetSpec;
+
+public class TaxonOaiPmhController extends AbstractOaiPmhController<TaxonBase, ITaxonService> {
+
+	protected List<String> getPropertyPaths() {
+		List<String> propertyPaths = new ArrayList<String>();
+		propertyPaths.add("createdBy");
+		propertyPaths.add("updatedBy");
+		propertyPaths.add("name");
+		propertyPaths.add("sec");
+		propertyPaths.add("relationsToThisTaxon");
+		propertyPaths.add("relationsToThisTaxon.fromTaxon");
+		propertyPaths.add("relationsToThisTaxon.toTaxon");
+		propertyPaths.add("relationsFromThisTaxon");
+		propertyPaths.add("relationsFromThisTaxon.toTaxon");
+		propertyPaths.add("relationsToThisTaxon.type");
+		propertyPaths.add("synonymRelations");
+		propertyPaths.add("synonymRelations.synonym");
+		propertyPaths.add("synonymRelations.type");
+		propertyPaths.add("descriptions");
+		return propertyPaths;
+	}
+    
+    @Override
+    protected void addSets(ModelAndView modelAndView) {
+    	Set<SetSpec> sets = new HashSet<SetSpec>();
+    	sets.add(SetSpec.TAXON);
+    	sets.add(SetSpec.SYNONYM);
+    	modelAndView.addObject("sets",sets);
+    }
+	
+    @Override
+    @Autowired
+	public void setService(ITaxonService service) {
+		this.service = service;
+	}
+}
