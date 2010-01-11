05c859661e802386d1effa8a78ed4b1482b3cb6b
Katja Luther <k.luther@bgbm.org>
2010-01-11 14:28:03 +0000

    #1390

--- cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/dao/hibernate/taxon/TaxonNodeDaoHibernateImplTest.java
+++ cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/dao/hibernate/taxon/TaxonNodeDaoHibernateImplTest.java
@@ -12,6 +12,9 @@ package eu.etaxonomy.cdm.persistence.dao.hibernate.taxon;
 import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import org.junit.After;
@@ -20,9 +23,12 @@ import org.junit.Test;
 import org.unitils.dbunit.annotation.DataSet;
 import org.unitils.spring.annotation.SpringBeanByType;
 
+import eu.etaxonomy.cdm.model.taxon.Taxon;
+import eu.etaxonomy.cdm.model.taxon.TaxonBase;
 import eu.etaxonomy.cdm.model.taxon.TaxonNode;
 import eu.etaxonomy.cdm.model.taxon.TaxonomicTree;
 import eu.etaxonomy.cdm.model.view.context.AuditEventContextHolder;
+import eu.etaxonomy.cdm.persistence.dao.taxon.ITaxonDao;
 import eu.etaxonomy.cdm.persistence.dao.taxon.ITaxonNodeDao;
 import eu.etaxonomy.cdm.persistence.dao.taxon.ITaxonomicTreeDao;
 import eu.etaxonomy.cdm.test.integration.CdmTransactionalIntegrationTest;
@@ -36,11 +42,16 @@ public class TaxonNodeDaoHibernateImplTest extends
 	@SpringBeanByType
 	private ITaxonomicTreeDao taxonomicTreeDao;
 	
-	private UUID uuid;
+	@SpringBeanByType
+	private ITaxonDao taxonDao;
+	
+	private UUID uuid1;
+	private UUID uuid2;
 	
 	@Before
 	public void setUp(){
-		uuid = UUID.fromString("0b5846e5-b8d2-4ca9-ac51-099286ea4adc");
+		uuid1 = UUID.fromString("0b5846e5-b8d2-4ca9-ac51-099286ea4adc");
+		uuid2 = UUID.fromString("770239f6-4fa8-496b-8738-fe8f7b2ad519");
 		AuditEventContextHolder.clearContext(); 
 	}
 	
@@ -60,7 +71,7 @@ public class TaxonNodeDaoHibernateImplTest extends
 	@Test
 	@DataSet
 	public void testFindByUuid() {
-		TaxonNode taxonNode = (TaxonNode) taxonNodeDao.findByUuid(uuid);
+		TaxonNode taxonNode = (TaxonNode) taxonNodeDao.findByUuid(uuid1);
 		TaxonomicTree.class.getDeclaredConstructors();
 		assertNotNull("findByUuid should return a taxon node", taxonNode);
 	}
@@ -73,5 +84,27 @@ public class TaxonNodeDaoHibernateImplTest extends
 		assertNotNull("findByUuid should return a taxon tree", taxonTree);
 		assertNotNull("taxonomic tree should have a name",taxonTree.getName());
 		assertEquals("taxonomic tree should have a name which is 'Name'",taxonTree.getName().getText(),"Name");
+		TaxonNode taxNode = (TaxonNode) taxonNodeDao.findByUuid(uuid1);
+		TaxonNode taxNode2 = (TaxonNode) taxonNodeDao.findByUuid(uuid2);
+		Set<TaxonNode> rootNodes = new HashSet<TaxonNode>();
+		//rootNodes.add(taxNode);
+		rootNodes.add(taxNode2);
+		taxonTree.setRootNodes(rootNodes);
+		taxNode.addChildNode(taxNode2, null, null,null);
+		
+		Taxon taxon2 = taxNode2.getTaxon();
+		Taxon taxon = taxNode.getTaxon();
+		UUID uuidTaxon = taxon.getUuid();
+		UUID uuidTaxon2 = taxon2.getUuid();
+	
+		List<TaxonBase> taxa = taxonDao.getAllTaxonBases(10, 0);
+		assertEquals("there should be only two taxa", 2, taxa.size());
+		
+		taxonNodeDao.delete(taxNode2);
+		
+		
+		taxa = taxonDao.getAllTaxonBases(10, 0);
+		assertEquals("there should be only one taxon left", 1, taxa.size());
+		
 	}
 }
