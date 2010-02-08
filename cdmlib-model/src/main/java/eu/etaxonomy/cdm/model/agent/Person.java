c7dcbe6ab999d68fa3bf1650a41ad2a122a359db
Katja Luther <k.luther@bgbm.org>
2010-02-08 09:14:08 +0000

    removed system.err.println()

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/agent/Person.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/agent/Person.java
@@ -168,9 +168,9 @@ public class Person extends TeamOrPersonBase<Person>{
 	public Person(String firstname, String lastname, String nomenclaturalTitel) {
 		this.setFirstname(firstname);
 		this.setLastname(lastname);
-		System.err.println("before - Set nomenclatural Title");
+		logger.debug("before - Set nomenclatural Title");
 		this.setNomenclaturalTitle(nomenclaturalTitel);
-		System.err.println("after - Set nomenclatural Title");
+		logger.debug("after - Set nomenclatural Title");
 	}
 	
 	
