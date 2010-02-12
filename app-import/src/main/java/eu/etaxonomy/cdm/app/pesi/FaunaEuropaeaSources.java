f2ab5b174fa8b2ace78f5bdea528524bdb60a4f7
Katja Luther <k.luther@bgbm.org>
2010-02-12 14:17:45 +0000

    fauna europaea activator moved to pesi

--- app-import/src/main/java/eu/etaxonomy/cdm/app/pesi/FaunaEuropaeaSources.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/pesi/FaunaEuropaeaSources.java
@@ -7,7 +7,7 @@
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
 
-package eu.etaxonomy.cdm.app.faunaEuropaea;
+package eu.etaxonomy.cdm.app.pesi;
 
 import org.apache.log4j.Logger;
 
