156ccc68754df8a6b94796bc4d17dd8c962d36b0
em.lee <em.lee@localhost>
2010-02-24 09:37:41 +0000

    .

---
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/pesi/out/PesiExportState.java
@@ -0,0 +1,31 @@
+// $Id$
+/**
+* Copyright (C) 2009 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+* 
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.io.pesi.out;
+
+import org.apache.log4j.Logger;
+import eu.etaxonomy.cdm.io.common.DbExportStateBase;
+
+/**
+ * @author a.mueller
+ * @author e.-m.lee
+ * @date 12.02.2010
+ *
+ */
+public class PesiExportState extends DbExportStateBase<PesiExportConfigurator>{
+	private static final Logger logger = Logger.getLogger(PesiExportState.class);
+
+	/**
+	 * @param config
+	 */
+	public PesiExportState(PesiExportConfigurator config) {
+		super(config);
+	}
+
+}
