9927c0a03213eec975fc0dfb1dbd5d82c60e292a
em.lee <em.lee@localhost>
2010-03-02 09:58:46 +0000

    Added cdm_test_eunmok.

--- app-import/src/main/java/eu/etaxonomy/cdm/app/common/CdmDestinations.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/common/CdmDestinations.java
@@ -173,6 +173,14 @@ public class CdmDestinations {
 		return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
 	}
 
+	public static ICdmDataSource cdm_test_eunmok(){
+		DatabaseTypeEnum dbType = DatabaseTypeEnum.MySQL;
+		String cdmServer = "192.168.2.10";
+		String cdmDB = "cdm_test_eunmok";
+		String cdmUserName = "edit";
+		return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
+	}
+
 	public static ICdmDataSource cdm_test_niels1(){
 		DatabaseTypeEnum dbType = DatabaseTypeEnum.MySQL;
 		String cdmServer = "192.168.2.10";
