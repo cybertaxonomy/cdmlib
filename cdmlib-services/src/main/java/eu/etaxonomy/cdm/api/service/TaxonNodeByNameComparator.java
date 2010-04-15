5b2f7c4d9c6303502534b2a98a36bd118e56a94a
Katja Luther <k.luther@bgbm.org>
2010-04-15 09:00:48 +0000

    #1605 sorting of names in tree (quotation marks should be ignored) and autonyms are handled like normal taxa

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/TaxonNodeByNameComparator.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/TaxonNodeByNameComparator.java
@@ -10,9 +10,14 @@
 package eu.etaxonomy.cdm.api.service;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Component;
 
+import eu.etaxonomy.cdm.model.name.NonViralName;
 import eu.etaxonomy.cdm.model.taxon.TaxonNode;
 
 /**
@@ -30,19 +35,61 @@ public class TaxonNodeByNameComparator extends AbstractStringComparator implemen
 	 */
 	@SuppressWarnings("unchecked")
 	public int compare(TaxonNode o1, TaxonNode o2) {
-			
+				
 		String titleCache1 = null;
 		String titleCache2 = null;
-		if(o1.getTaxon() != null && o1.getTaxon().getName() != null){
-			titleCache1 = o1.getTaxon().getName().getTitleCache();
+		
+		if(o1.getTaxon() != null && o1.getTaxon().getName() != null ){
+			
+			if (o1.getTaxon().getName() instanceof NonViralName){
+				NonViralName nonViralName = (NonViralName)o1.getTaxon().getName();
+				if (nonViralName.isInfraSpecific()){
+					if (nonViralName.getSpecificEpithet().equals(nonViralName.getInfraSpecificEpithet())){
+						titleCache1 = nonViralName.getNameCache() + " "+nonViralName.getAuthorshipCache();
+					}
+				}
+					
+			}
+			if (titleCache1 == null){
+				titleCache1 = o1.getTaxon().getName().getTitleCache();
+			}
 		}
 		if(o2.getTaxon() != null && o2.getTaxon().getName() != null){
-			titleCache2 = o2.getTaxon().getName().getTitleCache();
+			if (o2.getTaxon().getName() instanceof NonViralName){
+				NonViralName nonViralName = (NonViralName)o2.getTaxon().getName();
+				if (nonViralName.isInfraSpecific()){
+					if (nonViralName.getSpecificEpithet().equals(nonViralName.getInfraSpecificEpithet())){
+						titleCache2 = nonViralName.getNameCache() + " "+nonViralName.getAuthorshipCache();
+					}
+				}
+					
+			}
+			if (titleCache2 == null){
+				titleCache2 = o2.getTaxon().getName().getTitleCache();
+			}
 		}
 		
 		titleCache1 = applySubstitutionRules(titleCache1);
 		titleCache2 = applySubstitutionRules(titleCache2);
 		
+		StringTokenizer s2 = new StringTokenizer(titleCache1, "\""); 
+		if (s2.countTokens()>0){
+			titleCache1 = null;
+		}
+		while(s2.hasMoreTokens()){
+			titleCache1 += s2.nextToken();
+		}
+		s2 = new StringTokenizer(titleCache2, "\""); 
+		if (s2.countTokens()>0){
+			titleCache2 = null;
+		}
+		
+		while(s2.hasMoreTokens()){
+			titleCache2 += s2.nextToken();
+		}
+		
 		return titleCache1.compareTo(titleCache2);
 	}
+		
+	
 }
