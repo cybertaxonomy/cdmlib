4c8c894fb1a8cab206713cec27828e289a8ac81c
m.venin <m.venin@localhost>
2010-04-16 14:26:24 +0000

    First draft of interface for natural language generation functions.

---
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/INaturalLanguageGenerator.java
@@ -0,0 +1,22 @@
+package eu.etaxonomy.cdm.api.service;
+
+import java.util.Set;
+import java.util.List;
+
+import eu.etaxonomy.cdm.model.description.TaxonDescription;
+import eu.etaxonomy.cdm.model.description.TextData;
+import eu.etaxonomy.cdm.model.description.FeatureTree;
+import eu.etaxonomy.cdm.model.description.DescriptionBase;
+
+
+
+/**
+ * Interface for Natural Language generation 
+ * @author m.venin
+ * @date 12.04.2010
+ *
+ */
+
+public interface INaturalLanguageGenerator {
+	public List<TextData> generateNaturalLanguageDescription(FeatureTree featureTree, Set<DescriptionBase> descriptions);
+}
