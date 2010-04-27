203f951a8dab8d7af5398a748938eb7c2283516d
Andreas Müller <a.mueller@bgbm.org>
2010-04-27 18:19:27 +0000

    schema model version change

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/common/CdmMetaData.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/common/CdmMetaData.java
@@ -47,7 +47,7 @@ public class CdmMetaData extends CdmBase{
 	 * be handled by SCHEMA_VALIDATION.UPDATE
 	 * The last number represents the date of change.
 	 */
-	private static final String dbSchemaVersion = "2.4.1.1.201004011015";
+	private static final String dbSchemaVersion = "2.4.1.2.201004231015";
 	
 
 	/**
@@ -72,7 +72,7 @@ public class CdmMetaData extends CdmBase{
 	 * (e.g. when new terms were added)
 	 * The last number represents the date of change.
 	 */
-	private static final String termsVersion = "2.4.1.1.201004011015";
+	private static final String termsVersion = "2.4.1.2.201004231015";
 	
 	
 	public enum MetaDataPropertyName{
