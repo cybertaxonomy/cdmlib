fec2f2a6dd3f04d550bfc7598ee30d59a41cfde1
Andreas Müller <a.mueller@bgbm.org>
2010-04-28 11:03:40 +0000

    delete long /abbrev reference

--- cdmlib-model/src/main/resources/update/update_rXXX_rYYY.mysql.sql
+++ cdmlib-model/src/main/resources/update/update_rXXX_rYYY.mysql.sql
@@ -4,19 +4,6 @@
 -- change symmetric to symmetrical to be compatible with PostGreSQL
  ALTER TABLE `DefinedTermBase` CHANGE `symmetric` `symmetrical` bit(1) NULL  ;
  
--- new attribute fullReference in ReferenceBase
--- ALTER TABLE `Reference` ADD `fullreference_id` INT(11) NULL COMMENT 'added by CDM schema version 2.4 updater' AFTER `school_id` ;
-  -- set foreign key
--- ALTER TABLE `Reference` ADD CONSTRAINT FKA81607BA22D949E FOREIGN KEY (fullreference_id) REFERENCES `Reference` ;
- 
--- new attribute abbreviatedReference in ReferenceBase 
--- ALTER TABLE `Reference` ADD `abbreviatedreference_id` INT(11) NULL COMMENT 'added by CDM schema version 2.4 updater' AFTER `fullreference_id` ;
---   -- set foreign key
--- ALTER TABLE `Reference` ADD CONSTRAINT FKA215077A21D842A FOREIGN KEY (abbreviatedreference_id) REFERENCES `Reference` ;
-
-
-
-
 -- ##################### TERMS ########################################################
 
 --absence term max orderindex
