fb4e081186c179979c2420f5251372f75fb73b2a
n.hoffmann <n.hoffmann@localhost>
2010-05-07 15:38:16 +0000

    upgraded to cdm version 2.4

--- eclipse-plugin/_makeSources.sh
+++ eclipse-plugin/_makeSources.sh
@@ -1,27 +1,27 @@
 #! /bin/sh
 
-rm ./cdmlib-commons-2.3src.zip ./cdmlib-commons-2.3.jar ./cdmlib-model-2.3src.zip \
- ./cdmlib-model-2.3.jar ./cdmlib-persistence-2.3src.zip ./cdmlib-persistence-2.3.jar \
- ./cdmlib-services-2.3src.zip ./cdmlib-services-2.3.jar \
- ./cdmlib-io-2.3src.zip ./cdmlib-io-2.3.jar ./cdmlib-ext-2.3src.zip ./cdmlib-ext-2.3.jar > /dev/null 2>&1 
+rm ./cdmlib-commons-2.4src.zip ./cdmlib-commons-2.4.jar ./cdmlib-model-2.4src.zip \
+ ./cdmlib-model-2.4.jar ./cdmlib-persistence-2.4src.zip ./cdmlib-persistence-2.4.jar \
+ ./cdmlib-services-2.4src.zip ./cdmlib-services-2.4.jar \
+ ./cdmlib-io-2.4src.zip ./cdmlib-io-2.4.jar ./cdmlib-ext-2.4src.zip ./cdmlib-ext-2.4.jar > /dev/null 2>&1 
 
 
 LIBDIR=~/.m2/repository/eu/etaxonomy
 
-cp $LIBDIR/cdmlib-commons/2.3/cdmlib-commons-2.3.jar .
-cp $LIBDIR/cdmlib-commons/2.3/cdmlib-commons-2.3-sources.jar ./cdmlib-commons-2.3src.zip
+cp $LIBDIR/cdmlib-commons/2.4/cdmlib-commons-2.4.jar .
+cp $LIBDIR/cdmlib-commons/2.4/cdmlib-commons-2.4-sources.jar ./cdmlib-commons-2.4src.zip
 
-cp $LIBDIR/cdmlib-model/2.3/cdmlib-model-2.3.jar .
-cp $LIBDIR/cdmlib-model/2.3/cdmlib-model-2.3-sources.jar ./cdmlib-model-2.3src.zip
+cp $LIBDIR/cdmlib-model/2.4/cdmlib-model-2.4.jar .
+cp $LIBDIR/cdmlib-model/2.4/cdmlib-model-2.4-sources.jar ./cdmlib-model-2.4src.zip
 
-cp $LIBDIR/cdmlib-persistence/2.3/cdmlib-persistence-2.3.jar .
-cp $LIBDIR/cdmlib-persistence/2.3/cdmlib-persistence-2.3-sources.jar ./cdmlib-persistence-2.3src.zip
+cp $LIBDIR/cdmlib-persistence/2.4/cdmlib-persistence-2.4.jar .
+cp $LIBDIR/cdmlib-persistence/2.4/cdmlib-persistence-2.4-sources.jar ./cdmlib-persistence-2.4src.zip
 
-cp $LIBDIR/cdmlib-services/2.3/cdmlib-services-2.3.jar .
-cp $LIBDIR/cdmlib-services/2.3/cdmlib-services-2.3-sources.jar ./cdmlib-services-2.3src.zip
+cp $LIBDIR/cdmlib-services/2.4/cdmlib-services-2.4.jar .
+cp $LIBDIR/cdmlib-services/2.4/cdmlib-services-2.4-sources.jar ./cdmlib-services-2.4src.zip
 
-cp $LIBDIR/cdmlib-io/2.3/cdmlib-io-2.3.jar .
-cp $LIBDIR/cdmlib-io/2.3/cdmlib-io-2.3-sources.jar ./cdmlib-io-2.3src.zip
+cp $LIBDIR/cdmlib-io/2.4/cdmlib-io-2.4.jar .
+cp $LIBDIR/cdmlib-io/2.4/cdmlib-io-2.4-sources.jar ./cdmlib-io-2.4src.zip
 
-cp $LIBDIR/cdmlib-ext/2.3/cdmlib-ext-2.3.jar .
-cp $LIBDIR/cdmlib-ext/2.3/cdmlib-ext-2.3-sources.jar ./cdmlib-ext-2.3src.zip
+cp $LIBDIR/cdmlib-ext/2.4/cdmlib-ext-2.4.jar .
+cp $LIBDIR/cdmlib-ext/2.4/cdmlib-ext-2.4-sources.jar ./cdmlib-ext-2.4src.zip
