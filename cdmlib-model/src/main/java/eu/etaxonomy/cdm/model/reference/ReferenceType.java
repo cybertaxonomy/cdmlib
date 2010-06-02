888687b9679fa0d660e34430a1d2fdd07897752e
n.hoffmann <n.hoffmann@localhost>
2010-06-02 15:50:11 +0000

    added deprecation annotations to obsolete ReferenceTypes

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/reference/ReferenceType.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/reference/ReferenceType.java
@@ -66,8 +66,10 @@ public enum ReferenceType {
 	@XmlEnumValue("Web Page")
 	WebPage("Web Page", ReferenceBaseDefaultCacheStrategy.class), 
 	@XmlEnumValue("Printed Unit Base")
+	@Deprecated // all references are ReferenceBases this type should not longer be used. Use isPrintedUnit() for tests against this type instead.
 	PrintedUnitBase("Printed Unit Base", ReferenceBaseDefaultCacheStrategy.class), 
 	@XmlEnumValue("Publication Base")
+	@Deprecated // all references are ReferenceBases this type should not longer be used. Use isPublication() for tests against this type instead.
 	PublicationBase("Publication Base", ReferenceBaseDefaultCacheStrategy.class);
 	
 	
