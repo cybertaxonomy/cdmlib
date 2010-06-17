94f69ca300b9a67859e3f86aef279ae9ab0b70d6
Andreas Müller <a.mueller@bgbm.org>
2010-06-17 19:48:58 +0000

    specimen import refactored

--- app-import/src/main/java/eu/etaxonomy/cdm/app/synthesysImport/SpecimenImport.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/synthesysImport/SpecimenImport.java
@@ -15,7 +15,7 @@ import eu.etaxonomy.cdm.database.DbSchemaValidation;
 import eu.etaxonomy.cdm.database.ICdmDataSource;
 import eu.etaxonomy.cdm.io.common.CdmDefaultImport;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator.CHECK;
-import eu.etaxonomy.cdm.io.specimen.excel.SpecimenImportConfigurator;
+import eu.etaxonomy.cdm.io.specimen.excel.in.SpecimenExcelImportConfigurator;
 
 public class SpecimenImport {
 private static Logger logger = Logger.getLogger(SpecimenImport.class);
@@ -39,7 +39,7 @@ private static Logger logger = Logger.getLogger(SpecimenImport.class);
 		System.out.println("Start import from  Synthesys Specimen data("+ source.toString() + ") ...");
 		
 		ICdmDataSource destination = cdmDestination;
-		SpecimenImportConfigurator specimenImportConfigurator = SpecimenImportConfigurator.NewInstance(source,  destination);
+		SpecimenExcelImportConfigurator specimenImportConfigurator = SpecimenExcelImportConfigurator.NewInstance(source,  destination);
 		
 		specimenImportConfigurator.setSourceSecId("specimen");
 		specimenImportConfigurator.setCheck(check);
@@ -51,7 +51,7 @@ private static Logger logger = Logger.getLogger(SpecimenImport.class);
 		specimenImportConfigurator.setTaxonReference(null);
 		
 		// invoke import
-		CdmDefaultImport<SpecimenImportConfigurator> specimenImport = new CdmDefaultImport<SpecimenImportConfigurator>();
+		CdmDefaultImport<SpecimenExcelImportConfigurator> specimenImport = new CdmDefaultImport<SpecimenExcelImportConfigurator>();
 		//new Test().invoke(tcsImportConfigurator);
 		specimenImport.invoke(specimenImportConfigurator);
 		System.out.println("End import from SpecimenData ("+ source.toString() + ")...");
