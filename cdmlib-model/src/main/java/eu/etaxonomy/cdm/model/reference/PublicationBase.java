ba35eab6bca715394c7df763f60d7b7aa1bc9a16
Andreas Müller <a.mueller@bgbm.org>
2010-07-13 12:36:17 +0000

    minor

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/reference/PublicationBase.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/reference/PublicationBase.java
@@ -12,7 +12,6 @@ package eu.etaxonomy.cdm.model.reference;
 import javax.persistence.Entity;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
