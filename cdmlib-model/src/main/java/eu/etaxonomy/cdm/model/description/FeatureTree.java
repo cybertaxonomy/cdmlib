17093cc0fa5ff22f1daeb69d66ad08f40e5cb50f
Andreas Müller <a.mueller@bgbm.org>
2010-07-14 23:08:59 +0000

    minor

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/description/FeatureTree.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/description/FeatureTree.java
@@ -20,9 +20,7 @@ import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlIDREF;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
 
 import org.apache.log4j.Logger;
