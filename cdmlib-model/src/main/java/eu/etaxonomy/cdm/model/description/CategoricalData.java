8a7770db441e275975915706e5258fcf1a39933d
Andreas Müller <a.mueller@bgbm.org>
2010-07-14 22:30:31 +0000

    minor

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/description/CategoricalData.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/description/CategoricalData.java
@@ -15,8 +15,6 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
