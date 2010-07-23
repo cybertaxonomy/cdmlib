0cb712d1682f110a9e4e2f956c7d8e26096b47c5
m.venin <m.venin@localhost>
2010-07-23 14:20:40 +0000

    A few improvements, and code cleaning

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/DescriptionBuilder.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/DescriptionBuilder.java
@@ -1,10 +1,13 @@
 package eu.etaxonomy.cdm.api.service;
 
+import java.util.List;
+
+import eu.etaxonomy.cdm.model.common.Language;
 import eu.etaxonomy.cdm.model.description.DescriptionElementBase;
 import eu.etaxonomy.cdm.model.description.TextData;
 
 public abstract class DescriptionBuilder<T extends DescriptionElementBase> {
-	public abstract TextData build(T descriptionElement);
+	public abstract TextData build(T descriptionElement, List<Language> languages);
 	
 	
 }
