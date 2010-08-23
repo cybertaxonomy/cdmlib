7fce0a960c5171b78c12e9e2d28b1941780bd0ab
m.venin <m.venin@localhost>
2010-08-23 15:15:11 +0000

    A few improvements, and code cleaning

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/INaturalLanguageGenerator.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/INaturalLanguageGenerator.java
@@ -1,11 +1,13 @@
 package eu.etaxonomy.cdm.api.service;
 
+import java.util.Set;
 import java.util.List;
 
 import eu.etaxonomy.cdm.model.common.Language;
-import eu.etaxonomy.cdm.model.description.FeatureTree;
 import eu.etaxonomy.cdm.model.description.TaxonDescription;
 import eu.etaxonomy.cdm.model.description.TextData;
+import eu.etaxonomy.cdm.model.description.FeatureTree;
+import eu.etaxonomy.cdm.model.description.DescriptionBase;
 
 
 
@@ -21,5 +23,8 @@ public interface INaturalLanguageGenerator {
 	public List<TextData> generateNaturalLanguageDescription(FeatureTree featureTree, TaxonDescription descriptions);
 	
 	public List<TextData> generateNaturalLanguageDescription(FeatureTree featureTree,TaxonDescription description, Language language);
+	
+	public List<TextData> generatePreferredNaturalLanguageDescription(FeatureTree featureTree, TaxonDescription description, List<Language> languages);
+
 
 }
