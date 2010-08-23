4cad2e2b9de79bb912b583866fbf92f242b86f2d
Andreas Müller <a.mueller@bgbm.org>
2010-08-23 16:25:48 +0000

    cleaned BciServiceTest

--- cdmlib-ext/src/test/java/eu/etaxonomy/cdm/ext/bci/BciServiceTest.java
+++ cdmlib-ext/src/test/java/eu/etaxonomy/cdm/ext/bci/BciServiceTest.java
@@ -3,23 +3,14 @@
  */
 package eu.etaxonomy.cdm.ext.bci;
 
-import static org.junit.Assert.*;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import eu.etaxonomy.cdm.api.application.ICdmApplicationConfiguration;
 import eu.etaxonomy.cdm.ext.ipni.IIpniService;
-import eu.etaxonomy.cdm.ext.ipni.IIpniService.DelimitedFormat;
-import eu.etaxonomy.cdm.model.agent.Person;
-import eu.etaxonomy.cdm.model.common.Extension;
-import eu.etaxonomy.cdm.model.name.BotanicalName;
 import eu.etaxonomy.cdm.model.occurrence.Collection;
 
 /**
@@ -52,7 +43,7 @@ public class BciServiceTest {
 	}
 
 	@Test
-	public void testGetCollections(){
+	public void testGetCollectionsByCode(){
 		ICdmApplicationConfiguration config = null;
 		List<Collection> collectionList = service1.getCollectionsByCode("BG", config);
 		//expected web service result: urn:lsid:biocol.org:col:15727	http://biocol.org/urn:lsid:biocol.org:col:15727	University of Bergen Herbarium
@@ -62,35 +53,9 @@ public class BciServiceTest {
 		//title cache
 		Assert.assertEquals("Name for BG should be 'University of Bergen Herbarium'", "University of Bergen Herbarium", collection.getName());
 		Assert.assertEquals("LSID should be urn:lsid:biocol.org:col:15727", "urn:lsid:biocol.org:col:15727", collection.getLsid().getLsid());
-		System.out.println(collection.getLsid().getObject());
-		System.out.println(collection.getLsid().getNamespace());
-		System.out.println(collection.getLsid().getRevision());
 		
-//		//alternative names
-//		Assert.assertEquals("One extension for the alternative name should exist", 1, author.getExtensions().size());
-//		Extension alternativeName = author.getExtensions().iterator().next();
-//		Assert.assertEquals("Alternative name should be ", "Greuter, Werner Rodolfo", alternativeName.getValue());
-//		//dates
-//		String year = author.getLifespan().getYear();
-//		Assert.assertNotNull("Year should be not null", year);
-//		Assert.assertEquals("Year should be 1938", "1938", year);
-//		
-//		authorList = service1.getAuthors(null, "Greu*", null, null, null, config);
-//		//29367-1%1.1%Greuet%Claude%Greuet%A%%>Greuet, Claude
-//		//20000981-1%1.1%Greuning%J.V. van%Greuning%M%1993%>Greuning, J.V. van
-//		//3379-1%1.1%Greuter%Werner Rodolfo%Greuter%PS%1938-%>Greuter, Werner Rodolfo
-//		Assert.assertEquals("There should be exactly 3 result for 'Greu*'. But maybe this changes over time", 3, authorList.size());
-//		
-//		
-////		for (Person person : authorList){
-////			System.out.println(person.getTitleCache() + ";  " + person.getNomenclaturalTitle());
-////		}
-////		
-//		authorList = service1.getAuthors(null, "Greuter", null, null, DelimitedFormat.MINIMAL, config);
-//		Assert.assertEquals("There should be exactly 1 result for 'Greuter'", 1, authorList.size());
-//		author = authorList.get(0);
-//		Assert.assertTrue("No alternative names should exist in the minimal version", author.getExtensions().isEmpty());
-
+		collectionList = service1.getCollectionsByCode("CM", config);
+		Assert.assertEquals("There should be exactly 3 result for 'CM'. If not try http://www.biocol.org/rest/lookup/code/CM", 3, collectionList.size());
 		
 	}
 	
