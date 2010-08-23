9729a179986185427f04ccc208a83ccc25d8c8c6
m.venin <m.venin@localhost>
2010-08-23 14:13:24 +0000

    A few improvements, and code cleaning

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/AbstractCategoricalDescriptionBuilder.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/AbstractCategoricalDescriptionBuilder.java
@@ -2,18 +2,17 @@ package eu.etaxonomy.cdm.api.service;
 
 import java.util.List;
 
+import eu.etaxonomy.cdm.model.common.Language;
 import eu.etaxonomy.cdm.model.description.CategoricalData;
 import eu.etaxonomy.cdm.model.description.StateData;
 import eu.etaxonomy.cdm.model.description.TextData;
 
 public abstract class AbstractCategoricalDescriptionBuilder extends DescriptionBuilder<CategoricalData>{
 	
-	public TextData build(CategoricalData data) { 
-		   TextData textdata = doBuild(data.getStates());
-		   textdata.setFeature(data.getFeature());
-		   return textdata;
+	public TextData build(CategoricalData data, List<Language> languages) {
+		   return doBuild(data.getStates(), languages);
 		 }
 
-	protected abstract TextData doBuild(List<StateData> stateDatas);
+	protected abstract TextData doBuild(List<StateData> stateDatas, List<Language> languages);
 
 }
