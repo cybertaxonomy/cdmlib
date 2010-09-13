a2c32f05e246f3bd6c5a58f972e1e51c51bc565a
m.venin <m.venin@localhost>
2010-09-13 17:33:09 +0000

    Updates to add a new output as a String.

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/NaturalLanguageGenerator.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/NaturalLanguageGenerator.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 
 import eu.etaxonomy.cdm.model.description.CategoricalData;
@@ -23,6 +24,7 @@ public class NaturalLanguageGenerator implements INaturalLanguageGenerator {
 	private DescriptionBuilder<QuantitativeData> quantitativeDescriptionBuilder = new DefaultQuantitativeDescriptionBuilder();
 	private DescriptionBuilder<CategoricalData> categoricalDescriptionBuilder = new DefaultCategoricalDescriptionBuilder();
 	
+	private String previousFeatureName;
 	
 	/**
 	 * 
@@ -115,5 +117,87 @@ public class NaturalLanguageGenerator implements INaturalLanguageGenerator {
 	public void setCategoricalDescriptionBuilder(DescriptionBuilder<CategoricalData> categoricalDescriptionBuilder){
 		this.categoricalDescriptionBuilder = categoricalDescriptionBuilder;
 	}
+	
+	
+	public String generateStringNaturalLanguageDescription(FeatureTree featureTree, TaxonDescription description,	Language language) {
+		List<Language> languages = new ArrayList<Language>();
+		languages.add(language);
+		return buildString(featureTree.getRootChildren(), featureTree.getRoot(), description, languages).toString();
+	}
+	
+	/** recursive function that goes through a tree containing the order in which the description has to be generated,
+	 *  if an element of this tree matches one of the TaxonDescription, a DescriptionBuilder is called which returns a TextData with the corresponding description.
+	 * 
+	 * @param children
+	 * @param parent
+	 * @param description
+	 * @param language The language in which the description has to be written
+	 * @return
+	 */
+	private StringBuilder buildString(List<FeatureNode> children, FeatureNode parent, TaxonDescription description, List<Language> languages) {
+		StringBuilder stringbuilder = new StringBuilder();
+		if (!parent.isLeaf()){ // if this node is not a leaf, continue recursively (only the leaves of a FeatureTree contain states)
+			for (Iterator<FeatureNode> ifn = children.iterator() ; ifn.hasNext() ;){
+				FeatureNode fn = ifn.next();
+				StringBuilder tempsb = buildString(fn.getChildren(),fn,description, languages);
+				if (tempsb.length()>1) stringbuilder.append(tempsb.deleteCharAt(tempsb.length()-1));
+//				if (tempsb.length()>1) stringbuilder.append(tempsb);
+			}
+			stringbuilder.append('.');
+		}
+		else { //once a leaf is reached
+			Feature fref = parent.getFeature();
+			if (fref!=null) { // needs a better algorithm
+				int k=0;
+					Set<DescriptionElementBase> elements = description.getElements();
+					for (Iterator<DescriptionElementBase> deb = elements.iterator() ; deb.hasNext() ;){ // iterates over all the descriptions enclosed in the TaxonDescription
+						DescriptionElementBase descriptionElement = deb.next();
+						TextData textData;
+						if (descriptionElement.getFeature().equals(fref)){ // if one matches the corresponding feature associated to this leaf
+							if (descriptionElement instanceof CategoricalData) { // if this description is a CategoricalData, generate the according TextData
+								CategoricalData categoricalData = (CategoricalData) descriptionElement;
+								//textData = buildCategoricalDescr(categoricalData, language);
+								textData = categoricalDescriptionBuilder.build(categoricalData, languages);
+								//textData.putText(fref.getLabel(), Language.DEFAULT());
+								String featureName = StringUtils.substringBefore(fref.getLabel(), "<");
+								if (previousFeatureName==null){
+									previousFeatureName = featureName;
+									String featureString = categoricalDescriptionBuilder.buildFeature(fref,true);
+									stringbuilder.append(featureString.substring(0,1).toUpperCase() + featureString.substring(1));
+								}
+								else if (!featureName.contains(previousFeatureName)) {
+									stringbuilder.append(". ");
+									previousFeatureName = featureName;
+									String featureString = categoricalDescriptionBuilder.buildFeature(fref,true);
+									stringbuilder.append(featureString.substring(0,1).toUpperCase() + featureString.substring(1)); // if you want to print the name of the feature (Should it be an option ?)
+								}
+								stringbuilder.append(textData.getText(Language.DEFAULT()));
+								stringbuilder.append(',');
+							}
+							if (descriptionElement instanceof QuantitativeData) { // if this description is a QuantitativeData, generate the according TextData
+								QuantitativeData quantitativeData = (QuantitativeData) descriptionElement;
+								textData = quantitativeDescriptionBuilder.build(quantitativeData, languages);
+								String featureName = StringUtils.substringBefore(fref.getLabel(), "<");
+								if (previousFeatureName==null){
+									previousFeatureName = featureName;
+									String featureString = quantitativeDescriptionBuilder.buildFeature(fref,true);
+									stringbuilder.append(featureString.substring(0,1).toUpperCase() + featureString.substring(1));
+								}
+								else if (!featureName.contains(previousFeatureName)) {
+									stringbuilder.append(". ");
+									previousFeatureName = featureName;
+									String featureString = quantitativeDescriptionBuilder.buildFeature(fref,true);
+									stringbuilder.append(featureString.substring(0,1).toUpperCase() + featureString.substring(1)); // if you want to print the name of the feature (Should it be an option ?)
+								}
+								stringbuilder.append(textData.getText(Language.DEFAULT()));
+								stringbuilder.append(',');
+							}
+						}
+					}
+			}
+		}
+		return stringbuilder;
+	}
+
 
 }
