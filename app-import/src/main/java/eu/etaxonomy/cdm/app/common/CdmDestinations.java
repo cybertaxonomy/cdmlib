ec2e4bd4dc240ed2ef9956f88a01e188a31baad5
Andreas Müller <a.mueller@bgbm.org>
2010-09-21 13:52:09 +0000

    postgresql datasource

--- app-import/src/main/java/eu/etaxonomy/cdm/app/common/CdmDestinations.java
+++ app-import/src/main/java/eu/etaxonomy/cdm/app/common/CdmDestinations.java
@@ -335,6 +335,13 @@ public class CdmDestinations {
 		return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
 	}
 	
+	public static ICdmDataSource cdm_local_postgres_CdmTest(){
+		DatabaseTypeEnum dbType = DatabaseTypeEnum.PostgreSQL;
+		String cdmServer = "127.0.0.1";
+		String cdmDB = "CdmTest";
+		String cdmUserName = "edit";
+		return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
+	}
 
 	
 	
