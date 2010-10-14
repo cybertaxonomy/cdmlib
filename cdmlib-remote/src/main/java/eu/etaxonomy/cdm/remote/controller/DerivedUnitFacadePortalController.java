cce52cba006af02c3eefe2fc50c9d85e06aae7cd
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2010-10-14 09:39:41 +0000

    portal controller for DerivedUniFacades

---
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/DerivedUnitFacadePortalController.java
@@ -0,0 +1,25 @@
+// $Id$
+/**
+* Copyright (C) 2009 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+* 
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.remote.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author a.kohlbecker
+ * @date 14.10.2010
+ *
+ */
+@Controller
+@RequestMapping(value = {"/portal/derivedUnitFacade/{uuid}"})
+public class DerivedUnitFacadePortalController extends
+		DerivedUnitFacadeController {
+
+}
