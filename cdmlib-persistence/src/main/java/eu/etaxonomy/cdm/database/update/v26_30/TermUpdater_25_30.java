0bfa06c04b1c2ca3095406fb903f7725c81751d8
Andreas Müller <a.mueller@bgbm.org>
2010-11-16 23:55:24 +0000

    delete commented code

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/v26_30/TermUpdater_25_30.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/v26_30/TermUpdater_25_30.java
@@ -77,37 +77,7 @@ public class TermUpdater_25_30 extends TermUpdaterBase implements ITermUpdater {
 		uuidVocabulary = UUID.fromString("fe87ea8d-6e0a-4e5d-b0da-0ab8ea67ca77");
 		uuidAfterTerm = DeterminationModifier.uuidConfer;
 		list.add( SingleTermUpdater.NewInstance("Add 'affinis (aff.)' determination modifier", uuidTerm, description, label, abbrev, dtype, uuidVocabulary, Language.uuidLatin, true, uuidAfterTerm));
-//		
-//		//Habitat
-//		uuidTerm = UUID.fromString("fb16929f-bc9c-456f-9d40-dec987b36438");
-//		description = "Habitat";
-//		label = "Habitat";
-//		abbrev = "Habitat";
-//		dtype = Feature.class.getSimpleName();
-//		uuidVocabulary = uuidFeatureVocabulary;
-//		uuidAfterTerm = null;
-//		list.add( SingleTermUpdater.NewInstance("Add habitat feature", uuidTerm, description, label, abbrev, dtype, uuidVocabulary, Language.uuidEnglish, false, null));
-//
-//		//Habitat & Ecology
-//		uuidTerm = UUID.fromString("9fdc4663-4d56-47d0-90b5-c0bf251bafbb");
-//		description = "Habitat & Ecology";
-//		label = "Habitat & Ecology";
-//		abbrev = "Hab. & Ecol.";
-//		dtype = Feature.class.getSimpleName();
-//		uuidVocabulary = uuidFeatureVocabulary;
-//		uuidAfterTerm = null;
-//		list.add( SingleTermUpdater.NewInstance("Add habitat & ecology feature", uuidTerm, description, label, abbrev, dtype, uuidVocabulary, Language.uuidEnglish, false, null));
-//
-//		//Chromosome Numbers
-//		uuidTerm = UUID.fromString("6f677e98-d8d5-4bc5-80bf-affdb7e3945a");
-//		description = "Chromosome Numbers";
-//		label = "Chromosome Numbers";
-//		abbrev = "Chromosome Numbers";
-//		dtype = Feature.class.getSimpleName();
-//		uuidVocabulary = uuidFeatureVocabulary;
-//		uuidAfterTerm = null;
-//		list.add( SingleTermUpdater.NewInstance("Add chromosome number feature", uuidTerm, description, label, abbrev, dtype, uuidVocabulary, Language.uuidEnglish, false, null));
-//		
+		
 		return list;
 	}
 	
