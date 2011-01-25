b0f920b4743e6130c05a6a51c96e4d7822511923
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-01-25 16:21:52 +0000

    connecting to correct database version

--- cdmlib-ext/src/test/java/eu/etaxonomy/cdm/ext/sru/SruServiceWrapperTest.java
+++ cdmlib-ext/src/test/java/eu/etaxonomy/cdm/ext/sru/SruServiceWrapperTest.java
@@ -20,7 +20,7 @@ import eu.etaxonomy.cdm.model.reference.Reference;
  */
 public class SruServiceWrapperTest {
 	
-	static String baseUrl = "http://gso.gbv.de/sru/DB=2.1/";
+	static String baseUrl = "http://gso.gbv.de/sru/DB=1.83/";
 	
 	public static final Logger logger = Logger.getLogger(SruServiceWrapperTest.class);
 
@@ -39,19 +39,27 @@ public class SruServiceWrapperTest {
 // ******************************* TESTS ******************************************************/
 
 	@Test
-	@Ignore // ignore web accessing tests
+	//@Ignore // ignore web accessing tests
 	public void testDoSearchRetrieve(){
 		
-		List<Reference> refList = sruServiceWrapper.doSearchRetrieve("pica.tit=\"Linnaei Species Plantarum Europae\"", "dc");
+		List<Reference> refList_1 = sruServiceWrapper.doSearchRetrieve("pica.tit=\"Linnaei Species Plantarum\"", "dc");
 		// -> http://gso.gbv.de/sru/DB=2.1/?version=1.1&operation=searchRetrieve&query=pica.tit%3D%22Species+Plantarum%22&recordSchema=dc
 			
-		Assert.assertEquals("There should be exactly 2 result for 'Linnaei Species Plantarum Europae'", 2, refList.size());
-		Reference reference = refList.get(0);
-		logger.info(reference.toString());
+		Assert.assertEquals("There should be exactly 5 results for 'Linnaei Species Plantarum'", 5, refList_1.size());
+		Reference reference_1 = refList_1.get(0);
+		logger.info(reference_1.toString());
 		//title cache
-		Assert.assertEquals("Title Cache for Abies albertiana should be 'Linnaei Species Plantarum Europae Pars 2. Supplementum Plantarum Europaearum ...'", "Linnaei Species Plantarum Europae Pars 2. Supplementum Plantarum Europaearum ...", reference.getTitleCache());
+		Assert.assertEquals("Title of first entry should be 'Caroli Linnaei species plantarum'", "Caroli Linnaei species plantarum", reference_1.getTitleCache());
 
-	}	
+		//--------------------------
+		
+		List<Reference> refList_2 = sruServiceWrapper.doSearchRetrieve("pica.all = \"Species+plantarum\" and pica.dst = \"8305\"", "dc");
+		// -> http://gso.gbv.de/sru/DB=2.1/?version=1.1&operation=searchRetrieve&query=pica.tit%3D%22Species+Plantarum%22&recordSchema=dc
+			
+		Assert.assertTrue("There should be at least 1 result for 'species+plantarum' and digitized", refList_2.size() > 0);
+		Reference reference_2 = refList_2.get(0);
+		logger.info(reference_2.toString());
+	}
 
 
 }
