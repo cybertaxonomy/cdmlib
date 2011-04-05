2bdac5a685cb251d79eedd81bfd97db910fb6db3
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-04-05 11:11:22 +0000

    doLoadWithNodes end point for performance testing

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/PolytomousKeyController.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/PolytomousKeyController.java
@@ -9,9 +9,18 @@
 */
 package eu.etaxonomy.cdm.remote.controller;
 
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import eu.etaxonomy.cdm.api.service.IPolytomousKeyService;
 import eu.etaxonomy.cdm.model.description.PolytomousKey;
@@ -24,10 +33,34 @@ import eu.etaxonomy.cdm.model.description.PolytomousKey;
 @Controller
 @RequestMapping(value = {"/polytomousKey/{uuid}"})
 public class PolytomousKeyController extends BaseController<PolytomousKey, IPolytomousKeyService> {
-
+	public static final Logger logger = Logger.getLogger(PolytomousKeyController.class);
+	
 	@Autowired
 	public void setService(IPolytomousKeyService service) {
 		this.service = service;
+	}
+	
+	/**
+	 * @param uuid
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws IOException
+	 * @Deprecated ONLY FOR TESTING PURPOSES
+	 */
+	@RequestMapping(value = {"loadWithNodes"})
+	public ModelAndView doLoadWithNodes(
+			@PathVariable("uuid") UUID uuid,
+			HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+
+		logger.info("doLoadWithNodes() - " + request.getServletPath());
+
+		ModelAndView mv = new ModelAndView();
+		PolytomousKey key = service.loadWithNodes(uuid, null, null);
+		mv.addObject(key); 
+		return mv;
+		
 	}
 	
 }
