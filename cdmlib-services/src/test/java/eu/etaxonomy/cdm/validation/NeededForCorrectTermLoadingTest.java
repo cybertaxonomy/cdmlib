a5382f79ef1a8aa92625308031e216cb873a6792
Andreas Müller <a.mueller@bgbm.org>
2011-05-24 11:45:34 +0000

    fix non running tests with dummy test class in cdmlib-service ... validation

---
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/validation/NeededForCorrectTermLoadingTest.java
@@ -0,0 +1,28 @@
+/**
+* Copyright (C) 2009 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/ 
+
+package eu.etaxonomy.cdm.validation;
+
+import org.junit.Ignore;
+
+import eu.etaxonomy.cdm.test.integration.CdmIntegrationTest;
+
+/**
+ * NOTE: After moving validation tests from this package to cdmlib-model
+ * there came up an issue with termloading in TaxonServiceHibernateImplTest.
+ * Therefore this dummy class was created.
+ * It is still unclear what the reason is. See ticket #
+ *
+ *	
+ */
+@Ignore
+public class NeededForCorrectTermLoadingTest extends CdmIntegrationTest {
+	
+
+}
