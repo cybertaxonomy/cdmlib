d442fbe51b4ce9319a302046d683f2fd9c149e3f
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-06-27 10:13:42 +0000

    sending http error in case of file not found

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/MediaController.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/MediaController.java
@@ -1,9 +1,9 @@
 // $Id$
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -35,7 +35,7 @@ import eu.etaxonomy.cdm.model.media.MediaRepresentation;
 
 /**
  * TODO write controller documentation
- * 
+ *
  * @author a.kohlbecker
  * @date 24.03.2009
  */
@@ -44,48 +44,49 @@ import eu.etaxonomy.cdm.model.media.MediaRepresentation;
 @RequestMapping(value = {"/media/{uuid}"})
 public class MediaController extends AnnotatableController<Media, IMediaService>
 {
-	
-	/* (non-Javadoc)
-	 * @see eu.etaxonomy.cdm.remote.controller.GenericController#setService(eu.etaxonomy.cdm.api.service.IService)
-	 */
-	@Autowired
-	@Override
-	public void setService(IMediaService service) {
-		this.service = service;
-	}
-	private static final List<String> MEDIA_INIT_STRATEGY = Arrays.asList(new String []{
-			"*",
-			"representations",
-			"representations.parts"
-	});
-	
-	@RequestMapping(value = {"metadata"})
-	public ModelAndView doGetMediaMetaData(@PathVariable("uuid") UUID uuid,
-			HttpServletRequest request, HttpServletResponse response) throws IOException {
-		Map<String, String> result;
-		Media media = getCdmBaseInstance(uuid, response, MEDIA_INIT_STRATEGY);
-		
-		Set<MediaRepresentation> representations = media.getRepresentations();
-		//get first representation and retrieve the according metadata
 
-		Object[] repArray = representations.toArray();
-		Object mediaRep = repArray[0];
-		ModelAndView mv = new ModelAndView();
-		try {
-			if (mediaRep instanceof MediaRepresentation){
-				MediaRepresentation medRep = (MediaRepresentation) mediaRep;
-				URI uri = medRep.getParts().get(0).getUri();
-				ImageInfo imageInfo = ImageInfo.NewInstance(uri, 3000);
-				imageInfo.readMetaData(3000);
-				result = imageInfo.getMetaData();
-				mv.addObject(result);
-			}
-		} catch (HttpException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return mv;
-		
-	}
+    /* (non-Javadoc)
+     * @see eu.etaxonomy.cdm.remote.controller.GenericController#setService(eu.etaxonomy.cdm.api.service.IService)
+     */
+    @Autowired
+    @Override
+    public void setService(IMediaService service) {
+        this.service = service;
+    }
+    private static final List<String> MEDIA_INIT_STRATEGY = Arrays.asList(new String []{
+            "*",
+            "representations",
+            "representations.parts"
+    });
+
+    @RequestMapping(value = {"metadata"})
+    public ModelAndView doGetMediaMetaData(@PathVariable("uuid") UUID uuid,
+            HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String, String> result;
+        Media media = getCdmBaseInstance(uuid, response, MEDIA_INIT_STRATEGY);
+
+        Set<MediaRepresentation> representations = media.getRepresentations();
+        //get first representation and retrieve the according metadata
+
+        Object[] repArray = representations.toArray();
+        Object mediaRep = repArray[0];
+        ModelAndView mv = new ModelAndView();
+        URI uri = null;
+        try {
+            if (mediaRep instanceof MediaRepresentation){
+                MediaRepresentation medRep = (MediaRepresentation) mediaRep;
+                uri = medRep.getParts().get(0).getUri();
+                ImageInfo imageInfo = ImageInfo.NewInstanceWithMetaData(uri, 3000);
+                result = imageInfo.getMetaData();
+                if(result != null) {
+                    mv.addObject(result);
+                }
+            }
+        } catch (HttpException e) {
+            HttpStatusMessage.fromString("Reading media file from " + uri.toString() + " failed").setStatusCode(400).send(response);
+        }
+        return mv;
+
+    }
 
 }
