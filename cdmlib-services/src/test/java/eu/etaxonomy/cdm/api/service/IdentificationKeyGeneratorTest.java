eab5d963ad4b39573b231fb569742cde2e722a21
Andreas Müller <a.mueller@bgbm.org>
2011-08-30 12:32:49 +0000

    Add dummy class to avoid failing tests

---
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/IdentificationKeyGeneratorTest.java
@@ -0,0 +1,25 @@
+package eu.etaxonomy.cdm.api.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+/**
+ * Dummy class. Original class has been moved to model.strategy.
+ * This empty class needs to stay here to avoid test error with 
+ * transient terms.
+ * Can be removed once this issue is fixed in the tests
+ */
+
+public class IdentificationKeyGeneratorTest {
+		
+//*************************** TESTS *********************** /
+	
+
+	@Test
+	public void testInvoke() {
+		assertNotNull("Key should exist.","");
+	}
+
+
+}
