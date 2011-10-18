76493e0a4006e9e23c08512a02997042f1acabd2
Andreas Müller <a.mueller@bgbm.org>
2011-10-18 20:59:21 +0000

    DwcA-Import first steps

---
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/dwca/in/DwcaZipToStreamConverter.java
@@ -0,0 +1,139 @@
+// $Id$
+/**
+* Copyright (C) 2009 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+* 
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.io.dwca.in;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.log4j.Logger;
+
+import au.com.bytecode.opencsv.CSVReader;
+import eu.etaxonomy.cdm.io.dwca.jaxb.Archive;
+import eu.etaxonomy.cdm.io.dwca.jaxb.Core;
+import eu.etaxonomy.cdm.io.dwca.out.DwcaMetaDataRecord;
+
+/**
+ * This class transforms a Darwin Core Archive zip file into a set of CSVReaderInputStreams.
+ * For each data file included in the zip it creates one stream by evaluating the meta file.
+ * Ecological metadata handling is still unclear.
+ * @author a.mueller
+ * @date 17.10.2011
+ *
+ */
+public class DwcaZipToStreamConverter {
+	@SuppressWarnings("unused")
+	private static Logger logger = Logger.getLogger(DwcaZipToStreamConverter.class);
+
+	private final String META_XML = "meta.xml";
+	protected static final boolean IS_CORE = true;
+	
+	private URI dwcaZip;
+	private Map<String, DwcaMetaDataRecord> metaRecords = new HashMap<String, DwcaMetaDataRecord>(); 
+	private Archive archive;
+	
+/// ******************** FACTORY ********************************/	
+	
+	public static DwcaZipToStreamConverter NewInstance(URI dwcaZip){
+		return new DwcaZipToStreamConverter(dwcaZip);
+	}
+	
+
+//************************ CONSTRUCTOR *********************************/
+	
+	/**
+	 * Constructor
+	 * @param dwcaZip
+	 */
+	public DwcaZipToStreamConverter(URI dwcaZip) {
+		this.dwcaZip = dwcaZip;
+		initArchive();
+	}
+	
+
+	protected Archive getArchive(){
+			return this.archive;
+	}
+	
+	public CsvStream getCoreStream() throws IOException{
+		initArchive();
+		Core core = archive.getCore();
+		char fieldTerminatedBy = core.getFieldsTerminatedBy().charAt(0);
+		char fieldsEnclosedBy = core.getFieldsEnclosedBy().charAt(0);
+		int skipLines = core.getIgnoreHeaderLines();
+		
+		String fileLocation = core.getFiles().getLocation();
+		InputStream coreCsvInputStream = makeInputStream(fileLocation);
+		Reader coreReader = new InputStreamReader(coreCsvInputStream); 
+		CSVReader csvReader = new CSVReader(coreReader, fieldTerminatedBy,fieldsEnclosedBy,skipLines);
+		CsvStream coreStream = new CsvStream(csvReader, core);
+		//		InputStream s;
+//		s.
+		
+		return coreStream;
+	}
+
+
+	private void initArchive() {
+		if (archive == null){
+			try {
+				InputStream metaInputStream = makeInputStream(META_XML);
+				
+				JAXBContext jaxbContext = JAXBContext.newInstance("eu.etaxonomy.cdm.io.dwca.jaxb");
+				Unmarshaller unmarshaller =  jaxbContext.createUnmarshaller();
+				archive = (Archive)unmarshaller.unmarshal(metaInputStream);
+	
+				validateArchive(archive);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			} catch (JAXBException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+
+	private void validateArchive(Archive archive) {
+		if (archive.getCore().getFieldsTerminatedBy().length() > 1){
+			throw new IllegalStateException("CsvReader does not allow field delimiters with more than 1 character");
+		}
+		if (archive.getCore().getFieldsEnclosedBy().length() > 1){
+			throw new IllegalStateException("CsvReader does not allow field delimiters with more than 1 character");
+		}
+		
+	}
+
+
+	/**
+	 * @return
+	 * @throws IOException
+	 */
+	private InputStream makeInputStream(String name) throws IOException {
+		ZipFile zip = new ZipFile(new File(dwcaZip), ZipFile.OPEN_READ);
+		ZipEntry metaEntry = zip.getEntry(name);
+		InputStream metaInputStream = zip.getInputStream(metaEntry);
+		return metaInputStream;
+	}
+
+
+	
+	
+}
---
+++ cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/dwca/in/DwcaZipToStreamConverterTest.java
@@ -0,0 +1,83 @@
+// $Id$
+/**
+* Copyright (C) 2009 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+* 
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.io.dwca.in;
+
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+/**
+ * @author a.mueller
+ * @date 17.10.2011
+ *
+ */
+public class DwcaZipToStreamConverterTest {
+	@SuppressWarnings("unused")
+	private static final Logger logger = Logger.getLogger(DwcaZipToStreamConverterTest.class);
+	
+	URI uri;
+	DwcaZipToStreamConverter converter;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		String inputFile = "/eu/etaxonomy/cdm/io/dwca/in/DwcaZipToStreamConverterTest-input.zip";
+		URL url = this.getClass().getResource(inputFile);
+		uri = url.toURI();
+		assertNotNull("URI for the test file '" + inputFile + "' does not exist", uri);
+		converter = DwcaZipToStreamConverter.NewInstance(uri);
+		assertNotNull("Converter must be created",converter);
+	}
+	
+//************* TEST ********************************************
+
+	@Test
+	public void testInitMetadata(){
+		String coreEncoding = converter.getArchive().getCore().getEncoding();
+		Assert.assertEquals("Encoding for core must be 'UTF-8'", "UTF-8", coreEncoding);
+	}
+	
+	@Test 
+	public void testGetCoreStream(){
+		try {
+			CsvStream coreStream = converter.getCoreStream();
+			Assert.assertNotNull("core stream should not be null", coreStream);
+			Map<String, String> next = coreStream.read();
+			Assert.assertNotNull("Entry should exist in core stream", next);
+			
+			
+		} catch (IOException e) {
+			Assert.fail();
+		}
+		
+	}
+	
+	
+}
