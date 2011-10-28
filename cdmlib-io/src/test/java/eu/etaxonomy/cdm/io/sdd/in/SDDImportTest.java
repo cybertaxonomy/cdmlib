2bcbad392bc32bad1d90bb4b4d9634bf7b014116
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-10-28 13:33:12 +0000

    upgrading junit to 4.7 & adding jBenchmark dependency

--- cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/sdd/in/SDDImportTest.java
+++ cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/sdd/in/SDDImportTest.java
@@ -1,8 +1,8 @@
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -31,36 +31,36 @@ import eu.etaxonomy.cdm.test.integration.CdmTransactionalIntegrationTest;
  * @version 1.0
  */
 
-@Ignore // we ignore this test at the moment because it does not run with maven
+//@Ignore // we ignore this test at the moment because it does not run with maven
 public class SDDImportTest extends CdmTransactionalIntegrationTest {
-	
-	@SpringBeanByType
-	SDDImport sddImport;
-	
-	@SpringBeanByType
-	INameService nameService;
-	
-	private SDDImportConfigurator configurator;
-	
-	@Before
-	public void setUp() throws URISyntaxException {
-		URL url = this.getClass().getResource("/eu/etaxonomy/cdm/io/sdd/SDDImportTest-input.xml");
-		Assert.assertNotNull(url);
-		configurator = SDDImportConfigurator.NewInstance(url.toURI(), null);
-	}
-	
-	@Test
-	public void testInit() {
-		assertNotNull("sddImport should not be null", sddImport);
-		assertNotNull("nameService should not be null", nameService);
-	}
-	
-	@Test
-	public void testDoInvoke() {
-		sddImport.doInvoke(new SDDImportState(configurator));
-		this.setComplete();
-		this.endTransaction();
-		assertEquals("Number of TaxonNames should be 1", 1, nameService.count(null));
-	}
+
+    @SpringBeanByType
+    SDDImport sddImport;
+
+    @SpringBeanByType
+    INameService nameService;
+
+    private SDDImportConfigurator configurator;
+
+    @Before
+    public void setUp() throws URISyntaxException {
+        URL url = this.getClass().getResource("/eu/etaxonomy/cdm/io/sdd/SDDImportTest-input.xml");
+        Assert.assertNotNull(url);
+        configurator = SDDImportConfigurator.NewInstance(url.toURI(), null);
+    }
+
+    @Test
+    public void testInit() {
+        assertNotNull("sddImport should not be null", sddImport);
+        assertNotNull("nameService should not be null", nameService);
+    }
+
+    @Test
+    public void testDoInvoke() {
+        sddImport.doInvoke(new SDDImportState(configurator));
+        this.setComplete();
+        this.endTransaction();
+        assertEquals("Number of TaxonNames should be 1", 1, nameService.count(null));
+    }
 
 }
