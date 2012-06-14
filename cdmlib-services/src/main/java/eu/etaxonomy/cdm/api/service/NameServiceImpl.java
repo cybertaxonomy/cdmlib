6b3ae21b9f964a83cb9365a2a07948d93c9d6756
Andreas Müller <a.mueller@bgbm.org>
2012-06-14 09:46:19 +0000

    minor

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/NameServiceImpl.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/NameServiceImpl.java
@@ -13,7 +13,6 @@ package eu.etaxonomy.cdm.api.service;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
