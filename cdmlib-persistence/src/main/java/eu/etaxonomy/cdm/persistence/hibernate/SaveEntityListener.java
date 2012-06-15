bfdcd73b4b52e365f98201a33efef00590eea5cc
Katja Luther <k.luther@bgbm.org>
2012-06-15 09:40:00 +0000

    bugfixes in security interceptor

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/hibernate/SaveEntityListener.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/hibernate/SaveEntityListener.java
@@ -15,10 +15,15 @@ import org.hibernate.event.SaveOrUpdateEvent;
 import org.hibernate.event.SaveOrUpdateEventListener;
 import org.joda.time.DateTime;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import eu.etaxonomy.cdm.database.EvaluationFailedException;
 import eu.etaxonomy.cdm.model.common.ICdmBase;
 import eu.etaxonomy.cdm.model.common.User;
+//import eu.etaxonomy.cdm.permission.AuthorityPermission;
+import eu.etaxonomy.cdm.permission.CdmPermission;
+import eu.etaxonomy.cdm.permission.CdmPermissionEvaluator;
 
 public class SaveEntityListener implements SaveOrUpdateEventListener {
 	private static final long serialVersionUID = -4295612947856041686L;
@@ -27,16 +32,28 @@ public class SaveEntityListener implements SaveOrUpdateEventListener {
 
 	public void onSaveOrUpdate(SaveOrUpdateEvent event)	throws HibernateException {
 		Object entity = event.getObject();
+		
         if (entity != null){
+        	
             Class<?> entityClazz = entity.getClass();
 			if(ICdmBase.class.isAssignableFrom(entityClazz)) {
+				
 				ICdmBase cdmBase = (ICdmBase)entity;
 				cdmBase.setCreated(new DateTime());
+			
 				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-				if(authentication != null && authentication.getPrincipal() != null && authentication.getPrincipal() instanceof User) {
+				/*if(authentication != null && authentication.getPrincipal() != null && authentication.getPrincipal() instanceof User) {
 					User user = (User)authentication.getPrincipal();
 					cdmBase.setCreatedBy(user);
-				}
+					CdmPermissionEvaluator permissionEvaluator = new CdmPermissionEvaluator();
+					
+					
+					if (!permissionEvaluator.hasPermission(SecurityContextHolder.getContext().getAuthentication(), entity, CdmPermission.CREATE)){
+						
+						throw new EvaluationFailedException("Permission evaluation failed for creating " + entity);
+					 }
+				
+				}*/
 			}
         }		
 	}
--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/hibernate/SaveOrUpdateEntityListener.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/hibernate/SaveOrUpdateEntityListener.java
@@ -1,34 +1,101 @@
 package eu.etaxonomy.cdm.persistence.hibernate;
 
 import org.hibernate.HibernateException;
+import org.hibernate.annotations.Persister;
+import org.hibernate.engine.EntityKey;
+import org.hibernate.event.DirtyCheckEvent;
+import org.hibernate.event.DirtyCheckEventListener;
 import org.hibernate.event.SaveOrUpdateEvent;
 import org.hibernate.event.SaveOrUpdateEventListener;
+import org.hibernate.event.def.DefaultDirtyCheckEventListener;
+import org.hibernate.persister.entity.EntityPersister;
 import org.joda.time.DateTime;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import eu.etaxonomy.cdm.database.EvaluationFailedException;
+import eu.etaxonomy.cdm.model.common.CdmBase;
 import eu.etaxonomy.cdm.model.common.User;
 import eu.etaxonomy.cdm.model.common.VersionableEntity;
+import eu.etaxonomy.cdm.model.description.DescriptionBase;
+import eu.etaxonomy.cdm.model.description.SpecimenDescription;
+import eu.etaxonomy.cdm.model.description.TaxonDescription;
+import eu.etaxonomy.cdm.model.description.TaxonNameDescription;
+import eu.etaxonomy.cdm.model.name.TaxonNameBase;
+import eu.etaxonomy.cdm.model.taxon.Taxon;
+import eu.etaxonomy.cdm.permission.CdmPermission;
+import eu.etaxonomy.cdm.permission.CdmPermissionEvaluator;
 
 public class SaveOrUpdateEntityListener implements SaveOrUpdateEventListener {
 
 	public void onSaveOrUpdate(SaveOrUpdateEvent event)
 			throws HibernateException {
+		//System.err.println("SaveOrUpdateListener" + event.getEntity().getClass());
 		Object entity = event.getObject();
-		if(entity != null && VersionableEntity.class.isAssignableFrom(entity.getClass())) {
 			
-			VersionableEntity versionableEntity = (VersionableEntity)entity;
-			if (versionableEntity.getId()== 0){
-				versionableEntity.setUpdated(new DateTime());
-				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-				if(authentication != null && authentication.getPrincipal() != null && authentication.getPrincipal() instanceof User) {
-					User user = (User)authentication.getPrincipal();
-					versionableEntity.setUpdatedBy(user);
-				} 
+	
+		
+		
+		if(entity != null && CdmBase.class.isAssignableFrom(entity.getClass())){
+			
+			//CdmPermissionEvaluator permissionEvaluator = new CdmPermissionEvaluator();
+			
+			if (VersionableEntity.class.isAssignableFrom(entity.getClass())) {
+				VersionableEntity versionableEntity = (VersionableEntity)entity;
+				if (versionableEntity.getId()== 0){
+					
+					versionableEntity.setUpdated(new DateTime());
+					Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+					if(authentication != null && authentication.getPrincipal() != null && authentication.getPrincipal() instanceof User) {
+						User user = (User)authentication.getPrincipal();
+						versionableEntity.setUpdatedBy(user);
+					} 
+					if (SecurityContextHolder.getContext().getAuthentication()!= null){
+											
+						/*if (!permissionEvaluator.hasPermission(SecurityContextHolder.getContext().getAuthentication(), entity, CdmPermission.CREATE)){
+							if (entity instanceof SpecimenDescription){
+								//TODO
+							}else if (entity instanceof TaxonDescription){
+								TaxonDescription taxonDescription = (TaxonDescription) entity;
+								Taxon taxon = taxonDescription.getTaxon();
+								permissionEvaluator.hasPermission(SecurityContextHolder.getContext().getAuthentication(), taxon, CdmPermission.UPDATE);
+							} else if (entity instanceof TaxonNameDescription){
+									TaxonNameDescription taxonNameDescription = (TaxonNameDescription) entity;
+									TaxonNameBase taxonName = taxonNameDescription.getTaxonName();
+									permissionEvaluator.hasPermission(SecurityContextHolder.getContext().getAuthentication(), taxonName, CdmPermission.UPDATE);
+							} else{
+							
+								throw new EvaluationFailedException("Permission evaluation failed for test test" + event.getEntity());
+							}
+						}*/
+					}
+				}else{
+						if (SecurityContextHolder.getContext().getAuthentication()!= null){
+							/*eventListener.onDirtyCheck(dirtyCheckEvent);
+							if (dirtyCheckEvent.isDirty()){
+								System.err.println("This entity is dirty: " + entity);
+							}*/
+							
+						/*if (!permissionEvaluator.hasPermission(SecurityContextHolder.getContext().getAuthentication(), entity, CdmPermission.UPDATE)){
+							if (entity instanceof SpecimenDescription){
+								//TODO
+							}else if (entity instanceof TaxonDescription){
+								TaxonDescription taxonDescription = (TaxonDescription) entity;
+								Taxon taxon = taxonDescription.getTaxon();
+								permissionEvaluator.hasPermission(SecurityContextHolder.getContext().getAuthentication(), taxon, CdmPermission.UPDATE);
+							} else if (entity instanceof TaxonNameDescription){
+								TaxonNameDescription taxonNameDescription = (TaxonNameDescription) entity;
+								TaxonNameBase taxonName = taxonNameDescription.getTaxonName();
+								permissionEvaluator.hasPermission(SecurityContextHolder.getContext().getAuthentication(), taxonName, CdmPermission.UPDATE);
+							} else{
+							
+								throw new EvaluationFailedException("Permission evaluation failed for " + event.getEntity());
+							}
+						}*/
+					}
+				}
 			}
-			
 		}
-
 	}
 
 }
