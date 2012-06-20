639ed505acd88237fe541c0ef4e3f9cc46d2302e
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2012-06-20 06:24:32 +0000

    removing @Override from interface implementation

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/hibernate/search/UuidBridge.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/hibernate/search/UuidBridge.java
@@ -20,7 +20,6 @@ import org.hibernate.search.bridge.StringBridge;
  */
 public class UuidBridge implements StringBridge {
 
-    @Override
     public String objectToString(Object object) {
         if(object != null) {
             return ((UUID)object).toString();
