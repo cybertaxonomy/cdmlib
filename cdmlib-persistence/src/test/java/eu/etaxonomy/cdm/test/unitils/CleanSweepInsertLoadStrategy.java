57221ced3c645d528bba04a99929afbf0a7d358b
Andreas Müller <a.mueller@bgbm.org>
2012-07-02 13:30:37 +0000

    minor

--- cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/test/unitils/CleanSweepInsertLoadStrategy.java
+++ cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/test/unitils/CleanSweepInsertLoadStrategy.java
@@ -1,8 +1,6 @@
 package eu.etaxonomy.cdm.test.unitils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URL;
