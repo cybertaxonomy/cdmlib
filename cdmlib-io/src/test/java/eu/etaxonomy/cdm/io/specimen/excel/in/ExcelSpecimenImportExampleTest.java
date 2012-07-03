195466eb71b57bf306447a8c44fa7ba1df405974
Andreas Müller <a.mueller@bgbm.org>
2012-07-03 18:05:27 +0000

    fix ExcelSpecimenImportExampleTest, also running in maven

--- cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/specimen/excel/in/ExcelSpecimenImportExampleTest.java
+++ cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/specimen/excel/in/ExcelSpecimenImportExampleTest.java
@@ -1,11 +1,11 @@
 /**
-* Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
-* http://www.e-taxonomy.eu
-* 
-* The contents of this file are subject to the Mozilla Public License Version 1.1
-* See LICENSE.TXT at the top of this package for the full license terms.
-*/
+ * Copyright (C) 2007 EDIT
+ * European Distributed Institute of Taxonomy 
+ * http://www.e-taxonomy.eu
+ * 
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * See LICENSE.TXT at the top of this package for the full license terms.
+ */
 
 package eu.etaxonomy.cdm.io.specimen.excel.in;
 
@@ -13,9 +13,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 
@@ -32,6 +29,7 @@ import eu.etaxonomy.cdm.api.service.INameService;
 import eu.etaxonomy.cdm.api.service.IOccurrenceService;
 import eu.etaxonomy.cdm.io.common.CdmApplicationAwareDefaultImport;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator;
+import eu.etaxonomy.cdm.model.name.NomenclaturalCode;
 import eu.etaxonomy.cdm.model.occurrence.DerivedUnitBase;
 import eu.etaxonomy.cdm.model.occurrence.FieldObservation;
 import eu.etaxonomy.cdm.test.integration.CdmTransactionalIntegrationTest;
@@ -41,9 +39,9 @@ import eu.etaxonomy.cdm.test.integration.CdmTransactionalIntegrationTest;
  * @created 10.05.2011
  * @version 1.0
  */
-@Ignore //currently jenkins throws an exception
-public class ExcelSpecimenImportExampleTest extends CdmTransactionalIntegrationTest {
-	
+public class ExcelSpecimenImportExampleTest extends
+		CdmTransactionalIntegrationTest {
+
 	@SpringBeanByName
 	CdmApplicationAwareDefaultImport<?> defaultImport;
 
@@ -53,64 +51,76 @@ public class ExcelSpecimenImportExampleTest extends CdmTransactionalIntegrationT
 	@SpringBeanByType
 	IOccurrenceService occurrenceService;
 
-	
 	private IImportConfigurator configurator;
-	
+
 	@Before
 	public void setUp() {
 		String inputFile = "/eu/etaxonomy/cdm/io/specimen/excel/in/ExcelSpecimenImportExampleTest-input.xls";
 		URL url = this.getClass().getResource(inputFile);
-		assertNotNull("URL for the test file '" + inputFile + "' does not exist", url);
+		assertNotNull("URL for the test file '" + inputFile
+				+ "' does not exist", url);
 		try {
 			configurator = SpecimenCdmExcelImportConfigurator.NewInstance(url.toURI(), null);
+			configurator.setNomenclaturalCode(NomenclaturalCode.ICBN);
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 			Assert.fail();
 		}
 		assertNotNull("Configurator could not be created", configurator);
 	}
-	
+
 	@Test
 	public void testInit() {
 		assertNotNull("import instance should not be null", defaultImport);
 		assertNotNull("nameService should not be null", nameService);
 		assertNotNull("occurence service should not be null", occurrenceService);
 	}
-//	
-//	@Test
-//	public void testDoInvoke() {
-//		boolean result = defaultImport.invoke(configurator);
-//		assertTrue("Return value for import.invoke should be true", result);
-//		assertEquals("Number of specimen should be 3", 3, occurrenceService.count(DerivedUnitBase.class));
-//		assertEquals("Number of field observations should be 3", 3, occurrenceService.count(FieldObservation.class));
-//			
-////		printDataSet(System.out, new String[]{"SpecimenOrObservationBase","GatheringEvent","DerivationEvent"});
-//		
-//	}
-	
+
+	//
+	// @Test
+	// public void testDoInvoke() {
+	// boolean result = defaultImport.invoke(configurator);
+	// assertTrue("Return value for import.invoke should be true", result);
+	// assertEquals("Number of specimen should be 3", 3,
+	// occurrenceService.count(DerivedUnitBase.class));
+	// assertEquals("Number of field observations should be 3", 3,
+	// occurrenceService.count(FieldObservation.class));
+	//
+	// // printDataSet(System.out, new
+	// String[]{"SpecimenOrObservationBase","GatheringEvent","DerivationEvent"});
+	//
+	// }
+
 	@Test
 	@DataSet
 	@ExpectedDataSet
+//	@Ignore   //difference in expected dataset, also in local version
 	public void testResultSet() {
 		boolean result = defaultImport.invoke(configurator);
 		assertTrue("Return value for import.invoke should be true", result);
 		assertEquals("Number of specimen should be 3", 3, occurrenceService.count(DerivedUnitBase.class));
 		assertEquals("Number of field observations should be 3", 3, occurrenceService.count(FieldObservation.class));
-		
-		try {
-			String filePath = System.getProperty("java.io.tmpdir")+File.separator+"excelSpecimenOutput.xml";
-			File file = new File(filePath);
-			FileOutputStream myOut = new FileOutputStream(file);
-			System.out.println(file.getAbsolutePath());
-			printDataSet(myOut, new String[]{"AgentBase","Collection","DerivationEvent","DeterminationEvent","DescriptionElementBase",
-					"DescriptionBase","Extension","GatheringEvent","GatheringEvent_DefinedTermBase","LanguageString","OriginalSourceBase",
-					"Reference","TaxonBase","TaxonNameBase","TypeDesignationBase",
-					"TaxonNameBase_TypeDesignationBase","SpecimenOrObservationBase","DefinedTermBase","TermVocabulary","Representation"});
-//			printDataSet(myOut);
-		} catch (FileNotFoundException e) {
-			Assert.fail(e.getLocalizedMessage());
-		}
-		
+
+//		try {
+//			String filePath = System.getProperty("java.io.tmpdir")
+//					+ File.separator + "excelSpecimenOutput.xml";
+//			File file = new File(filePath);
+//			FileOutputStream myOut = new FileOutputStream(file);
+//			System.out.println(file.getAbsolutePath());
+//			printDataSet(myOut, new String[] { "AgentBase", "Collection",
+//					"DerivationEvent", "DeterminationEvent",
+//					"DescriptionElementBase", "DescriptionBase", "Extension",
+//					"GatheringEvent", "GatheringEvent_DefinedTermBase",
+//					"LanguageString", "OriginalSourceBase", "Reference",
+//					"TaxonBase", "TaxonNameBase", "TypeDesignationBase",
+//					"TaxonNameBase_TypeDesignationBase",
+//					"SpecimenOrObservationBase", "DefinedTermBase",
+//					"TermVocabulary", "Representation" });
+//			// printDataSet(myOut);
+//		} catch (FileNotFoundException e) {
+//			Assert.fail(e.getLocalizedMessage());
+//		}
+
 	}
 
 }
