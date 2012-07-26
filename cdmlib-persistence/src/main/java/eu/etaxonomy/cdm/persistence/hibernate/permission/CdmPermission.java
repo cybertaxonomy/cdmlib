ef11d59e155c4e81ef42a7462d7415d1a4cfe5af
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2012-07-26 07:38:48 +0000

    preliminar commit on security and permissions

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/hibernate/permission/CdmPermission.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/hibernate/permission/CdmPermission.java
@@ -1,13 +1,12 @@
 /**
-* Copyright (C) 2009 EDIT
-* European Distributed Institute of Taxonomy
-* http://www.e-taxonomy.eu
-*
-* The contents of this file are subject to the Mozilla Public License Version 1.1
-* See LICENSE.TXT at the top of this package for the full license terms.
-*/ 
+ * Copyright (C) 2009 EDIT
+ * European Distributed Institute of Taxonomy
+ * http://www.e-taxonomy.eu
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * See LICENSE.TXT at the top of this package for the full license terms.
+ */
 package eu.etaxonomy.cdm.persistence.hibernate.permission;
-
 
 /**
  * @author k.luther
@@ -15,7 +14,10 @@ package eu.etaxonomy.cdm.persistence.hibernate.permission;
  *
  */
 public enum CdmPermission {
-	CREATE, READ, UPDATE, DELETE, DENYALL, ADMIN;
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE,
+    DENYALL,
+    ADMIN;
 }
-
-
