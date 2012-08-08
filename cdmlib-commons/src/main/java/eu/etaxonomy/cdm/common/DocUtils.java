41b50db8e38a5b0ed1b2f908729507ac12f3794d
Cherian Mathew <c.mathew@bgbm.org>
2012-08-08 08:27:39 +0000

    added apt conversion method for input stream

--- cdmlib-commons/src/main/java/eu/etaxonomy/cdm/common/DocUtils.java
+++ cdmlib-commons/src/main/java/eu/etaxonomy/cdm/common/DocUtils.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
@@ -70,6 +71,56 @@ public class DocUtils {
             reader = ReaderFactory.newReader( aptFile, "UTF-8" );
         } catch (FileNotFoundException e) {
             return "Error in generating documentation : " + e.getMessage();
+        } catch (UnsupportedEncodingException e) {
+            return "Error in generating documentation : " + e.getMessage();
+        }
+
+        try {
+            parser.parse( reader, sink );
+        } catch (ParseException e) {
+            return "Error in generating documentation : " + e.getMessage();
+        }
+        
+        return baos.toString();
+    }
+    
+    /**
+     * Converts an apt input stream into html.
+     * 
+     * @param aptInputStream apt input stream
+     *
+     * @return html as string or error message if exception
+     * 
+     */
+    public static String convertAptToHtml(InputStream aptInputStream) {
+        PlexusContainer container;
+        try {
+            container = new DefaultPlexusContainer();
+        } catch (PlexusContainerException e) {
+            return "Error in generating documentation : " + e.getMessage();
+        }    
+        //FIXME : Plexus does not seem to work for looking up Sink Factory, so XhtmlSinkFactory is called directory
+        //SinkFactory sinkFactory = (SinkFactory) container.lookup( SinkFactory.ROLE, "html" ); // Plexus lookup
+        
+        SinkFactory sinkFactory = new XhtmlSinkFactory();        
+        
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Sink sink;
+        try {
+            sink = sinkFactory.createSink(baos);
+        } catch (IOException e) {
+            return "Error in generating documentation : " + e.getMessage();
+        }
+
+        AptParser parser;
+        try {
+            parser = (AptParser)container.lookup(Parser.ROLE, "apt");
+        } catch (ComponentLookupException e) {
+            return "Error in generating documentation : " + e.getMessage();
+        }
+        Reader reader;
+        try {
+            reader = ReaderFactory.newReader( aptInputStream, "UTF-8" );
         } catch (UnsupportedEncodingException e) {
             return "Error in generating documentation : " + e.getMessage();
         }
