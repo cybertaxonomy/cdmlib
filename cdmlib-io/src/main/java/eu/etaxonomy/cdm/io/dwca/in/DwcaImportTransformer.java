24b1e40f8aee48dcc542c06171e4adecb0897eb2
Andreas Müller <a.mueller@bgbm.org>
2012-09-11 13:13:15 +0000

    add dwca import transformer for features

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/dwca/in/DwcaImportTransformer.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/dwca/in/DwcaImportTransformer.java
@@ -10,9 +10,13 @@
 
 package eu.etaxonomy.cdm.io.dwca.in;
 
+import java.util.UUID;
+
 import org.apache.log4j.Logger;
 
 import eu.etaxonomy.cdm.io.common.mapping.InputTransformerBase;
+import eu.etaxonomy.cdm.io.common.mapping.UndefinedTransformerMethodException;
+import eu.etaxonomy.cdm.model.description.Feature;
 
 /**
  * @author a.mueller
@@ -21,8 +25,29 @@ import eu.etaxonomy.cdm.io.common.mapping.InputTransformerBase;
 public final class DwcaImportTransformer extends InputTransformerBase {
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(DwcaImportTransformer.class);
-	
 
+	/* (non-Javadoc)
+	 * @see eu.etaxonomy.cdm.io.common.mapping.InputTransformerBase#getFeatureByKey(java.lang.String)
+	 */
+	@Override
+	public Feature getFeatureByKey(String key) throws UndefinedTransformerMethodException {
+		if (key == null){
+			return null;
+		}else if (key.equalsIgnoreCase("morphology")){
+			return Feature.Morphology();
+		}
+		return super.getFeatureByKey(key);
+	}
+
+	/* (non-Javadoc)
+	 * @see eu.etaxonomy.cdm.io.common.mapping.InputTransformerBase#getFeatureUuid(java.lang.String)
+	 */
+	@Override
+	public UUID getFeatureUuid(String key) throws UndefinedTransformerMethodException {
+		return super.getFeatureUuid(key);
+	}
+	
+	
 
 	
 	
