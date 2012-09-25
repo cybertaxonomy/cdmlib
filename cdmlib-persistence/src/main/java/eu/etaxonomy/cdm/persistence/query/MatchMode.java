287378b539af6d0af7cd3069fc12f50a9f189caf
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2012-09-25 13:02:36 +0000

    simpler and more predictable persistence/query/MatchMode.java

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/query/MatchMode.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/query/MatchMode.java
@@ -2,52 +2,90 @@
 /**
 
 * Copyright (C) 2009 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
 package eu.etaxonomy.cdm.persistence.query;
 
+
 /**
- * 
- * 
+ *
+ *
  * @author a.babadshanjan
  * @created 03.03.2009
  * @version 1.0
  */
 public enum MatchMode {
-	EXACT("="),
-	LIKE("LIKE"),
-	BEGINNING("LIKE"),
-	ANYWHERE("LIKE"),
-	END("LIKE");
-	
-	private String matchOperator;
-	
-	MatchMode(String matchOperator){
-		this.matchOperator = matchOperator;
-	}
-	
-	public String queryStringFrom(String queryString){
-		if(queryString == null){
-			return "";
-		}
-		queryString = queryString.replace('*', '%');
-		switch(this){	
-			case BEGINNING:
-				return queryString + "%";			
-			case ANYWHERE:
-				return "%" + queryString + "%";
-			case END:
-				return "%" + queryString;
-			default:
-				return queryString;
-		}
-	}
-	
-	public String getMatchOperator(){
-		return matchOperator;
-	}
+
+    /**
+     * translates into <code>field = term</code>
+     */
+    EXACT("="),
+    /**
+     * translates into <code>field LIKE %term</code>
+     */
+    BEGINNING("LIKE"),
+    /**
+     * translates into <code>field LIKE %term%</code>
+     */
+    ANYWHERE("LIKE"),
+    /**
+     * translates into <code>field LIKE term%</code>
+     */
+    END("LIKE");
+
+    private String matchOperator;
+
+    MatchMode(String matchOperator){
+        this.matchOperator = matchOperator;
+    }
+
+    public String queryStringFrom(String queryString){
+        if(queryString == null){
+            return "";
+        }
+        queryString = queryString.replace('*', '%');
+        switch(this){
+            case BEGINNING:
+                queryString = appendWildcard(queryString);
+                break;
+            case END:
+                queryString = prependWildcard(queryString);
+                break;
+            case ANYWHERE:
+                queryString = appendWildcard(queryString);
+                queryString = prependWildcard(queryString);
+                break;
+        }
+        return queryString;
+    }
+
+    /**
+     * @param queryString
+     * @return
+     */
+    private String prependWildcard(String queryString) {
+        if(!queryString.startsWith("%")){
+            queryString += "%";
+        }
+        return queryString;
+    }
+
+    /**
+     * @param queryString
+     * @return
+     */
+    private String appendWildcard(String queryString) {
+        if(!queryString.endsWith("%")){
+            queryString += "%";
+        }
+        return queryString;
+    }
+
+    public String getMatchOperator(){
+        return matchOperator;
+    }
 }
--- cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/dao/hibernate/common/IdentifiableDaoBaseTest.java
+++ cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/dao/hibernate/common/IdentifiableDaoBaseTest.java
@@ -37,126 +37,126 @@ import eu.etaxonomy.cdm.test.integration.CdmIntegrationTest;
 @DataSet
 public class IdentifiableDaoBaseTest extends CdmIntegrationTest {
 
-	@SpringBeanByType
-	private  TaxonDaoHibernateImpl identifiableDao;
+    @SpringBeanByType
+    private  TaxonDaoHibernateImpl identifiableDao;
 
-	private UUID uuid;
+    private UUID uuid;
 
-	@Before
-	public void setUp() {
-		uuid = UUID.fromString("496b1325-be50-4b0a-9aa2-3ecd610215f2");
-	}
+    @Before
+    public void setUp() {
+        uuid = UUID.fromString("496b1325-be50-4b0a-9aa2-3ecd610215f2");
+    }
 
 /************ TESTS ********************************/
 
-	/**
-	 * Test method for {@link eu.etaxonomy.cdm.persistence.dao.hibernate.common.IdentifiableDaoBase#IdentifiableDaoBase(java.lang.Class)}.
-	 */
-	@Test
-	public void testIdentifiableDaoBase() {
-		assertNotNull(identifiableDao);
-	}
+    /**
+     * Test method for {@link eu.etaxonomy.cdm.persistence.dao.hibernate.common.IdentifiableDaoBase#IdentifiableDaoBase(java.lang.Class)}.
+     */
+    @Test
+    public void testIdentifiableDaoBase() {
+        assertNotNull(identifiableDao);
+    }
 
-	/**
-	 * Test method for {@link eu.etaxonomy.cdm.persistence.dao.hibernate.common.IdentifiableDaoBase#findByTitle(java.lang.String)}.
-	 */
-	@Test
-	public void testFindByTitle() {
-		List<TaxonBase> results = identifiableDao.findByTitle("Lorem");
-		assertNotNull("findByTitle should return a list",results);
-		assertEquals("findByTitle should return one entity", 1, results.size());
-		assertEquals("findByTitle should return an entity with uuid " + uuid,uuid, results.get(0).getUuid());
-	}
+    /**
+     * Test method for {@link eu.etaxonomy.cdm.persistence.dao.hibernate.common.IdentifiableDaoBase#findByTitle(java.lang.String)}.
+     */
+    @Test
+    public void testFindByTitle() {
+        List<TaxonBase> results = identifiableDao.findByTitle("Lorem");
+        assertNotNull("findByTitle should return a list",results);
+        assertEquals("findByTitle should return one entity", 1, results.size());
+        assertEquals("findByTitle should return an entity with uuid " + uuid,uuid, results.get(0).getUuid());
+    }
 
-	@Test
-	public void testCountByTitle() {
-		int result = identifiableDao.countByTitle("%");
-		assertNotNull("findByTitle should return an integer",result);
-		assertEquals("findByTitle should return 2", 2, result);
+    @Test
+    public void testCountByTitle() {
+        int result = identifiableDao.countByTitle("%");
+        assertNotNull("findByTitle should return an integer",result);
+        assertEquals("findByTitle should return 2", 2, result);
 
-		result = identifiableDao.countByTitle("%", MatchMode.LIKE, null);
-		assertNotNull("findByTitle should return an integer",result);
-		assertEquals("findByTitle should return 2", 2, result);
+        result = identifiableDao.countByTitle("%", MatchMode.ANYWHERE, null);
+        assertNotNull("findByTitle should return an integer",result);
+        assertEquals("findByTitle should return 2", 2, result);
 
-		result = identifiableDao.countByTitle("Lorem");
-		assertNotNull("findByTitle should return an integer",result);
-		assertEquals("findByTitle should return 1", 1, result);
+        result = identifiableDao.countByTitle("Lorem");
+        assertNotNull("findByTitle should return an integer",result);
+        assertEquals("findByTitle should return 1", 1, result);
 
-		result = identifiableDao.countByTitle("Lorem", MatchMode.LIKE, null);
-		assertNotNull("findByTitle should return an integer",result);
-		assertEquals("findByTitle should return 1", 1, result);
-	}
-	@Test
-	public void testGetRights() {
-		TaxonBase taxon = identifiableDao.findByUuid(uuid);
-		assert taxon != null : "IdentifiableEntity must exist";
+        result = identifiableDao.countByTitle("Lorem", MatchMode.ANYWHERE, null);
+        assertNotNull("findByTitle should return an integer",result);
+        assertEquals("findByTitle should return 1", 1, result);
+    }
+    @Test
+    public void testGetRights() {
+        TaxonBase taxon = identifiableDao.findByUuid(uuid);
+        assert taxon != null : "IdentifiableEntity must exist";
 
-		List<Rights> rights = identifiableDao.getRights(taxon, null, null, null);
+        List<Rights> rights = identifiableDao.getRights(taxon, null, null, null);
 
-		assertNotNull("getRights should return a List",rights);
-		assertFalse("the list should not be empty",rights.isEmpty());
-		assertEquals("getRights should return 2 Rights instances",2,rights.size());
-	}
+        assertNotNull("getRights should return a List",rights);
+        assertFalse("the list should not be empty",rights.isEmpty());
+        assertEquals("getRights should return 2 Rights instances",2,rights.size());
+    }
 
-	@Test
-	public void testGetCredits() {
-		TaxonBase taxon = identifiableDao.findByUuid(uuid);
-		assert taxon != null : "IdentifiableEntity must exist";
-		taxon.getCredits();
+    @Test
+    public void testGetCredits() {
+        TaxonBase taxon = identifiableDao.findByUuid(uuid);
+        assert taxon != null : "IdentifiableEntity must exist";
+        taxon.getCredits();
 
-		List<Credit> credits = identifiableDao.getCredits(taxon, null, null);
+        List<Credit> credits = identifiableDao.getCredits(taxon, null, null);
 
-		assertNotNull("getCredits should return a List",credits);
-		assertFalse("the list should not be empty",credits.isEmpty());
-		assertEquals("getCredits should return 3 Credit instances",3,credits.size());
-	}
+        assertNotNull("getCredits should return a List",credits);
+        assertFalse("the list should not be empty",credits.isEmpty());
+        assertEquals("getCredits should return 3 Credit instances",3,credits.size());
+    }
 
-	@Test
-	public void testCreditsOrder() {
-		TaxonBase taxon = identifiableDao.findByUuid(uuid);
-		assert taxon != null : "IdentifiableEntity must exist";
-		List<Credit> credits = taxon.getCredits();
+    @Test
+    public void testCreditsOrder() {
+        TaxonBase taxon = identifiableDao.findByUuid(uuid);
+        assert taxon != null : "IdentifiableEntity must exist";
+        List<Credit> credits = taxon.getCredits();
 
-		assertNotNull("getCredits should return a List",credits);
-		assertFalse("the list should not be empty",credits.isEmpty());
-		assertEquals("getCredits should return 3 Credit instances",3,credits.size());
-		assertEquals("My first credit", credits.get(0).getText());
-		assertEquals("My second credit", credits.get(1).getText());
-		assertEquals("My third credit", credits.get(2).getText());
-	}
+        assertNotNull("getCredits should return a List",credits);
+        assertFalse("the list should not be empty",credits.isEmpty());
+        assertEquals("getCredits should return 3 Credit instances",3,credits.size());
+        assertEquals("My first credit", credits.get(0).getText());
+        assertEquals("My second credit", credits.get(1).getText());
+        assertEquals("My third credit", credits.get(2).getText());
+    }
 
 
-	@Test
-	public void testSources() throws Exception {
-		TaxonBase taxon = identifiableDao.findByUuid(uuid);
-		assert taxon != null : "IdentifiableEntity must exist";
+    @Test
+    public void testSources() throws Exception {
+        TaxonBase taxon = identifiableDao.findByUuid(uuid);
+        assert taxon != null : "IdentifiableEntity must exist";
 
-		List<IdentifiableSource> sources = identifiableDao.getSources(taxon, null, null,null);
+        List<IdentifiableSource> sources = identifiableDao.getSources(taxon, null, null,null);
 
-		assertNotNull("getSources should return a List", sources);
-		assertFalse("the list should not be empty", sources.isEmpty());
-		assertEquals("getSources should return 2 OriginalSource instances",2, sources.size());
-	}
+        assertNotNull("getSources should return a List", sources);
+        assertFalse("the list should not be empty", sources.isEmpty());
+        assertEquals("getSources should return 2 OriginalSource instances",2, sources.size());
+    }
 
-	@Test
-	public void testGetByLsidWithoutVersion() throws Exception {
-		LSID lsid = new LSID("urn:lsid:example.org:namespace:1");
-		TaxonBase result = identifiableDao.find(lsid);
-		assertNotNull(result);
-	}
+    @Test
+    public void testGetByLsidWithoutVersion() throws Exception {
+        LSID lsid = new LSID("urn:lsid:example.org:namespace:1");
+        TaxonBase result = identifiableDao.find(lsid);
+        assertNotNull(result);
+    }
 
-	@Test
-	public void testGetByLsidWithVersionCurrent() throws Exception {
-		LSID lsid = new LSID("urn:lsid:example.org:namespace:1:2");
-		TaxonBase result = identifiableDao.find(lsid);
-		assertNotNull(result);
-	}
+    @Test
+    public void testGetByLsidWithVersionCurrent() throws Exception {
+        LSID lsid = new LSID("urn:lsid:example.org:namespace:1:2");
+        TaxonBase result = identifiableDao.find(lsid);
+        assertNotNull(result);
+    }
 
-	@Test
-	public void testGetByLsidWithVersionPast() throws Exception {
-		LSID lsid = new LSID("urn:lsid:example.org:namespace:1:1");
-		TaxonBase result = identifiableDao.find(lsid);
-		assertNotNull(result);
-	}
+    @Test
+    public void testGetByLsidWithVersionPast() throws Exception {
+        LSID lsid = new LSID("urn:lsid:example.org:namespace:1:1");
+        TaxonBase result = identifiableDao.find(lsid);
+        assertNotNull(result);
+    }
 
 }
