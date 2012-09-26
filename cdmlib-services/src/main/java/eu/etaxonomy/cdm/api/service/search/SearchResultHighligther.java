47b5fe891c160f461910578f404dc484031ef256
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2012-09-26 16:05:03 +0000

    upgrading to lucene 2.9 and fixing #3114 (fieldHighlightMap is empty with lucene wildcard searches) - code needs cleanup

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/search/SearchResultHighligther.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/search/SearchResultHighligther.java
@@ -23,18 +23,18 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.TermPositionVector;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.Searcher;
 import org.apache.lucene.search.highlight.Fragmenter;
 import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
 import org.apache.lucene.search.highlight.QueryScorer;
 import org.apache.lucene.search.highlight.Scorer;
 import org.apache.lucene.search.highlight.SimpleFragmenter;
 import org.apache.lucene.search.highlight.SimpleSpanFragmenter;
-import org.apache.lucene.search.highlight.SpanScorer;
 import org.apache.lucene.search.highlight.TokenSources;
 
 /**
- * This SearchResultHighligther is using the QueryScorer by default even if the SpanScorer is meant to be the new default scorer in Lucene,
+ * This SearchResultHighligther is using the
+ * QueryScorer by default even if the SpanScorer is meant to be the new default scorer in Lucene,
  * see https://issues.apache.org/jira/browse/LUCENE-1685 and https://issues.apache.org/jira/browse/LUCENE-2013.
  * The SpanScorer was causing problems with phrase queries (see https://dev.e-taxonomy.eu/trac/ticket/2961)
  * whereas the QueryScorer was returning good results.
@@ -52,7 +52,7 @@ public class SearchResultHighligther {
 
     public static final Logger logger = Logger.getLogger(SearchResultHighligther.class);
 
-    private boolean useSpanScorer = false;
+    private boolean useSpanScorer = true;
 
     public boolean isUseSpanScorer() {
         return useSpanScorer;
@@ -144,21 +144,43 @@ public class SearchResultHighligther {
     private String[] getFragmentsWithHighlightedTerms(TokenStream stream, Query query, String fieldName, String fieldContents, int fragmentNumber,
             int fragmentSize) throws IOException {
 
-        Fragmenter fragmenter;
-        Scorer scorer;
-        if(useSpanScorer){
-            scorer = new SpanScorer(query, fieldName, new CachingTokenFilter(stream));
-            fragmenter = new SimpleSpanFragmenter((SpanScorer)scorer, fragmentSize);
+        /*
+         * for (int i = 0; i < hits.length(); i++) {
+       String text = hits.doc(i).get(FIELD_NAME);
+       CachingTokenFilter tokenStream = new CachingTokenFilter(analyzer.tokenStream(FIELD_NAME,
+           new StringReader(text)));
+       Highlighter highlighter = new Highlighter(this, new SpanScorer(query, FIELD_NAME, tokenStream));
+       highlighter.setTextFragmenter(new SimpleFragmenter(40));
+       tokenStream.reset();
+
+       String result = highlighter.getBestFragments(tokenStream, text, maxNumFragmentsRequired,
+           "...");
+         */
+
+        Fragmenter fragmenter = null;
+        Highlighter highlighter = null;
+        if(false && useSpanScorer){
+//            new CachingTokenFilter(stream);
+//            SpanScorer scorer = new SpanScorer(query, fieldName);
+//            highlighter = null;// new Highlighter(this, scorer);
+//            fragmenter = null; //new SimpleSpanFragmenter((SpanScorer)scorer, fragmentSize);
         } else {
+            Scorer scorer;
             scorer = new QueryScorer(query, fieldName);
             fragmenter = new SimpleFragmenter(fragmentSize);
+            highlighter = new Highlighter(scorer);
         }
 
-        Highlighter highlighter = new Highlighter(scorer);
         highlighter.setTextFragmenter(fragmenter);
         highlighter.setMaxDocCharsToAnalyze(Integer.MAX_VALUE);
 
-        String[] fragments = highlighter.getBestFragments(stream, fieldContents, fragmentNumber);
+        String[] fragments = null;
+        try {
+            fragments = highlighter.getBestFragments(stream, fieldContents, fragmentNumber);
+        } catch (InvalidTokenOffsetsException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return fragments;
     }
 
