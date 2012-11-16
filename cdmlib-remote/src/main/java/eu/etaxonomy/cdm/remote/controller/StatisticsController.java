33a4750b4494b7b49aec76ade8f76d3e6571e6d3
s.buers <s.buers@localhost>
2012-11-16 12:32:54 +0000

    just trying to solving subversion commit problems

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/StatisticsController.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/StatisticsController.java
@@ -1,26 +1,23 @@
 package eu.etaxonomy.cdm.remote.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import eu.etaxonomy.cdm.api.service.IClassificationService;
-import eu.etaxonomy.cdm.api.service.ITaxonService;
-import eu.etaxonomy.cdm.api.service.IdentifiableServiceBase;
+import eu.etaxonomy.cdm.api.service.IStatisticsService;
 import eu.etaxonomy.cdm.api.service.statistics.Statistics;
 import eu.etaxonomy.cdm.api.service.statistics.StatisticsConfigurator;
 import eu.etaxonomy.cdm.api.service.statistics.StatisticsPartEnum;
-import eu.etaxonomy.cdm.api.service.statistics.StatisticsTypeEnum;
-import eu.etaxonomy.cdm.model.common.AnnotatableEntity;
-import eu.etaxonomy.cdm.model.taxon.Classification;
-import eu.etaxonomy.cdm.model.taxon.TaxonBase;
-import eu.etaxonomy.cdm.persistence.dao.hibernate.common.IdentifiableDaoBase;
-import eu.etaxonomy.cdm.persistence.dao.hibernate.taxon.TaxonDaoHibernateImpl;
-import eu.etaxonomy.cdm.persistence.dao.taxon.IClassificationDao;
-import eu.etaxonomy.cdm.persistence.dao.taxon.ITaxonDao;
-import eu.etaxonomy.cdm.strategy.cache.common.IdentifiableEntityDefaultCacheStrategy;
+import eu.etaxonomy.cdm.model.common.IdentifiableEntity;
 
 /**
  * @author sybille
@@ -30,95 +27,109 @@ import eu.etaxonomy.cdm.strategy.cache.common.IdentifiableEntityDefaultCacheStra
 @Controller
 @RequestMapping(value = { "/statistic" })
 public class StatisticsController extends
-		BaseController<TaxonBase, ITaxonService> {
+		BaseController<IdentifiableEntity<?>, IStatisticsService> {
 
-	
+	private static final Logger logger = Logger
+			.getLogger(StatisticsController.class);
+
 	@Autowired
-    private IClassificationService clService;
-	
-	@Autowired
-	private ITaxonDao taxonDao;
-	
-	@Autowired
-	private IClassificationDao classificationDao;
-	
+	private IClassificationService clService;
+
 	@Override
 	@Autowired
-	public void setService(ITaxonService service) {
-		// TODO Auto-generated method stub
-
+	public void setService(IStatisticsService service) {
+		this.service = service;
+//		System.out.println();
 	}
-	
-	
 
-	
-	
 	StatisticsConfigurator configurator;
-	
-	@RequestMapping(value = { "count" }, method = RequestMethod.GET)
-	public Statistics count(@RequestParam(value = "All", required = false) boolean all,
-			@RequestParam(value = "Classification", required = false) boolean classification,
-			@RequestParam(value = "Filter", required=false) StatisticsTypeEnum filter) {
-		
-		configurator = new StatisticsConfigurator();
-		
-		
-		
-		if(all||(!all&&!classification)){
-			configurator.addPart(StatisticsPartEnum.ALL);
-		}
-		
-		if(classification ){
-			configurator.addPart(StatisticsPartEnum.CLASSIFICATION);
-		}
-		
-		//TODO count in classification
-		switch (filter) {
-		case CLASSIFICATION:
-			//that does not make to much sense yet:
-			configurator.addFilter(Classification.NewInstance("classification"));
-			
-		case ALL_TAXA:
-		case ACCEPTED_TAXA:
-		case ALL_REFERENCES:
-		case SYNONYMS:
-		case TAXON_NAMES:
-		case NOMECLATURAL_REFERENCES:
-		case DESCRIPTIVE_SOURCE_REFERENCES:
-			
-			
-			break;
 
-		default:
-			
-			//TODO set to default filter
-			break;
-		}
+	@RequestMapping(value = { "statistics" }, method = RequestMethod.GET)
+	public ModelAndView doStatistics(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
 		
-		return getStatistics();
+		configurator= new StatisticsConfigurator();
+		ModelAndView mv = new ModelAndView();
+		
+		//TODO fill configurator;
+//		service.getStatistics(configurator);
+		Integer i =((Integer)service.countAll(configurator));
+		logger.info(i.toString());
+//		mv.addObject((Integer)service.count(Taxon.class));
+		mv.addObject(i);
+		return mv;
 	}
+
 	
+	// 16.11.2012
+	
+	
+	// public Statistics count(@RequestParam(value = "All", required = false)
+	// boolean all,
+	// @RequestParam(value = "Classification", required = false) boolean
+	// classification,
+	// @RequestParam(value = "Filter", required=false) StatisticsTypeEnum
+	// filter) {
+	//
+	// configurator = new StatisticsConfigurator();
+	//
+	//
+	//
+	// if(all||(!all&&!classification)){
+	// configurator.addPart(StatisticsPartEnum.ALL);
+	// }
+	//
+	// if(classification ){
+	// configurator.addPart(StatisticsPartEnum.CLASSIFICATION);
+	// }
+	//
+	// //TODO count in classification
+	// switch (filter) {
+	// case CLASSIFICATION:
+	// //that does not make to much sense yet:
+	// configurator.addFilter(Classification.NewInstance("classification"));
+	//
+	// case ALL_TAXA:
+	// case ACCEPTED_TAXA:
+	// case ALL_REFERENCES:
+	// case SYNONYMS:
+	// case TAXON_NAMES:
+	// case NOMECLATURAL_REFERENCES:
+	// case DESCRIPTIVE_SOURCE_REFERENCES:
+	//
+	//
+	// break;
+	//
+	// default:
+	//
+	// //TODO set to default filter
+	// break;
+	// }
+	//
+	// return getStatistics();
+	// }
+
 	// this could be a service on it's own:
 	private Statistics getStatistics() {
-		Statistics result = new Statistics();
+//		Statistics result = new Statistics();
 		for (StatisticsPartEnum part : configurator.getPartList()) {
-			if (part.compareTo(StatisticsPartEnum.ALL)==0){
-				//TODO iterate over filter and call countAll(class type);
-//				System.out.println(taxonDao.cou);
-			}
-			else if (part.compareTo(StatisticsPartEnum.CLASSIFICATION)==0){
+			if (part.compareTo(StatisticsPartEnum.ALL) == 0) {
+				// TODO iterate over filter and call countAll(class type);
+				// System.out.println(taxonDao.cou);
+			} else if (part.compareTo(StatisticsPartEnum.CLASSIFICATION) == 0) {
 				// TODO get all classifications
-				//TODO find out if i have to parse through references or if there is already a method
-				
+				// TODO find out if i have to parse through references or if
+				// there is already a method
+
 			}
 		}
 		return null;
 	}
 
-// TODO countAll();
-//	Statistics countAll(IdentifiableServiceBase<IdentifiableEntity, IIdentifiableDao<T>> service){
-//		
-//	}
-	
+	// TODO countAll();
+	// Statistics countAll(IdentifiableServiceBase<IdentifiableEntity,
+	// IIdentifiableDao<T>> service){
+	//
+	// }
 
 }
