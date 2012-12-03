43258c67664c64423ff0843a65219212ee0d9cc4
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2012-12-03 13:04:01 +0000

    reverting changes from r16676 r16677 (maven 3 related)

--- cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/application/FirstDataInserterTest.java
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/application/FirstDataInserterTest.java
@@ -14,7 +14,6 @@ import org.unitils.dbunit.annotation.DataSet;
 import org.unitils.dbunit.annotation.ExpectedDataSet;
 
 import eu.etaxonomy.cdm.test.integration.CdmTransactionalIntegrationTest;
-import eu.etaxonomy.cdm.test.unitils.CleanSweepInsertLoadStrategy;
 
 /**
  * test for the {@link FirstDataInserter}
@@ -29,7 +28,7 @@ public class FirstDataInserterTest extends CdmTransactionalIntegrationTest {
     private final String[] tableNames = new String[]{"USERACCOUNT", "USERACCOUNT_GRANTEDAUTHORITYIMPL", "GRANTEDAUTHORITYIMPL", "CDMMETADATA"};
 
     @Test
-    @DataSet(loadStrategy=CleanSweepInsertLoadStrategy.class, value="FirstDataInserterTest.testBlankDB.xml")
+    @DataSet(value="FirstDataInserterTest.testBlankDB.xml")
     @ExpectedDataSet(value="FirstDataInserterTest.testBlankDB-result.xml")
     public void testBlankDB(){
 
