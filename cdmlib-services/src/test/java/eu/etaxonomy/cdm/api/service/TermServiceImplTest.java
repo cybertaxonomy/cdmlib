38ab112656e33d4ce32d2a6e989725859676a4a1
s.buers <s.buers@localhost>
2012-12-10 13:11:47 +0000

    merged from trunk and finished "ALL" part, descriptive source references has to be tested!

--- cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/TermServiceImplTest.java
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/TermServiceImplTest.java
@@ -17,34 +17,31 @@ import java.net.URI;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
-import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.unitils.spring.annotation.SpringBeanByType;
 
-import eu.etaxonomy.cdm.api.service.DeleteResult.DeleteStatus;
-import eu.etaxonomy.cdm.api.service.config.TermDeletionConfigurator;
 import eu.etaxonomy.cdm.api.service.pager.Pager;
 import eu.etaxonomy.cdm.model.common.DefinedTermBase;
 import eu.etaxonomy.cdm.model.common.Language;
+import eu.etaxonomy.cdm.model.common.Representation;
 import eu.etaxonomy.cdm.model.common.TermVocabulary;
 import eu.etaxonomy.cdm.model.location.NamedArea;
-import eu.etaxonomy.cdm.model.location.TdwgArea;
 import eu.etaxonomy.cdm.model.name.Rank;
 import eu.etaxonomy.cdm.model.name.SpecimenTypeDesignationStatus;
-import eu.etaxonomy.cdm.test.integration.CdmIntegrationTest;
+import eu.etaxonomy.cdm.test.integration.CdmTransactionalIntegrationTest;
 
 /**
  * @author a.mueller
  * @created 27.05.2008
  * @version 1.0
  */
-public class TermServiceImplTest extends CdmIntegrationTest {
+public class TermServiceImplTest extends CdmTransactionalIntegrationTest{
     @SuppressWarnings("unused")
     private static final Logger logger = Logger.getLogger(TermServiceImplTest.class);
 
     @SpringBeanByType
-    private ITermService service;
+    private ITermService termService;
 
     @SpringBeanByType
     private IVocabularyService vocabularyService;
@@ -66,7 +63,7 @@ public class TermServiceImplTest extends CdmIntegrationTest {
     public void testGetTermByUri() {
         String uriStr = "http://any.uri.com";
         URI uri = URI.create(uriStr);
-        DefinedTermBase<?> term = service.getByUri(uri);
+        DefinedTermBase<?> term = termService.getByUri(uri);
         assertNotNull(term);
         //for testing only
 //		TermVocabulary<?> voc = term.getVocabulary();
@@ -78,7 +75,7 @@ public class TermServiceImplTest extends CdmIntegrationTest {
         //I do not understand where the vocabulary data comes from (checked persistence TermsDataSet-with_auditing_info.xml) but somehow this does not apply
         String uriNotExistStr = "http://www.notExisting.com";
         URI uriNotExist = URI.create(uriNotExistStr);
-        DefinedTermBase<?> termNotExist = service.getByUri(uriNotExist);
+        DefinedTermBase<?> termNotExist = termService.getByUri(uriNotExist);
         assertNull(termNotExist);
     }
 
@@ -95,13 +92,13 @@ public class TermServiceImplTest extends CdmIntegrationTest {
         // Rank.Domain
         String strUUID = "ffca6ec8-8b88-417b-a6a0-f7c992aac19b";
         UUID uuid = UUID.fromString(strUUID);
-        DefinedTermBase<?> term = service.find(uuid);
+        DefinedTermBase<?> term = termService.find(uuid);
         assertNotNull(term);
         assertEquals(Rank.DOMAIN(), term);
         //NULL
         String strUUIDNotExist = "00000000-8b88-417b-a6a0-f7c992aac19c";
         UUID uuidNotExist = UUID.fromString(strUUIDNotExist);
-        DefinedTermBase<?> termNotExist = service.find(uuidNotExist);
+        DefinedTermBase<?> termNotExist = termService.find(uuidNotExist);
         assertNull(termNotExist);
     }
 
@@ -139,7 +136,7 @@ public class TermServiceImplTest extends CdmIntegrationTest {
      */
     public void testGetAreaByTdwgAbbreviation(){
         String tdwgAbbreviation = "GER-OO";
-        NamedArea germany = service.getAreaByTdwgAbbreviation(tdwgAbbreviation);
+        NamedArea germany = termService.getAreaByTdwgAbbreviation(tdwgAbbreviation);
         assertEquals(tdwgAbbreviation, germany.getRepresentation(Language.DEFAULT()).getAbbreviatedLabel());
     }
 
@@ -150,10 +147,40 @@ public class TermServiceImplTest extends CdmIntegrationTest {
      *    and thus will cause unitils empty the according tables
      */
     public void testListTerms() {
-        Pager<SpecimenTypeDesignationStatus> results = (Pager)service.page(SpecimenTypeDesignationStatus.class, null,null,null,null);
+        Pager<SpecimenTypeDesignationStatus> results = (Pager)termService.page(SpecimenTypeDesignationStatus.class, null,null,null,null);
         assertNotNull("Results should not be null",results);
     }
 	
+    @Ignore
+    @Test
+    public void testTitleCacheUpdate(){
+    	String uuid = "ae787603-3070-4298-9ca6-4cbe73378122";
+    	UUID fromString = UUID.fromString(uuid);
+    	DefinedTermBase<?> termBase = termService.find(fromString);
+    	
+    	// change label
+    	String expectedTitleCache = termBase.getLabel() + "append";
+    	termBase.setLabel(expectedTitleCache);
+    	
+    	commitAndStartNewTransaction(null);
 
+    	termBase = termService.find(fromString);
+    	assertEquals("Title cache did not update after setting the label and saving the term", expectedTitleCache, termBase.getTitleCache());
+
+    	// add new representation for default language
+    	String expecteTitleCacheAfterRepresentationChange = "new label";
+    	Representation representation = termBase.getRepresentation(Language.DEFAULT());
+    	representation.setLabel(expecteTitleCacheAfterRepresentationChange);
+    	termBase.addRepresentation(representation);
+    	
+    	//this will create another termBase in the DB which has the same UUID -> test failure
+//    	termBase.addRepresentation(Representation.NewInstance(expecteTitleCacheAfterRepresentationChange, "", "", Language.DEFAULT()));////new Representation(expecteTitleCacheAfterRepresentationChange, "", "", Language.DEFAULT()));
+
+    	
+    	commitAndStartNewTransaction(null);
+    	
+    	termBase = termService.find(fromString);
+    	assertEquals("Title cache did not update after adding a new representation for default language and saving the term", expecteTitleCacheAfterRepresentationChange, termBase.getTitleCache());
+    }
 	
 }
