c48513aae4ec67e6c3763a6680c86b4a1fbd9a0b
s.buers <s.buers@localhost>
2012-12-18 11:49:19 +0000

    für patricia

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/hibernate/statistics/StatisticsDaoHibernateImpl.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/hibernate/statistics/StatisticsDaoHibernateImpl.java
@@ -1,14 +1,18 @@
 package eu.etaxonomy.cdm.persistence.dao.hibernate.statistics;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
+import org.hibernate.Criteria;
 import org.hibernate.Query;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
-import eu.etaxonomy.cdm.model.common.DescriptionElementSource;
-import eu.etaxonomy.cdm.model.reference.Reference;
+import eu.etaxonomy.cdm.model.taxon.Classification;
+import eu.etaxonomy.cdm.model.taxon.Synonym;
+import eu.etaxonomy.cdm.model.taxon.Taxon;
+import eu.etaxonomy.cdm.model.taxon.TaxonBase;
+import eu.etaxonomy.cdm.model.taxon.TaxonNode;
 import eu.etaxonomy.cdm.persistence.dao.hibernate.common.DaoBase;
 import eu.etaxonomy.cdm.persistence.dao.statistics.IStatisticsDao;
 
@@ -31,27 +35,84 @@ public class StatisticsDaoHibernateImpl extends DaoBase implements
 						+ where);
 		return (Long) query.uniqueResult();
 	}
-	
+
 	@Override
-	public Long countNomenclaturalReferences(Class clazz){
+	public Long countNomenclaturalReferences(Class clazz) {
 		return countNomenclaturalReferences("where" + clazz.getSimpleName());
 	}
 
 	/**
 	 * @return be aware that the returned long might be null
 	 */
-	
+
 	public Long countDescriptiveSourceReferences() {
 		Query query;
 		Long count = new Long(0);
-		
-		// count sources from Descriptions:
-		query = getSession().createQuery("select count(distinct d.descriptionSources) from DescriptionBase as d join d.descriptionSources");
-		
-		
-		query = getSession().createQuery("select count(distinct s.citation) from DescriptionElementBase as d join d.sources as s");
-		return (Long)query.uniqueResult();
-	}
-	
 
+		// count sources from Descriptions:
+		query = getSession()
+				.createQuery(
+						"select count(distinct d.descriptionSources) from DescriptionBase as d join d.descriptionSources");
+
+		query = getSession()
+				.createQuery(
+						"select count(distinct s.citation) from DescriptionElementBase as d join d.sources as s");
+		return (Long) query.uniqueResult();
+	}
+
+	// TODO remove this method:
+	@Override
+	public void tryArround() {
+		Criteria criteria = getSession().createCriteria(Synonym.class);
+		// Query query =
+		// System.out.println("query: "+((Query) criteria).getQueryString());
+		// System.out.println(getSession().createCriteria(Synonym.class);
+		criteria.list();
+		// criteria.
+		// System.out.println(criteria.toString());
+		System.out.println("");
+
+	}
+
+	@Override
+	public Long countTaxaInClassification(Class<? extends TaxonBase> clazz,
+			Classification classification) {
+
+		if (clazz.equals(TaxonBase.class)) {
+
+			return countTaxaInClassification(Taxon.class, classification)
+					+ countTaxaInClassification(Synonym.class, classification);
+		}
+		Criteria criteria = getSession().createCriteria(TaxonNode.class);
+
+		if (clazz.equals(Taxon.class)) {
+			criteria = getSession().createCriteria(TaxonNode.class);
+			criteria.add(Restrictions.eq("classification", classification));
+			criteria.setProjection(Projections.rowCount());
+			// criteria.
+			return Long.valueOf((Integer) criteria.uniqueResult());
+		}
+
+		else if (clazz.equals(Synonym.class)) {
+			// criteria= getSession().createCriteria(TaxonNode.class);
+			Query query = getSession()
+					.createQuery(
+							"create table t as select taxon from TaxonNode where classification = :classification; "
+									+ "select (distinct sr.uuid) from t join t.synonymRelations as sr"
+					// "select distinct sr.uuid from"
+					// +
+					// " (select taxon from TaxonNode tn where tn.classification = :classification) "
+					// +
+					// "as t "
+					// + "join t.synonymRelations as sr "
+					);
+			query.setParameter("classification", classification);
+			System.out.println(query.getQueryString());
+			List<Taxon> tList = query.list();
+			return new Long(0);
+		}
+
+		return new Long(0);
+
+	}
 }
