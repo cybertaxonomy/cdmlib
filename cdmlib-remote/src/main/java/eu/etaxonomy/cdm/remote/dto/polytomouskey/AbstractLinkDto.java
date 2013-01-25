8018950c52fedfdea991966a4d491b16ecf95fb4
l.morris <l.morris@localhost>
2013-01-25 17:52:51 +0000

    Added DTO classes for returning a flat data structure for displaying each row of a Polytomous key in linked style.

---
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/dto/polytomouskey/AbstractLinkDto.java
@@ -0,0 +1,19 @@
+// $Id$
+/**
+* Copyright (C) 2009 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+* 
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.remote.dto.polytomouskey;
+
+/**
+ * @author l.morris
+ * @date Jan 25, 2013
+ *
+ */
+public abstract class AbstractLinkDto {
+
+}
