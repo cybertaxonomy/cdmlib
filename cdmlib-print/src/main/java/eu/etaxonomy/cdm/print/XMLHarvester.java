571606d1d777af15f36efd8db23d2e84eb21a7f0
l.morris <l.morris@localhost>
2013-02-21 12:41:50 +0000

    Made Xpath for selecting features from descriptions more specific so that only the feature associated with a particular Taxon is selected. Fix IndexOutOfBounds exception for parsing date string.

--- cdmlib-print/src/main/java/eu/etaxonomy/cdm/print/XMLHarvester.java
+++ cdmlib-print/src/main/java/eu/etaxonomy/cdm/print/XMLHarvester.java
@@ -15,10 +15,12 @@ import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
+import org.jdom.output.XMLOutputter;
 import org.jdom.xpath.XPath;
 
 import eu.etaxonomy.cdm.common.monitor.IProgressMonitor;
@@ -76,15 +78,16 @@ public class XMLHarvester {
 			List<Element> childFeatureNodes = children.getChildren();
 			
 			for(Element childNode : childFeatureNodes){
+				
 				UUID uuid = XMLHelper.getUuid(childNode);
 				Element featureNodeElement = factory.getFeatureNode(uuid);
 				Element featureElement = factory.getFeatureForFeatureNode(uuid);
-				featureElement.setName(featureElement.getName().toLowerCase(Locale.ENGLISH));
-				SimplifiedFeatureNode simplifiedFeatureNode = new SimplifiedFeatureNode(featureElement, featureTreeRecursive(featureNodeElement));
-				
+				featureElement.setName(featureElement.getName().toLowerCase(Locale.FRENCH));//We set it to French here but all the names are in English
+				SimplifiedFeatureNode simplifiedFeatureNode = new SimplifiedFeatureNode(featureElement, featureTreeRecursive(featureNodeElement));				
 				result.add(simplifiedFeatureNode);
 			}
 		}
+		
 		return result;
 	}
 	
@@ -136,7 +139,7 @@ public class XMLHarvester {
 		
 		result.addContent(root);
 		
-//		cleanDateFields(result);
+		cleanDateFields(result);
 		
 		return result;
 	}
@@ -156,6 +159,11 @@ public class XMLHarvester {
 			
 			for(Element node : nodes){
 				String textWithRubbish = node.getText() ;
+				
+				if (textWithRubbish.length() < 5) {
+				logger.debug("textWithRubbish: " + textWithRubbish);
+				return;
+				}
 				String cleanedText = textWithRubbish.substring(0, 4);
 				node.setText(cleanedText);	
 				
@@ -188,7 +196,7 @@ public class XMLHarvester {
 		Element fullTaxonElement = factory.getAcceptedTaxonElement(taxonElement);
 		
 		populateTypeDesignations(fullTaxonElement);
-		
+	
 		// get descriptions
 		if(configurator.isDoDescriptions()){
 			populateDescriptions(fullTaxonElement);
@@ -236,12 +244,12 @@ public class XMLHarvester {
 	 */
 	private void populateChildren(Element taxonNodeElement){
 		
+		logger.setLevel(Level.INFO);
 		logger.info("populating branch");
 		
 		List<Element> childNodeElements = factory.getChildNodes(taxonNodeElement);
 		
 		for(Element childNodeElement : childNodeElements){
-			logger.info("Creating content for child node");
 			
 			populateTreeNodeContainer(childNodeElement);			
 			XMLHelper.addContent(childNodeElement, "childNodes", taxonNodeElement);
@@ -249,7 +257,7 @@ public class XMLHarvester {
 	}
 	
 	/**
-	 * Retrieves descriptions for the given taxonElement
+	 * Retrieves descriptions for the given taxonElement and adds them to a SimplifiedFeatureNode
 	 * 
 	 * @param taxonElement
 	 */
@@ -257,15 +265,24 @@ public class XMLHarvester {
 		taxonElement.removeChild("descriptions");
 		
 		Element rawDescriptions = factory.getDescriptions(taxonElement);
+		//logger.setLevel(Level.DEBUG);
 		
-		Element descriptions = new Element("descriptions");
-		
-		Element features = new Element("features");
-		
-		for(SimplifiedFeatureNode simplifiedFeatureNode : simplifiedFeatureTree){
+		logger.debug("The taxonElement is " + XMLHelper.getUuid(taxonElement) + " name " + XMLHelper.getTitleCache(taxonElement));
+				
+		Element descriptions = new Element("descriptions");		
+		Element features = new Element("features");		
+	
+		for(SimplifiedFeatureNode simplifiedFeatureNode : simplifiedFeatureTree){ 
 			
 			try {
+				
 				processFeatureNode(simplifiedFeatureNode, rawDescriptions, features);
+				
+				//UUID featureUuid = XMLHelper.getUuid(simplifiedFeatureNode.getFeatureElement());
+				//String featureTitleCache = XMLHelper.getTitleCache(simplifiedFeatureNode.getFeatureElement());
+				//logger.debug(" The feature uuid is " + featureUuid + " and name is " + featureTitleCache);
+				
+			
 			} catch (JDOMException e) {
 				logger.error(e);
 			}
@@ -275,34 +292,87 @@ public class XMLHarvester {
 	}
 	
 	private void processFeatureNode(SimplifiedFeatureNode featureNode, Object context, Element parentElement)  throws JDOMException{
-		// gets the feature elements with the current feature uuid
-		UUID featureUuid = XMLHelper.getUuid(featureNode.getFeatureElement());
-		String featurePattern = "//feature[contains(uuid,'" + featureUuid + "')]";
+		
+		// gets the feature elements with the current feature uuid	
+		UUID featureUuid = XMLHelper.getUuid(featureNode.getFeatureElement());	
+
+		String featurePatternold = "//feature[contains(uuid,'" + featureUuid + "')]";
+		//String featurePattern = "/ArrayList[1]/e[1]/elements[1]/e[1]/feature/uuid[.='" + featureUuid + "']";
+		//Xpath is now more specific so that only the feature associated with a particular Taxon and not the
+		//Taxon's parent or children are selected.
+		//Alternative would be to ensure the context object only contains descriptions for the Taxon element of interest
+		//Need to look at the taxonPortalController.doGetDescriptions to change this
+		String featurePattern = "/ArrayList[1]/e/elements/e/feature[contains(uuid,'" + featureUuid + "')]";
+		
+		Element feature = (Element) XPath.selectSingleNode(context, featurePattern);
+		
+		if(feature != null){  //the featurePattern was found in the raw descriptions data
+						
+			List<Element> descriptionElementElements = XPath.selectNodes(context, featurePattern + "/..");
+			
+			logger.debug("No of desc elements " +  descriptionElementElements.size() + " featureUUID " + featureUuid + " feature type is " + XMLHelper.getTitleCache(featureNode.getFeatureElement()));
+			// add matching description elements as children to this feature element
+			for(Element descriptionElementElement : descriptionElementElements){
+				
+				descriptionElementElement.removeChild("feature"); 
+				descriptionElementElement.setName("descriptionelement");
+				XMLHelper.addContent(descriptionElementElement, "descriptionelements", feature);
+				
+			}
+			XMLHelper.addContent(feature, parentElement);
+		}else if(featureNode.getChildren().size() > 0){
+			
+			Element featureElement = featureNode.getFeatureElement();
+			Element featureElementClone = (Element) featureElement.clone();
+			feature = (Element) featureElementClone.detach();
+			
+			XMLHelper.addContent(feature, parentElement); 
+		}		
+		
+		// recurse into children
+		for(SimplifiedFeatureNode childFeatureNode : featureNode.getChildren()){
+			
+			logger.debug("No of featureNode children " + featureNode.getChildren().size());//always 10
+			
+			UUID childFeatureUuid = XMLHelper.getUuid(childFeatureNode.getFeatureElement());
+			String childFeatureTitleCache = XMLHelper.getTitleCache(childFeatureNode.getFeatureElement());
+			logger.debug(" The feature is " + childFeatureUuid + " name " + childFeatureTitleCache);
+
+			//9 features in each simplifiedFeatureNode but some of the feature elements are null for a particular featureTitleCache,
+			//e.g. Ecology, Description - Description has child features
+			
+			//creates the second level features i.e. descriptions/features/feature/feature for the description
+			processFeatureNode(childFeatureNode, context, feature);
+		}
+	}
+	
+	
+	/*private Element processDescriptionsRecursive(Object context, SimplifiedFeatureNode simplifiedFeatureNode) throws JDOMException{
+		// gets the feature elements with the current uuid
+		String featurePattern = "//feature[contains(uuid,'" + simplifiedFeatureNode.getUuid() + "')]";
 		
 		Element feature = (Element) XPath.selectSingleNode(context, featurePattern);
 		
 		if(feature != null){
+			// recurse into children
+			for(SimplifiedFeatureNode childFeatureNode : simplifiedFeatureNode.getChildren()){
+				Element childFeatureWithElements = processDescriptionsRecursive(context, childFeatureNode);
+				XMLHelper.addContent(childFeatureWithElements, "features", feature);
+			}
+			
 			// get the parents of all feature elements with the current uuid
 			List<Element> descriptionElementElements = XPath.selectNodes(context, featurePattern + "/..");
+			
 			// add matching description elements as children to this feature element
 			for(Element descriptionElementElement : descriptionElementElements){
 				descriptionElementElement.removeChild("feature");
 				descriptionElementElement.setName("descriptionelement");
 				XMLHelper.addContent(descriptionElementElement, "descriptionelements", feature);
 			}
-			XMLHelper.addContent(feature, parentElement);	
-		}else if(featureNode.getChildren().size() > 0){
-			Element featureElement = featureNode.getFeatureElement();
-			Element featureElementClone = (Element) featureElement.clone();
-			feature = (Element) featureElementClone.detach();
-			XMLHelper.addContent(feature, parentElement);
-		}		
-		
-		// recurse into children
-		for(SimplifiedFeatureNode childFeatureNode : featureNode.getChildren()){
-			processFeatureNode(childFeatureNode, context, feature);
 		}
-	}
+		
+		return feature;
+	}*/
 	
 	/**
 	 * Retrieves the synonymy for the given taxonElement 
