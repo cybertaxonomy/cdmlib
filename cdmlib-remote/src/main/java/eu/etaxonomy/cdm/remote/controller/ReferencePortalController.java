7d22a47cae240d92057d5b0d8023c0bf2cf6710f
l.morris <l.morris@localhost>
2013-06-13 14:44:55 +0000

    Added new controller to get toMany relations for authorTeam in nomenclaturalReference

---
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/ReferencePortalController.java
@@ -0,0 +1,34 @@
+// $Id$
+/**
+* Copyright (C) 2009 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+* 
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.remote.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author l.morris
+ * @date Jun 13, 2013
+ *
+ */
+@Controller
+@RequestMapping(value = {"/portal/reference/{uuid}"})
+public class ReferencePortalController extends ReferenceController {
+
+    public ReferencePortalController(){
+        setInitializationStrategy(Arrays.asList(new String[]{
+                "$",
+                "authorTeam.*"
+             }));
+    }
+
+}
