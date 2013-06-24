1d3f5c876b7b9557da5406c1cefeaf06274dbe59
p.kelbert <p.kelbert@localhost>
2013-06-24 07:15:17 +0000

    debug ABCD import new boolean names in configurator new class for User Interaction

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/specimen/excel/in/SpecimenSynthesysExcelImportConfigurator.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/specimen/excel/in/SpecimenSynthesysExcelImportConfigurator.java
@@ -11,7 +11,9 @@ package eu.etaxonomy.cdm.io.specimen.excel.in;
 
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.apache.log4j.Logger;
 
@@ -39,6 +41,9 @@ public class SpecimenSynthesysExcelImportConfigurator extends ImportConfigurator
     private Map<String, Team> titleCacheTeam;
     private Map<String, Person> titleCachePerson;
     private String defaultAuthor="";
+    
+    private Map<String,UUID> namedAreaDecisions = new HashMap<String,UUID>();
+    private Reference<?> dataReference;
 
 
 	//TODO
@@ -188,6 +193,35 @@ public class SpecimenSynthesysExcelImportConfigurator extends ImportConfigurator
 
     public String getDefaultAuthor(){
      return defaultAuthor;
+    }
+
+    
+    public Map<String,UUID> getNamedAreaDecisions() {
+        return namedAreaDecisions;
+    }
+
+    public void setNamedAreaDecisions(Map<String,UUID> namedAreaDecisions) {
+        this.namedAreaDecisions = namedAreaDecisions;
+    }
+
+    public void putNamedAreaDecision(String areaStr,UUID uuid){
+        this.namedAreaDecisions.put(areaStr,uuid);
+    }
+
+    public UUID getNamedAreaDecision(String areaStr){
+        return namedAreaDecisions.get(areaStr);
+    }
+
+    /**
+     * @param ref
+     */
+    public void setDataReference(Reference<?> ref) {
+        this.dataReference=ref;
+
+    }
+
+    public Reference<?> getDataReference() {
+        return dataReference;
     }
 
 
