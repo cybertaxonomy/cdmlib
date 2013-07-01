246110b64ada659959176d3ba75c89651e72fcd3
Alexander Oppermann <a.oppermann@bgbm.org>
2013-07-01 09:28:10 +0000

    rename package

---
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/excel/stream/ExcelStreamImportState.java
@@ -0,0 +1,34 @@
+// $Id$
+/**
+* Copyright (C) 2007 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+* 
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+
+package eu.etaxonomy.cdm.io.excel.stream;
+
+import org.apache.log4j.Logger;
+
+import eu.etaxonomy.cdm.io.dwca.in.DwcaDataImportStateBase;
+import eu.etaxonomy.cdm.io.dwca.in.DwcaImportState;
+
+/**
+ * 
+ * @author a.oppermann
+ * @date 08.05.2013
+ *
+ *
+ *<ROW extends ExcelRowBase>
+ */
+public class ExcelStreamImportState extends DwcaDataImportStateBase<ExcelStreamImportConfigurator>{
+	@SuppressWarnings("unused")
+	private static final Logger logger = Logger.getLogger(DwcaImportState.class);
+
+	public ExcelStreamImportState(ExcelStreamImportConfigurator config) {
+		super(config);
+	}
+	
+}
