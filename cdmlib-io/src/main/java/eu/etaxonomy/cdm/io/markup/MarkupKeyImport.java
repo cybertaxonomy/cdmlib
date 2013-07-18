8b8d0e2f03c8884bb3a768075cbac9adc682aea4
Andreas Müller <a.mueller@bgbm.org>
2013-07-18 12:47:36 +0000

    merging trunk to pesiimport3

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/markup/MarkupKeyImport.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/markup/MarkupKeyImport.java
@@ -128,6 +128,12 @@ public class MarkupKeyImport  extends MarkupImportBase  {
 			if (isMyEndingElement(next, parentEvent)) {
 				completeCouplet(state, parentEvent, parentNode, num, childList);
 				return;
+			} else if (next.isCharacters()){
+				handleNotYetImplementedElement(next);
+				String mainQuestion = next.asCharacters().getData();
+				mainQuestion = mainQuestion.replaceAll("\\s+", " ").trim();
+				KeyStatement question = KeyStatement.NewInstance(mainQuestion);
+				if (parentNode != null){ parentNode.setStatement(question);}  //work in progress
 			} else if (isStartingElement(next, QUESTION)) {
 				handleQuestion(state, reader, next, childList);
 			} else if (isStartingElement(next, KEYNOTES)) {
@@ -256,9 +262,8 @@ public class MarkupKeyImport  extends MarkupImportBase  {
 		}
 		return;
 	}
-	
-	
-	/**
+
+		/**
 	 * Returns the taxon text of the toTaxon element and handles all annotations as ';'-concatenated modifying text.
 	 * Footnote refs are not yet handled.
 	 * @param state
