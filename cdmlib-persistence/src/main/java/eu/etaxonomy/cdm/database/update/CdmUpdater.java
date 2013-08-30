dbf432964050c41d377265333b11210398d9978b
Andreas Müller <a.mueller@bgbm.org>
2013-08-30 15:47:48 +0000

    fix update hibernate_sequences

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/CdmUpdater.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/CdmUpdater.java
@@ -9,14 +9,16 @@
 */
 package eu.etaxonomy.cdm.database.update;
 
+import java.sql.ResultSet;
+
 import org.apache.log4j.Logger;
 
 import eu.etaxonomy.cdm.common.monitor.DefaultProgressMonitor;
 import eu.etaxonomy.cdm.common.monitor.IProgressMonitor;
 import eu.etaxonomy.cdm.database.CdmDataSource;
 import eu.etaxonomy.cdm.database.ICdmDataSource;
-import eu.etaxonomy.cdm.database.update.v30_31.TermUpdater_314_315;
 import eu.etaxonomy.cdm.database.update.v31_33.SchemaUpdater_31_33;
+import eu.etaxonomy.cdm.database.update.v31_33.TermUpdater_31_33;
 
 /**
  * @author a.mueller
@@ -47,6 +49,7 @@ public class CdmUpdater {
 		
 		int steps = currentSchemaUpdater.countSteps(datasource, monitor);
 		steps += currentTermUpdater.countSteps(datasource, monitor);
+		steps++;  //for hibernate_sequences update
 		
 		String taskName = "Update to schema version " + currentSchemaUpdater.getTargetVersion() + " and to term version " + currentTermUpdater.getTargetVersion(); //+ currentSchemaUpdater.getVersion();
 		monitor.beginTask(taskName, steps);
@@ -56,6 +59,7 @@ public class CdmUpdater {
 			result &= currentSchemaUpdater.invoke(datasource, monitor);
 			if (result == true){
 				result &= currentTermUpdater.invoke(datasource, monitor);
+				updateHibernateSequence(datasource, monitor);
 			}
 			if (result == false){
 				datasource.rollback();
@@ -81,8 +85,72 @@ public class CdmUpdater {
 		return result;
 	}
 	
+
+	/**
+	 * Updating terms often inserts new terms, vocabularies and representations.
+	 * Therefore the counter in hibernate_sequences must be increased.
+	 * We do this once at the end of term updating.
+	 * @return true if update was successful, false otherwise
+	 */
+	private boolean updateHibernateSequence(ICdmDataSource datasource, IProgressMonitor monitor) {
+		boolean result = true;
+		monitor.subTask("Update hibernate sequences");
+		try {
+			String sql = "SELECT * FROM hibernate_sequences ";
+			ResultSet rs = datasource.executeQuery(sql);
+			while (rs.next()){
+				String table = rs.getString("sequence_name");
+				Integer val = rs.getInt("next_val");
+				result &= updateSingleValue(datasource,monitor, table, val);
+			}
+		} catch (Exception e) {
+			String message = "Exception occurred when trying to update hibernate_sequences table: " + e.getMessage();
+			monitor.warning(message, e);
+			logger.error(message);
+			result = false;
+		}finally{
+			monitor.worked(1);
+		}
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @param datasource
+	 * @param monitor
+	 * @param table
+	 * @param oldVal
+	 * @return
+	 */
+	private boolean updateSingleValue(ICdmDataSource datasource, IProgressMonitor monitor, String table, Integer oldVal){
+		try {
+			String sql = " SELECT max(id) FROM %s ";
+			Integer newVal = (Integer)datasource.getSingleValue(String.format(sql, table));
+			
+			//This is how {@link PooledOptimizer#generate(org.hibernate.id.enhanced.AccessCallback)} works
+			//it substracts the increment size from the value in hibernate_sequences to get the initial value.
+			//Haven't checked why.
+			int incrementSize = 10;
+			newVal = newVal + incrementSize;
+			if (newVal != null && newVal >= oldVal){
+				sql = " UPDATE hibernate_sequences " +
+						" SET next_val = %d " +
+						" WHERE sequence_name = '%s' ";
+				datasource.executeUpdate(String.format(sql, newVal + 1 , table) );
+			}
+			return true;
+		} catch (Exception e) {
+			String message = "Exception occurred when trying to read or update hibernate_sequences table for value " + table + ": " + e.getMessage();
+			monitor.warning(message, e);
+			logger.error(message);
+			return false;
+		}
+		
+	}
+	
+	
 	private ITermUpdater getCurrentTermUpdater() {
-		return TermUpdater_314_315.NewInstance();
+		return TermUpdater_31_33.NewInstance();
 	}
 
 	/**
