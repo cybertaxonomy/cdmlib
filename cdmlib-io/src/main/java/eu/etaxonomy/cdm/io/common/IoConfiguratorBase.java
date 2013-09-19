6462dfe0e783443e1b1a8e1fc45d496dcea7d994
Andreas Müller <a.mueller@bgbm.org>
2013-09-19 11:32:58 +0000

    change cdmApp type in ioConfig to interface  #3726

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/common/IoConfiguratorBase.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/common/IoConfiguratorBase.java
@@ -12,7 +12,7 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
-import eu.etaxonomy.cdm.api.application.CdmApplicationController;
+import eu.etaxonomy.cdm.api.application.ICdmApplicationConfiguration;
 import eu.etaxonomy.cdm.common.monitor.IProgressMonitor;
 import eu.etaxonomy.cdm.common.monitor.NullProgressMonitor;
 import eu.etaxonomy.cdm.config.Configuration;
@@ -34,7 +34,7 @@ public abstract class IoConfiguratorBase extends ObservableBase implements IIoCo
 //	protected Class<ICdmIO>[] ioClassList;
 	private DbSchemaValidation dbSchemaValidation = DbSchemaValidation.VALIDATE;
 	
-	protected CdmApplicationController cdmApp = null;
+	protected ICdmApplicationConfiguration cdmApp = null;
 	
 	//authentification token
 	UsernamePasswordAuthenticationToken authenticationToken;
@@ -61,14 +61,14 @@ public abstract class IoConfiguratorBase extends ObservableBase implements IIoCo
 		this.dbSchemaValidation = dbSchemaValidation;
 	}
 	
-	public CdmApplicationController getCdmAppController(){
+	public ICdmApplicationConfiguration getCdmAppController(){
 		return this.cdmApp;
 	}
 
 	/**
 	 * @param cdmApp the cdmApp to set
 	 */
-	public void setCdmAppController(CdmApplicationController cdmApp) {
+	public void setCdmAppController(ICdmApplicationConfiguration cdmApp) {
 		this.cdmApp = cdmApp;
 	}
 		
