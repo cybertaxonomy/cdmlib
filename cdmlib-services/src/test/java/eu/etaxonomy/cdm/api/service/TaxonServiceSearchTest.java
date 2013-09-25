e63753b4b010062e0b65c046417e13bf24d6eb7f
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2013-09-25 11:15:36 +0000

    cleaning up

--- cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/TaxonServiceSearchTest.java
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/TaxonServiceSearchTest.java
@@ -885,7 +885,7 @@ public class TaxonServiceSearchTest extends CdmTransactionalIntegrationTest {
 
         Pager<SearchResult<TaxonBase>> pager;
 
-        /*
+
         pager = taxonService.findTaxaAndNamesByFullText(
                 EnumSet.of(TaxaAndNamesSearchMode.doTaxa, TaxaAndNamesSearchMode.doSynonyms),
                 "Abies", null, null, null, null, true, null, null, null, null);
@@ -897,12 +897,10 @@ public class TaxonServiceSearchTest extends CdmTransactionalIntegrationTest {
                 "Abies", null, null, null, null, true, null, null, null, null);
         Assert.assertEquals("Expecting 1 entity", Integer.valueOf(1), pager.getCount());
 
-        **/
 
         pager = taxonService.findTaxaAndNamesByFullText(
                 EnumSet.of(TaxaAndNamesSearchMode.doTaxaByCommonNames),
                 "Abies", null, null, null, null, true, null, null, null, null);
-// FIXME
         Assert.assertEquals("Expecting 0 entity", Integer.valueOf(0), pager.getCount());
 
 
