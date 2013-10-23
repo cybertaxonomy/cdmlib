091ab1939bcf4ac609d173ce92624cb0aab40ad8
Patric Plitzner <p.plitzner@bgbm.org>
2013-10-23 09:00:59 +0000

    - DefinedTerm is ordered as in enum (#3752)

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/common/DefinedTerm.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/common/DefinedTerm.java
@@ -9,7 +9,6 @@
 package eu.etaxonomy.cdm.model.common;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -19,11 +18,8 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-import org.codehaus.plexus.util.StringUtils;
 import org.hibernate.envers.Audited;
 import org.hibernate.search.annotations.Indexed;
-
-import eu.etaxonomy.cdm.model.location.NamedArea;
 
 
 /**
@@ -49,39 +45,39 @@ public class DefinedTerm extends DefinedTermBase<DefinedTerm> {
 	private static final UUID uuidFemale = UUID.fromString("b4cfe0cb-b35c-4f97-9b6b-2b3c096ea2c0");
 	private static final UUID uuidHermaphrodite = UUID.fromString("0deddc65-2505-4c77-91a7-17d0de24afcc");
 	private static final UUID uuidUnknown = UUID.fromString("4f5e4c51-a664-48ad-8238-2e9f49eaf8dd");
-	
+
 	//Marker
 	private static final UUID uuidIts1 = UUID.fromString("3f2de4f6-d1a2-4c3a-be70-3c997ef92543");
-	
-	protected static Map<UUID, DefinedTerm> termMap = null;		
-	
-	
+
+	protected static Map<UUID, DefinedTerm> termMap = null;
+
+
 	protected static DefinedTerm getTermByUuid(UUID uuid){
 		if (termMap == null){
 			return null;
 		}else{
-			return (DefinedTerm)termMap.get(uuid);
+			return termMap.get(uuid);
 		}
 	}
 
-	
+
 	public static DefinedTerm NewInstance(TermType termType, String description, String label, String labelAbbrev){
 		return new DefinedTerm(termType, description, label, labelAbbrev);
 	}
 
-	
+
 	public static DefinedTerm NewModifierInstance(String description, String label, String labelAbbrev){
 		return new DefinedTerm(TermType.Modifier, description, label, labelAbbrev);
 	}
-	
+
 	public static DefinedTerm NewStageInstance(String description, String label, String labelAbbrev){
 		return new DefinedTerm(TermType.Stage, description, label, labelAbbrev);
 	}
-	
+
 	public static DefinedTerm NewSexInstance(String description, String label, String labelAbbrev){
 		return new DefinedTerm(TermType.Sex, description, label, labelAbbrev);
 	}
-	
+
 	public static DefinedTerm NewScopeInstance(String description, String label, String labelAbbrev){
 		return new DefinedTerm(TermType.Scope, description, label, labelAbbrev);
 	}
@@ -89,23 +85,23 @@ public class DefinedTerm extends DefinedTermBase<DefinedTerm> {
 	public static DefinedTerm NewDnaMarkerInstance(String description, String label, String labelAbbrev){
 		return new DefinedTerm(TermType.DnaMarker, description, label, labelAbbrev);
 	}
-	
-		
+
+
 //******************* CONSTRUCTOR ***********************************/
 
 	//for hibernate/javassist use only
 	@Deprecated
 	protected DefinedTerm(){
 		super(TermType.Unknown);
-	};
-	
+	}
+
 	public DefinedTerm(TermType type, String description, String label, String labelAbbrev) {
 		super(type, description, label, labelAbbrev);
 	}
 
 //*************************** TERM MAP *********************/
-	
-	
+
+
 	public static final DefinedTerm DETERMINATION_MODIFIER_AFFINIS(){
 		return getTermByUuid(uuidAffinis);
 	}
@@ -113,7 +109,7 @@ public class DefinedTerm extends DefinedTermBase<DefinedTerm> {
 	public static final DefinedTerm DETERMINATION_MODIFIER_CONFER(){
 		return getTermByUuid(uuidConfer);
 	}
-	
+
 	public static DefinedTerm SEX_MALE(){
 		return getTermByUuid(uuidMale);
 	}
@@ -121,11 +117,11 @@ public class DefinedTerm extends DefinedTermBase<DefinedTerm> {
 	public static DefinedTerm SEX_FEMALE(){
 		return getTermByUuid(uuidFemale);
 	}
-	
+
 	public static DefinedTerm SEX_HERMAPHRODITE(){
 		return getTermByUuid(uuidHermaphrodite);
 	}
-	
+
 	public static DefinedTerm SEX_UNKNOWN(){
 		return getTermByUuid(uuidUnknown);
 	}
@@ -133,8 +129,8 @@ public class DefinedTerm extends DefinedTermBase<DefinedTerm> {
 	public static DefinedTerm ITS1_MARKER(){
 		return getTermByUuid(uuidIts1);
 	}
-	
-	
+
+
 	@Override
 	public void resetTerms() {
 		termMap = null;
@@ -146,8 +142,8 @@ public class DefinedTerm extends DefinedTermBase<DefinedTerm> {
 			termMap = new HashMap<UUID, DefinedTerm>();
 		}
 		for (DefinedTerm term : termVocabulary.getTerms()){
-			termMap.put(term.getUuid(), (DefinedTerm)term);  //TODO casting
-		}		
+			termMap.put(term.getUuid(), term);  //TODO casting
+		}
 	}
 
 	@Override
@@ -165,7 +161,7 @@ public class DefinedTerm extends DefinedTermBase<DefinedTerm> {
 //            DefinedTerm partOf = (DefinedTerm)terms.get(partOfUuid);
 //            partOf.addIncludes(newInstance);
 //        }
-//		
+//
 //	}
 
 }
