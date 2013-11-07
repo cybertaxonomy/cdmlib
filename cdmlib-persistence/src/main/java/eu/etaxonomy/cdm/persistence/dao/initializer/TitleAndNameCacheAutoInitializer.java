c726bdfa2dbb379fcceca1391146244193fd55f7
Andreas Müller <a.mueller@bgbm.org>
2013-11-07 01:19:57 +0000

    handle proxy handling in titleAndNameCacheAutoInitializer

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/initializer/TitleAndNameCacheAutoInitializer.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/initializer/TitleAndNameCacheAutoInitializer.java
@@ -9,7 +9,7 @@
 */
 package eu.etaxonomy.cdm.persistence.dao.initializer;
 
-import eu.etaxonomy.cdm.model.common.CdmBase;
+import eu.etaxonomy.cdm.hibernate.HibernateProxyHelper;
 import eu.etaxonomy.cdm.model.common.IdentifiableEntity;
 import eu.etaxonomy.cdm.model.name.NonViralName;
 import eu.etaxonomy.cdm.model.name.TaxonNameBase;
@@ -37,7 +37,7 @@ public class TitleAndNameCacheAutoInitializer extends AutoPropertyInitializer<Id
     @Override
     public void initialize(IdentifiableEntity<?> bean) {
 
-
+    	bean = HibernateProxyHelper.deproxy(bean, IdentifiableEntity.class);
         // we will implement a bit of redundancy here in order
         // to avoid too much casting
         if(bean instanceof NonViralName){
