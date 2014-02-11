a3bbc2ed00eedad68ee81680925d179b234fd0c7
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2014-02-11 16:17:40 +0000

    A utils class which helps finding security related exceptions in Throwables

--- cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/test/integration/CdmTransactionalIntegrationTestWithSecurity.java
+++ cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/test/integration/CdmTransactionalIntegrationTestWithSecurity.java
@@ -1,13 +1,11 @@
 package eu.etaxonomy.cdm.test.integration;
 
 import org.apache.log4j.Logger;
-import org.junit.After;
-import org.junit.Before;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.unitils.spring.annotation.SpringApplicationContext;
 
 import eu.etaxonomy.cdm.database.PermissionDeniedException;
+import eu.etaxonomy.cdm.persistence.hibernate.permission.SecurityExceptionUtils;
 
 @SpringApplicationContext("file:./target/test-classes/eu/etaxonomy/cdm/applicationContext-securityTest.xml")
 public abstract class CdmTransactionalIntegrationTestWithSecurity extends  CdmTransactionalIntegrationTest {
@@ -22,13 +20,7 @@ public abstract class CdmTransactionalIntegrationTestWithSecurity extends  CdmTr
      */
     public static RuntimeException findSecurityRuntimeException(Throwable exception) {
 
-        if( PermissionDeniedException.class.isInstance(exception) || AccessDeniedException.class.isInstance(exception) ){
-            return (RuntimeException) exception;
-        } else if(exception != null ){
-            return findSecurityRuntimeException(exception.getCause());
-        }
-        return null;
-
+        return SecurityExceptionUtils.findSecurityRuntimeException(exception);
     }
 
 
@@ -40,13 +32,8 @@ public abstract class CdmTransactionalIntegrationTestWithSecurity extends  CdmTr
      * @param exception the nested <code>Throwable</code> to search in.
      * @return
      */
-    public <T extends Throwable> T  findThrowableOfTypeIn(Class<T> clazz, Throwable exception) {
-        if( PermissionDeniedException.class.isInstance(exception) ){
-            return (T)exception;
-        } else if(exception != null ){
-            return findThrowableOfTypeIn(clazz, exception.getCause());
-        }
-        return null;
+    public static <T extends Throwable> T  findThrowableOfTypeIn(Class<T> clazz, Throwable exception) {
+        return SecurityExceptionUtils.findThrowableOfTypeIn(clazz, exception);
     }
 
 }
