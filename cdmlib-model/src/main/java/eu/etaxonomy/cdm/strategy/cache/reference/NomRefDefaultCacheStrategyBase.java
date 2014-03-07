04cfb8002c41d6b1d6c0aec6e97b4068af0a69fa
Andreas Müller <a.mueller@bgbm.org>
2014-03-07 11:32:51 +0000

    Better handling for short citation of references

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/cache/reference/NomRefDefaultCacheStrategyBase.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/cache/reference/NomRefDefaultCacheStrategyBase.java
@@ -21,7 +21,6 @@ import eu.etaxonomy.cdm.strategy.StrategyBase;
 /**
  * @author a.mueller
  * @created 29.06.2008
- * @version 1.0
  */
 public abstract class NomRefDefaultCacheStrategyBase<T extends Reference> extends StrategyBase implements  INomenclaturalReferenceCacheStrategy<T>{
 	private static final long serialVersionUID = -725290113353165022L;
@@ -77,26 +76,28 @@ public abstract class NomRefDefaultCacheStrategyBase<T extends Reference> extend
 	protected abstract String getTitleWithoutYearAndAuthor(T reference, boolean isAbbrev);
 
 	
+	@Override
 	public String getCitation(T referenceBase) {
 		StringBuilder stringBuilder = new StringBuilder();
 		
+		String nextConcat = "";
+		
 		TeamOrPersonBase<?> team = referenceBase.getAuthorTeam();
 		if (team != null &&  ! (team.getTitleCache() == null) && ! team.getTitleCache().trim().equals("")){
-			//String author = CdmUtils.Nz(team == null? "" : team.getTitleCache());
-			stringBuilder.append(team.getTitleCache() + afterAuthor);
+			stringBuilder.append(team.getTitleCache() );
+			nextConcat = afterAuthor;
 		}
 		
-		String year = CdmUtils.Nz(referenceBase.getYear());
-		if (!"".equals(year)){
-			stringBuilder.append(beforeYear + year);
+		String year = referenceBase.getYear();
+		if (StringUtils.isNotBlank(year)){
+			stringBuilder.append(nextConcat + year);
 		}
 		
 		return stringBuilder.toString();
 	}
 	
-	/* (non-Javadoc)
-	 * @see eu.etaxonomy.cdm.strategy.cache.reference.INomenclaturalReferenceCacheStrategy#getBeforeMicroReference()
-	 */
+
+	@Override
 	public String getBeforeMicroReference(){
 		return beforeMicroReference;
 	}
@@ -116,6 +117,7 @@ public abstract class NomRefDefaultCacheStrategyBase<T extends Reference> extend
 		return result;
 	}
 	
+	@Override
 	public String getNomenclaturalCitation(T nomenclaturalReference, String microReference) {
 		if (nomenclaturalReference.isProtectedTitleCache()){
 			return nomenclaturalReference.getTitleCache();
