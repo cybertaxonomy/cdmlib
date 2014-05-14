8e86263f16acfa961d7894e76748cd507cc95a5b
Patric Plitzner <p.plitzner@bgbm.org>
2014-05-14 07:15:41 +0000

    - made move methods session-safe by reloading the cdm entities  - enhanced/added test  - made constructor of PreservationMethod protected to avoid JavaAssistEnhancementException

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/occurrence/PreservationMethod.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/occurrence/PreservationMethod.java
@@ -1,8 +1,8 @@
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -34,10 +34,10 @@ import eu.etaxonomy.cdm.model.molecular.Cloning;
 /**
  * This class is a specialization of {@link MaterialOrMethodEvent} which allows to
  * specifically store temperature and XXX which are common parameters for preparation.
- * 
+ *
  * {@link #getDefinedMaterialOrMethod() Defined methods} taken to describe a Preservation Method
  * should be taken from a vocabulary of type {@link TermType#PreservationMethod}
- * 
+ *
  * http://rs.tdwg.org/ontology/voc/Collection.rdf#SpecimenPreservationMethodTypeTerm
  *
  * @author a.mueller
@@ -64,19 +64,19 @@ public class PreservationMethod extends MaterialOrMethodEvent implements Cloneab
     @ManyToOne(fetch = FetchType.LAZY)
     @IndexedEmbedded // no depth for terms
     private DefinedTerm medium;
-	
+
 	@XmlElement(name = "Temperature")
 	@Field(analyze = Analyze.NO)
 	@NumericField
 	private Double temperature;
-	
-	
-	// ******************** FACTORY METHOD ******************/	
-	
+
+
+	// ******************** FACTORY METHOD ******************/
+
 	public static PreservationMethod NewInstance(){
 		return new PreservationMethod();
 	}
-	
+
     public static PreservationMethod NewInstance(DefinedTerm definedMaterialOrMethod, String methodText){
     	return new PreservationMethod(definedMaterialOrMethod, methodText, null, null);
     }
@@ -85,18 +85,18 @@ public class PreservationMethod extends MaterialOrMethodEvent implements Cloneab
 		return new PreservationMethod(definedMaterialOrMethod, methodText, preservationMedium, temperature);
 	}
 
-	
+
 	// ********************* CONSTRUCTOR ********************/
-		
+
 	//for hibernate use only
-	private PreservationMethod(){};
-	
+	protected PreservationMethod(){};
+
     private PreservationMethod(DefinedTerm definedMaterialOrMethod, String methodText, DefinedTerm medium, Double temperature){
     	super(definedMaterialOrMethod, methodText);
     	this.medium = medium;
     	this.temperature = temperature;
     }
-	
+
 	// ********************* GETTER / SETTER ********************/
 
 	public DefinedTerm getMedium() {
@@ -117,20 +117,20 @@ public class PreservationMethod extends MaterialOrMethodEvent implements Cloneab
 	public void setTemperature(Double temperature) {
 		this.temperature = temperature;
 	}
-	
+
 	// ********************* CLONE ********************/
-	/** 
+	/**
 	 * Clones <i>this</i> {@link Cloning}. This is a shortcut that enables to create
 	 * a new instance that differs only slightly from <i>this</i> cloning by
 	 * modifying only some of the attributes.<BR><BR>
-	 *  
+	 *
 	 * @see EventBase#clone()
 	 * @see java.lang.Object#clone()
 	 */
 	@Override
 	public Object clone()  {
 		PreservationMethod result = (PreservationMethod)super.clone();
-		
+
 		//don't change medium, temperature
 		return result;
 	}
