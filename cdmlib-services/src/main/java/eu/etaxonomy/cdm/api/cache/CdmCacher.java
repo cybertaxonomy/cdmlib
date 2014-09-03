731f8a84c4f6e84639ad20a6d192017ad078c9cc
Cherian Mathew <c.mathew@bgbm.org>
2014-09-03 16:34:49 +0000

    added possiblity to initialise some specific terms (like default language) which could be called outside a session

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/cache/CdmCacher.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/cache/CdmCacher.java
@@ -14,54 +14,62 @@ import eu.etaxonomy.cdm.model.common.DefinedTermBase;
 /**
  * CDM Entity Cacher class based on EhCache.
  * The cacher allows null values to be cached.
- * 
+ *
  * @author cmathew
  *
  * @param <T>
  */
 
 public abstract class CdmCacher<T extends CdmBase> implements ICdmCacher<T> {
-			
 
-	
-	private static final String DEFAULT_CACHE_NAME = "defaultCache";	
-	
+
+
+	private static final String DEFAULT_CACHE_NAME = "defaultCache";
+
 	/**
 	 * Constructor which initialises a singleton {@link net.sf.ehcache.CacheManager}
-	 * 
+	 *
 	 */
 	public CdmCacher() {
 		init();
 	}
-	
+
 	/**
 	 * Initialises an empty singleton {@link net.sf.ehcache.CacheManager} and
-	 * sets itself as the cacher object in specific CDM Entity objects. 
-	 * 
+	 * sets itself as the cacher object in specific CDM Entity objects.
+	 *
 	 */
-	private void init() {		
+	private void init() {
 		// Remove all caches
 		getDefaultCacheManager().removalAll();
 		// Create default cache
 		getDefaultCacheManager().addCache(new Cache(getDefaultCacheConfiguration()));
 		// We start first only with DefinedTermBase
 		DefinedTermBase.setCacher(this);
-		
+
+		initDefaultTerms();
 	}
-	
+
+	/**
+	 * Initialises any default terms if required.
+	 * This method should be implemented in sub-classes if any
+	 * pre-initialisation is necessary.
+	 */
+	protected void initDefaultTerms() {}
+
 	/**
 	 * Returns the singleton default cache manager.
-	 * 
+	 *
 	 * @return
 	 */
 	private static CacheManager getDefaultCacheManager() {
 		// this ensures a singleton cache manager
 		return CacheManager.create();
 	}
-	
+
 	/**
 	 * Returns the default cache configuration.
-	 * 
+	 *
 	 * @return
 	 */
 	private CacheConfiguration getDefaultCacheConfiguration() {
@@ -72,15 +80,15 @@ public abstract class CdmCacher<T extends CdmBase> implements ICdmCacher<T> {
 	    .eternal(false)
 	    // default ttl and tti set to 2 hours
 	    .timeToLiveSeconds(60*60*2)
-	    .timeToIdleSeconds(60*60*2);	   
+	    .timeToIdleSeconds(60*60*2);
 	    // This is 2.6.9 API
-		//.maxEntriesLocalDisk(1000);		
+		//.maxEntriesLocalDisk(1000);
 	    //.persistence(new PersistenceConfiguration().strategy(Strategy.LOCALTEMPSWAP));
 	}
-	
+
 	/**
 	 * Returns the default cache
-	 * 
+	 *
 	 * @return
 	 */
 	private static Cache getDefaultCache() {
@@ -90,40 +98,41 @@ public abstract class CdmCacher<T extends CdmBase> implements ICdmCacher<T> {
 	/**
 	 * Puts the (Key,Value) pair of ({@link java.util.UUID}, {@link eu.etaxonomy.cdm.model.common.CdmBase}),
 	 * in the cache
-	 * 
+	 *
 	 * @param uuid
 	 * @param cdmEntity
 	 */
-	public void put(UUID uuid, T cdmEntity) {
+	public <T extends CdmBase> void put(UUID uuid, T cdmEntity) {
 		getDefaultCache().put(new Element(uuid, cdmEntity));
 	}
-	
+
 	/**
 	 * Gets the cache element corresponding to the given {@link java.util.UUID}
-	 * 
+	 *
 	 * @param uuid
 	 * @return
 	 */
 	private Element getCacheElement(UUID uuid) {
 		return getDefaultCache().get(uuid);
 	}
-		
+
 	/**
 	 * Get CDM Entity for given {@link java.util.UUID} from the cache
-	 * 
+	 *
 	 * @param uuid
 	 * @return
 	 */
 	private T getCdmEntity(UUID uuid) {
-		return  (T)getDefaultCache().get(uuid).getObjectValue();				
+		return  (T)getDefaultCache().get(uuid).getObjectValue();
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see eu.etaxonomy.cdm.remote.cache.ICdmCacher#load(java.util.UUID)
 	 */
-	
-	public T load(UUID uuid) {
-		Element e = getCacheElement(uuid);			
+
+	@Override
+    public T load(UUID uuid) {
+		Element e = getCacheElement(uuid);
 		T cdmEntity;
 		if (e == null) {
 
@@ -136,13 +145,14 @@ public abstract class CdmCacher<T extends CdmBase> implements ICdmCacher<T> {
 		}
 		return cdmEntity;
 	}
-	
+
 
 	/* (non-Javadoc)
 	 * @see eu.etaxonomy.cdm.model.ICdmCacher#getFromCache(java.util.UUID)
 	 */
-	public T getFromCache(UUID uuid) {
-		Element e = getCacheElement(uuid);			
+	@Override
+    public T getFromCache(UUID uuid) {
+		Element e = getCacheElement(uuid);
 		T cdmEntity;
 		if (e == null) {
 			return null;
@@ -150,36 +160,38 @@ public abstract class CdmCacher<T extends CdmBase> implements ICdmCacher<T> {
 		    return(T)e.getObjectValue();
 		}
 	}
-	
+
 
 	/* (non-Javadoc)
 	 * @see eu.etaxonomy.cdm.model.ICdmCacher#exists(java.util.UUID)
 	 */
-	public boolean exists(UUID uuid) {
+	@Override
+    public boolean exists(UUID uuid) {
 		return getCacheElement(uuid) != null;
 	}
-	
+
 
 	/* (non-Javadoc)
 	 * @see eu.etaxonomy.cdm.model.ICdmCacher#existsAndIsNotNull(java.util.UUID)
 	 */
-	public boolean existsAndIsNotNull(UUID uuid) {
-		Element e = getCacheElement(uuid);			
+	@Override
+    public boolean existsAndIsNotNull(UUID uuid) {
+		Element e = getCacheElement(uuid);
 		T cdmEntity;
 		if (e != null) {
-			return (T)e.getObjectValue() != null;		   
-		} 
+			return (T)e.getObjectValue() != null;
+		}
 		return false;
 	}
-	
+
 	/**
 	 * Finds CDM Entity by uuid
-	 * 
+	 *
 	 * @param uuid
 	 * @return
 	 */
 	protected abstract T findByUuid(UUID uuid);
-	
+
 
 
 }
--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/cache/CdmDaoCacher.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/cache/CdmDaoCacher.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import eu.etaxonomy.cdm.model.common.CdmBase;
+import eu.etaxonomy.cdm.model.common.Language;
 import eu.etaxonomy.cdm.persistence.dao.hibernate.common.CdmGenericDaoImpl;
 
 /**
- * CDM Entity Cacher class which uses CDM DAOs to find cdm entities
- * 
+ * CDM Entity Cacher class which uses CDM DAOs to find / load cdm entities
+ *
  * @author cmathew
  *
  * @param <T>
@@ -20,7 +21,19 @@ public class CdmDaoCacher<T extends CdmBase> extends CdmCacher<T> {
 
 	@Autowired
 	CdmGenericDaoImpl genericDao;
-	
+
+	/* (non-Javadoc)
+	 * @see eu.etaxonomy.cdm.api.cache.CdmCacher#initDefaultTerms()
+	 */
+	@Override
+	protected void initDefaultTerms() {
+	    // adding default language to the cdm cacher
+        put(Language.uuidEnglish,Language.ENGLISH());
+    }
+
+	/* (non-Javadoc)
+	 * @see eu.etaxonomy.cdm.api.cache.CdmCacher#findByUuid(java.util.UUID)
+	 */
 	@Override
 	protected T findByUuid(UUID uuid) {
 		return (T)genericDao.findByUuidWithoutFlush(uuid);
