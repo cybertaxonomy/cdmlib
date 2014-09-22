8992bc7fe9cd90341884511a42ae14189f08a74e
Andreas Müller <a.mueller@bgbm.org>
2014-09-22 16:28:12 +0000

    fix Specimen import test

--- cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/specimen/abcd206/in/SpecimenImportConfiguratorTest.java
+++ cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/specimen/abcd206/in/SpecimenImportConfiguratorTest.java
@@ -18,6 +18,7 @@ import java.net.URL;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.unitils.dbunit.annotation.DataSet;
 import org.unitils.spring.annotation.SpringBeanByName;
@@ -26,12 +27,15 @@ import org.unitils.spring.annotation.SpringBeanByType;
 import eu.etaxonomy.cdm.api.service.ICommonService;
 import eu.etaxonomy.cdm.api.service.INameService;
 import eu.etaxonomy.cdm.api.service.IOccurrenceService;
+import eu.etaxonomy.cdm.api.service.IReferenceService;
 import eu.etaxonomy.cdm.api.service.ITaxonNodeService;
 import eu.etaxonomy.cdm.api.service.ITermService;
 import eu.etaxonomy.cdm.io.common.CdmApplicationAwareDefaultImport;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator;
 import eu.etaxonomy.cdm.model.name.TaxonNameBase;
 import eu.etaxonomy.cdm.model.occurrence.DerivedUnit;
+import eu.etaxonomy.cdm.model.occurrence.FieldUnit;
+import eu.etaxonomy.cdm.model.reference.Reference;
 import eu.etaxonomy.cdm.model.taxon.TaxonNode;
 import eu.etaxonomy.cdm.test.integration.CdmTransactionalIntegrationTest;
 import eu.etaxonomy.cdm.test.unitils.CleanSweepInsertLoadStrategy;
@@ -59,9 +63,14 @@ public class SpecimenImportConfiguratorTest extends CdmTransactionalIntegrationT
 
 	@SpringBeanByType
 	ITaxonNodeService taxonNodeService;
+	
+	@SpringBeanByType
+	private IReferenceService referenceService;
+
 
 
 	private IImportConfigurator configurator;
+	private IImportConfigurator configurator2;
 
 	@Before
 	public void setUp() {
@@ -75,6 +84,18 @@ public class SpecimenImportConfiguratorTest extends CdmTransactionalIntegrationT
             Assert.fail();
         }
         assertNotNull("Configurator2 could not be created", configurator);
+        
+        //test2
+        String inputFile2 = "/eu/etaxonomy/cdm/io/specimen/abcd206/in/Campanula_ABCD_import_3_taxa_11_units.xml";
+		URL url2 = this.getClass().getResource(inputFile2);
+		assertNotNull("URL for the test file '" + inputFile2 + "' does not exist", url2);
+		try {
+			configurator2 = Abcd206ImportConfigurator.NewInstance(url2.toURI(), null,false);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+		assertNotNull("Configurator could not be created", configurator2);
 	}
 
 	@Test
@@ -101,4 +122,26 @@ public class SpecimenImportConfiguratorTest extends CdmTransactionalIntegrationT
         assertEquals("Number of TaxonNodes is incorrect", 3, taxonNodeService.count(TaxonNode.class));
         assertEquals("Number of specimen and observation is incorrect", 10, occurrenceService.count(DerivedUnit.class));
     }
+	
+
+	@Test
+	@DataSet(value="SpecimenImportConfiguratorTest.doInvoke2.xml",  loadStrategy=CleanSweepInsertLoadStrategy.class)
+	public void testDoInvoke2() {
+		boolean result = defaultImport.invoke(configurator2);
+		assertTrue("Return value for import.invoke should be true", result);
+		assertEquals("Number of TaxonNames is incorrect", 4, nameService.count(TaxonNameBase.class));
+		/*
+		 * 5 taxon nodes:
+		 *
+         * Classification
+         * - Campanula
+         *   - Campanula patula
+         *   - Campanula tridentata
+         *   - Campanula lactiflora
+         */
+        assertEquals("Number of TaxonNodes is incorrect", 5, taxonNodeService.count(TaxonNode.class));
+		assertEquals("Number of derived units is incorrect", 11, occurrenceService.count(DerivedUnit.class));
+		assertEquals("Number of field units is incorrect", 11, occurrenceService.count(FieldUnit.class));
+		assertEquals("Number of field units is incorrect", 1, referenceService.count(Reference.class));
+	}
 }
