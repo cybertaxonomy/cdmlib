99f0bd2f51c29f753bf114c767cebcd45f598637
Andreas Müller <a.mueller@bgbm.org>
2015-02-06 23:16:13 +0000

    annotations and imports

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/VersionableServiceBase.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/VersionableServiceBase.java
@@ -1,9 +1,9 @@
 // $Id$
 /**
  * Copyright (C) 2007 EDIT
- * European Distributed Institute of Taxonomy 
+ * European Distributed Institute of Taxonomy
  * http://www.e-taxonomy.eu
- * 
+ *
  * The contents of this file are subject to the Mozilla Public License Version 1.1
  * See LICENSE.TXT at the top of this package for the full license terms.
  */
@@ -22,7 +22,6 @@ import eu.etaxonomy.cdm.api.service.config.DeleteConfiguratorBase;
 import eu.etaxonomy.cdm.api.service.exception.ReferencedObjectUndeletableException;
 import eu.etaxonomy.cdm.api.service.pager.Pager;
 import eu.etaxonomy.cdm.api.service.pager.impl.DefaultPagerImpl;
-import eu.etaxonomy.cdm.model.agent.AgentBase;
 import eu.etaxonomy.cdm.model.common.CdmBase;
 import eu.etaxonomy.cdm.model.common.VersionableEntity;
 import eu.etaxonomy.cdm.model.view.AuditEvent;
@@ -33,44 +32,48 @@ import eu.etaxonomy.cdm.persistence.dao.common.IVersionableDao;
 public abstract class VersionableServiceBase<T extends VersionableEntity, DAO extends IVersionableDao<T>> extends ServiceBase<T,DAO> implements IVersionableService<T> {
 	@Autowired
     protected ICommonService commonService;
-	@Transactional(readOnly = true)
+	@Override
+    @Transactional(readOnly = true)
 	public Pager<AuditEventRecord<T>> pageAuditEvents(T t, Integer pageSize,	Integer pageNumber, AuditEventSort sort, List<String> propertyPaths) {
 		Integer numberOfResults = dao.countAuditEvents(t, sort);
-			
+
 		List<AuditEventRecord<T>> results = new ArrayList<AuditEventRecord<T>>();
 		if(numberOfResults > 0) { // no point checking again //TODO use AbstractPagerImpl.hasResultsInRange(numberOfResults, pageNumber, pageSize)
 			results = dao.getAuditEvents(t, pageSize, pageNumber, sort,propertyPaths);
 		}
-		
+
 		return new DefaultPagerImpl<AuditEventRecord<T>>(pageNumber, numberOfResults, pageSize, results);
 	}
 
-	@Transactional(readOnly = true)
+	@Override
+    @Transactional(readOnly = true)
 	public AuditEventRecord<T> getNextAuditEvent(T t) {
 		return dao.getNextAuditEvent(t);
 	}
 
-	@Transactional(readOnly = true)
+	@Override
+    @Transactional(readOnly = true)
 	public AuditEventRecord<T> getPreviousAuditEvent(T t) {
 		return dao.getPreviousAuditEvent(t);
 	}
-	
-	@Transactional(readOnly = true)
+
+	@Override
+    @Transactional(readOnly = true)
 	public Pager<AuditEventRecord<T>> pageAuditEvents(Class<? extends T> clazz,AuditEvent from,AuditEvent to, List<AuditCriterion> criteria, Integer pageSize, Integer pageNumber, AuditEventSort sort,List<String> propertyPaths) {
 		Integer numberOfResults = dao.countAuditEvents(clazz, from, to, criteria);
-		
+
 		List<AuditEventRecord<T>> results = new ArrayList<AuditEventRecord<T>>();
 		if(numberOfResults > 0) { // no point checking again //TODO use AbstractPagerImpl.hasResultsInRange(numberOfResults, pageNumber, pageSize)
 			results = dao.getAuditEvents(clazz,from,to,criteria, pageSize, pageNumber, sort,propertyPaths);
 		}
-		
+
 		return new DefaultPagerImpl<AuditEventRecord<T>>(pageNumber, numberOfResults, pageSize, results);
 	}
-	
+
 	 /**
      * the basic isDeletable method return false if the object is referenced from any other object.
      */
-    
+
     @Override
     public DeleteResult isDeletable(T base, DeleteConfiguratorBase config){
     	DeleteResult result = new DeleteResult();
@@ -88,6 +91,4 @@ public abstract class VersionableServiceBase<T extends VersionableEntity, DAO ex
     	}
     	return result;
     }
-    
-    
-    }
+}
