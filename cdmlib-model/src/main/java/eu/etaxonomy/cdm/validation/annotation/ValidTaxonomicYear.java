3539fb8f629ed65fec1a7f93211cf661b743cf9d
Andreas Müller <a.mueller@bgbm.org>
2015-02-18 17:26:15 +0000

    add valid taxonomic year validation  #4125

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/validation/annotation/ValidTaxonomicYear.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/validation/annotation/ValidTaxonomicYear.java
@@ -7,12 +7,9 @@
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
 
-
 package eu.etaxonomy.cdm.validation.annotation;
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.FIELD;
+
 import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -22,17 +19,17 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-import eu.etaxonomy.cdm.validation.constraint.NullOrNotEmptyValidator;
+import eu.etaxonomy.cdm.validation.constraint.ValidTaxonomicYearValidator;
 
-@Target( { TYPE, METHOD, FIELD, ANNOTATION_TYPE })
+@Target( { METHOD })
 @Retention(RUNTIME)
-@Constraint(validatedBy = NullOrNotEmptyValidator.class)
+@Constraint(validatedBy = ValidTaxonomicYearValidator.class)
 @Documented
-public @interface NullOrNotEmpty {
+public @interface ValidTaxonomicYear {
 
-	String message() default "{eu.etaxonomy.cdm.validation.annotation.NullOrNotEmpty.message}";
+	String message() default "{eu.etaxonomy.cdm.validation.annotation.ValidTaxonomicYear.message}";
 
 	Class<? extends Payload>[] payload() default {};
 
-	Class<?>[] groups() default {};
+	Class<?>[] groups() default {};  //Level3
 }
---
+++ cdmlib-model/src/test/java/eu/etaxonomy/cdm/validation/ValidTaxonomicYearTest.java
@@ -0,0 +1,125 @@
+/**
+* Copyright (C) 2009 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+
+package eu.etaxonomy.cdm.validation;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+
+import eu.etaxonomy.cdm.model.common.DefaultTermInitializer;
+import eu.etaxonomy.cdm.model.name.BotanicalName;
+import eu.etaxonomy.cdm.model.name.Rank;
+import eu.etaxonomy.cdm.model.name.ZoologicalName;
+import eu.etaxonomy.cdm.model.reference.Reference;
+import eu.etaxonomy.cdm.model.reference.ReferenceFactory;
+import eu.etaxonomy.cdm.strategy.parser.TimePeriodParser;
+import eu.etaxonomy.cdm.validation.constraint.ValidTaxonomicYearValidator;
+
+
+
+/**
+ * NOTE: In this test, the words "valid" and "invalid", loaded though
+ * these terms are when applied to taxonomic names, only mean "passes the
+ * rules of this validator" or not and should not be confused with the strict
+ * nomenclatural and taxonomic sense of these words.
+ *
+ * @author ben.clark
+ *
+ *
+ */
+public class ValidTaxonomicYearTest extends ValidationTestBase {
+	@SuppressWarnings("unused")
+    private static final Logger logger = Logger.getLogger(ValidTaxonomicYearTest.class);
+
+
+	private Validator validator;
+
+	private BotanicalName name;
+
+	private Reference<?> beforeLineRef;
+    private Reference<?> afterLineRef;
+
+
+	@Before
+	public void setUp() {
+		DefaultTermInitializer vocabularyStore = new DefaultTermInitializer();
+		vocabularyStore.initialize();
+		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+		validator = validatorFactory.getValidator();
+		name = BotanicalName.NewInstance(Rank.SPECIES());
+		name.setNameCache("Aus aus");
+		name.setAuthorshipCache("L.");
+		name.setFullTitleCache("Aus aus L.");
+		name.setTitleCache("Aus aus L.", true);
+
+		beforeLineRef = ReferenceFactory.newBook();
+		beforeLineRef.setDatePublished(TimePeriodParser.parseString("1752"));
+
+	    afterLineRef = ReferenceFactory.newBook();
+	    afterLineRef.setDatePublished(TimePeriodParser.parseString("1754"));
+
+	}
+
+
+/****************** TESTS *****************************/
+
+	@Test
+	public void testNotBeforeLine() {
+        name.setNomenclaturalReference(beforeLineRef);
+	    Set<ConstraintViolation<BotanicalName>> constraintViolations  = validator.validate(name, Level3.class);
+        assertHasConstraintOnValidator((Set)constraintViolations, ValidTaxonomicYearValidator.class);
+	}
+
+	@Test
+    public void testAfterLine() {
+        name.setNomenclaturalReference(afterLineRef);
+        Set<ConstraintViolation<BotanicalName>> constraintViolations  = validator.validate(name, Level3.class);
+        assertNoConstraintOnValidator((Set)constraintViolations, ValidTaxonomicYearValidator.class);
+    }
+
+    @Test
+    public void testNoNomRef() {
+        name.setNomenclaturalReference(null);
+        Set<ConstraintViolation<BotanicalName>> constraintViolations  = validator.validate(name, Level3.class);
+        assertNoConstraintOnValidator((Set)constraintViolations, ValidTaxonomicYearValidator.class);
+    }
+
+    @Test
+    public void testZooName() {
+
+        ZoologicalName zooName = ZoologicalName.NewInstance(Rank.SPECIES());
+
+        zooName.setNomenclaturalReference(null);
+        zooName.setPublicationYear(1788);
+        Set<ConstraintViolation<ZoologicalName>> constraintViolations  = validator.validate(zooName, Level3.class);
+        assertNoConstraintOnValidator((Set)constraintViolations, ValidTaxonomicYearValidator.class);
+
+        //currently we do not use the publication year for zooName.getYear, but this may change in future.
+        //we may adapt this test then
+        zooName.setPublicationYear(1730);
+        constraintViolations  = validator.validate(zooName, Level3.class);
+        assertNoConstraintOnValidator((Set)constraintViolations, ValidTaxonomicYearValidator.class);
+
+        zooName.setNomenclaturalReference(beforeLineRef);
+        constraintViolations  = validator.validate(zooName, Level3.class);
+        assertHasConstraintOnValidator((Set)constraintViolations, ValidTaxonomicYearValidator.class);
+
+
+    }
+
+
+}
