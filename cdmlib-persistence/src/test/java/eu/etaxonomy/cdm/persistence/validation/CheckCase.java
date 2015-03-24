1be86351d8802e3e91e9131bc27c033fde4aa3f4
Andreas Müller <a.mueller@bgbm.org>
2015-03-24 10:57:12 +0000

    merge validation commits r23146, r23162, r23163 into trunk

--- cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/validation/CheckCase.java
+++ cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/validation/CheckCase.java
@@ -1,11 +1,11 @@
 /**
-* Copyright (C) 2009 EDIT
-* European Distributed Institute of Taxonomy
-* http://www.e-taxonomy.eu
-*
-* The contents of this file are subject to the Mozilla Public License Version 1.1
-* See LICENSE.TXT at the top of this package for the full license terms.
-*/
+ * Copyright (C) 2009 EDIT
+ * European Distributed Institute of Taxonomy
+ * http://www.e-taxonomy.eu
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * See LICENSE.TXT at the top of this package for the full license terms.
+ */
 package eu.etaxonomy.cdm.persistence.validation;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
@@ -20,19 +20,25 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
+/**
+ * A Mock class for testing entity validation tasks. DO NOT MODIFY UNLESS YOU
+ * ALSO MODIFY THE UNIT TESTS MAKING USE OF THIS CLASS!
+ *
+ * @author ayco_holleman
+ *
+ */
 @Target({ METHOD, FIELD, ANNOTATION_TYPE })
 @Retention(RUNTIME)
 @Constraint(validatedBy = CheckCaseValidator.class)
 @Documented
 public @interface CheckCase {
-	String message() default "Casing is wrong";
+    // Do not modify message. Unit tests depend on it
+    String message() default "Casing is wrong";
 
+    Class<?>[] groups() default {};
 
-	Class<?>[] groups() default {};
+    // Do not modify severity. Unit tests depend on it
+    Class<? extends Payload>[] payload() default {};
 
-
-	Class<? extends Payload>[] payload() default {};
-
-
-	CaseMode value();
+    CaseMode value();
 }
--- cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/validation/Employee.java
+++ cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/validation/Employee.java
@@ -1,11 +1,11 @@
 /**
-* Copyright (C) 2009 EDIT
-* European Distributed Institute of Taxonomy
-* http://www.e-taxonomy.eu
-*
-* The contents of this file are subject to the Mozilla Public License Version 1.1
-* See LICENSE.TXT at the top of this package for the full license terms.
-*/
+ * Copyright (C) 2009 EDIT
+ * European Distributed Institute of Taxonomy
+ * http://www.e-taxonomy.eu
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * See LICENSE.TXT at the top of this package for the full license terms.
+ */
 package eu.etaxonomy.cdm.persistence.validation;
 
 import java.util.List;
@@ -17,80 +17,77 @@ import eu.etaxonomy.cdm.validation.Level2;
 import eu.etaxonomy.cdm.validation.Level3;
 
 /**
- * A Mock class for testing entity validation tasks. DO NOT MODIFY UNLESS YOU ALSO MODIFY
- * THE UNIT TESTS MAKING USE OF THIS CLASS!
- * 
+ * A Mock class for testing entity validation tasks. DO NOT MODIFY UNLESS YOU
+ * ALSO MODIFY THE UNIT TESTS MAKING USE OF THIS CLASS!
+ *
  * @author ayco_holleman
- * 
+ *
  */
 @SuppressWarnings("serial")
 public class Employee extends CdmBase {
 
-	@CheckCase(value = CaseMode.UPPER, groups = { Level2.class })
-	private String firstName;
-	@CheckCase(value = CaseMode.UPPER, groups = { Level3.class })
-	private String lastName;
-	@Valid
-	private Company company;
-	@Valid
-	private List<Address> addresses;
+    @CheckCase(value = CaseMode.UPPER, groups = { Level2.class })
+    private String firstName;
+    @CheckCase(value = CaseMode.UPPER, groups = { Level3.class })
+    private String lastName;
+    @Valid
+    private Company company;
+    @Valid
+    private List<Address> addresses;
 
+    public Employee() {
+    }
 
-	public Employee(){
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getFirstName(){
-		return firstName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setFirstName(String firstName){
-		this.firstName = firstName;
-	}
+    public Company getCompany() {
+        return company;
+    }
 
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 
-	public String getLastName(){
-		return lastName;
-	}
-	public void setLastName(String lastName){
-		this.lastName = lastName;
-	}
+    public List<Address> getAddresses() {
+        return addresses;
+    }
 
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
 
-	public Company getCompany(){
-		return company;
-	}
-	public void setCompany(Company company){
-		this.company = company;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        Employee emp = (Employee) obj;
+        return firstName.equals(emp.firstName) && lastName.equals(emp.lastName);
+    }
 
-
-	public List<Address> getAddresses(){
-		return addresses;
-	}
-	public void setAddresses(List<Address> addresses){
-		this.addresses = addresses;
-	}
-
-
-	public boolean equals(Object obj){
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		Employee emp = (Employee) obj;
-		return firstName.equals(emp.firstName) && lastName.equals(emp.lastName);
-	}
-
-
-	public int hashCode(){
-		int hash = 17;
-		hash = (hash * 31) + firstName.hashCode();
-		hash = (hash * 31) + lastName.hashCode();
-		return hash;
-	}
+    @Override
+    public int hashCode() {
+        int hash = 17;
+        hash = (hash * 31) + (firstName == null ? 0 : firstName.hashCode());
+        hash = (hash * 31) + (lastName == null ? 0 : lastName.hashCode());
+        return hash;
+    }
 
 }
