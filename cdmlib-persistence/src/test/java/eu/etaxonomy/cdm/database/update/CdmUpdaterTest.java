43133fb17763a4f9911900442fbb0d30256c4dcf
Andreas Müller <a.mueller@bgbm.org>
2015-03-26 12:24:38 +0000

    add tests to avoid inconsistencies in recursive cdm updater calls

--- cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/database/update/CdmUpdaterTest.java
+++ cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/database/update/CdmUpdaterTest.java
@@ -10,6 +10,7 @@
 package eu.etaxonomy.cdm.database.update;
 
 
+import java.lang.reflect.Method;
 import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
@@ -23,6 +24,9 @@ import eu.etaxonomy.cdm.common.AccountStore;
 import eu.etaxonomy.cdm.database.CdmDataSource;
 import eu.etaxonomy.cdm.database.DatabaseTypeEnum;
 import eu.etaxonomy.cdm.database.ICdmDataSource;
+import eu.etaxonomy.cdm.database.update.v24_25.SchemaUpdater_24_25;
+import eu.etaxonomy.cdm.database.update.v24_25.TermUpdater_24_25;
+import eu.etaxonomy.cdm.model.metadata.CdmMetaData;
 
 /**
  * @author a.mueller
@@ -89,9 +93,6 @@ public class CdmUpdaterTest {
 //		return makeDestination(dbType, cdmServer, cdmDB, -1, cdmUserName, null);
 //	}
 	
-	
-	
-	
 	private static ICdmDataSource makeDestination(DatabaseTypeEnum dbType, String cdmServer, String cdmDB, int port, String cdmUserName, String pwd ){
 		//establish connection
 		pwd = AccountStore.readOrStorePassword(cdmServer, cdmDB, cdmUserName, pwd);
@@ -105,7 +106,75 @@ public class CdmUpdaterTest {
 			throw new RuntimeException("Unsupported DatabaseType");
 		}
 		return destination;
+	}
+	
+	@Test
+	public void testRecursiveCallSchemaUpdater(){
+		CdmUpdater updater = new CdmUpdater();
+		ISchemaUpdater currentUpdater = null;
+		try {
+			Method method =  CdmUpdater.class.getDeclaredMethod("getCurrentSchemaUpdater");
+			method.setAccessible(true);
+			
+			currentUpdater = (ISchemaUpdater)method.invoke(updater);
+		} catch (Exception e) {
+			Assert.fail("CdmUpdater.getCurrentSchemaUpdater not found:" + e.getMessage());;
+		}
+		ISchemaUpdater lastUpdater = null;
+		ISchemaUpdater tmpUpdater = currentUpdater;
+		
+		int i = 0;
+		//get very first schema updater available (= SchemaUpdater_24_25) by recursive call to getPreviousUpdater
+		while (tmpUpdater.getPreviousUpdater() != null && i++<1000){
+			tmpUpdater = tmpUpdater.getPreviousUpdater();
+			lastUpdater = tmpUpdater;
+		}
+		Assert.assertNotNull("Current Updater must not be null", currentUpdater);
+		Assert.assertEquals("Very first schema updater must be schemaUpdater_24_25. Something seems to be wrong in recursive call of getPreviousSchemaUpdater", SchemaUpdater_24_25.class, lastUpdater.getClass());
+		
+		i = 0;
+		while (tmpUpdater.getNextUpdater() != null && i++<1000){
+			tmpUpdater = tmpUpdater.getNextUpdater();
+			lastUpdater = tmpUpdater;
+		}
+		Assert.assertEquals("Current schema updater not found by recursive call firstUpdater.getNextUpdater()", currentUpdater.getClass(), lastUpdater.getClass());
+		
+		//test correct schema version string
+		Assert.assertEquals(CdmMetaData.getDbSchemaVersion(), currentUpdater.getTargetVersion());
+	}
+	
+	@Test
+	public void testRecursiveCallTermUpdater(){
+		CdmUpdater updater = new CdmUpdater();
+		ITermUpdater currentUpdater = null;
+		try {
+			Method method =  CdmUpdater.class.getDeclaredMethod("getCurrentTermUpdater");
+			method.setAccessible(true);
+			currentUpdater = (ITermUpdater)method.invoke(updater);
+		} catch (Exception e) {
+			Assert.fail("CdmUpdater.getCurrentTermUpdater not found:" + e.getMessage());;
+		}
+		Assert.assertNotNull("Current Updater must not be null", currentUpdater);
+		ITermUpdater lastUpdater = null;
+		ITermUpdater tmpUpdater = currentUpdater;
+		int i = 0;
+		//get very first term updater available (= TermUpdater_24_25) by recursive call to getPreviousUpdater
+		while (tmpUpdater.getPreviousUpdater() != null && i++<1000){
+			tmpUpdater = tmpUpdater.getPreviousUpdater();
+			lastUpdater = tmpUpdater;
+		}
+		
+		Assert.assertEquals(TermUpdater_24_25.class, lastUpdater.getClass());
+		
+		i = 0;
+		while (tmpUpdater.getNextUpdater() != null && i++<1000){
+			tmpUpdater = tmpUpdater.getNextUpdater();
+			lastUpdater = tmpUpdater;
+		}
+		Assert.assertEquals("Current term updater not found by recursive call firstUpdater.getNextUpdater()", currentUpdater.getClass(), lastUpdater.getClass());
 
+		//test correct schema version string
+		Assert.assertEquals(CdmMetaData.getTermsVersion(), currentUpdater.getTargetVersion());
 	}
 	
 }
