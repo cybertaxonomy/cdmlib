83cd7deee1e483ec21dbe223282260ef9eacefee
Andreas Müller <a.mueller@bgbm.org>
2015-03-28 10:46:38 +0000

    Fixing failing search test, using better utf8 handling

---
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/TaxonServiceSearchTestUtf8Constants.java
@@ -0,0 +1,7 @@
+package eu.etaxonomy.cdm.api.service;
+
+final class TaxonServiceSearchTestUtf8Constants {
+
+	public  static final String RUSSIAN_ABIES_ALBA_LONG = "Бальзам ньыв (лат. Abies balsamea) – быдмассэзлӧн пожум котырись ньыв увтырын торья вид. Ньывпуыс быдмӧ 14–20 метра вылына да овлӧ 10–60 см кыза диаметрын. Ньывпу пантасьӧ Ойвыв Америкаын.";
+	protected static final String RUSSIAN_ABIES_ALBA_SHORT = "Бальзам ньыв";
+}
