98fb9ce4653e38ae4044d55b25b9c466cbd33e13
Andreas M��ller <a.mueller@bgbm.org>
2015-04-30 22:12:59 +0000

    Implement bidirectionality for QuantitativeData-StatisticalMeasurementValue #4845

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/description/StatisticalMeasurementValue.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/description/StatisticalMeasurementValue.java
@@ -1,8 +1,8 @@
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -15,6 +15,8 @@ import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.xml.bind.annotation.XmlAccessType;
@@ -29,8 +31,10 @@ import javax.xml.bind.annotation.XmlType;
 import org.apache.log4j.Logger;
 import org.hibernate.envers.Audited;
 import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.IndexedEmbedded;
 
 import eu.etaxonomy.cdm.model.common.DefinedTerm;
+import eu.etaxonomy.cdm.model.common.TermType;
 import eu.etaxonomy.cdm.model.common.VersionableEntity;
 
 /**
@@ -42,7 +46,7 @@ import eu.etaxonomy.cdm.model.common.VersionableEntity;
  * <P>
  * This class corresponds to CharacterMeasureDataType according
  * to the SDD schema.
- * 
+ *
  * @author m.doering
  * @created 08-Nov-2007 13:06:54
  */
@@ -55,10 +59,10 @@ import eu.etaxonomy.cdm.model.common.VersionableEntity;
 public class StatisticalMeasurementValue extends VersionableEntity implements IModifiable, Cloneable{
 	private static final long serialVersionUID = -3576311887760351982L;
 	private static final Logger logger = Logger.getLogger(StatisticalMeasurementValue.class);
-	
+
 	@XmlElement(name = "Value")
 	private float value;
-	
+
 	@XmlElementWrapper(name = "Modifiers")
 	@XmlElement(name = "Modifier")
 	@XmlIDREF
@@ -66,31 +70,44 @@ public class StatisticalMeasurementValue extends VersionableEntity implements IM
 	@ManyToMany(fetch = FetchType.LAZY)
 //	@NotNull // avoids creating a UNIQUE key for this field -> not needed for ManyToMany
 	private Set<DefinedTerm> modifiers = new HashSet<DefinedTerm>();
-	
+
 	@XmlElement(name = "StatisticalMeasureType")
 	@XmlIDREF
 	@XmlSchemaType(name = "IDREF")
 	@ManyToOne(fetch = FetchType.LAZY)
 	private StatisticalMeasure type;
 
+	@XmlElement(name = "QuantitativeData")
+    @XmlIDREF
+    @XmlSchemaType(name = "IDREF")
+    @ManyToOne(fetch = FetchType.LAZY )
+    @JoinTable(
+         name = "DescriptionElementBase_StatisticalMeasurementValue"
+             , joinColumns = @JoinColumn(name = "statisticalvalues_id")
+             , inverseJoinColumns = @JoinColumn(name = "DescriptionElementBase_id")
+    )
+    @IndexedEmbedded(depth=1)
+	private QuantitativeData quantitativeData;
 
-	/** 
+
+
+    /**
 	 * Class constructor: creates a new empty statistical measurement value
 	 * instance.
 	 */
 	protected StatisticalMeasurementValue(){
 		super();
 	}
-	
-	/** 
+
+	/**
 	 * Creates a new empty statistical measurement value instance.
 	 */
 	public static StatisticalMeasurementValue NewInstance(){
 		return new StatisticalMeasurementValue();
 	}
-	
-	
-	/** 
+
+
+	/**
 	 * Creates a new empty statistical measurement value instance.
 	 */
 	public static StatisticalMeasurementValue NewInstance(StatisticalMeasure type, float value){
@@ -99,15 +116,15 @@ public class StatisticalMeasurementValue extends VersionableEntity implements IM
 		result.setType(type);
 		return result;
 	}
-	
-	/** 
+
+	/**
 	 * Returns the type of {@link StatisticalMeasure statistical measure} used in
 	 * <i>this</i> statistical measurement value.
 	 */
 	public StatisticalMeasure getType(){
 		return this.type;
 	}
-	/** 
+	/**
 	 * @see	#getType()
 	 */
 	public void setType(StatisticalMeasure type){
@@ -115,7 +132,7 @@ public class StatisticalMeasurementValue extends VersionableEntity implements IM
 	}
 
 
-	/** 
+	/**
 	 * Returns the numerical value used to describe the {@link Feature feature}
 	 * corresponding to the {@link QuantitativeData quantitative data} <i>this</i>
 	 * statistical measurement value belongs to.
@@ -123,34 +140,51 @@ public class StatisticalMeasurementValue extends VersionableEntity implements IM
 	public float getValue(){
 		return this.value;
 	}
-	/** 
+	/**
 	 * @see	#getValue()
 	 */
 	public void setValue(float value){
 		this.value = value;
 	}
-	
-	
-	/** 
+
+
+    /**
+     * Returns the {@link QuantitativeData quantitative data} <i>this</i>
+     * statistical measurement value belongs too.
+     */
+    public QuantitativeData getQuantitativeData() {
+        return quantitativeData;
+    }
+
+    //for bidirectional use only
+    @Deprecated
+    protected void setQuantitativeData(QuantitativeData quantitativeData) {
+        this.quantitativeData = quantitativeData;
+    }
+
+
+	/**
 	 * Returns the set of terms of {@link TermType type} Modifier used to qualify the validity
 	 * or probability of <i>this</i> statistical measurement value.
 	 * This is only metainformation.
 	 */
-	public Set<DefinedTerm> getModifiers() {
+	@Override
+    public Set<DefinedTerm> getModifiers() {
 		return modifiers;
 	}
 
 	/**
 	 * Adds a {@link Modifier modifier} to the set of {@link #getModifiers() modifiers}
 	 * used to qualify the validity of <i>this</i> statistical measurement value.
-	 * 
+	 *
 	 * @param modifier	the modifier to be added to <i>this</i> statistical measurement value
 	 * @see    	   		#getModifiers()
 	 */
-	public void addModifier(DefinedTerm modifier) {
+	@Override
+    public void addModifier(DefinedTerm modifier) {
 		this.modifiers.add(modifier);
 	}
-	/** 
+	/**
 	 * Removes one element from the set of {@link #getModifiers() modifiers}
 	 * used to qualify the validity of <i>this</i> statistical measurement value.
 	 *
@@ -158,18 +192,19 @@ public class StatisticalMeasurementValue extends VersionableEntity implements IM
 	 * @see     		#getModifiers()
 	 * @see     		#addModifier(Modifier)
 	 */
-	public void removeModifier(DefinedTerm modifier) {
+	@Override
+    public void removeModifier(DefinedTerm modifier) {
 		this.modifiers.remove(modifier);
 	}
-	
+
 
 //*********************************** CLONE *****************************************/
 
-	/** 
+	/**
 	 * Clones <i>this</i> statistical measurement value. This is a shortcut that enables to create
 	 * a new instance that differs only slightly from <i>this</i> statistical measurement value by
 	 * modifying only some of the attributes.
-	 * 
+	 *
 	 * @see eu.etaxonomy.cdm.model.common.VersionableEntity#clone()
 	 * @see java.lang.Object#clone()
 	 */
@@ -178,13 +213,13 @@ public class StatisticalMeasurementValue extends VersionableEntity implements IM
 
 		try {
 			StatisticalMeasurementValue result = (StatisticalMeasurementValue)super.clone();
-			
+
 			//modifiers
 			result.modifiers = new HashSet<DefinedTerm>();
 			for (DefinedTerm modifier : getModifiers()){
 				result.modifiers.add(modifier);
 			}
-			
+
 			return result;
 			//no changes to: value, type
 		} catch (CloneNotSupportedException e) {
