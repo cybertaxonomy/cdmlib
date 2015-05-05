d4361fc7a5ebcba49c8b5f000c88ba4028fcf85f
Andreas M��ller <a.mueller@bgbm.org>
2015-05-05 12:50:32 +0000

    implement datasource type inference for external datasources: #3910

---
+++ cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/database/WrappedCdmDataSourceTest.java
@@ -0,0 +1,48 @@
+// $Id$
+/**
+* Copyright (C) 2015 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.database;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.unitils.spring.annotation.SpringBeanByType;
+
+import eu.etaxonomy.cdm.persistence.dao.agent.IAgentDao;
+import eu.etaxonomy.cdm.test.integration.CdmIntegrationTest;
+
+/**
+ * @author a.mueller
+ * @date 05.05.2015
+ *
+ */
+public class WrappedCdmDataSourceTest extends CdmIntegrationTest {
+
+    @SpringBeanByType
+    private IAgentDao agentDao;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    @Ignore
+    public void testByDatabaseMetaData() {
+//        still needs to be implemented
+    }
+
+    @Override
+    public void createTestDataSet() throws FileNotFoundException {};
+
+}
