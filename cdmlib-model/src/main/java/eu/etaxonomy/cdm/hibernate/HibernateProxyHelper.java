e309a2b842cf63332cee4a2a0dd1b8dd222ae005
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2015-05-18 16:35:14 +0200

    adding method to get the intentifier from hibernate proxies

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/hibernate/HibernateProxyHelper.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/hibernate/HibernateProxyHelper.java
@@ -1,13 +1,15 @@
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
 
 package eu.etaxonomy.cdm.hibernate;
+
+import java.io.Serializable;
 
 import org.apache.log4j.Logger;
 import org.hibernate.proxy.HibernateProxy;
@@ -21,8 +23,8 @@ import org.hibernate.proxy.LazyInitializer;
 public class HibernateProxyHelper {
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(HibernateProxyHelper.class);
-	
-	
+
+
 	// ************************** Hibernate proxies *******************/
 	/* (non-Javadoc)
 	 * @see eu.etaxonomy.cdm.model.common.IProxyHelper#deproxy(java.lang.Object, java.lang.Class)
@@ -34,7 +36,7 @@ public class HibernateProxyHelper {
 	         return clazz.cast(object);
 	     }
 	 }
-	 
+
 		/**
 		 * Unwrap the target instance from the proxy.
 		 */
@@ -46,10 +48,8 @@ public class HibernateProxyHelper {
 				return object;
 			}
 		}
-	        
-	 /* (non-Javadoc)
-	 * @see eu.etaxonomy.cdm.model.common.IProxyHelper#isInstanceOf(java.lang.Object, java.lang.Class)
-	 */
+
+
 	public static boolean isInstanceOf(Object object, Class clazz) throws ClassCastException {
 	     if (clazz == null){
 	    	 return false;
@@ -62,12 +62,20 @@ public class HibernateProxyHelper {
 	         return clazz.isAssignableFrom(object.getClass());
 	     }
 	 }
-	
+
+	public static Serializable getIdentifierOf(Object object) {
+        if (object instanceof HibernateProxy) {
+            return  ((HibernateProxy) object).getHibernateLazyInitializer().getIdentifier();
+        } else {
+            throw new ClassCastException("Cannot cast the given Object to a known Hibernate proxy.");
+        }
+    }
+
 	/**
 	 * Get the class of an instance or the underlying class
 	 * of a proxy (without initializing the proxy!). It is
 	 * almost always better to use the entity name!
-	 * 
+	 *
 	 * delegates calls to {@link org.hibernate.proxy.HibernateProxyHelper}
 	 */
 	public static Class getClassWithoutInitializingProxy(Object object) {
