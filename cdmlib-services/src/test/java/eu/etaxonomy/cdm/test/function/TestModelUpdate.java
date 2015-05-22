3f2f14dbb8ab0314b9135413e9a9dbe4768479d8
Andreas M��ller <a.mueller@bgbm.org>
2015-05-22 11:48:29 +0000

    adapt ModelUpdate tester

--- cdmlib-services/src/test/java/eu/etaxonomy/cdm/test/function/TestModelUpdate.java
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/test/function/TestModelUpdate.java
@@ -5,7 +5,7 @@
 *
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
-*/ 
+*/
 
 package eu.etaxonomy.cdm.test.function;
 
@@ -18,33 +18,32 @@ import eu.etaxonomy.cdm.database.CdmDataSource;
 import eu.etaxonomy.cdm.database.DbSchemaValidation;
 import eu.etaxonomy.cdm.database.ICdmDataSource;
 import eu.etaxonomy.cdm.database.update.CdmUpdater;
-import eu.etaxonomy.cdm.model.agent.Person;
-import eu.etaxonomy.cdm.model.common.IntextReference;
-import eu.etaxonomy.cdm.model.common.Language;
-import eu.etaxonomy.cdm.model.common.LanguageString;
-import eu.etaxonomy.cdm.model.description.TaxonDescription;
-import eu.etaxonomy.cdm.model.description.TextData;
-import eu.etaxonomy.cdm.model.name.BotanicalName;
 import eu.etaxonomy.cdm.model.name.NomenclaturalCode;
-import eu.etaxonomy.cdm.model.taxon.Taxon;
 
+/**
+ * This class is meant for functional testing of model changes. It is not meant
+ * for running in maven.
+ * @author a.mueller
+ * @date 22.05.2015
+ *
+ */
 public class TestModelUpdate {
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(TestModelUpdate.class);
 
-	
+
 	private void testMySQL(){
-		DbSchemaValidation schema = DbSchemaValidation.VALIDATE;
+		DbSchemaValidation schema = DbSchemaValidation.CREATE;
 
 //		DatabaseTypeEnum dbType = DatabaseTypeEnum.MySQL;
-		
+
 		String server = "localhost";
-		String database = (schema == DbSchemaValidation.VALIDATE  ? "cdm34" : "cdm35");
-		database = "campanula_test";
+		String database = (schema == DbSchemaValidation.VALIDATE  ? "cdm35" : "cdm36");
+//		database = "campanula_test";
 		String username = "edit";
 		CdmDataSource dataSource = CdmDataSource.NewMySqlInstance(server, database, username, AccountStore.readOrStorePassword(server, database, username, null));
 
-    	
+
  		try {
 			CdmUpdater updater = new CdmUpdater();
 			if (schema == DbSchemaValidation.VALIDATE){
@@ -53,11 +52,11 @@ public class TestModelUpdate {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		//CdmPersistentDataSource.save(dataSource.getName(), dataSource);
 		CdmApplicationController appCtr;
 		appCtr = CdmApplicationController.NewInstance(dataSource,schema);
-		
+
 //		Person person = Person.NewInstance();
 //		TextData textData = TextData.NewInstance();
 //		Taxon taxon = Taxon.NewInstance(null, null);
@@ -71,25 +70,25 @@ public class TestModelUpdate {
 //		appCtr.getCommonService().createFullSampleData();
 
 		//		insertSomeData(appCtr);
-//		deleteHighLevelNode(appCtr);   //->problem with Duplicate Key in Classification_TaxonNode 		
-		
+//		deleteHighLevelNode(appCtr);   //->problem with Duplicate Key in Classification_TaxonNode
+
 		appCtr.close();
 	}
-	
-	
+
+
 
 //	String server = "localhost";
 //	String database = "testCDM";
 //	String username = "postgres";
-//	dataSource = CdmDataSource.NewInstance(DatabaseTypeEnum.PostgreSQL, server, database, DatabaseTypeEnum.PostgreSQL.getDefaultPort(), username, AccountStore.readOrStorePassword(server, database, username, null)); 
-	
-	
+//	dataSource = CdmDataSource.NewInstance(DatabaseTypeEnum.PostgreSQL, server, database, DatabaseTypeEnum.PostgreSQL.getDefaultPort(), username, AccountStore.readOrStorePassword(server, database, username, null));
+
+
 //	//SQLServer
 //	database = "CDMTest";
 //	int port = 1433;
 //	username = "pesiexport";
 ////	dataSource = CdmDataSource.NewSqlServer2005Instance(server, database, port, username, AccountStore.readOrStorePassword(server, database, username, null));
-//	
+//
 	//H2
 //	String path = "C:\\Users\\a.mueller\\eclipse\\svn\\cdmlib-trunk\\cdmlib-remote-webapp\\src\\test\\resources\\h2";
 //	String path = "C:\\Users\\a.mueller\\.cdmLibrary\\writableResources\\h2\\LocalH2_test34";
@@ -105,32 +104,32 @@ public class TestModelUpdate {
 	private void updateRemoteWebappTestH2(){
 		String path = "C:\\Users\\a.mueller\\eclipse\\svn\\cdmlib-trunk\\cdmlib\\cdmlib-remote-webapp\\src\\test\\resources\\h2";
 		ICdmDataSource dataSource = CdmDataSource.NewH2EmbeddedInstance("cdmTest", "sa", "", path, NomenclaturalCode.ICNAFP);
-		
-    	
+
+
  		try {
 			CdmUpdater updater = new CdmUpdater();
 			updater.updateToCurrentVersion(dataSource, DefaultProgressMonitor.NewInstance());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		//CdmPersistentDataSource.save(dataSource.getName(), dataSource);
 		CdmApplicationController appCtr;
 		appCtr = CdmApplicationController.NewInstance(dataSource,DbSchemaValidation.VALIDATE);
 		appCtr.close();
 	}
-	
+
 
 	private void test(){
 		System.out.println("Start Datasource");
 		testMySQL();
-		
+
 //		updateRemoteWebappTestH2();
-		
+
     	//testDatabaseChange();
-		
+
 		//testSqlServer();
-		
+
 		//CdmUtils.findLibrary(au.com.bytecode.opencsv.CSVReader.class);
 		//testPostgreServer();
 		//testLocalHsql();
@@ -139,7 +138,7 @@ public class TestModelUpdate {
 //		testH2();
 		System.out.println("\nEnd Datasource");
 	}
-	
+
 	/**
 	 * @param args
 	 */
