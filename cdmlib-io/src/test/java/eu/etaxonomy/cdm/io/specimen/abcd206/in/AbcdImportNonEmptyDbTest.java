c1b3fb1c66c15af1c49a9406fec26025a32b2d4b
Andreas M��ller <a.mueller@bgbm.org>
2015-05-27 14:39:45 +0000

    try to fix ignored test  #3279

--- cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/specimen/abcd206/in/AbcdImportNonEmptyDbTest.java
+++ cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/specimen/abcd206/in/AbcdImportNonEmptyDbTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.unitils.dbunit.annotation.DataSet;
 import org.unitils.spring.annotation.SpringBeanByName;
@@ -91,7 +90,6 @@ public class AbcdImportNonEmptyDbTest extends CdmTransactionalIntegrationTest {
 
 	@Test
     @DataSet( value="AbcdImportNonEmptyDbTest.xml")  //loadStrategy=CleanSweepInsertLoadStrategy.class
-    @Ignore
 	public void testDoInvoke() {
         boolean result = defaultImport.invoke(configurator);
         assertTrue("Return value for import.invoke should be true", result);
