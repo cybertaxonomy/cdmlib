2222043f214c50f6e0164ab241d66f2ef0ab6127
Andreas M��ller <a.mueller@bgbm.org>
2015-06-04 14:25:05 +0000

    improve handling of substeps in update scripts (immediately stop updating if one step fails)

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/UpdaterBase.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/UpdaterBase.java
@@ -1,9 +1,9 @@
 // $Id$
 /**
 * Copyright (C) 2009 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -21,27 +21,27 @@ import eu.etaxonomy.cdm.model.metadata.CdmMetaData;
 
 /**
  * Common updater base class for updating schema or terms.
- * 
+ *
  * @see CdmUpdater
  * @see ISchemaUpdater
  * @see ITermUpdater
- * 
+ *
  * @author a.mueller
  * @date 16.11.2010
  *
  */
 public abstract class UpdaterBase<T extends ISchemaUpdaterStep, U extends IUpdater<U>> implements IUpdater<U> {
 	private static final Logger logger = Logger.getLogger(TermUpdaterBase.class);
-	
+
 	protected List<T> list;
 	protected String startVersion;
 	protected String targetVersion;
-	
-	
+
+
 	protected abstract boolean updateVersion(ICdmDataSource datasource, IProgressMonitor monitor, CaseType caseType) throws SQLException;
-	
+
 	protected abstract String getCurrentVersion(ICdmDataSource datasource, IProgressMonitor monitor, CaseType caseType) throws SQLException;
-	
+
 	@Override
 	public int countSteps(ICdmDataSource datasource, IProgressMonitor monitor, CaseType caseType){
 		int result = 0;
@@ -50,15 +50,15 @@ public abstract class UpdaterBase<T extends ISchemaUpdaterStep, U extends IUpdat
 			for (T step: list){
 				result++; //+= list.size();
 				result += step.getInnerSteps().size();
-			}	
+			}
 			if (getPreviousUpdater() != null){
 				result += getPreviousUpdater().countSteps(datasource, monitor, caseType);
 			}
 		}
 		return result;
 	}
-	
-	
+
+
 	private boolean isToBeInvoked(ICdmDataSource datasource, IProgressMonitor monitor, CaseType caseType) {
 		boolean result = true;
 		String datasourceVersion;
@@ -68,61 +68,61 @@ public abstract class UpdaterBase<T extends ISchemaUpdaterStep, U extends IUpdat
 			monitor.warning("SQLException", e1);
 			return false;
 		}
-		
+
 		boolean isAfterMyStartVersion = isAfterMyStartVersion(datasourceVersion, monitor);
 		boolean isBeforeMyStartVersion = isBeforeMyStartVersion(datasourceVersion, monitor);
 //		boolean isBeforeMyTargetVersion = isBeforeMyTargetVersion(targetVersion, monitor);
 		boolean isBeforeMyTargetVersion = isBeforeMyTargetVersion(targetVersion, monitor);
 		boolean isDatasourceBeforeMyTargetVersion = isBeforeMyTargetVersion(datasourceVersion, monitor);
-		
+
 		result &= isDatasourceBeforeMyTargetVersion;
 		result &= !(isAfterMyStartVersion && isBeforeMyTargetVersion);
 		result &= ! (isBeforeMyStartVersion && getPreviousUpdater() == null);
 		result &= !isBeforeMyTargetVersion;
 		return result;
 	}
-	
-	
+
+
 	@Override
 	public boolean invoke(ICdmDataSource datasource, IProgressMonitor monitor, CaseType caseType) throws Exception{
 		String currentLibrarySchemaVersion = CdmMetaData.getDbSchemaVersion();
 		return invoke(currentLibrarySchemaVersion, datasource, monitor, caseType);
 	}
-	
+
 	@Override
 	public boolean invoke(String targetVersion, ICdmDataSource datasource, IProgressMonitor monitor, CaseType caseType) throws Exception{
 		boolean result = true;
 		String datasourceVersion;
-		
+
 		try {
 			datasourceVersion = getCurrentVersion(datasource, monitor, caseType);
 		} catch (SQLException e1) {
 			monitor.warning("SQLException", e1);
 			return false;
-		}		
+		}
 
-		
+
 		boolean isAfterMyStartVersion = isAfterMyStartVersion(datasourceVersion, monitor);
 		boolean isBeforeMyStartVersion = isBeforeMyStartVersion(datasourceVersion, monitor);
 //		boolean isAfterMyTargetVersion = isAfterMyTargetVersion(targetVersion, monitor);
 		boolean isBeforeMyTargetVersion = isBeforeMyTargetVersion(targetVersion, monitor);
 		boolean isDatasourceBeforeMyTargetVersion = isBeforeMyTargetVersion(datasourceVersion, monitor);
-		
-		
-		
+
+
+
 		if (! isDatasourceBeforeMyTargetVersion){
 			String warning = "Target version ("+targetVersion+") is not before updater target version ("+this.targetVersion+"). Nothing to update.";
 			monitor.warning(warning);
 			return true;
 		}
-		
+
 		if (isAfterMyStartVersion && isBeforeMyTargetVersion){
 			String warning = "Database version is higher than updater start version but lower than updater target version";
 			RuntimeException exeption = new RuntimeException(warning);
 			monitor.warning(warning, exeption);
 			throw exeption;
 		}
-		
+
 		if (isBeforeMyStartVersion){
 			if (getPreviousUpdater() == null){
 				String warning = "Database version is before updater version but no previous version updater exists";
@@ -132,16 +132,16 @@ public abstract class UpdaterBase<T extends ISchemaUpdaterStep, U extends IUpdat
 			}
 			result &= getPreviousUpdater().invoke(startVersion, datasource, monitor, caseType);
 		}
-		
 
-		
+
+
 		if (isBeforeMyTargetVersion){
 			String warning = "Target version ("+targetVersion+") is lower than updater target version ("+this.targetVersion+")";
 			RuntimeException exeption = new RuntimeException(warning);
 			monitor.warning(warning, exeption);
 			throw exeption;
 		}
-		
+
 		if (result == false){
 			return result;
 		}
@@ -158,18 +158,18 @@ public abstract class UpdaterBase<T extends ISchemaUpdaterStep, U extends IUpdat
 			}else{
 				datasource.rollback();
 			}
-			
+
 		} catch (Exception e) {
 			datasource.rollback();
 			logger.error("Error occurred while trying to run updater: " + this.getClass().getName());
 			result = false;
 		}
 		return result;
-	
+
 	}
-	
+
 //	protected abstract boolean handleSingleStep(ICdmDataSource datasource,	IProgressMonitor monitor, boolean result, ISchemaUpdaterStep step, boolean isInnerStep) throws Exception;
-	
+
 	protected boolean handleSingleStep(ICdmDataSource datasource, IProgressMonitor monitor, boolean result, ISchemaUpdaterStep step, boolean isInnerStep, CaseType caseType)
 			throws Exception {
 		try {
@@ -178,6 +178,9 @@ public abstract class UpdaterBase<T extends ISchemaUpdaterStep, U extends IUpdat
 			result &= (invokeResult != null);
 			for (ISchemaUpdaterStep innerStep : step.getInnerSteps()){
 				result &= handleSingleStep(datasource, monitor, result, innerStep, true, caseType);
+				if (!result){
+				    break;
+				}
 			}
 //			if (! isInnerStep){
 				monitor.worked(1);
@@ -189,7 +192,7 @@ public abstract class UpdaterBase<T extends ISchemaUpdaterStep, U extends IUpdat
 		}
 		return result;
 	}
-	
+
 	protected boolean isAfterMyStartVersion(String dataSourceSchemaVersion, IProgressMonitor monitor) {
 		int depth = 4;
 		int compareResult = CdmMetaData.compareVersion(dataSourceSchemaVersion, startVersion, depth, monitor);
@@ -226,7 +229,7 @@ public abstract class UpdaterBase<T extends ISchemaUpdaterStep, U extends IUpdat
 	@Override
 	public abstract U getPreviousUpdater();
 
-	
+
 	/**
 	 * @return
 	 */
