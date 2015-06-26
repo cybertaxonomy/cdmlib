933331e0e0f6f1ae2ddce197313cd347166a4c36
Cherian Mathew <c.mathew@bgbm.org>
2015-06-26 11:23:44 +0200

    #5032 Add new test service for remoting clients

---
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/TestServiceImpl.java
@@ -0,0 +1,30 @@
+// $Id$
+/**
+* Copyright (C) 2015 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.api.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author cmathew
+ * @date 26 Jun 2015
+ *
+ */
+@Service
+public class TestServiceImpl implements ITestService {
+
+    /* (non-Javadoc)
+     * @see eu.etaxonomy.cdm.api.service.ITestService#wait(int)
+     */
+    @Override
+    public void waitFor(long timeToWaitInMs) throws InterruptedException {
+        Thread.sleep(timeToWaitInMs);
+    }
+
+}
