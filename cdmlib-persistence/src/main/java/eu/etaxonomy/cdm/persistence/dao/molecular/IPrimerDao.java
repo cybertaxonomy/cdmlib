8b5c709dff1cc20254b51db35f9ad54c19e0d0b4
Patrick Plitzner <p.plitzner@bgbm.org>
2015-06-29 19:09:25 +0200

    Add find- and countByTitle() methods to primer and amplification service

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/molecular/IPrimerDao.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/molecular/IPrimerDao.java
@@ -12,9 +12,14 @@ package eu.etaxonomy.cdm.persistence.dao.molecular;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.criterion.Criterion;
+
 import eu.etaxonomy.cdm.model.molecular.Primer;
 import eu.etaxonomy.cdm.persistence.dao.common.IAnnotatableDao;
+import eu.etaxonomy.cdm.persistence.dao.initializer.IBeanInitializer;
 import eu.etaxonomy.cdm.persistence.dto.UuidAndTitleCache;
+import eu.etaxonomy.cdm.persistence.query.MatchMode;
+import eu.etaxonomy.cdm.persistence.query.OrderHint;
 
 /**
  * @author pplitzner
@@ -29,5 +34,34 @@ public interface IPrimerDao extends IAnnotatableDao<Primer>{
      * @return a list of {@link UuidAndTitleCache}
      */
     public List<UuidAndTitleCache<Primer>> getPrimerUuidAndTitleCache();
+
+
+    /**
+     * Return a count of {@link Primer}s matching the given query string in the titleCache, optionally with a particular MatchMode
+     *
+     * @param queryString the query string to filter by
+     * @param matchmode use a particular type of matching (can be null - defaults to exact matching)
+     * @param criteria extra restrictions to apply
+     * @return a count of instances of type T matching the queryString
+     */
+    public Integer countByTitle(String queryString, MatchMode matchmode, List<Criterion> criteria);
+
+    /**
+    * Return a List of {@link Primer}s matching the given query string, optionally with a particular MatchMode
+    *
+    * @param queryString the query string to filter by
+    * @param matchmode use a particular type of matching (can be null - defaults to exact matching)
+    * @param criteria extra restrictions to apply
+    * @param pageSize The maximum number of rights returned (can be null for all rights)
+    * @param pageNumber The offset (in pageSize chunks) from the start of the result set (0 - based)
+    * @param propertyPaths properties to initialize - see {@link IBeanInitializer#initialize(Object, List)}
+    * @param orderHints
+    *            Supports path like <code>orderHints.propertyNames</code> which
+    *            include *-to-one properties like createdBy.username or
+    *            authorTeam.persistentTitleCache
+    * @return a List of instances of type T matching the queryString
+    */
+    public List<Primer> findByTitle(String queryString, MatchMode matchmode, List<Criterion> criteria,
+            Integer pageSize, Integer pageNumber, List<OrderHint> orderHints, List<String> propertyPaths);
 
 }
