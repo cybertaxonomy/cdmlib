b9e5fce54a7216001da37f07843d3a4c22b48c75
Patrick Plitzner <p.plitzner@bgbm.org>
2015-11-09 17:13:50 +0100

    added DAO for SingleReads #5200

---
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/molecular/ISingleReadDao.java
@@ -0,0 +1,23 @@
+// $Id$
+/**
+* Copyright (C) 2014 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.persistence.dao.molecular;
+
+import eu.etaxonomy.cdm.model.molecular.SingleRead;
+import eu.etaxonomy.cdm.persistence.dao.common.IAnnotatableDao;
+
+
+/**
+ * @author pplitzner
+ * @date 11.03.2014
+ *
+ */
+public interface ISingleReadDao extends IAnnotatableDao<SingleRead>{
+
+}
