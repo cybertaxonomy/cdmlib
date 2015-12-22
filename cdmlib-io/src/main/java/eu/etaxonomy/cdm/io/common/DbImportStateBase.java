26b282cdf62e9b47ff32074e6b89554e8e5b27c7
Andreas Müller <a.mueller@bgbm.org>
2015-12-22 14:59:43 +0100

    Cleanup DbImportStateBase

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/common/DbImportStateBase.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/common/DbImportStateBase.java
@@ -1,9 +1,9 @@
 // $Id$
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -23,43 +23,37 @@ import eu.etaxonomy.cdm.model.reference.Reference;
 /**
  * @author a.mueller
  * @created 11.05.2009
- * @version 1.0
  */
 public abstract class DbImportStateBase<CONFIG extends DbImportConfiguratorBase, STATE extends DbImportStateBase> extends ImportStateBase<CONFIG, CdmImportBase> implements IPartitionedState {
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(DbImportStateBase.class);
-	
+
 	public static final String CURRENT_OBJECT_NAMESPACE = "CurrentObjectNamespace";
 	public static final String CURRENT_OBJECT_ID = "CurrentObjectId";
-	
-	private Map<String, User> usernameMap = new HashMap<String, User>();
-	
-	
+
+	private final Map<String, User> usernameMap = new HashMap<String, User>();
+
+
 	private Reference<?> partitionSourceReference;
-	
-	private RelatedObjectsHelper relatedObjectsHelper = new RelatedObjectsHelper();;
+
+	private final RelatedObjectsHelper relatedObjectsHelper = new RelatedObjectsHelper();
 	//holds the classifications needed for this partition, the key is a value that differentiate classifications
 	//like the taxons reference (secundum)
-	
-	
+
+
 	/**
 	 * @param config
 	 */
 	protected DbImportStateBase(CONFIG config) {
 		super(config);
 	}
-	
-	/* (non-Javadoc)
-	 * @see eu.etaxonomy.cdm.io.common.IPartitionedState#addRelatedObject(java.lang.Object, java.lang.String, eu.etaxonomy.cdm.model.common.CdmBase)
-	 */
+
+	@Override
 	public void addRelatedObject(Object namespace, String id, CdmBase relatedObject) {
 		this.relatedObjectsHelper.addRelatedObjet(namespace, id, relatedObject);
 	}
 
-	
-	/* (non-Javadoc)
-	 * @see eu.etaxonomy.cdm.io.common.IPartitionedState#getRelatedObject(java.lang.Object, java.lang.String)
-	 */
+	@Override
 	public CdmBase getRelatedObject(Object namespace, String id) {
 		return relatedObjectsHelper.getRelatedObject(namespace, id);
 	}
@@ -69,27 +63,31 @@ public abstract class DbImportStateBase<CONFIG extends DbImportConfiguratorBase,
 		T result = CdmBase.deproxy(cdmBase, clazz);
 		return result;
 	}
-	
-	public void setRelatedObjects(Map<Object, Map<String, ? extends CdmBase>> relatedObjects) {
+
+	@Override
+    public void setRelatedObjects(Map<Object, Map<String, ? extends CdmBase>> relatedObjects) {
 		relatedObjectsHelper.setRelatedObjects(relatedObjects);
 	}
-	
+
 
 	/**
 	 * Stores the source reference for the time of a transaction. Needs to be
 	 * deleted manually after transaction finishes to avoid memory leaks.
 	 * @return
 	 */
-	public Reference getTransactionalSourceReference() {
+	@Override
+    public Reference<?> getTransactionalSourceReference() {
 		return partitionSourceReference;
 	}
 
-	
-	public void resetTransactionalSourceReference(){
+
+	@Override
+    public void resetTransactionalSourceReference(){
 		this.partitionSourceReference = null;
 	}
-	
-	public void makeTransactionalSourceReference(IReferenceService service){
+
+	@Override
+    public void makeTransactionalSourceReference(IReferenceService service){
 		//TODO handle undefined sourceRefUuid
 		this.partitionSourceReference = service.find(this.getConfig().getSourceRefUuid());
 		if (this.partitionSourceReference == null){
@@ -97,7 +95,7 @@ public abstract class DbImportStateBase<CONFIG extends DbImportConfiguratorBase,
 			service.saveOrUpdate(this.partitionSourceReference);
 		}
 	}
-	
+
 	public User getUser(String username){
 		return usernameMap.get(username);
 	}
@@ -106,5 +104,5 @@ public abstract class DbImportStateBase<CONFIG extends DbImportConfiguratorBase,
 		usernameMap.put(username, user);
 	}
 
-	
+
 }
