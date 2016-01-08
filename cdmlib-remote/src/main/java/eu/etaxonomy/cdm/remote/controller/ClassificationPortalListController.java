77db7b53bfb159745414f2563e0d2cf9b95b417b
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2016-01-08 13:30:51 +0100

    #4962 fixing LIE by re-adding init strategy for taxonNames

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/ClassificationPortalListController.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/ClassificationPortalListController.java
@@ -55,7 +55,6 @@ public class ClassificationPortalListController extends IdentifiableListControll
     });
 
     private static final List<String> NODE_INIT_STRATEGY = Arrays.asList(new String[]{
-            "taxon.sec",
             "taxon.name.rank",
             });
 
@@ -152,7 +151,7 @@ public class ClassificationPortalListController extends IdentifiableListControll
         rank = findRank(rankUuid);
 
 //        long start = System.currentTimeMillis();
-        List<TaxonNode> rootNodes = service.listRankSpecificRootNodes(tree, rank, null, null, DEFAULT_INIT_STRATEGY);
+        List<TaxonNode> rootNodes = service.listRankSpecificRootNodes(tree, rank, null, null, NODE_INIT_STRATEGY);
 //        System.err.println("service.listRankSpecificRootNodes() " + (System.currentTimeMillis() - start));
         return rootNodes;
     }
