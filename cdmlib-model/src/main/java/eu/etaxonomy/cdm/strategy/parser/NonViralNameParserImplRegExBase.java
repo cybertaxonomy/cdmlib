c0e106fe949ba0e81550cdb05f12fbd8aa43e23e
Andreas Müller <a.mueller@bgbm.org>
2016-01-14 16:33:04 +0100

    Further fix right quotation mark parsing

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/parser/NonViralNameParserImplRegExBase.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/parser/NonViralNameParserImplRegExBase.java
@@ -84,7 +84,8 @@ public abstract class NonViralNameParserImplRegExBase  {
 
 
     //AuthorString
-	protected static String authorPart = "(" + "(O[’']|d[’']|D[’']|L[’']|'t|ten\\s||le\\s|zur\\s)?" + "(" + capital2charDotWord + "|DC.)" + "('" + nonCapitalDotWord + ")?" + "|[vV][ao]n(\\sder)?|da|du|de(n|l|\\sla)?)" ;
+    protected static String qm = "[" + UTF8.RIGHT_SINGLE_QUOT + "']";
+    protected static String authorPart = "(" + "([OdDL]"+qm+"|[’']t|ten\\s||le\\s|zur\\s)?" + "(" + capital2charDotWord + "|DC.)" + "('" + nonCapitalDotWord + ")?" + "|[vV][ao]n(\\sder)?|da|du|de(n|l|\\sla)?)" ;
     protected static String author = "(" + authorPart + "(" + fWs + "|-)" + ")+" + "(f\\.|fil\\.|secundus)?" ;
     protected static String finalTeamSplitter = "(" + fWs + "(&)" + fWs + "|" + oWs + "et" + oWs + ")";
     protected static String notFinalTeamSplitter = "(?:" + fWs + "," + fWs + "|" + finalTeamSplitter + ")";
