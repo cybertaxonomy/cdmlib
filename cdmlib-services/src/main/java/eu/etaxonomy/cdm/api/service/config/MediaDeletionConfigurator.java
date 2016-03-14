67f24c2eee3170db714f0a754eafe11285b7cbf5
Katja Luther <k.luther@bgbm.org>
2016-03-14 12:36:26 +0100

    minor

---
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/config/MediaDeletionConfigurator.java
@@ -0,0 +1,19 @@
+// $Id$
+/**
+* Copyright (C) 2016 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.api.service.config;
+
+/**
+ * @author k.luther
+ * @date 10.03.2016
+ *
+ */
+public class MediaDeletionConfigurator extends DeleteConfiguratorBase {
+
+}
