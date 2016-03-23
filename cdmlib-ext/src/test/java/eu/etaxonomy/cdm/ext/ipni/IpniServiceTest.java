b30bd45ba69568994a085e9db555345ae746996f
Andreas Müller <a.mueller@bgbm.org>
2016-03-23 16:38:50 +0100

    Unignore IPNI Test

--- cdmlib-ext/src/test/java/eu/etaxonomy/cdm/ext/ipni/IpniServiceTest.java
+++ cdmlib-ext/src/test/java/eu/etaxonomy/cdm/ext/ipni/IpniServiceTest.java
@@ -10,7 +10,6 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import eu.etaxonomy.cdm.api.application.ICdmApplicationConfiguration;
@@ -27,7 +26,6 @@ import eu.etaxonomy.cdm.model.reference.Reference;
  * @author a.mueller
  *
  */
-@Ignore //preliminary set to ignore
 public class IpniServiceTest {
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(IpniServiceTest.class);
