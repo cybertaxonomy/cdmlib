daa409e98df5153e4505cd8712f28a73d1efe9f4
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2016-03-24 10:53:56 +0100

    #4634 cleaning strings from image EXIF IPTC TIFF metadata

--- cdmlib-commons/src/main/java/eu/etaxonomy/cdm/common/media/ImageInfo.java
+++ cdmlib-commons/src/main/java/eu/etaxonomy/cdm/common/media/ImageInfo.java
@@ -1,9 +1,9 @@
 // $Id$
 /**
 * Copyright (C) 2009 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -34,16 +34,16 @@ import eu.etaxonomy.cdm.common.UriUtils;
  */
 public  class ImageInfo extends MediaInfo {
 	private static Logger logger = Logger.getLogger(ImageInfo.class);
-	
+
 	private int width;
 	private int height;
 	private int bitPerPixel;
-	private URI imageUri;
+	private final URI imageUri;
 
 	private Map<String, String> metaData;
-	
+
 //********************** Factory Methods ******************************/
-	
+
 	public static ImageInfo NewInstance(URI imageUri, Integer timeOut) throws IOException, HttpException {
 		ImageInfo instance = new ImageInfo(imageUri);
 		instance.readSuffix();
@@ -51,25 +51,25 @@ public  class ImageInfo extends MediaInfo {
 		instance.readImageInfo(timeOut);
 		return instance;
 	}
-	
+
 	public static ImageInfo NewInstanceWithMetaData(URI imageUri, Integer timeOut) throws IOException, HttpException {
 		ImageInfo instance = NewInstance(imageUri, timeOut);
 		instance.readMetaData(timeOut);
 		return instance;
 	}
-	
+
 //*********************** CONSTRUCTOR **************************************/
-	
+
 	private ImageInfo(URI imageUri){
 		this.imageUri = imageUri;
 	}
-	
+
 //*************************** GETTER /SETTER *******************************/
-	
+
 	public URI getUri() {
 		return imageUri;
 	}
-	
+
 	public int getWidth() {
 		return width;
 	}
@@ -81,25 +81,25 @@ public  class ImageInfo extends MediaInfo {
 	public int getBitPerPixel() {
 		return bitPerPixel;
 	}
-	
+
 	public Map<String, String> getMetaData(){
 		return metaData;
 	}
-	
+
 //**************************** METHODS *****************************/
-	
+
 	private void readSuffix(){
 		String path = imageUri.getPath();
-				
+
 		String suffix = path.substring(StringUtils.lastIndexOf(path, '.') + 1);
 		setSuffix(suffix);
 	}
-	
+
 	private void readImageLength() throws ClientProtocolException, IOException, HttpException{
-		long length = UriUtils.getResourceLength(imageUri, null); 
+		long length = UriUtils.getResourceLength(imageUri, null);
 		setLength(length);
 	}
-	
+
 	/**
 	 * Reads the image infos (width, height, bitPerPixel, metadata, format, mime type)
 	 * @param timeOut
@@ -107,33 +107,33 @@ public  class ImageInfo extends MediaInfo {
 	 * @throws HttpException
 	 */
 	private void readImageInfo(Integer timeOut) throws IOException, HttpException{
-		
+
 		InputStream inputStream;
 		try {
 			inputStream = UriUtils.getInputStream(imageUri);
 			org.apache.sanselan.ImageInfo imageInfo = Sanselan.getImageInfo(inputStream, null);
-						
+
 			setFormatName(imageInfo.getFormatName());
 			setMimeType(imageInfo.getMimeType());
 			width = imageInfo.getWidth();
 			height = imageInfo.getHeight();
 			bitPerPixel = imageInfo.getBitsPerPixel();
-		    
+
 		} catch (ImageReadException e) {
 			logger.error("Could not read: " + imageUri + ". " + e.getMessage());
 			throw new IOException(e);
-		}	
+		}
 	}
 
-	
+
 	public Map<String, String> readMetaData(Integer timeOut) throws IOException, HttpException {
 
 		try {
 			InputStream inputStream = UriUtils.getInputStream(imageUri);
-			
+
 			IImageMetadata mediaData;
 			mediaData = Sanselan.getMetadata(inputStream, null);
-			
+
 			if (mediaData != null){
 				metaData = new HashMap<String, String>();
 				for (Object object : mediaData.getItems()){
@@ -143,9 +143,9 @@ public  class ImageInfo extends MediaInfo {
 						//key.replace("/", "");
 						int index = key.indexOf("/");
 						key = key.substring(0, index);
-						metaData.put(key, item.getText());	
+						metaData.put(key, text(item));
 					}else{
-						metaData.put(item.getKeyword(), item.getText());						
+						metaData.put(item.getKeyword(), text(item));
 					}
 				}
 			}
@@ -156,12 +156,29 @@ public  class ImageInfo extends MediaInfo {
 		return metaData;
 	}
 
+    /**
+     * Wrapper for the Item.getText() method which applies cleaning of the text representation.
+     *
+     * <ol>
+     * <li>Strings are surrounded by single quotes, these must be removed</li>
+     * </ol>
+     * @param item
+     * @return
+     */
+    private String text(Item item) {
+        String  text = item.getText();
+        if(text.startsWith("'") && text.endsWith("'")) {
+            text = text.substring(1 , text.length() - 1);
+        }
+        return text;
+    }
+
 // ******************* TO STRING **********************************/
-	
+
 	@Override
 	public String toString(){
         return getFormatName() + " [" + getMimeType()+ "] w:" + width + " h:" + height + " depth:" + bitPerPixel;
 	}
 
-	
+
 }
