889c7e91874f1f51e76a2b87378d6fd2ef7c323a
Katja Luther <k.luther@bgbm.org>
2016-08-31 14:52:43 +0200

    fix #6045: fixing the NaturalOrderComparator and complete the test

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/taxon/TaxonNaturalComparator.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/taxon/TaxonNaturalComparator.java
@@ -8,8 +8,9 @@
 */
 package eu.etaxonomy.cdm.model.taxon;
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Iterator;
+import java.util.List;
 
 /**
  * Comparator to compare {@link TaxonNode taxon nodes} by its user defined ordering
@@ -22,58 +23,77 @@ public class TaxonNaturalComparator implements Comparator<TaxonNode> {
 
 	}
 
-	@Override
+	@SuppressWarnings("null")
+    @Override
 	public int compare(TaxonNode node1, TaxonNode node2) {
-		if (node1.isAncestor(node2)) {
-            return 1;
+	   // System.out.println("compare node 1: "+ node1.getTaxon().getTitleCache() + " - node 2: " + node2.getTaxon().getTitleCache());
+	    if (node1.equals(node2)) {
+
+	        return 0;
         }
-		if (node2.isAncestor(node1)) {
+
+	    if (node1.isAncestor(node2)) {
+
             return -1;
         }
-		if (node1.equals(node2)) {
-            return 0;
+		if (node2.isAncestor(node1)) {
+
+            return 1;
         }
+
 
 		String[] splitNode1 = node1.treeIndex().split("#");
 		String[] splitNode2 = node2.treeIndex().split("#");
-		if (splitNode1.length < splitNode2.length) {
-            return 1;
-        } else if (splitNode2.length < splitNode1.length) {
-            return -1;
-        }
+
 
 		if (node1.getParent().equals(node2.getParent())){
 			return node1.getSortIndex().compareTo(node2.getSortIndex());
 		}
-		String lastEqualAncestorTreeIndex = null;
+		String lastEqualAncestorTreeIndex = "";
+		List<TaxonNode> ancestorAndNode= new ArrayList<TaxonNode>();
+		ancestorAndNode.add(node1);
+		ancestorAndNode.addAll(node1.getAncestors());
+		java.util.Collections.sort(ancestorAndNode, new TreeIndexComparator());
 
-		Iterator<TaxonNode> ancestorsNode1 = node1.getAncestors().iterator();
-		Iterator<TaxonNode> ancestorsNode2 = node2.getAncestors().iterator();
+
+		List<TaxonNode> ancestorAndNode2= new ArrayList<TaxonNode>();
+        ancestorAndNode2.add(node2);
+        ancestorAndNode2.addAll(node2.getAncestors());
+        java.util.Collections.sort(ancestorAndNode2, new TreeIndexComparator());
+
 		for (int i = 0; i < splitNode1.length; i++){
 			if (!splitNode1[i].equals(splitNode2[i])){
 				// take the last equal ancestor and compare the sortindex
 				if (lastEqualAncestorTreeIndex != null){
 					TaxonNode lastEqualTreeIndexAncestorNode1 = null;
 					TaxonNode lastEqualTreeIndexAncestorNode2 = null;
-					while (ancestorsNode1.hasNext()){
-						TaxonNode next1 = ancestorsNode1.next();
-						String[] split = next1.treeIndex().split("#");
-						if (split[split.length-1].equals(lastEqualAncestorTreeIndex)){
-							lastEqualTreeIndexAncestorNode1 = next1;
+					for (TaxonNode next1 :ancestorAndNode){
+
+						if (next1.treeIndex().equals(lastEqualAncestorTreeIndex+"#"+splitNode1[i]+ "#") ){
+						    lastEqualTreeIndexAncestorNode1 = next1;
 						}
 					}
-					while (ancestorsNode2.hasNext()){
-						TaxonNode next2 = ancestorsNode2.next();
-						String[] split = next2.treeIndex().split("#");
-						if (split[split.length-1].equals(lastEqualAncestorTreeIndex)){
-							lastEqualTreeIndexAncestorNode2 = next2;
+					for (TaxonNode next2 :ancestorAndNode2){
+
+						if (next2.treeIndex().equals(lastEqualAncestorTreeIndex+"#"+splitNode2[i]+ "#")){
+						    lastEqualTreeIndexAncestorNode2 = next2;
 						}
 					}
 					return lastEqualTreeIndexAncestorNode1.getSortIndex().compareTo(lastEqualTreeIndexAncestorNode2.getSortIndex());
 				}
 			}
-			lastEqualAncestorTreeIndex = splitNode1[i];
+			if (!splitNode1[i].equals("")){
+			    lastEqualAncestorTreeIndex = lastEqualAncestorTreeIndex+"#"+splitNode1[i];
+			}
 		}
 		return 0;
+	}
+
+	private final class TreeIndexComparator implements Comparator<TaxonNode> {
+	    @Override
+	    public int compare(TaxonNode node1,TaxonNode node2)
+	    {
+	        return node1.treeIndex().compareTo(node2.treeIndex());
+	    }
 	}
 }
