47da1bc95b0c225ce9901aae29e672bf26b0f1ea
Katja Luther <k.luther@bgbm.org>
2016-09-04 21:12:13 +0200

    extend excel import to cover simple tropicos data

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/excel/taxa/TaxonExcelImportState.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/excel/taxa/TaxonExcelImportState.java
@@ -1,9 +1,9 @@
 // $Id$
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -20,6 +20,8 @@ import org.apache.log4j.Logger;
 import eu.etaxonomy.cdm.io.excel.common.ExcelImportConfiguratorBase;
 import eu.etaxonomy.cdm.io.excel.common.ExcelImportState;
 import eu.etaxonomy.cdm.model.agent.TeamOrPersonBase;
+import eu.etaxonomy.cdm.model.taxon.Classification;
+import eu.etaxonomy.cdm.model.taxon.Taxon;
 import eu.etaxonomy.cdm.model.taxon.TaxonBase;
 
 /**
@@ -30,14 +32,17 @@ import eu.etaxonomy.cdm.model.taxon.TaxonBase;
 public class TaxonExcelImportState extends ExcelImportState<ExcelImportConfiguratorBase, NormalExplicitRow>{
 	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(TaxonExcelImportState.class);
-	
+
     /** Already processed authors */
 	private Set<String> authors = new HashSet<String>();
 
 	private Map<Integer, TaxonBase> taxonMap= new HashMap<Integer, TaxonBase>();
 	private Map<String, TeamOrPersonBase> authorMap= new HashMap<String, TeamOrPersonBase>();
-	
-	
+	private Taxon parent;
+	private Classification classification;
+
+
+
 	public TaxonExcelImportState(ExcelImportConfiguratorBase config) {
 		super(config);
 	}
@@ -56,6 +61,13 @@ public class TaxonExcelImportState extends ExcelImportState<ExcelImportConfigura
 		this.authors = authors;
 	}
 
+	public Taxon getParent(){
+	    return parent;
+	}
+
+	public void setParent(Taxon parent){
+	    this.parent = parent;
+    }
 
 	/**
 	 * @param parentId
@@ -71,9 +83,25 @@ public class TaxonExcelImportState extends ExcelImportState<ExcelImportConfigura
 	 * @param taxon
 	 */
 	public void putTaxon(Integer taxonId, TaxonBase taxonBase) {
-		taxonMap.put(taxonId, taxonBase);
+
+
+	    taxonMap.put(taxonId, taxonBase);
 	}
 
-	
+    /**
+     * @return the classification
+     */
+    public Classification getClassification() {
+        return classification;
+    }
+
+    /**
+     * @param classification the classification to set
+     */
+    public void setClassification(Classification classification) {
+        this.classification = classification;
+    }
+
+
 
 }
