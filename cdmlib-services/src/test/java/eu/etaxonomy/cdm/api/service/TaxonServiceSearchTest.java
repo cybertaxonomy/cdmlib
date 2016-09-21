59dbcede8659c9ac9fb00c12fc7b586eed2bbd3b
Andreas Müller <a.mueller@bgbm.org>
2016-09-21 17:47:07 +0200

    cleanup

--- cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/TaxonServiceSearchTest.java
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/TaxonServiceSearchTest.java
@@ -1268,7 +1268,7 @@ public class TaxonServiceSearchTest extends CdmTransactionalIntegrationTest {
 
         createRandomTaxonWithCommonName(NUM_OF_NEW_RADOM_ENTITIES);
 
-        IFindTaxaAndNamesConfigurator configurator = new FindTaxaAndNamesConfiguratorImpl();
+        IFindTaxaAndNamesConfigurator configurator = new FindTaxaAndNamesConfiguratorImpl<>();
         configurator.setTitleSearchString("Wei"+UTF8.SHARP_S+"%");
         configurator.setMatchMode(MatchMode.BEGINNING);
         configurator.setDoTaxa(false);
