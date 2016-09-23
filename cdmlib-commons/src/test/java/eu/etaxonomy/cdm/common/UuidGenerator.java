7dde073194a690174f4b1f941a70d1278c04a1a3
Andreas Müller <a.mueller@bgbm.org>
2016-09-23 11:00:49 +0200

    Cleanup

--- cdmlib-commons/src/test/java/eu/etaxonomy/cdm/common/UuidGenerator.java
+++ cdmlib-commons/src/test/java/eu/etaxonomy/cdm/common/UuidGenerator.java
@@ -1,9 +1,9 @@
 // $Id$
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -14,12 +14,12 @@ import java.util.UUID;
 import org.apache.log4j.Logger;
 
 /**
- * 
+ *
  * @author a.mueller
  */
 public class UuidGenerator {
 	private static final Logger logger = Logger.getLogger(UuidGenerator.class);
-	
+
 	/**
 	 * @param args
 	 */
@@ -28,6 +28,7 @@ public class UuidGenerator {
 		for (int i = 0; i < 100; i++){
 			System.out.println(UUID.randomUUID());
 		}
+
 	}
 
 }
