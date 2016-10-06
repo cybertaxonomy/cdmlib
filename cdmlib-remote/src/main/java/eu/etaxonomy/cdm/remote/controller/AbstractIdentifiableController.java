b4b7bfe8dd6aa6b0eba0ac8f4dce2fff2d20e2c0
Andreas Müller <a.mueller@bgbm.org>
2016-10-06 14:51:36 +0200

    fix #6122 add titleCache service to remote

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/AbstractIdentifiableController.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/AbstractIdentifiableController.java
@@ -9,8 +9,18 @@
 */
 package eu.etaxonomy.cdm.remote.controller;
 
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import eu.etaxonomy.cdm.api.service.IIdentifiableEntityService;
 import eu.etaxonomy.cdm.model.common.IdentifiableEntity;
@@ -31,5 +41,31 @@ public abstract class AbstractIdentifiableController <T extends IdentifiableEnti
         super.initBinder(binder);
         binder.registerCustomEditor(MatchMode.class, new MatchModePropertyEditor());
     }
+
+    /**
+     * List identifiable entities by markers
+     *
+     * @param type
+     * @param request
+     * @param response
+     * @return
+     * @see AbstractIdentifiableListController#doFindByIdentifier(Class, String, String, Integer, Integer, MatchMode, Boolean, HttpServletRequest, HttpServletResponse)
+     * @throws IOException
+     */
+    @RequestMapping(method = RequestMethod.GET, value={"titleCache"})
+    public String doGetTitleCache(
+            @PathVariable("uuid") UUID uuid,
+            @RequestParam(value = "refresh", defaultValue= "false", required = false) Boolean refresh,
+            HttpServletRequest request,
+            HttpServletResponse response
+            )
+            throws IOException {
+
+        if (logger.isDebugEnabled()){logger.info("doGetTitleCache  : " + request.getRequestURI() + "?" + request.getQueryString() );}
+
+        return service.getTitleCache(uuid, refresh);
+    }
+
+
 
 }
