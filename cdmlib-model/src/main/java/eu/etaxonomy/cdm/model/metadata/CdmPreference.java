c67d01cbc536bbe5ecc73878b561bdf093c7eae4
Andreas Müller <a.mueller@bgbm.org>
2016-11-11 11:23:46 +0100

    ref #5970 remove transient from allowOverride

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/metadata/CdmPreference.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/metadata/CdmPreference.java
@@ -15,7 +15,6 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Transient;
 
 
 /**
@@ -80,7 +79,6 @@ public final class CdmPreference implements Serializable {
 
     //if false, the preference should not be overridden by local preferences,
 	//if false local preferences override database preferences
-    @Transient  //preliminary until model change implemented
     private boolean allowOverride = false;
 
     @Embeddable
@@ -222,9 +220,5 @@ public final class CdmPreference implements Serializable {
     public void setAllowOverride(boolean allowOverride) {
         this.allowOverride = allowOverride;
     }
-
-
-
-
 
 }
