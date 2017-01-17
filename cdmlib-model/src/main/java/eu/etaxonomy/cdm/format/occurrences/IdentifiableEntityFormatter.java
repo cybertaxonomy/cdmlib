733faa22d00562db5b2cb520a6ff20fd6da5b0bd
Patrick Plitzner <p.plitzner@bgbm.org>
2017-01-17 17:42:53 +0100

    Initialise sample designation to format keys for IdentifiableEntity

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/format/occurrences/IdentifiableEntityFormatter.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/format/occurrences/IdentifiableEntityFormatter.java
@@ -8,6 +8,12 @@
 */
 package eu.etaxonomy.cdm.format.occurrences;
 
+import java.util.List;
+import java.util.UUID;
+
+import eu.etaxonomy.cdm.model.common.IdentifiableEntity;
+import eu.etaxonomy.cdm.model.common.Identifier;
+
 
 /**
  * @author pplitzner
@@ -23,18 +29,18 @@ public class IdentifiableEntityFormatter extends AbstractCdmFormatter {
     @Override
     protected void initFormatKeys(Object object) {
         super.initFormatKeys(object);
-//        IdentifiableEntity identifiableEntity = (IdentifiableEntity)object;
-//        List<Identifier> identifiers = identifiableEntity.getIdentifiers();
-//        String identifierString = null;
-//        for (Identifier identifier : identifiers) {
-//            if(identifier.getType()!=null && identifier.getType().equals(DerivateLabelProvider.getSampleDesignationTerm())){
-//                //first sample designation is the current
-//                identifierString = identifier.toString();
-//            }
-//        }
-//        if(identifierString!=null){
-//            formatKeyMap.put(FormatKey.FIELD_NUMBER, identifierString);
-//        }
+        IdentifiableEntity identifiableEntity = (IdentifiableEntity)object;
+        List<Identifier> identifiers = identifiableEntity.getIdentifiers();
+        String identifierString = null;
+        for (Identifier identifier : identifiers) {
+            if(identifier.getType()!=null && identifier.getType().getUuid().equals(UUID.fromString("fadeba12-1be3-4bc7-9ff5-361b088d86fc"))){
+                identifierString = identifier.getIdentifier();
+                break;
+            }
+        }
+        if(identifierString!=null){
+            formatKeyMap.put(FormatKey.SAMPLE_DESIGNATION, identifierString);
+        }
     }
 
 }
