3786c4c9973a9249a6680a31501dfea67c47309d
Andreas Müller <a.mueller@bgbm.org>
2017-01-26 16:57:20 +0100

    fix #6369 add interface for all names

---
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/name/ITaxonName.java
@@ -0,0 +1,18 @@
+/**
+* Copyright (C) 2017 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.model.name;
+
+/**
+ * @author a.mueller
+ * @date 26.01.2017
+ *
+ */
+public interface ITaxonName extends IBotanicalName, ICultivarPlantName, IBacterialName, IZoologicalName {
+
+}
