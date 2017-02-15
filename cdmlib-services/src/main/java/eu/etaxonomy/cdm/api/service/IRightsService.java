bce902735914907679ff6426197a9f5365fa702b
Katja Luther <k.luther@bgbm.org>
2017-02-15 15:19:21 +0100

    ref  #6262: create dao and service for rights

---
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/IRightsService.java
@@ -0,0 +1,20 @@
+/**
+* Copyright (C) 2017 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.api.service;
+
+import eu.etaxonomy.cdm.model.media.Rights;
+
+/**
+ * @author k.luther
+ * @date 15.02.2017
+ *
+ */
+public interface IRightsService extends IAnnotatableService<Rights>{
+
+}
---
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/RightsServiceImpl.java
@@ -0,0 +1,42 @@
+/**
+* Copyright (C) 2017 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.api.service;
+
+import org.apache.log4j.Logger;
+
+import eu.etaxonomy.cdm.model.media.Rights;
+import eu.etaxonomy.cdm.persistence.dao.common.IRightsDao;
+
+/**
+ * @author k.luther
+ * @date 15.02.2017
+ *
+ */
+public class RightsServiceImpl extends AnnotatableServiceBase<Rights, IRightsDao> implements IRightsService {
+    private static final Logger logger = Logger.getLogger(RightsServiceImpl.class);
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void setDao(IRightsDao dao) {
+        assert dao != null;
+        this.dao = dao;
+
+    }
+
+    /**
+     * Constructor
+     */
+    public RightsServiceImpl(){
+        if (logger.isDebugEnabled()) { logger.debug("Load RightsService Bean"); }
+    }
+
+
+
+}
