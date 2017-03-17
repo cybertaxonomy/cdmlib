d688ff5c263efb461522f478823e7c4135881d95
Katja Luther <k.luther@bgbm.org>
2017-03-17 12:58:14 +0100

    ref #6314: start implementing handleName in outputmodel export

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/outputmodel/OutputModelClassificationExport.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/outputmodel/OutputModelClassificationExport.java
@@ -8,15 +8,28 @@
 */
 package eu.etaxonomy.cdm.io.outputmodel;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
+import eu.etaxonomy.cdm.common.CdmUtils;
 import eu.etaxonomy.cdm.io.common.CdmExportBase;
 import eu.etaxonomy.cdm.io.common.ICdmExport;
 import eu.etaxonomy.cdm.io.common.mapping.out.IExportTransformer;
+import eu.etaxonomy.cdm.model.agent.Person;
+import eu.etaxonomy.cdm.model.agent.Team;
+import eu.etaxonomy.cdm.model.agent.TeamOrPersonBase;
 import eu.etaxonomy.cdm.model.common.ICdmBase;
 import eu.etaxonomy.cdm.model.common.IIdentifiableEntity;
-import eu.etaxonomy.cdm.model.name.ITaxonNameBase;
+import eu.etaxonomy.cdm.model.description.DescriptionElementBase;
+import eu.etaxonomy.cdm.model.description.Feature;
+import eu.etaxonomy.cdm.model.description.TaxonDescription;
+import eu.etaxonomy.cdm.model.media.Media;
+import eu.etaxonomy.cdm.model.media.MediaRepresentation;
+import eu.etaxonomy.cdm.model.media.MediaRepresentationPart;
 import eu.etaxonomy.cdm.model.name.Rank;
+import eu.etaxonomy.cdm.model.name.TaxonNameBase;
 import eu.etaxonomy.cdm.model.reference.Reference;
 import eu.etaxonomy.cdm.model.taxon.Classification;
 import eu.etaxonomy.cdm.model.taxon.Synonym;
@@ -63,6 +76,7 @@ public class OutputModelClassificationExport
                 }
             }
         }
+      //  state.getProcessor().createFinalResult(ExportResult)
     }
 
     /**
@@ -71,7 +85,7 @@ public class OutputModelClassificationExport
      */
     private void handleTaxon(OutputModelExportState state, TaxonNode taxonNode) {
         Taxon taxon = taxonNode.getTaxon();
-        ITaxonNameBase name = taxon.getName();
+        TaxonNameBase name = taxon.getName();
         handleName(state, name);
         for (Synonym syn : taxon.getSynonyms()){
             handleSynonym(state, syn);
@@ -121,7 +135,7 @@ public class OutputModelClassificationExport
      * @param syn
      */
     private void handleSynonym(OutputModelExportState state, Synonym syn) {
-       ITaxonNameBase name = syn.getName();
+       TaxonNameBase name = syn.getName();
        handleName(state, name);
 
        OutputModelTable table = OutputModelTable.SYNONYM;
@@ -140,9 +154,176 @@ public class OutputModelClassificationExport
      * @param state
      * @param name
      */
-    private void handleName(OutputModelExportState state, ITaxonNameBase name) {
+    private void handleName(OutputModelExportState state, TaxonNameBase name) {
         Rank rank = name.getRank();
+        OutputModelTable table = OutputModelTable.SCIENTIFIC_NAME;
+        String[] csvLine = new String[table.getSize()];
 
+        csvLine[table.getIndex(OutputModelTable.NAME_ID)] = getId(state, name);
+        csvLine[table.getIndex(OutputModelTable.RANK)] = getTitleCache(rank);
+        csvLine[table.getIndex(OutputModelTable.RANK_SEQUENCE)] = String.valueOf(rank.getOrderIndex());
+        csvLine[table.getIndex(OutputModelTable.FULL_NAME_WITH_AUTHORS)] = getTropicosTitleCache(name);
+        csvLine[table.getIndex(OutputModelTable.FULL_NAME_NO_AUTHORS)] = name.getNameCache();
+        csvLine[table.getIndex(OutputModelTable.GENUS_UNINOMIAL)] = name.getGenusOrUninomial();
+        csvLine[table.getIndex(OutputModelTable.INFRAGENERIC_EPITHET)] = name.getInfraGenericEpithet();
+        csvLine[table.getIndex(OutputModelTable.SPECIFIC_EPITHET)] = name.getSpecificEpithet();
+        csvLine[table.getIndex(OutputModelTable.INFRASPECIFIC_EPITHET)] = name.getInfraSpecificEpithet();
+        csvLine[table.getIndex(OutputModelTable.BAS_AUTHORTEAM_FK)] = getId(state,name.getBasionymAuthorship());
+        csvLine[table.getIndex(OutputModelTable.BAS_EX_AUTHORTEAM_FK)] = getId(state, name.getExBasionymAuthorship());
+        csvLine[table.getIndex(OutputModelTable.COMB_AUTHORTEAM_FK)] = getId(state,name.getCombinationAuthorship());
+        csvLine[table.getIndex(OutputModelTable.COMB_EX_AUTHORTEAM_FK)] = getId(state, name.getExCombinationAuthorship());
+        csvLine[table.getIndex(OutputModelTable.AUTHOR_TEAM_STRING)] = name.getAuthorshipCache();
+        Reference nomRef = (Reference)name.getNomenclaturalReference();
+        if (nomRef != null){
+            csvLine[table.getIndex(OutputModelTable.PUBLICATION_TYPE)] = nomRef.getType().name();
+        }else{
+            csvLine[table.getIndex(OutputModelTable.PUBLICATION_TYPE)] = "";
+        }
+        if (nomRef.getInReference() != null){
+            Reference inReference = nomRef.getInReference();
+            csvLine[table.getIndex(OutputModelTable.ABBREV_TITLE)] = CdmUtils.Nz(inReference.getAbbrevTitle());
+            csvLine[table.getIndex(OutputModelTable.FULL_TITLE)] = CdmUtils.Nz(inReference.getTitle());
+
+            TeamOrPersonBase author = inReference.getAuthorship();
+            if (author != null){
+                csvLine[table.getIndex(OutputModelTable.ABBREV_REF_AUTHOR)] = CdmUtils.Nz(author.getNomenclaturalTitle());
+                csvLine[table.getIndex(OutputModelTable.ABBREV_REF_AUTHOR)] = CdmUtils.Nz(author.getTitleCache());
+            }else{
+                csvLine[table.getIndex(OutputModelTable.ABBREV_REF_AUTHOR)] = "";
+                csvLine[table.getIndex(OutputModelTable.ABBREV_REF_AUTHOR)] = "";
+            }
+        }else{
+            csvLine[table.getIndex(OutputModelTable.ABBREV_TITLE)] = "";
+            csvLine[table.getIndex(OutputModelTable.FULL_TITLE)] = "";
+            csvLine[table.getIndex(OutputModelTable.ABBREV_REF_AUTHOR)]= "";
+            csvLine[table.getIndex(OutputModelTable.ABBREV_REF_AUTHOR)] = "";
+            csvLine[table.getIndex(OutputModelTable.ABBREV_REF_AUTHOR)] = "";
+        }
+        if (state.getActualTaxonBase() instanceof Taxon ){
+            Taxon actualTaxon = (Taxon)state.getActualTaxonBase();
+            Set<TaxonDescription> descriptions = actualTaxon.getDescriptions();
+            String protologueUriString = "";
+            for (TaxonDescription description : descriptions){
+                if (!description.getElements().isEmpty()){
+                    for (DescriptionElementBase element : description.getElements()){
+                        if (element.getFeature().equals(Feature.PROTOLOGUE())){
+                            if (!element.getMedia().isEmpty()){
+                                List<Media> media = element.getMedia();
+                                for (Media mediaElement: media){
+                                    Iterator<MediaRepresentation> it =  mediaElement.getRepresentations().iterator();
+                                    while(it.hasNext()){
+                                        MediaRepresentation rep = it.next();
+                                        List<MediaRepresentationPart> parts = rep.getParts();
+                                        for (MediaRepresentationPart part: parts){
+                                            protologueUriString += ", " +part.getUri().toString();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }else{
+            csvLine[table.getIndex(OutputModelTable.PROTOLOGUE_URI)] = "";
+        }
+
+        state.getProcessor().put(table, name, csvLine);
+/*
+ * EditName_ID
+Tropicos_ID
+IPNI_ID
+
+
+InfragenericRank
+
+
+InfraspecificRank
+Collation
+Volume (Issue)
+Detail
+DatePublished
+YearPublished
+TitlePageYear
+ProtologueURI
+NomenclaturalStatus
+NomenclaturalStatusAbbreviation
+HomotypicGroup_Fk
+HomotypicGroupSequenceNumber
+TypeString
+
+ *
+ */
+    }
+
+    /**
+     * @param name
+     * @return
+     */
+    private String getTropicosTitleCache(TaxonNameBase name) {
+        String basionymStart = "(";
+        String basionymEnd = ") ";
+        String exAuthorSeperator = " ex ";
+        TeamOrPersonBase combinationAuthor = name.getCombinationAuthorship();
+        TeamOrPersonBase exCombinationAuthor = name.getExCombinationAuthorship();
+        TeamOrPersonBase basionymAuthor = name.getBasionymAuthorship();
+        TeamOrPersonBase exBasionymAuthor = name.getExBasionymAuthorship();
+
+        String combinationAuthorString = null;
+        if (combinationAuthor instanceof Team){
+            for (Person teamMember:((Team)combinationAuthor).getTeamMembers()){
+                combinationAuthorString = createTropicosAuthorString(teamMember);
+            }
+        }else{
+            combinationAuthorString = createTropicosAuthorString((Person)combinationAuthor);
+        }
+        String exCombinationAuthorString = null;
+        if (exCombinationAuthor instanceof Team){
+            for (Person teamMember:((Team)exCombinationAuthor).getTeamMembers()){
+                exCombinationAuthorString = createTropicosAuthorString(teamMember);
+            }
+        }else{
+            exCombinationAuthorString = createTropicosAuthorString((Person)exCombinationAuthor);
+        }
+
+        String basionymAuthorString = null;
+        if (basionymAuthor instanceof Team){
+            for (Person teamMember:((Team)basionymAuthor).getTeamMembers()){
+                basionymAuthorString = createTropicosAuthorString(teamMember);
+            }
+        }else{
+            basionymAuthorString = createTropicosAuthorString((Person)basionymAuthor);
+        }
+
+        String exBasionymAuthorString = null;
+        if (exBasionymAuthor instanceof Team){
+            for (Person teamMember:((Team)exBasionymAuthor).getTeamMembers()){
+                exBasionymAuthorString = createTropicosAuthorString(teamMember);
+            }
+        }else{
+            exBasionymAuthorString = createTropicosAuthorString((Person)exBasionymAuthor);
+        }
+        String completeAuthorString =  basionymStart + CdmUtils.Nz(exBasionymAuthorString) +exAuthorSeperator + CdmUtils.Nz(exBasionymAuthorString) + basionymEnd + CdmUtils.Nz(exCombinationAuthorString) + exAuthorSeperator + CdmUtils.Nz(combinationAuthorString);
+
+        return completeAuthorString;
+    }
+
+    /**
+     * @param teamMember
+     */
+    private String createTropicosAuthorString(Person teamMember) {
+        String nomAuthorString = "";
+        String[] splittedAuthorString = teamMember.getNomenclaturalTitle().split("\\s");
+        int index = 0;
+        for (String split: splittedAuthorString){
+            if ( index < splittedAuthorString.length-1 && (split.length()==1 || split.endsWith("."))){
+                nomAuthorString += split;
+            }else{
+                nomAuthorString = nomAuthorString +" "+ split;
+            }
+            index++;
+        }
+        return nomAuthorString;
     }
 
     /**
--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/outputmodel/OutputModelExportState.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/outputmodel/OutputModelExportState.java
@@ -12,6 +12,7 @@ import eu.etaxonomy.cdm.io.common.ExportResult;
 import eu.etaxonomy.cdm.io.common.ExportResult.ExportResultState;
 import eu.etaxonomy.cdm.io.common.ExportStateBase;
 import eu.etaxonomy.cdm.io.common.mapping.out.IExportTransformer;
+import eu.etaxonomy.cdm.model.taxon.TaxonBase;
 
 /**
  * @author k.luther
@@ -20,26 +21,31 @@ import eu.etaxonomy.cdm.io.common.mapping.out.IExportTransformer;
  */
 public class OutputModelExportState extends ExportStateBase<OutputModelConfigurator, IExportTransformer>{
 
-    private ExportResult result = ExportResult.NewInstance();
+    private ExportResult result;
 
     private OutputModelResultProcessor processor = new OutputModelResultProcessor(this);
+
+    private TaxonBase actualTaxonBase;
 
     /**
      * @param config
      */
     protected OutputModelExportState(OutputModelConfigurator config) {
         super(config);
+        result = ExportResult.NewInstance(config.getResultType());
 
     }
 
     /**
      * @return the result
      */
+    @Override
     public ExportResult getResult() {return result;}
 
     /**
      * @param result the result to set
      */
+    @Override
     public void setResult(ExportResult result) {this.result = result;}
 
     /**
@@ -55,6 +61,10 @@ public class OutputModelExportState extends ExportStateBase<OutputModelConfigura
     public OutputModelResultProcessor getProcessor() {
         return processor;
     }
+
+    public void setActualTaxonBase(TaxonBase actualTaxonBase){ this.actualTaxonBase = actualTaxonBase;}
+
+    public TaxonBase getActualTaxonBase() {return actualTaxonBase;}
 
 
 
