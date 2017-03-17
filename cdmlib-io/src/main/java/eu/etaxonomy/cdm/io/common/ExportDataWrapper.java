c5e92bc3e4884f8fc50030826de445060c24ba41
Katja Luther <k.luther@bgbm.org>
2017-03-17 12:59:41 +0100

    ref #6519: implementing wrapper for export data

---
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/common/ExportDataWrapper.java
@@ -0,0 +1,69 @@
+/**
+* Copyright (C) 2017 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.io.common;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author k.luther
+ * @date 17.03.2017
+ *
+ */
+public class ExportDataWrapper<T> implements Serializable{
+
+    private static final long serialVersionUID = 4500184563547082579L;
+
+    T exportData;
+    ExportResultType type;
+
+    private ExportDataWrapper(){
+
+    }
+
+    public static final ExportDataWrapper<List<byte[]>> NewListByteArrayInstance(){
+        ExportDataWrapper<List<byte[]>> result = new ExportDataWrapper<>();
+        result.type = ExportResultType.LIST_BYTE_ARRAY;
+        return result;
+    }
+
+    public static final ExportDataWrapper<Map<String,byte[]>> NewMapByteArrayInstance(){
+        ExportDataWrapper<Map<String, byte[]>> result = new ExportDataWrapper<>();
+        result.type = ExportResultType.MAP_BYTE_ARRAY;
+        return result;
+    }
+
+    public static final ExportDataWrapper<byte[]> NewByteArrayInstance(){
+        ExportDataWrapper<byte[]> result = new ExportDataWrapper<>();
+        result.type = ExportResultType.BYTE_ARRAY;
+        return result;
+    }
+
+    public void setValue(T value){this.exportData = value;}
+
+    public T getExportData(){return this.exportData;}
+
+    public void addExportData(byte[] data){
+        if (type.equals(ExportResultType.BYTE_ARRAY)){
+            exportData = (T)data;
+        } else if (type.equals(ExportResultType.LIST_BYTE_ARRAY)){
+            ((List<byte[]>)exportData).add(data);
+        }
+
+    }
+
+    public void addExportData(byte[] data, String key){
+        if (type.equals(ExportResultType.MAP_BYTE_ARRAY)){
+            ((Map<String, byte[]>)exportData).put(key,data);
+        }
+
+    }
+
+}
---
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/common/ExportResultType.java
@@ -0,0 +1,23 @@
+/**
+* Copyright (C) 2017 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.io.common;
+
+/**
+ * @author k.luther
+ * @date 17.03.2017
+ *
+ */
+public enum ExportResultType {
+    BYTE_ARRAY,
+    LIST_BYTE_ARRAY,
+    MAP_BYTE_ARRAY
+    ;
+
+
+}
