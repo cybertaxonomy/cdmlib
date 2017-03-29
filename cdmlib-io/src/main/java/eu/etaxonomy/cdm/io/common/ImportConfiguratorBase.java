776d80b28fadaf8449f65dfe1f881164df463aad
unknown <a.mueller@BGBM-PESIHPC.bgbm.fu-berlin.de>
2017-03-29 17:01:46 +0200

    add featureTreeTitle and uuidFeatureTree to import configurator base

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/common/ImportConfiguratorBase.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/common/ImportConfiguratorBase.java
@@ -29,13 +29,16 @@ import eu.etaxonomy.cdm.model.reference.Reference;
 /**
  * @author a.mueller
  * @created 20.06.2008
- * @param <STATE>
+ * @param <STATE> the import state
  */
 @Component
-public abstract class ImportConfiguratorBase<STATE extends ImportStateBase, SOURCE> extends IoConfiguratorBase implements IImportConfigurator{
+public abstract class ImportConfiguratorBase<STATE extends ImportStateBase, SOURCE>
+        extends IoConfiguratorBase
+        implements IImportConfigurator{
 
+    private static final long serialVersionUID = 7223140465020160905L;
 
-	//check
+    //check
 	private CHECK check = CHECK.CHECK_AND_IMPORT;
 
 	//editor
@@ -53,10 +56,14 @@ public abstract class ImportConfiguratorBase<STATE extends ImportStateBase, SOUR
 	//nullValues
 	private boolean ignoreNull = false;
 
+	private UUID uuidFeatureTree;
+
+	private String featureTreeTitle;
+
 	//Nomenclatural Code
 	private NomenclaturalCode nomenclaturalCode = null;
 
-	private Map<Integer, Feature>  featureMap = new HashMap<Integer, Feature>();
+	private Map<Integer, Feature>  featureMap = new HashMap<>();
 
 	 /* The classification name for the first classification.
 	  * Needs only to be defined if the import does not handle the naming
@@ -392,6 +399,20 @@ public abstract class ImportConfiguratorBase<STATE extends ImportStateBase, SOUR
         askUserForHelp=interaction;
     }
 
+    public UUID getUuidFeatureTree() {
+        return uuidFeatureTree;
+    }
 
+    public void setUuidFeatureTree(UUID uuidFeatureTree) {
+        this.uuidFeatureTree = uuidFeatureTree;
+    }
+
+    public String getFeatureTreeTitle() {
+        return featureTreeTitle;
+    }
+
+    public void setFeatureTreeTitle(String featureTreeTitle) {
+        this.featureTreeTitle = featureTreeTitle;
+    }
 
 }
