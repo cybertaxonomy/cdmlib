00da181d3506de7e36636fb89741ed949a5e00bb
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2017-09-21 15:29:50 +0200

    ref #6964 base implementation of the RegistrationController

---
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/RegistrationController.java
@@ -0,0 +1,120 @@
+/**
+* Copyright (C) 2007 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+
+package eu.etaxonomy.cdm.remote.controller;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import eu.etaxonomy.cdm.api.service.IRegistrationService;
+import eu.etaxonomy.cdm.model.name.Registration;
+import eu.etaxonomy.cdm.model.name.RegistrationStatus;
+import io.swagger.annotations.Api;
+
+/**
+ * TODO write controller documentation
+ *
+ * @author a.kohlbecker
+ * @date 24.03.2009
+ */
+
+@Controller
+@Api("registration")
+@RequestMapping(value = {"/registration/{uuid}", "/registration/{localID}"})
+public class RegistrationController extends BaseController<Registration, IRegistrationService>
+{
+
+    public RegistrationController(){
+        setInitializationStrategy(Arrays.asList(new String[]{
+                "$",
+                "name.$",
+                "typeDesignations.$"
+             }));
+    }
+
+    /* (non-Javadoc)
+     * @see eu.etaxonomy.cdm.remote.controller.GenericController#setService(eu.etaxonomy.cdm.api.service.IService)
+     */
+    @Autowired
+    @Override
+    public void setService(IRegistrationService service) {
+        this.service = service;
+    }
+
+    @Override
+    public Registration doGet(@PathVariable("uuid") UUID uuid,
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+
+        Registration reg = super.doGet(uuid, request, response);
+        if(reg != null){
+            if(userIsAutheticated() && userIsAnnonymous() && !reg.getStatus().equals(RegistrationStatus.PUBLISHED)) {
+                // completely hide the fact that there is a registration
+                HttpStatusMessage.create("No such Registration", HttpServletResponse.SC_NO_CONTENT).send(response);
+            }
+        }
+        return reg;
+    }
+
+ /*
+    @Override
+    public Registration doGetbyLocalId(@PathVariable("localID") Integer localID,
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+
+        Registration reg = super.doGet(localID, request, response);
+        if(reg != null){
+            if(userIsAutheticated() && userIsAnnonymous() && !reg.getStatus().equals(RegistrationStatus.PUBLISHED)) {
+                // completely hide the fact that there is a registration
+                HttpStatusMessage.create("No such Registration", HttpServletResponse.SC_NO_CONTENT);
+            }
+        }
+        return reg;
+    }
+*/
+
+    // =============================================================
+    // TODO move into userHelper Class? See Vaddin CdmUserHelper!
+    /**
+     * @return
+     */
+    private Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+
+    public boolean userIsAutheticated() {
+        Authentication authentication = getAuthentication();
+        if(authentication != null){
+            return authentication.isAuthenticated();
+        }
+        return false;
+    }
+
+
+    public boolean userIsAnnonymous() {
+        Authentication authentication = getAuthentication();
+        return authentication != null
+                && authentication.isAuthenticated()
+                && authentication instanceof AnonymousAuthenticationToken;
+    }
+    // =============================================================
+}
