a693f6d7b748a05b86b46ca411b5cea9a877208a
Andreas Müller <a.mueller@bgbm.org>
2017-10-20 10:41:00 +0200

    ref #7028 added preference predicate in cdmlib

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/metadata/PreferencePredicate.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/metadata/PreferencePredicate.java
@@ -31,8 +31,9 @@ public enum PreferencePredicate  implements IEnumTerm<PreferencePredicate>{
 	ShowTaxonAssociations(UUID.fromString("849c24f9-b62b-4f70-b0a0-1b02182b3433"), "ShowTaxonAssociations", "editor.STA"),
 	ShowLifeForm(UUID.fromString("85870e7d-a6a3-4c9b-97d6-eb27e6516860"), "ShowLifeForm", "editor.SLF"),
 	AbcdImportConfig(UUID.fromString("65380375-d041-458c-8275-c36cdc1f34df"), "AbcdImportConfig", "import.ABCD"),
-	BioCaseProvider(UUID.fromString("bd22c85c-f4e8-4771-ae7b-5750868762c4"), "BioCaseProvider", "import.BP")
-	;
+	BioCaseProvider(UUID.fromString("bd22c85c-f4e8-4771-ae7b-5750868762c4"), "BioCaseProvider", "import.BP"),
+	DistributionStatusVaadin(UUID.fromString(""), "Distribution Status Vaadin", "vaadin.distribution.statuslist")
+    ;
 
 
     private static final String KEY_PREFIX = "model.";
