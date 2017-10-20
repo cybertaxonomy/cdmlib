04ea849a1a6b5f4b1c2ecf09f5d4e9aa7e31337a
Andreas Müller <a.mueller@bgbm.org>
2017-10-20 10:35:37 +0200

    cleanup

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/excel/distribution/DistributionImportConfigurator.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/excel/distribution/DistributionImportConfigurator.java
@@ -14,19 +14,19 @@ import org.apache.log4j.Logger;
 
 import eu.etaxonomy.cdm.database.ICdmDataSource;
 import eu.etaxonomy.cdm.io.common.IImportConfigurator;
-import eu.etaxonomy.cdm.io.excel.common.ExcelImportState;
 import eu.etaxonomy.cdm.io.excel.common.ExcelImportConfiguratorBase;
+import eu.etaxonomy.cdm.io.excel.common.ExcelImportState;
 import eu.etaxonomy.cdm.model.reference.Reference;
 import eu.etaxonomy.cdm.model.reference.ReferenceFactory;
 
 /**
  * @author a.babadshanjan
  * @created 10.11.2008
- * @version 1.0
  */
 public class DistributionImportConfigurator extends ExcelImportConfiguratorBase implements IImportConfigurator {
 
-	private static final Logger logger = Logger.getLogger(DistributionImportConfigurator.class);
+    private static final long serialVersionUID = -4283236727158858201L;
+    private static final Logger logger = Logger.getLogger(DistributionImportConfigurator.class);
 
 	@Override
     @SuppressWarnings("unchecked")
@@ -34,7 +34,7 @@ public class DistributionImportConfigurator extends ExcelImportConfiguratorBase 
 		ioClassList = new Class[] {
 				DistributionImport.class
 		};
-	};
+	}
 
 	public static DistributionImportConfigurator NewInstance(URI uri, ICdmDataSource destination){
 		return new DistributionImportConfigurator(uri, destination);
@@ -50,20 +50,11 @@ public class DistributionImportConfigurator extends ExcelImportConfiguratorBase 
 	}
 
 
-
-
-	/* (non-Javadoc)
-	 * @see eu.etaxonomy.cdm.io.common.IImportConfigurator#getNewState()
-	 */
 	@Override
     public ExcelImportState getNewState() {
 		return new ExcelImportState(this);
 	}
 
-
-	/* (non-Javadoc)
-	 * @see eu.etaxonomy.cdm.io.common.ImportConfiguratorBase#getSourceReference()
-	 */
 	@Override
 	public Reference getSourceReference() {
 		//TODO
