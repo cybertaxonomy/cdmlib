2cc98a0b430091ad78e671f7d26cd0f7e3805914
Andreas Müller <a.mueller@bgbm.org>
2017-11-21 10:25:22 +0100

    ref #6965 move CsvImportBase to "in" package

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/csv/in/CsvImportConfiguratorBase.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/csv/in/CsvImportConfiguratorBase.java
@@ -6,7 +6,7 @@
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
-package eu.etaxonomy.cdm.io.csv;
+package eu.etaxonomy.cdm.io.csv.in;
 
 import java.io.IOException;
 import java.io.InputStream;
