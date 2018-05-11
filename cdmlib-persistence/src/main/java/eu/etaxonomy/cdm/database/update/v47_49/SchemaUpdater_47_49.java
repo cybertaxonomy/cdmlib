80f29fef23a18e429d2ed35f122857870df74695
Andreas Müller <a.mueller@bgbm.org>
2018-05-11 13:57:37 +0200

    Revert "ref #7276 revert "make User.email a unique field""
    This reverts commit 80cbc74164e5b37dbaa8e64d90126e745a9a05e3.

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/v47_49/SchemaUpdater_47_49.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/v47_49/SchemaUpdater_47_49.java
@@ -18,12 +18,14 @@ import org.apache.log4j.Logger;
 import eu.etaxonomy.cdm.database.update.ClassBaseTypeUpdater;
 import eu.etaxonomy.cdm.database.update.ColumnAdder;
 import eu.etaxonomy.cdm.database.update.ColumnNameChanger;
+import eu.etaxonomy.cdm.database.update.ColumnTypeChanger;
 import eu.etaxonomy.cdm.database.update.ISchemaUpdater;
 import eu.etaxonomy.cdm.database.update.ISchemaUpdaterStep;
 import eu.etaxonomy.cdm.database.update.SchemaUpdaterBase;
 import eu.etaxonomy.cdm.database.update.SimpleSchemaUpdaterStep;
 import eu.etaxonomy.cdm.database.update.TableNameChanger;
 import eu.etaxonomy.cdm.database.update.TermRepresentationUpdater;
+import eu.etaxonomy.cdm.database.update.v33_34.UsernameConstraintUpdater;
 import eu.etaxonomy.cdm.database.update.v41_47.SchemaUpdater_41_47;
 
 /**
@@ -64,6 +66,14 @@ public class SchemaUpdater_47_49 extends SchemaUpdaterBase {
 
 		List<ISchemaUpdaterStep> stepList = new ArrayList<>();
 
+        //#6581 make nomenclatural reference and OriginalSource
+        stepName = "Make nomenclatural reference and OriginalSource";
+        tableName = "TaxonName";
+        newColumnName = "nomenclaturalSource_id";
+        String referencedTable = "OriginalSourceBase";
+        step = ColumnAdder.NewIntegerInstance(stepName, tableName, newColumnName, INCLUDE_AUDIT, !NOT_NULL, referencedTable);
+        stepList.add(step);
+
 		//#7109 nom. valid => nom. val.
 		stepName = "nom valid => nom. val. (abbrevLabel)";
 		UUID uuidTerm = UUID.fromString("bd036217-5499-4ccd-8f4c-72e06158db93");
@@ -94,8 +104,7 @@ public class SchemaUpdater_47_49 extends SchemaUpdaterBase {
         query = "UPDATE @@PermissionGroup@@ "
                 + " SET uuid='1739df71-bf73-4dc6-8320-aaaf72cb555f', name='Admin' "
                 + " WHERE  name='admin' or name='Admin'";
-        tableName = "PermissionGroup";
-        step = SimpleSchemaUpdaterStep.NewAuditedInstance(stepName, query, tableName, -99);
+        step = SimpleSchemaUpdaterStep.NewNonAuditedInstance(stepName, query, -99);
         stepList.add(step);
 
         //#7405 Rename WorkingSet to DescriptiveDataSet
@@ -120,13 +129,6 @@ public class SchemaUpdater_47_49 extends SchemaUpdaterBase {
         oldTableName = "WorkingSet_TaxonNode";
         changeSingleWorkingSetTableName(stepList, oldTableName);
 
-        stepName = "Update hibernate_sequences for WorkingSet renaming";
-        query = " UPDATE hibernate_sequences "
-                + " SET sequence_name = 'DescriptiveDataSet' "
-                + " WHERE sequence_name = WorkingSet";
-        step = SimpleSchemaUpdaterStep.NewNonAuditedInstance(stepName, query, -99);
-        stepList.add(step);
-
         //#2335 Make WorkingSet IdentifiableEntity
         stepName = "Make DescriptiveDataSet IdentifiableEntity";
         tableName = "DescriptiveDataSet";
@@ -146,7 +148,113 @@ public class SchemaUpdater_47_49 extends SchemaUpdaterBase {
         stepList.add(step);
 
 
+        //#7238 rename lastName and firstName
+        stepName = "rename lastName";
+        tableName = "AgentBase";
+        String oldColumnName = "lastname";
+        newColumnName = "familyName";
+        int size = 255;
+        step = ColumnNameChanger.NewVarCharInstance(stepName, tableName, oldColumnName, newColumnName, size, INCLUDE_AUDIT);
+        stepList.add(step);
+
+        //... firstName
+        stepName = "rename firstName";
+        tableName = "AgentBase";
+        oldColumnName = "firstname";
+        newColumnName = "givenName";
+        size = 255;
+        step = ColumnNameChanger.NewVarCharInstance(stepName, tableName, oldColumnName, newColumnName, size, INCLUDE_AUDIT);
+        stepList.add(step);
+
+        //#7210 Add salt field for User
+        stepName = "Add salt field for User";
+        tableName = "UserAccount";
+        newColumnName = "salt";
+        step = ColumnAdder.NewStringInstance(stepName, tableName, newColumnName, length, INCLUDE_AUDIT);
+        stepList.add(step);
+
+        //#6943 Add combination 'in'-author
+        stepName = "Add combination 'in'-author";
+        tableName = "TaxonName";
+        newColumnName = "inCombinationAuthorship_id";
+        referencedTable = "AgentBase";
+        step = ColumnAdder.NewIntegerInstance(stepName, tableName, newColumnName, INCLUDE_AUDIT, !NOT_NULL, referencedTable);
+        stepList.add(step);
+
+        //#6943 Add basionym 'in'-author
+        stepName = "Add basionym 'in'-author";
+        tableName = "TaxonName";
+        newColumnName = "inBasionymAuthorship_id";
+        referencedTable = "AgentBase";
+        step = ColumnAdder.NewIntegerInstance(stepName, tableName, newColumnName, INCLUDE_AUDIT, !NOT_NULL, referencedTable);
+        stepList.add(step);
+
+        //#6916 Link IntextReference to OriginalSource
+        stepName = "Link IntextReference to OriginalSource";
+        tableName = "IntextReference";
+        newColumnName = "source_id";
+        referencedTable = "OriginalSourceBase";
+        step = ColumnAdder.NewIntegerInstance(stepName, tableName, newColumnName, INCLUDE_AUDIT, !NOT_NULL, referencedTable);
+        stepList.add(step);
+
+        //#6720 Make individual count a string
+        stepName = "Make individual count a string";
+        tableName = "SpecimenOrObservationBase";
+        String columnName = "individualCount";
+        size = 255;
+        step = ColumnTypeChanger.NewInt2StringInstance(stepName, tableName, columnName, size, INCLUDE_AUDIT, null, !NOT_NULL);
+        stepList.add(step);
+
+        updateSpecimenTypeDesignationStatusOrder(stepList);
+
+        //#7144 Set Country area level
+        stepName = "Set Country area level";
+        query = " UPDATE @@DefinedTermBase@@ " +
+                " SET level_id = ( SELECT id FROM (SELECT id FROM DefinedTermBase WHERE uuid = '79db63a4-1563-461e-8e41-48f5722feca4') as drv) " +
+                " WHERE DTYPE = 'Country' ";
+        step = SimpleSchemaUpdaterStep.NewNonAuditedInstance(stepName, query, -99);
+        stepList.add(step);
+
+
+
+        //7276  Make User.emailAddress a unique field
+        //TODO H2 / PostGreSQL / SQL Server
+        //User.email unique
+        stepName = "Update User.emailAdress unique index";
+        tableName = "UserAccount";
+        columnName = "emailAddress";
+        step = UsernameConstraintUpdater.NewInstance(stepName, tableName, columnName);
+        stepList.add(step);
+
         return stepList;
+    }
+
+    /**
+     * @param stepList
+     */
+    private void updateSpecimenTypeDesignationStatusOrder(List<ISchemaUpdaterStep> stepList) {
+        VocabularyOrderUpdater updater = VocabularyOrderUpdater.NewInstance(stepList);
+        updater.add("a407dbc7-e60c-46ff-be11-eddf4c5a970d", 1);
+        updater.add("05002d46-083e-4b27-8731-2e7c28a8825c", 2);
+        updater.add("93ef8257-0a08-47bb-9b36-542417ae7560", 3);
+        updater.add("7a1a8a53-78f4-4fc0-89f7-782e94992d08", 4);
+        updater.add("f3b60bdb-4638-4ca9-a0c7-36e77d8459bb", 5);
+        updater.add("052a5ff0-8e9a-4355-b24f-5e4bb6071f44", 6);
+        updater.add("26e13359-8f77-4e40-a85a-56c01782fce0", 7);
+        updater.add("7afc2f4f-f70a-4aa5-80a5-87764f746bde", 8);
+        updater.add("989a2715-71d5-4fbe-aa9a-db9168353744", 9);
+        updater.add("95b90696-e103-4bc0-b60b-c594983fb566", 10);
+        updater.add("eb7df2e5-d9a7-479d-970c-c6f2b0a761d7", 11);
+        updater.add("497137f3-b614-4183-8a22-97fcd6e2bdd8", 12);
+        updater.add("7244bc51-14d8-41a6-9524-7dc5303bba29", 13);
+        updater.add("0c39e2a5-2fe0-4d4f-819a-f609b5340339", 14);
+        updater.add("01d91053-7004-4984-aa0d-9f4de59d6205", 15);
+        updater.add("8d2fed1f-242e-4bcf-bbd7-e85133e479dc", 16);
+        updater.add("49c96cae-6be6-401e-9b36-1bc12d9dc8f9", 17);
+        updater.add("643513d0-32f5-46ba-840b-d9b9caf8160f", 18);
+        updater.add("b7807acc-f559-474e-ad4a-e7a41e085e34", 19);
+        updater.add("230fd762-b143-49de-ac2e-744bcc48a63b", 20);
+        updater.add("7194020b-a326-4b47-9bfe-9f31a30aba7f", 21);
     }
 
     /**
@@ -162,6 +270,10 @@ public class SchemaUpdater_47_49 extends SchemaUpdaterBase {
             stepName = "Rename " +  oldTableName + ".workingSet_id";
             String oldColumnName = "WorkingSet_id";
             String newColumnName = "DescriptiveDataSet_id";
+            if ("WorkingSet_DescriptionBase".equals(oldTableName)){
+                oldColumnName = "WorkingSets_id";
+                newColumnName = "DescriptiveDataSets_id";
+            }
             step = ColumnNameChanger.NewIntegerInstance(stepName, newTableName, oldColumnName, newColumnName, INCLUDE_AUDIT);
             stepList.add(step);
         }
