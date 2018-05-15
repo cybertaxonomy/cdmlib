bd86479422e0ea2d14529d32da5b41eeb08f513c
Andreas Müller <a.mueller@bgbm.org>
2018-05-15 17:09:05 +0200

    add annoations to FullCoverageDataGenerator Registration

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/data/FullCoverageDataGenerator.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/data/FullCoverageDataGenerator.java
@@ -895,6 +895,7 @@ public class FullCoverageDataGenerator {
 		registration.setInstitution(Institution.NewInstance());
 		User submitter = User.NewInstance("submitter", "12345");
 		registration.setSubmitter(submitter);
+		handleAnnotatableEntity(registration);;
 
 		cdmBases.add(submitter);
 		cdmBases.add(bacName);
