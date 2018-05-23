16345de54aa7134a6cbb0636a7454c8f2b4b2e27
Katja Luther <k.luther@bgbm.org>
2018-05-23 12:33:19 +0200

    adapt source reference in ABCD import

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/specimen/abcd206/in/Abcd206Import.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/specimen/abcd206/in/Abcd206Import.java
@@ -159,11 +159,11 @@ public class Abcd206Import extends SpecimenImportBase<Abcd206ImportConfigurator,
                             state.setRef(state.getConfig().getSourceReference());
                         }else{
                             state.setRef(ReferenceFactory.newGeneric());
+                            state.getRef().setUri(sourceUri);
+                            state.getRef().setTitle(StringUtils.substringAfter(sourceUri.toString(), "dsa="));
 
-                            if (state.getConfig().getSourceReferenceTitle() != null){
+                            if (!StringUtils.isBlank(state.getConfig().getSourceReferenceTitle())){
                                 state.getRef().setTitle(state.getConfig().getSourceReferenceTitle());
-                            } else{
-                                state.getRef().setTitle("ABCD Import Source Reference");
                             }
                         }
 
