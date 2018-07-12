cybertaxonomy/cdmlib

commit 7dcfc7717da6ce0afda4bc76a1671f1a39eeabed

removing almost unused type check methods from TaggedText

cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/cache/TaggedCacheHelper.java
@@ -58,7 +58,7 @@ public class TaggedCacheHelper {
         boolean wasSeparator = true;
         while (index < tags.size()){
 
-            if (! tags.get(index).isSeparator()){
+            if (! tags.get(index).is(TagEnum.separator)){
                 if (wasSeparator == false){
                     tags.add(index++, TaggedText.NewWhitespaceInstance());
                 }else{

cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/cache/TaggedText.java
@@ -11,8 +11,6 @@ package eu.etaxonomy.cdm.strategy.cache;
 
 import java.io.Serializable;
 
-import javax.persistence.Transient;
-
 import org.apache.commons.lang.StringUtils;
 
 import eu.etaxonomy.cdm.common.CdmUtils;
@@ -60,51 +58,9 @@ public class TaggedText implements Serializable{
 		this.type = type;
 	}
 
-// *************************** DELEGATES ************************************/
-
-	@Transient
-	protected boolean isName() {
-		return type.isName();
+	protected boolean is(TagEnum type) {
+		return type.equals(type);
 	}
-	@Transient
-	protected boolean isRank() {
-		return type.isRank();
-	}
-	@Transient
-	protected boolean isAuthors() {
-		return type.isAuthors();
-	}
-	@Transient
-	protected boolean isAppendedPhrase() {
-		return type.isAppendedPhrase();
-	}
-	@Transient
-	protected boolean isReference() {
-		return type.isReference();
-	}
-	@Transient
-	protected boolean isYear() {
-		return type.isYear();
-	}
-	@Transient
-	protected boolean isFullName() {
-		return type.isFullName();
-	}
-	@Transient
-	protected boolean isNomStatus() {
-		return type.isNomStatus();
-	}
-	@Transient
-	protected boolean isSeparator() {
-		return type.isSeparator();
-	}
-	@Transient
-	protected boolean isHybridSign() {
-		return type.isHybridSign();
-	}
-
-
-// ********************** toString() ***********************************************/
 
 	@Override
 	public String toString(){

commit 93d11c3e8c119167bb6ab1cc7ccea93310e90dd8

removing bug which was introduced in 7dcfc77

cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/cache/TaggedCacheHelper.java
@@ -58,7 +58,7 @@ public class TaggedCacheHelper {
         boolean wasSeparator = true;
         while (index < tags.size()){
 
-            if (! tags.get(index).is(TagEnum.separator)){
+            if (! tags.get(index).getType().isSeparator()){
                 if (wasSeparator == false){
                     tags.add(index++, TaggedText.NewWhitespaceInstance());
                 }else{

cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/cache/TaggedText.java
@@ -79,10 +79,6 @@ public class TaggedText implements Serializable{
 		this(type, text, null);
 	}
 
-	protected boolean is(TagEnum type) {
-		return type.equals(type);
-	}
-
 	@Override
 	public String toString(){
 		String result = CdmUtils.concat(":", type.toString(), text);