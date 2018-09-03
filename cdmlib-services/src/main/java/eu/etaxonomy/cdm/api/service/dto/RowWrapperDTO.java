aac70fda9ec4e8ba564d3014d18dd9a25d9a0261
Patrick Plitzner <p.plitzner@bgbm.org>
2018-09-03 10:23:29 +0200

    ref #7674 Add equals() and hashCode() to RowWrapperDTO

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/dto/RowWrapperDTO.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/dto/RowWrapperDTO.java
@@ -88,4 +88,51 @@ public abstract class RowWrapperDTO <T extends DescriptionBase> implements Seria
             categoricalData.setStateDataOnly(new ArrayList<>((Collection<State>) newValue));
         }
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((featureToElementMap == null) ? 0 : featureToElementMap.hashCode());
+        result = prime * result + ((taxonNode == null) ? 0 : taxonNode.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        RowWrapperDTO other = (RowWrapperDTO) obj;
+        if (description == null) {
+            if (other.description != null) {
+                return false;
+            }
+        } else if (!description.equals(other.description)) {
+            return false;
+        }
+        if (featureToElementMap == null) {
+            if (other.featureToElementMap != null) {
+                return false;
+            }
+        } else if (!featureToElementMap.equals(other.featureToElementMap)) {
+            return false;
+        }
+        if (taxonNode == null) {
+            if (other.taxonNode != null) {
+                return false;
+            }
+        } else if (!taxonNode.equals(other.taxonNode)) {
+            return false;
+        }
+        return true;
+    }
+
 }
