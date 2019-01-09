b5d00686ded403812e1e9b6b524f78ee2694302e
Katja Luther <k.luther@bgbm.org>
2019-01-09 14:37:45 +0100

    ref #7901: cleaning PreferencePredicates

---
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/metadata/TermDisplayEnum.java
@@ -0,0 +1,37 @@
+/**
+* Copyright (C) 2019 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.model.metadata;
+
+/**
+ * @author k.luther
+ * @since 09.01.2019
+ *
+ */
+public enum TermDisplayEnum {
+    IdInVocabulary("IdInVocabulary", "Show IdInVocabulary"),
+    Symbol1("Symbol1", "Show Symbol1"),
+    Symbol2("Symbol2", "Show Symbol2"),
+    Title("Title", "Show Title");
+
+    String label;
+    String key;
+
+    private TermDisplayEnum(String key, String label){
+        this.label = label;
+        this.key = key;
+    }
+
+    public String getLabel(){
+        return label;
+    }
+
+    public String getKey(){
+        return key;
+    }
+}
