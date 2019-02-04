425411f486ad411a73aa8f894131db7c9098278e
Andreas Müller <a.mueller@bgbm.org>
2019-02-04 02:10:05 +0100

    ref #3767 ref #3572 add prefix parsing without http to DOI

--- cdmlib-commons/src/test/java/eu/etaxonomy/cdm/common/DoiTest.java
+++ cdmlib-commons/src/test/java/eu/etaxonomy/cdm/common/DoiTest.java
@@ -13,13 +13,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * 
+ *
  * Test class for testing the {@link DOI} class.
- * 
+ *
  * For doi syntax see also http://www.doi.org/doi_handbook/2_Numbering.html
  * or
- * http://stackoverflow.com/questions/27910/finding-a-doi-in-a-document-or-page  
- * 
+ * http://stackoverflow.com/questions/27910/finding-a-doi-in-a-document-or-page
+ *
  * @author a.mueller
  *
  */
@@ -38,24 +38,24 @@ public class DoiTest {
 		DOI doi = DOI.fromString(validDoi);
 		Assert.assertEquals("10.1002", doi.getPrefix());
 		Assert.assertEquals("1234", doi.getSuffix());
-		
+
 		validDoi = "10.1002/(SICI)1522-2594(199911)42:5<952::AID-MRM16>3.0.CO;2-S";
 		doi = DOI.fromString(validDoi);
 		Assert.assertEquals("10.1002", doi.getPrefix());
 		Assert.assertEquals("(SICI)1522-2594(199911)42:5<952::AID-MRM16>3.0.CO;2-S", doi.getSuffix());
-		
+
 		validDoi = "10.1007.10/978-3-642-28108-2_19";
 		doi = DOI.fromString(validDoi);
 		Assert.assertEquals("10.1007.10", doi.getPrefix());
 		Assert.assertEquals("978-3-642-28108-2_19", doi.getSuffix());
-		
+
 		validDoi="10.1579/0044-7447(2006)35\\[89:RDUICP\\]2.0.CO;2";
 		doi = DOI.fromString(validDoi);
 		Assert.assertEquals("10.1579", doi.getPrefix());
 		Assert.assertEquals("0044-7447(2006)35\\[89:RDUICP\\]2.0.CO;2", doi.getSuffix());
 
 	}
-	
+
 	@Test
 	public void testFromRegistrantCodeAndSuffix() {
 		DOI doi = DOI.fromRegistrantCodeAndSuffix("1579", "978-3-642-28108-2_19");
@@ -63,7 +63,7 @@ public class DoiTest {
 		Assert.assertEquals("978-3-642-28108-2_19", doi.getSuffix());
 		Assert.assertNotEquals("1234", doi.getSuffix());
 	}
-	
+
 	@Test
 	public void testParserFail() {
 		String invalidDoi = "10.4515260,51.1656910";  //must never match to avoid matches with geo coordinates
@@ -88,20 +88,29 @@ public class DoiTest {
 		DOI doi = DOI.fromString(validDoi);
 		Assert.assertEquals("10.1002", doi.getPrefix());
 		Assert.assertEquals("1234", doi.getSuffix());
-		
+
 		validDoi = "http://doi.org/10.1002/1234";
 		doi = DOI.fromString(validDoi);
 		Assert.assertEquals("10.1002", doi.getPrefix());
 		Assert.assertEquals("1234", doi.getSuffix());
-	
-		
+
 		validDoi = "http://doi.org/urn:doi:10.123:456ABC%2Fzyz";
 		doi = DOI.fromString(validDoi);
 		Assert.assertEquals("10.123", doi.getPrefix());
 		Assert.assertEquals("456ABC/zyz", doi.getSuffix());  //urn must be percentage encoded ( / -> %2F)
-		
+
+        validDoi = "doi.org/urn:doi:10.123:456ABC%2Fzyz";
+        doi = DOI.fromString(validDoi);
+        Assert.assertEquals("10.123", doi.getPrefix());
+        Assert.assertEquals("456ABC/zyz", doi.getSuffix());  //urn must be percentage encoded ( / -> %2F)
+
+        validDoi = "dx.doi.org/urn:doi:10.123:456ABC%2Fzyz";
+        doi = DOI.fromString(validDoi);
+        Assert.assertEquals("10.123", doi.getPrefix());
+        Assert.assertEquals("456ABC/zyz", doi.getSuffix());  //urn must be percentage encoded ( / -> %2F)
+
 	}
-	
+
 	@Test
 	public void testEquals() {
 		String validDoi = "10.1002/12a4";
@@ -113,7 +122,7 @@ public class DoiTest {
 		DOI doi3 = DOI.fromString(validDoi);
 		Assert.assertNotEquals("Different DOIs must not be equal", doi1, doi3);
 	}
-	
+
 	@Test
 	public void testAsURI() {
 		//mandatory encoding according to http://www.doi.org/doi_handbook/2_Numbering.html#2.5.2.4
@@ -121,24 +130,24 @@ public class DoiTest {
 		DOI doi1 = DOI.fromString(validDoi);
 		String uri = doi1.asURI();
 		Assert.assertEquals(DOI.HTTP_DOI_ORG + "10.1002/1234%2556%2278%2390%2012%3f34", uri);
-		
+
 		//recommendedEncoding
 		validDoi = "10.1002/1234<56>78{90}12^34";
 		doi1 = DOI.fromString(validDoi);
 		uri = doi1.asURI();
 		Assert.assertEquals(DOI.HTTP_DOI_ORG + "10.1002/1234%3c56%3e78%7b90%7d12%5e34", uri);
-		
+
 		//recommendedEncoding (cont.)
 		validDoi = "10.1002/1234[56]78`90|12\\34+56";
 		doi1 = DOI.fromString(validDoi);
 		uri = doi1.asURI();
 		Assert.assertEquals(DOI.HTTP_DOI_ORG + "10.1002/1234%5b56%5d78%6090%7c12%5c34%2b56", uri);
-		
+
 	}
 
-	
 
-	
+
+
 	private void testInvalid(String invalidDoi) {
 		try {
 			DOI.fromString(invalidDoi);
@@ -146,6 +155,6 @@ public class DoiTest {
 		} catch (IllegalArgumentException e) {
 			//OK
 		}
-	}		
+	}
 
 }
