72c67e903c8cea6d67ac575feabfbc1ae9796647
Katja Luther <k.luther@bgbm.org>
2019-02-28 11:49:47 +0100

    ref #8124: adapt NaturalLanguageGenerator to usage in nameFacts

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/INaturalLanguageGenerator.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/INaturalLanguageGenerator.java
@@ -3,30 +3,30 @@ package eu.etaxonomy.cdm.api.service;
 import java.util.List;
 
 import eu.etaxonomy.cdm.model.common.Language;
-import eu.etaxonomy.cdm.model.description.TaxonDescription;
-import eu.etaxonomy.cdm.model.description.TextData;
+import eu.etaxonomy.cdm.model.description.DescriptionBase;
 import eu.etaxonomy.cdm.model.description.FeatureTree;
+import eu.etaxonomy.cdm.model.description.TextData;
 
 
 
 /**
- * Interface for Natural Language generation 
+ * Interface for Natural Language generation
  * @author m.venin
  * @since 12.04.2010
  *
  */
 
 public interface INaturalLanguageGenerator {
-	
-	public List<TextData> generateNaturalLanguageDescription(FeatureTree featureTree, TaxonDescription descriptions);
-	
-	public List<TextData> generateNaturalLanguageDescription(FeatureTree featureTree,TaxonDescription description, Language language);
-	
-	public List<TextData> generatePreferredNaturalLanguageDescription(FeatureTree featureTree, TaxonDescription description, List<Language> languages);
 
-	public TextData generateSingleTextData(FeatureTree featureTree, TaxonDescription description);
-	
-	public TextData generateSingleTextData(FeatureTree featureTree, TaxonDescription description, Language language);
-	
-	public TextData generatePreferredSingleTextData(FeatureTree featureTree, TaxonDescription description, List<Language> languages);
+	public List<TextData> generateNaturalLanguageDescription(FeatureTree featureTree, DescriptionBase descriptions);
+
+	public List<TextData> generateNaturalLanguageDescription(FeatureTree featureTree,DescriptionBase description, Language language);
+
+	public List<TextData> generatePreferredNaturalLanguageDescription(FeatureTree featureTree, DescriptionBase description, List<Language> languages);
+
+	public TextData generateSingleTextData(FeatureTree featureTree, DescriptionBase description);
+
+	public TextData generateSingleTextData(FeatureTree featureTree, DescriptionBase description, Language language);
+
+	public TextData generatePreferredSingleTextData(FeatureTree featureTree, DescriptionBase description, List<Language> languages);
 }
