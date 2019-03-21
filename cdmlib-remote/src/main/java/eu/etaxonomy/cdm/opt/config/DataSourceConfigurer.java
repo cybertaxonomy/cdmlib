cybertaxonomy/cdmlib

commit 9b55e52549fac1c6db9160c533b2e9ffc8a1442c

ref #8189 CdmUtils uses the spring environment to find user.home

cdmlib-remote/src/main/java/eu/etaxonomy/cdm/opt/config/DataSourceConfigurer.java
@@ -26,13 +26,12 @@ import org.hibernate.dialect.H2CorrectedDialect;
 import org.hibernate.dialect.MySQL5MyISAMUtf8Dialect;
 import org.hibernate.dialect.PostgreSQL82Dialect;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.io.FileSystemResource;
@@ -85,6 +84,9 @@ import eu.etaxonomy.cdm.remote.config.AbstractWebApplicationConfigurer;
  *
  */
 @Configuration
+// cdmlib-remote.properties is used by developers to define the datasource bean to use from
+// the datasources.xml. It is not relevant for production systems.
+@PropertySource("file:${user.home}/.cdmLibrary/cdmlib-remote.properties")
 public class DataSourceConfigurer extends AbstractWebApplicationConfigurer {
 
     public static final Logger logger = Logger.getLogger(DataSourceConfigurer.class);
@@ -137,9 +139,6 @@ public class DataSourceConfigurer extends AbstractWebApplicationConfigurer {
     public void setBeanDefinitionFile(String filename){
         beanDefinitionFile = filename;
     }
-
-    @Autowired
-    private ConfigurableEnvironment env;
 
     private String dataSourceId = null;
 

cdmlib-commons/src/main/java/eu/etaxonomy/cdm/common/AccountStore.java
@@ -24,7 +24,7 @@ public class AccountStore {
 
     private static Logger logger = Logger.getLogger(AccountStore.class);
 
-    public final static File accountsFile = new File(CdmUtils.PER_USER_CDM_FOLDER + File.separator + ".dbaccounts.properties");
+    public final static File accountsFile = new File(CdmUtils.perUserCdmFolder + File.separator + ".dbaccounts.properties");
 
     public static String getAccountsFileName(){
         try {

cdmlib-commons/src/main/java/eu/etaxonomy/cdm/common/CdmUtils.java
@@ -31,6 +31,8 @@ import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 
 /**
  * Util class for consistent access to and creation of per instance application configuration files.
@@ -38,13 +40,13 @@ import org.apache.log4j.Logger;
  * @author a.mueller
  * @author a.kohlbecker
  */
-public class CdmUtils {
+public class CdmUtils implements EnvironmentAware {
 
     private static final Logger logger = Logger.getLogger(CdmUtils.class);
 
     // ============= TODO externalize into CdmFileUtils class ? ========== //
 
-    private static final String USER_HOME = System.getProperty("user.home");
+    private static String userHome = null;
 
     /**
      * The per user cdm folder name: ".cdmLibrary"
@@ -54,7 +56,7 @@ public class CdmUtils {
     /**
      * The per user cdm folder "~/.cdmLibrary"
      */
-    public final static File PER_USER_CDM_FOLDER = new File(USER_HOME + File.separator + CDM_FOLDER_NAME );
+    public static File perUserCdmFolder = null;
 
     /**
      * suggested sub folder for web app related data and configurations.
@@ -68,8 +70,20 @@ public class CdmUtils {
     //folder separator
     static String folderSeparator;
 
+    protected Environment env;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.env = environment;
+        if(userHome == null){
+            CdmUtils.userHome = env.getRequiredProperty("user.home");
+            CdmUtils.perUserCdmFolder = new File(userHome + File.separator + CDM_FOLDER_NAME );
+            logger.info("user.home is set to " + CdmUtils.userHome);
+        }
+    }
+
     public static File getCdmHomeDir() {
-        return new File(PER_USER_CDM_FOLDER + File.separator);
+        return new File(perUserCdmFolder + File.separator);
     }
 
 	/**
@@ -668,5 +682,6 @@ public class CdmUtils {
     public static String quoteRegExWithWildcard(String regEx){
         return Pattern.quote(regEx).replace("*", "\\E.*\\Q").replace("\\Q\\E", "");
     }
+
 
 }

cdmlib-print/src/main/java/eu/etaxonomy/cdm/print/out/PublishOutputModuleBase.java
@@ -173,7 +173,7 @@ public abstract class PublishOutputModuleBase implements IPublishOutputModule {
 		locationList.add(shippedStylesheetsDir);
 
 		// TODO this should be configured in a central place, see #2387
-		File userdir = new File(CdmUtils.PER_USER_CDM_FOLDER + l + "stylesheets"
+		File userdir = new File(CdmUtils.perUserCdmFolder + l + "stylesheets"
 				+ l + getOutputFileSuffix());
 		locationList.add(userdir);
 

cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/config/AbstractWebApplicationConfigurer.java
@@ -18,6 +18,7 @@ import java.util.Properties;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.context.WebApplicationContext;
 
 import eu.etaxonomy.cdm.common.CdmUtils;
@@ -30,6 +31,9 @@ import eu.etaxonomy.cdm.common.CdmUtils;
 public abstract class AbstractWebApplicationConfigurer {
 
     public static final Logger logger = Logger.getLogger(AbstractWebApplicationConfigurer.class);
+
+    @Autowired
+    protected ConfigurableEnvironment env;
 
     private static final String CDMLIB_REMOTE_PROPERTIES = "cdmlib-remote.properties";
 
@@ -47,7 +51,7 @@ public abstract class AbstractWebApplicationConfigurer {
             userDefinedProperties = new Properties();
             try {
                 InputStream in = new FileInputStream(
-                            CdmUtils.PER_USER_CDM_FOLDER
+                            CdmUtils.perUserCdmFolder
                             + java.io.File.separator
                             + CDMLIB_REMOTE_PROPERTIES
                     );
@@ -87,24 +91,21 @@ public abstract class AbstractWebApplicationConfigurer {
      * @return
      */
     protected String findProperty(String property, boolean required) {
-        // 1. look for the property in the ServletContext
-        Object obj = webApplicationContext.getServletContext().getAttribute(property);
-        String value = (String)obj;
-        // 2. look for the property in environment variables of the OS
-        if(value == null){
-            value = System.getProperty(property);
-        }
-        if(value == null && userDefinedProperties != null){
-            value = userDefinedProperties.getProperty(property);
-        }
-        if(value == null && required){
-            logger.error("property {" + property + "} not found.");
-            logger.error("--> This property can be set in three optional ways:");
-            logger.error("--> 		1. as attribute to the ServletContext");
-            logger.error("--> 		2. as system property e.g. -D" + property);
-            logger.error("--> 		3. in ~/.cdmLibrary/cdmlib-remote.properties");
-            logger.error("Stopping application ...");
-            System.exit(-1);
+        String value = null;
+        if(required){
+            try {
+                value = env.getRequiredProperty(property);
+            } catch (IllegalStateException e) {
+                logger.error("property {" + property + "} not found.");
+                logger.error("--> This property can be set in three optional ways:");
+                logger.error("--> 		1. as attribute to the ServletContext");
+                logger.error("--> 		2. as system property e.g. -D" + property);
+                // logger.error("--> 		3. in ~/.cdmLibrary/cdmlib-remote.properties");
+                logger.error("Stopping application ...");
+                System.exit(-1);
+            }
+        } else {
+            value = env.getProperty(property);
         }
         return value;
     }

commit cf9ba32780e880ce442593ab4e52eb4c43ae52ce

ref #8189 cdmlib-remote.properties as not required property source fixes bug introduced in 9b55e525

cdmlib-remote/src/main/java/eu/etaxonomy/cdm/opt/config/DataSourceConfigurer.java
@@ -86,7 +86,7 @@ import eu.etaxonomy.cdm.remote.config.AbstractWebApplicationConfigurer;
 @Configuration
 // cdmlib-remote.properties is used by developers to define the datasource bean to use from
 // the datasources.xml. It is not relevant for production systems.
-@PropertySource("file:${user.home}/.cdmLibrary/cdmlib-remote.properties")
+@PropertySource(value="file:${user.home}/.cdmLibrary/cdmlib-remote.properties", ignoreResourceNotFound=true)
 public class DataSourceConfigurer extends AbstractWebApplicationConfigurer {
 
     public static final Logger logger = Logger.getLogger(DataSourceConfigurer.class);