c4fa45c22e5e247ef92aa64e59f9c83eb1719a53
Katja Luther <k.luther@bgbm.org>
2019-04-05 14:31:22 +0200

    add test for adding member to group

--- cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/GroupServiceImplTest.java
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/GroupServiceImplTest.java
@@ -2,8 +2,11 @@ package eu.etaxonomy.cdm.api.service;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
@@ -13,9 +16,9 @@ import org.unitils.spring.annotation.SpringBeanByType;
 import eu.etaxonomy.cdm.model.common.GrantedAuthorityImpl;
 import eu.etaxonomy.cdm.model.common.Group;
 import eu.etaxonomy.cdm.model.common.User;
-import eu.etaxonomy.cdm.test.integration.CdmIntegrationTest;
+import eu.etaxonomy.cdm.test.integration.CdmTransactionalIntegrationTest;
 
-public class GroupServiceImplTest extends CdmIntegrationTest {
+public class GroupServiceImplTest extends CdmTransactionalIntegrationTest {
 	private static final Logger logger = Logger.getLogger(GroupServiceImplTest.class);
 
     @SpringBeanByType
@@ -41,6 +44,32 @@ public class GroupServiceImplTest extends CdmIntegrationTest {
     	assertNull(group);
     	user1 = userService.find(UserUUID);
     	assertNotNull(user1);
+
+    }
+
+
+
+    @Test
+    public void testAddMemberToGroup(){
+        String admin_all = "ADMIN_ALL";
+        GrantedAuthorityImpl testAuthority = GrantedAuthorityImpl.NewInstance(admin_all);
+        Group group = Group.NewInstance("TestGroup");
+        group.addGrantedAuthority(testAuthority);
+        UUID groupUUID =  groupService.saveGroup(group);
+        User user1 = User.NewInstance("TestUser1", "pwd");
+        UUID UserUUID =  userService.save(user1).getUuid();
+        group.addMember(user1);
+        List<Group> groups = new ArrayList<>();
+        groups.add(group);
+        groupService.merge(groups, true);
+        commitAndStartNewTransaction();
+
+
+        group = groupService.find(groupUUID);
+        assertNotNull(group);
+        assertNotNull(group.getMembers());
+        user1 = userService.find(UserUUID);
+        assertTrue(group.getMembers().iterator().next().equals(user1));
 
     }
 
