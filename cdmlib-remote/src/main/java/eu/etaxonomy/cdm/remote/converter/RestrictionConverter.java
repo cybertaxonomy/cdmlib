5c5308813ea9af3faa9b36d649e63228ccfe5b21
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2019-07-04 20:39:15 +0200

    RestrictionConverter converts uuid strings to UUID objetcs which have beed passed as json value

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/converter/RestrictionConverter.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/converter/RestrictionConverter.java
@@ -9,6 +9,9 @@
 package eu.etaxonomy.cdm.remote.converter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import org.springframework.core.convert.converter.Converter;
 
@@ -38,7 +41,21 @@ public class RestrictionConverter implements Converter<String, Restriction<?>>  
     public Restriction<?> convert(String source) {
 
             try {
-                return objectMapper.readValue(source, Restriction.class);
+                Restriction restriction = objectMapper.readValue(source, Restriction.class);
+                // the below loop is detects UUID string representations and converts them to UUIDs
+                // such conversion is needed for all user types, we are only handling UUIDs here quickly and dirty
+                // TODO think about the best solution, handle the string to object conversion in
+                // CdmEntityDaoBase.createRestriction(String propertyName, Object value, MatchMode matchMode) ?
+                List<Object> convertedValues = new ArrayList<>(restriction.getValues().size());
+                for(Object val : restriction.getValues()){
+                    if(val.toString().matches("([a-f\\d]{8}(-[a-f\\d]{4}){3}-[a-f\\d]{12}?)")){
+                        convertedValues.add(UUID.fromString(val.toString()));
+                    } else {
+                        convertedValues.add(val);
+                    }
+                }
+                restriction.setValues(convertedValues);
+                return restriction ;
             } catch (JsonParseException | JsonMappingException e) {
                 throw new IllegalArgumentException(e);
             }catch (IOException e) {
