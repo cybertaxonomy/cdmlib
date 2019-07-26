dc572c3ea10bfa73dc301586423bd97c41115243
Andreas Müller <a.mueller@bgbm.org>
2019-07-27 01:19:05 +0200

    adapt DescriptiveDataSet to allow also descriptive systems with features not only characters

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/description/DescriptiveDataSet.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/description/DescriptiveDataSet.java
@@ -41,8 +41,8 @@ import eu.etaxonomy.cdm.model.common.Language;
 import eu.etaxonomy.cdm.model.location.NamedArea;
 import eu.etaxonomy.cdm.model.name.Rank;
 import eu.etaxonomy.cdm.model.taxon.TaxonNode;
-import eu.etaxonomy.cdm.model.term.TermTree;
 import eu.etaxonomy.cdm.model.term.Representation;
+import eu.etaxonomy.cdm.model.term.TermTree;
 import eu.etaxonomy.cdm.strategy.cache.description.DescriptiveDataSetDefaultCacheStrategy;
 
 /**
@@ -82,8 +82,8 @@ public class DescriptiveDataSet extends IdentifiableEntity<DescriptiveDataSetDef
 	@XmlIDREF
 	@XmlSchemaType(name = "IDREF")
 	@ManyToOne(fetch = FetchType.LAZY)
-	@Cascade({CascadeType.SAVE_UPDATE,CascadeType.MERGE})
-	private TermTree<Character> descriptiveSystem;
+	@Cascade({CascadeType.SAVE_UPDATE, CascadeType.MERGE})
+	private TermTree<? extends Feature> descriptiveSystem;
 
 	@XmlElementWrapper(name = "Descriptions")
 	@XmlElement(name = "Description")
@@ -307,10 +307,10 @@ public class DescriptiveDataSet extends IdentifiableEntity<DescriptiveDataSetDef
 		}
 	}
 
-	public TermTree<Character> getDescriptiveSystem() {
+	public TermTree<? extends Feature> getDescriptiveSystem() {
 		return descriptiveSystem;
 	}
-	public void setDescriptiveSystem(TermTree<Character> descriptiveSystem) {
+	public void setDescriptiveSystem(TermTree<? extends Feature> descriptiveSystem) {
 		this.descriptiveSystem = descriptiveSystem;
 	}
 
--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/DescriptiveDataSetService.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/DescriptiveDataSetService.java
@@ -452,7 +452,7 @@ public class DescriptiveDataSetService
         DescriptiveDataSet dataSet = load(descriptiveDataSetUuid);
         SpecimenOrObservationBase specimen = occurrenceService.load(specimenUuid);
 
-        Set<Character> datasetFeatures = dataSet.getDescriptiveSystem().getDistinctTerms();
+        Set<? extends Feature> datasetFeatures = dataSet.getDescriptiveSystem().getDistinctTerms();
         List<DescriptionElementBase> matchingDescriptionElements = new ArrayList<>();
 
         for (SpecimenDescription specimenDescription : (Set<SpecimenDescription>) specimen.getDescriptions()) {
