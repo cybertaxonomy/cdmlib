9ecae4a9e80a0fe751d303a2c6b7f3714b8f0d74
Andreas Müller <a.mueller@bgbm.org>
2019-09-24 07:36:14 +0200

    unignore EuroPlusMedCondensedDistributionComposerTest

--- cdmlib-ext/src/test/java/eu/etaxonomy/cdm/ext/geo/EuroPlusMedCondensedDistributionComposerTest.java
+++ cdmlib-ext/src/test/java/eu/etaxonomy/cdm/ext/geo/EuroPlusMedCondensedDistributionComposerTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import eu.etaxonomy.cdm.api.service.dto.CondensedDistribution;
@@ -28,7 +27,7 @@ import eu.etaxonomy.cdm.test.TermTestBase;
  * @author a.mueller
  * @since 15.06.2016
  */
-@Ignore
+//@Ignore
 public class EuroPlusMedCondensedDistributionComposerTest extends TermTestBase {
 
     @Test
