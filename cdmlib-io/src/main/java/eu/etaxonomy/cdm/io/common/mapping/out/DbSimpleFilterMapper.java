869b76f8d35649605085d233f395df609560f677
Andreas Müller <a.mueller@bgbm.org>
2019-10-11 13:48:20 +0200

    ref #2717 improve SimpleFilterMapper to use allowed and not allowed values

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/common/mapping/out/DbSimpleFilterMapper.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/common/mapping/out/DbSimpleFilterMapper.java
@@ -1,8 +1,8 @@
 /**
 * Copyright (C) 2007 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -11,6 +11,8 @@ package eu.etaxonomy.cdm.io.common.mapping.out;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -20,36 +22,48 @@ import eu.etaxonomy.cdm.model.common.CdmBase;
 
 /**
  * This mapper removes objects from the stream of objects to be mapped.
- * This is a very perliminarye implementation. In future there will be at least an 
+ * This is a very preliminary implementation. In future there will be at least an
  * interface to implement and some more parameter to better define which values to include/exclude.
- * 
+ *
  * Current implementation
- * 
+ *
  * @author a.mueller
  * @since 13.02.2012
  */
 public class DbSimpleFilterMapper extends DbSingleAttributeExportMapperBase<DbExportStateBase<?, IExportTransformer>> {
 	private static final Logger logger = Logger.getLogger(DbSimpleFilterMapper.class);
-	
-	
-	
+
+	/**
+	 * Returns a filter mapper which filters out objects which have
+	 * <code>null</code> value for the given source attribute.
+	 */
 	public static DbSimpleFilterMapper NewSingleNullAttributeInstance(String cdmFilterCriterionAttribute, String reason){
-		return new DbSimpleFilterMapper(null, cdmFilterCriterionAttribute, null, reason);
+	    Set<Object> notAllowedValues = new HashSet<>();
+	    notAllowedValues.add(null);
+	    return new DbSimpleFilterMapper(null, cdmFilterCriterionAttribute, null, notAllowedValues, null, reason);
 	}
 
+    public static DbSimpleFilterMapper NewAllowedValueInstance(String cdmFilterCriterionAttribute,
+            Set<?> allowedValues, Set<?> notAllowedValues, String reason){
+        return new DbSimpleFilterMapper(null, cdmFilterCriterionAttribute,
+                allowedValues, notAllowedValues, null, reason);
+    }
+
 //*************************** VARIABLES ***************************************************************//
+
 	private String filterReason;
-	
+	private Set<?> allowedValues;
+	private Set<?> notAllowedValues;
+
+
 //*************************** CONSTRUCTOR ***************************************************************//
-	
-	/**
-	 * @param dbAttributString
-	 * @param cdmAttributeString
-	 * @param defaultValue
-	 */
-	protected DbSimpleFilterMapper(String dbAttributString, String cdmAttributeString, Object defaultValue, String filterReason) {
+
+	protected DbSimpleFilterMapper(String dbAttributString, String cdmAttributeString,
+	        Set<?> allowedValues, Set<?> notAllowedValues, Object defaultValue, String filterReason) {
 		super(cdmAttributeString, dbAttributString, defaultValue);
 		this.filterReason = filterReason;
+		this.allowedValues = allowedValues;
+		this.notAllowedValues = notAllowedValues;
 	}
 
 	@Override
@@ -71,28 +85,25 @@ public class DbSimpleFilterMapper extends DbSingleAttributeExportMapperBase<DbEx
 			return true; // do nothing
 		}
 	}
-	
 
 	private boolean isFiltered(CdmBase cdmBase) {
 		Object value = super.getValue(cdmBase);
-		return (value == null);
+		if (allowedValues != null && !allowedValues.contains(value)){
+		    return true;  //filtered out
+		}else if (notAllowedValues.contains(value)){
+		    return true;
+		}else{
+		    return false;
+		}
 	}
 
-	/* (non-Javadoc)
-	 * @see eu.etaxonomy.cdm.io.common.CdmSingleAttributeMapperBase#getTypeClass()
-	 */
 	@Override
 	public Class getTypeClass() {
 		return null;  //not needed
 	}
-	
 
 	@Override
 	protected int getSqlType() {
 		return -1;  // not needed
 	}
-
-	
-	
-	
 }
