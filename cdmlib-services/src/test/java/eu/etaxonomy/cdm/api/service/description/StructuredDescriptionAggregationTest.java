6d2d9cf51daf0875a1ad8716095cd39bcd9d4b4c
Andreas Müller <a.mueller@bgbm.org>
2019-11-21 08:58:05 +0100

    ref #8713 add test class for StructuredDescriptionAggregation

---
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/description/StructuredDescriptionAggregationTest.java
@@ -0,0 +1,149 @@
+/**
+* Copyright (C) 2019 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.api.service.description;
+
+import java.io.FileNotFoundException;
+import java.util.UUID;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.unitils.spring.annotation.SpringBeanByType;
+
+import eu.etaxonomy.cdm.api.application.ICdmRepository;
+import eu.etaxonomy.cdm.api.service.IClassificationService;
+import eu.etaxonomy.cdm.api.service.IDescriptionService;
+import eu.etaxonomy.cdm.api.service.IReferenceService;
+import eu.etaxonomy.cdm.api.service.ITaxonService;
+import eu.etaxonomy.cdm.api.service.ITermService;
+import eu.etaxonomy.cdm.model.name.IBotanicalName;
+import eu.etaxonomy.cdm.model.name.Rank;
+import eu.etaxonomy.cdm.model.name.TaxonNameFactory;
+import eu.etaxonomy.cdm.model.reference.Reference;
+import eu.etaxonomy.cdm.model.reference.ReferenceFactory;
+import eu.etaxonomy.cdm.model.taxon.Classification;
+import eu.etaxonomy.cdm.model.taxon.Taxon;
+import eu.etaxonomy.cdm.model.taxon.TaxonNode;
+import eu.etaxonomy.cdm.test.integration.CdmTransactionalIntegrationTest;
+
+/**
+ * @author a.mueller
+ * @since 21.11.2019
+ */
+public class StructuredDescriptionAggregationTest extends CdmTransactionalIntegrationTest {
+
+    @SuppressWarnings("unused")
+    private static Logger logger = Logger.getLogger(DistributionAggregationTest.class);
+
+    private static final UUID T_LAPSANA_UUID = UUID.fromString("f65d47bd-4f49-4ab1-bc4a-bc4551eaa1a8");
+
+    private static final UUID T_LAPSANA_COMMUNIS_UUID = UUID.fromString("2a5ceebb-4830-4524-b330-78461bf8cb6b");
+
+    private static final UUID T_LAPSANA_COMMUNIS_COMMUNIS_UUID = UUID.fromString("441a3c40-0c84-11de-8c30-0800200c9a66");
+
+    private static final UUID T_LAPSANA_COMMUNIS_ADENOPHORA_UUID = UUID.fromString("e4acf200-63b6-11dd-ad8b-0800200c9a66");
+
+    private static final UUID T_LAPSANA_COMMUNIS_ALPINA_UUID = UUID.fromString("596b1325-be50-4b0a-9aa2-3ecd610215f2");
+
+    private static final UUID CLASSIFICATION_UUID = UUID.fromString("4b266053-a841-4980-b548-3f21d8d7d712");
+
+    @SpringBeanByType
+    private ICdmRepository repository;
+
+    @SpringBeanByType
+    private ITermService termService;
+
+    @SpringBeanByType
+    private IDescriptionService descriptionService;
+
+    @SpringBeanByType
+    private ITaxonService taxonService;
+
+    @SpringBeanByType
+    private IClassificationService classificationService;
+
+    @SpringBeanByType
+    private IReferenceService referenceService;
+
+    private StructuredDescriptionAggregation engine;
+
+    @Test
+    @Override
+    public void createTestDataSet() throws FileNotFoundException {
+
+        // --- References --- //
+        Reference sec = ReferenceFactory.newDatabase();
+        sec.setTitleCache("Test", true);
+        Reference nomRef = ReferenceFactory.newBook();
+        sec.setTitleCache("Sp.Pl.", true);
+
+        referenceService.save(sec);
+        referenceService.save(nomRef);
+
+
+        // --- Taxa --- //
+        //  Lapsana
+        //        L. communis
+        //            L. communis subsp. communis
+        //            L. communis subsp. adenophora
+        //            L. communis subsp. alpina
+        //  Sonchella
+        //        S. dentata
+        //        S. stenoma
+        IBotanicalName n_lapsana = TaxonNameFactory.NewBotanicalInstance(Rank.GENUS());
+        n_lapsana.setTitleCache("Lapsana", true);
+        Taxon t_lapsana = Taxon.NewInstance(n_lapsana, sec);
+        t_lapsana.setUuid(T_LAPSANA_UUID);
+        taxonService.saveOrUpdate(t_lapsana);
+
+        IBotanicalName n_lapsana_communis = TaxonNameFactory.NewBotanicalInstance(Rank.SPECIES());
+        n_lapsana_communis.setTitleCache("L. communis", true);
+        Taxon t_lapsana_communis = Taxon.NewInstance(n_lapsana_communis, sec);
+        t_lapsana_communis.setUuid(T_LAPSANA_COMMUNIS_UUID);
+        taxonService.saveOrUpdate(t_lapsana_communis);
+
+        IBotanicalName n_lapsana_communis_communis = TaxonNameFactory.NewBotanicalInstance(Rank.SUBSPECIES());
+        n_lapsana_communis_communis.setTitleCache("L. communis subsp. communis", true);
+        Taxon t_lapsana_communis_communis = Taxon.NewInstance(n_lapsana_communis_communis, sec);
+        t_lapsana_communis_communis.setUuid(T_LAPSANA_COMMUNIS_COMMUNIS_UUID);
+        taxonService.saveOrUpdate(t_lapsana_communis_communis);
+
+        IBotanicalName n_lapsana_communis_adenophora = TaxonNameFactory.NewBotanicalInstance(Rank.SUBSPECIES());
+        n_lapsana_communis_adenophora.setTitleCache("L. communis subsp. adenophora", true);
+        Taxon t_lapsana_communis_adenophora = Taxon.NewInstance(n_lapsana_communis_adenophora, sec);
+        t_lapsana_communis_adenophora.setUuid(T_LAPSANA_COMMUNIS_ADENOPHORA_UUID);
+        taxonService.saveOrUpdate(t_lapsana_communis_adenophora);
+
+        IBotanicalName n_lapsana_communis_alpina = TaxonNameFactory.NewBotanicalInstance(Rank.SUBSPECIES());
+        n_lapsana_communis_alpina.setTitleCache("L. communis subsp. alpina", true);
+        Taxon t_lapsana_communis_alpina = Taxon.NewInstance(n_lapsana_communis_alpina, sec);
+        t_lapsana_communis_alpina.setUuid(T_LAPSANA_COMMUNIS_ALPINA_UUID);
+        taxonService.saveOrUpdate(t_lapsana_communis_alpina);
+
+        // --- Classification --- //
+        Classification classification = Classification.NewInstance("TestClassification");
+        classification.setUuid(CLASSIFICATION_UUID);
+        classificationService.save(classification);
+        TaxonNode node_lapsana = classification.addChildTaxon(t_lapsana, sec, null);
+        TaxonNode node_lapsana_communis = node_lapsana.addChildTaxon(t_lapsana_communis, sec, null);
+        node_lapsana_communis.addChildTaxon(t_lapsana_communis_communis, sec, null);
+        node_lapsana_communis.addChildTaxon(t_lapsana_communis_adenophora, sec, null);
+        node_lapsana_communis.addChildTaxon(t_lapsana_communis_alpina, sec, null);
+        classificationService.saveOrUpdate(classification);
+
+        commitAndStartNewTransaction(null);
+
+        writeDbUnitDataSetFile(new String[] {
+                "TAXONBASE", "TAXONNAME",
+                "REFERENCE", "DESCRIPTIONELEMENTBASE", "DESCRIPTIONBASE",
+                "AGENTBASE", "CLASSIFICATION",  "TAXONNODE",
+                "HOMOTYPICALGROUP", "LANGUAGESTRING",
+                "HIBERNATE_SEQUENCES"
+         });
+    }
+}
