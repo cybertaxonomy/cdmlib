79ff326b55e92b7d000d4b8bd50f6d292905ea6d
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2020-03-26 17:19:04 +0100

    ref #8907 basing testGetTaxonNodeUuidAndTitleCacheOfacceptedTaxaByClassificationForNameWithoutRank on TaxonNodeDaoHibernateImplTest.findWithoutRank.xml

--- cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/dao/hibernate/taxon/TaxonNodeDaoHibernateImplTest.java
+++ cdmlib-persistence/src/test/java/eu/etaxonomy/cdm/persistence/dao/hibernate/taxon/TaxonNodeDaoHibernateImplTest.java
@@ -35,7 +35,6 @@ import eu.etaxonomy.cdm.model.common.CdmBase;
 import eu.etaxonomy.cdm.model.common.Language;
 import eu.etaxonomy.cdm.model.common.LanguageString;
 import eu.etaxonomy.cdm.model.name.Rank;
-import eu.etaxonomy.cdm.model.name.TaxonName;
 import eu.etaxonomy.cdm.model.name.TaxonNameFactory;
 import eu.etaxonomy.cdm.model.taxon.Classification;
 import eu.etaxonomy.cdm.model.taxon.Taxon;
@@ -352,22 +351,15 @@ public class TaxonNodeDaoHibernateImplTest extends CdmTransactionalIntegrationTe
 
     }
     @Test
-    @DataSet ("TaxonDaoHibernateImplTest.testGetTaxaByNameAndArea.xml")
+    @DataSet ("TaxonNodeDaoHibernateImplTest.findWithoutRank.xml")
     public final void testGetTaxonNodeUuidAndTitleCacheOfacceptedTaxaByClassificationForNameWithoutRank(){
         //test name without rank
-        Classification classification = classificationDao.findByUuid(classificationUuid);
-        TaxonName name = TaxonNameFactory.NewBotanicalInstance(null);
-        Taxon taxon = Taxon.NewInstance(name, null);
-        taxon.setTitleCache("Ricris asplenioid UNPLACED", false);
-        TaxonNode node = taxonNodeDao.load(UUID.fromString("6d6b43aa-3a77-4be5-91d0-00b702fc5d6e"));
-        TaxonNode child = node.addChildTaxon(taxon, null, null);
-        taxonNodeDao.saveOrUpdate(child);
-
-        String pattern = "Ricris asplenioid UNPLACED*";
+        Classification classification = classificationDao.findByUuid(ClassificationUuid);
+        String pattern = "Acherontia kohlbeckeri*";
         List<UuidAndTitleCache<TaxonNode>> result = taxonNodeDao.getTaxonNodeUuidAndTitleCacheOfAcceptedTaxaByClassification(classification, null, pattern, true);
         assertNotNull(result);
         assertEquals(1, result.size());
-        assertEquals("Ricris asplenioid UNPLACED", result.get(0).getTitleCache());
+        assertEquals("4f73adcc-a535-4fbe-a97a-c05ee8b12191", result.get(0).getUuid().toString()); // titleCache:Acherontia kohlbeckeri rank: Unknown Rank
 
     }
 
