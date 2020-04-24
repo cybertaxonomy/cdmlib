43804768f3e2aa68b509b97c7ed49793bfbc6109
Andreas Müller <a.mueller@bgbm.org>
2020-04-24 17:46:40 +0200

    fix #8964 update later homonym labels and titleCaches

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/v512_515/SchemaUpdater_5120_5150.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/v512_515/SchemaUpdater_5120_5150.java
@@ -55,6 +55,21 @@ public class SchemaUpdater_5120_5150 extends SchemaUpdaterBase {
 
 		List<ISchemaUpdaterStep> stepList = new ArrayList<>();
 
+	    //#8964 update label for later homonym
+        stepName = "update label for later homonym";
+        UUID uuidTerm = UUID.fromString("80f06f65-58e0-4209-b811-cb40ad7220a6");
+        String label = "is later homonym of";
+        UUID uuidLanguage = Language.uuidEnglish;
+        TermRepresentationUpdater.NewInstanceWithTitleCache(stepList, stepName,
+                uuidTerm, label, label, null, uuidLanguage);
+
+        stepName = "update label for treated as later homonym";
+        uuidTerm = UUID.fromString("2990a884-3302-4c8b-90b2-dfd31aaa2778");
+        label = "is treated as later homonym of";
+        uuidLanguage = Language.uuidEnglish;
+        TermRepresentationUpdater.NewInstanceWithTitleCache(stepList, stepName,
+                uuidTerm, label, label, null, uuidLanguage);
+
         return stepList;
     }
 
