30ae270f7a3c0b67df4ac0288ba3eb5442c4e680
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2020-04-28 17:40:38 +0200

    ref #6113 tempororily disabling the OptionsController

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/OptionsController.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/controller/OptionsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
-@Controller
+// @Controller
 public class OptionsController {
 
 	@RequestMapping(
