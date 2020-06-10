9a666f9ecdaff7a574bc48ea0ed8e2fba4a9b975
Andreas Müller <a.mueller@bgbm.org>
2020-06-10 13:20:11 +0200

    ref #9061 a very first try to create an import analyzer framework (with example AltitudeExcelImport)

---
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/fact/altitude/in/AltitudeExcelFormatAnalyzer.java
@@ -0,0 +1,36 @@
+/**
+* Copyright (C) 2020 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.io.fact.altitude.in;
+
+import eu.etaxonomy.cdm.io.fact.altitude.in.analyze.ExcelFormatAnalyzer;
+
+/**
+ * @author a.mueller
+ * @since 02.06.2020
+ */
+public class AltitudeExcelFormatAnalyzer extends ExcelFormatAnalyzer {
+
+    private AltitudeExcelFormatAnalyzer() {
+        super(requiredColumns(), optionalColumns(), optionalMultiColumns());
+        // TODO Auto-generated constructor stub
+    }
+
+    private static String[] requiredColumns() {
+        return new String[]{"taxonUuid", "Altitude Min", "Altitude Max"};
+    }
+
+    private static String[] optionalColumns() {
+        return new String[]{"nameCache", "nameFullCache"};
+    }
+    private static String[] optionalMultiColumns() {
+        return new String[]{};
+    }
+
+
+}
