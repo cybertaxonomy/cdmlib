d846e295450ab6a0df7a64552f7789a48ca22a93
Andreas Müller <a.mueller@bgbm.org>
2020-06-27 11:01:28 +0200

    ref #9071 add additional ExtendedTimePeriodFormatter test

--- cdmlib-model/src/test/java/eu/etaxonomy/cdm/format/common/ExtendedTimePeriodFormatterTest.java
+++ cdmlib-model/src/test/java/eu/etaxonomy/cdm/format/common/ExtendedTimePeriodFormatterTest.java
@@ -25,6 +25,8 @@ public class ExtendedTimePeriodFormatterTest {
 
     private ExtendedTimePeriodFormatter formatter;
 
+    private String endash = TimePeriod.SEP;
+
     @Before
     public void setUp() throws Exception {
         formatter = ExtendedTimePeriodFormatter.NewDefaultInstance();
@@ -44,9 +46,9 @@ public class ExtendedTimePeriodFormatterTest {
         Integer endDay2 = 20;
         Integer endMonth2 = 11;
         Integer endYear2 = 1800;
+
         ExtendedTimePeriod tp1 = ExtendedTimePeriod.NewExtendedYearInstance(startYear,endYear,startYear2,endYear2);
 
-        String endash = TimePeriod.SEP;
         assertNotNull(tp1);
         Assert.assertEquals("(1787"+endash+")1788"+endash+"1799("+endash+"1800)", formatter.format(tp1));
         tp1.setStartDay(startDay);
@@ -58,6 +60,21 @@ public class ExtendedTimePeriodFormatterTest {
         tp1.setExtremeEndDay(endDay2);
         tp1.setExtremeEndMonth(endMonth2);
         Assert.assertEquals("(24 May 1787"+endash+")25 Jun 1788"+endash+"21 Dec 1799("+endash+"20 Nov 1800)", formatter.format(tp1)); //date formatting may change in future
+
+        tp1 = ExtendedTimePeriod.NewExtendedMonthInstance(startMonth, endMonth, startMonth2, endMonth2);
+
+        assertNotNull(tp1);
+        Assert.assertEquals("(May"+endash+")Jun"+endash+"Dec("+endash+"Nov)", formatter.format(tp1));
+        tp1.setStartDay(startDay);
+        tp1.setStartMonth(startMonth);
+        tp1.setEndDay(endDay);
+        tp1.setEndMonth(endMonth);
+        tp1.setExtremeStartDay(startDay2);
+        tp1.setExtremeStartMonth(startMonth2);
+        tp1.setExtremeEndDay(endDay2);
+        tp1.setExtremeEndMonth(endMonth2);
+        Assert.assertEquals("(24 May"+endash+")25 Jun"+endash+"21 Dec("+endash+"20 Nov)", formatter.format(tp1)); //date formatting may change in future
+
 
         tp1.setFreeText("My extended period");
         Assert.assertEquals("My extended period", formatter.format(tp1));
