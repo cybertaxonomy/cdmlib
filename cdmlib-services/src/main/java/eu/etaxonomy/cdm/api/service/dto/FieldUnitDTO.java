6d399b0dc16d542534cc699a0fe1ca1f6fd47451
Andreas Müller <a.mueller@bgbm.org>
2020-09-09 16:37:23 +0200

    cleanup

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/dto/FieldUnitDTO.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/dto/FieldUnitDTO.java
@@ -1,10 +1,16 @@
+/**
+* Copyright (C) 2016 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
 package eu.etaxonomy.cdm.api.service.dto;
 
 import eu.etaxonomy.cdm.model.occurrence.FieldUnit;
 
-
-
-public class FieldUnitDTO extends DerivateDTO{
+public class FieldUnitDTO extends DerivateDTO {
 
     private static final long serialVersionUID = 3981843956067273220L;
 
@@ -15,14 +21,9 @@ public class FieldUnitDTO extends DerivateDTO{
 	private String collectionString;
 
 	private boolean hasType;
-	//private List<UUID> taxonRelatedDerivedUnits = new ArrayList<>();
 
 	private GatheringEventDTO gatheringEvent;
 
-
-	/**
-     * @param fieldUnit
-     */
     public FieldUnitDTO(FieldUnit fieldUnit) {
         super(fieldUnit);
         if (fieldUnit.getGatheringEvent() != null){
@@ -32,70 +33,39 @@ public class FieldUnitDTO extends DerivateDTO{
         setListLabel(fieldUnit.getTitleCache());
     }
 
-    /**
-     * @return the country
-     */
     public String getCountry() {
         return country;
     }
-    /**
-     * @param country the country to set
-     */
     public void setCountry(String country) {
         this.country = country;
     }
-    /**
-     * @return the collectionString
-     */
+
     @Override
     public String getCollection() {
         return collectionString;
     }
-    /**
-     * @param collectionString the collectionString to set
-     */
     @Override
     public void setCollection(String collection) {
         this.collectionString = collection;
     }
 
-    /**
-     * @return the collectionString
-     */
-
     public String getCollectingString() {
         return collectingString;
     }
-    /**
-     * @param collectionString the collectionString to set
-     */
     public void setCollectingString(String collectingString) {
         this.collectingString = collectingString;
     }
 
-    /**
-     * @return the date
-     */
     public String getDate() {
         return date;
     }
-    /**
-     * @param date the date to set
-     */
     public void setDate(String date) {
         this.date = date;
     }
 
-
-    /**
-     * @return the hasType
-     */
     public boolean isHasType() {
         return hasType;
     }
-    /**
-     * @param hasType the hasType to set
-     */
     public void setHasType(boolean hasType) {
         this.hasType = hasType;
     }
@@ -106,33 +76,4 @@ public class FieldUnitDTO extends DerivateDTO{
     public void setGatheringEvent(GatheringEventDTO gatheringEvent) {
         this.gatheringEvent = gatheringEvent;
     }
-
-
-//    /**
-//     * @return the taxonRelatedDerivedUnits
-//     */
-//    public List<UUID> getTaxonRelatedDerivedUnits() {
-//        return taxonRelatedDerivedUnits;
-//    }
-//
-//
-//    /**
-//     * @param taxonRelatedDerivedUnits the taxonRelatedDerivedUnits to set
-//     */
-//    public void setTaxonRelatedDerivedUnits(List<UUID> taxonRelatedDerivedUnits) {
-//        this.taxonRelatedDerivedUnits = taxonRelatedDerivedUnits;
-//    }
-
-
-//    /**
-//     * @param derivedUnitDTO
-//     */
-//    public void addTaxonRelatedDerivedUnits(DerivateDTO derivedUnitDTO) {
-//        if (this.taxonRelatedDerivedUnits == null){
-//            this.taxonRelatedDerivedUnits = new ArrayList<>();
-//        }
-//        this.taxonRelatedDerivedUnits.add(derivedUnitDTO.getUuid());
-//
-//    }
-
 }
