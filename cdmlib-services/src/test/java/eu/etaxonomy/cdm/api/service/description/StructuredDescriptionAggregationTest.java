d8798a32c8c1e62691a22417889eb026c5153955
Andreas Müller <a.mueller@bgbm.org>
2020-09-16 11:19:04 +0200

    fix logging in StructuredDescriptionAggregationTest

--- cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/description/StructuredDescriptionAggregationTest.java
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/description/StructuredDescriptionAggregationTest.java
@@ -77,7 +77,7 @@ import eu.etaxonomy.cdm.test.unitils.CleanSweepInsertLoadStrategy;
 public class StructuredDescriptionAggregationTest extends CdmTransactionalIntegrationTest {
 
     @SuppressWarnings("unused")
-    private static Logger logger = Logger.getLogger(DistributionAggregationTest.class);
+    private static Logger logger = Logger.getLogger(StructuredDescriptionAggregationTest.class);
 
     private static final UUID T_LAPSANA_UUID = UUID.fromString("f65d47bd-4f49-4ab1-bc4a-bc4551eaa1a8");
     private static final UUID TN_LAPSANA_UUID = UUID.fromString("f4d29e9f-6484-4184-af2e-9704e96a17e3");
