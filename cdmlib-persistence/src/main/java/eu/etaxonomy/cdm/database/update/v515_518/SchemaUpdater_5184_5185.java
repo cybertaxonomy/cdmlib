c3fed23d5ff764d64c9a7a0595fedaadcd700f40
Andreas Müller <a.mueller@bgbm.org>
2020-11-14 18:41:33 +0100

    ref #6591 fix SchemaUpdater_5184_5185 preliminary and add update script for moving original spellings

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/v515_518/SchemaUpdater_5184_5185.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/v515_518/SchemaUpdater_5184_5185.java
@@ -16,6 +16,8 @@ import org.apache.log4j.Logger;
 import eu.etaxonomy.cdm.database.update.ISchemaUpdater;
 import eu.etaxonomy.cdm.database.update.ISchemaUpdaterStep;
 import eu.etaxonomy.cdm.database.update.SchemaUpdaterBase;
+import eu.etaxonomy.cdm.database.update.SimpleSchemaUpdaterStep;
+import eu.etaxonomy.cdm.database.update.SingleTermRemover;
 import eu.etaxonomy.cdm.model.metadata.CdmMetaData.CdmVersion;
 
 /**
@@ -28,7 +30,8 @@ public class SchemaUpdater_5184_5185 extends SchemaUpdaterBase {
 	private static final Logger logger = Logger.getLogger(SchemaUpdater_5184_5185.class);
 
 	private static final CdmVersion startSchemaVersion = CdmVersion.V_05_18_04;
-	private static final CdmVersion endSchemaVersion = CdmVersion.V_05_18_05;
+	//FIXME
+	private static final CdmVersion endSchemaVersion = CdmVersion.V_05_18_04;
 
 // ********************** FACTORY METHOD *************************************
 
@@ -45,6 +48,15 @@ public class SchemaUpdater_5184_5185 extends SchemaUpdaterBase {
 
 		List<ISchemaUpdaterStep> stepList = new ArrayList<>();
 
+        //#6591
+        OriginalSpellingMover.NewInstance(stepList);
+
+        String stepName = "remove original spelling name relationship type";
+        String uuidTerm = "264d2be4-e378-4168-9760-a9512ffbddc4";
+        String checkUsedQueries = "SELECT count(*) FROM @@NameRelationship@@ nr "
+                + " INNER JOIN @@DefinedTermBase@@ nrType ON nrType.id = nr.type_id "
+                + " WHERE nrType.uuid = '264d2be4-e378-4168-9760-a9512ffbddc4'";
+        SingleTermRemover.NewInstance(stepList, stepName, uuidTerm, checkUsedQueries, -99);
 
         return stepList;
     }
