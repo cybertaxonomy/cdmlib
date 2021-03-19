a9a9d9f07b3d65d0d5df9fa2454cf7c1b4fca7ae
Andreas Müller <a.mueller@bgbm.org>
2021-03-19 13:28:09 +0100

    cleanup and fix compile error related to ref #9515

--- cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/specimen/gbif/in/GbifImportState.java
+++ cdmlib-io/src/main/java/eu/etaxonomy/cdm/io/specimen/gbif/in/GbifImportState.java
@@ -27,14 +27,11 @@ import eu.etaxonomy.cdm.model.taxon.Classification;
 /**
  * @author k.luther
  * @since 15.07.2016
- *
  */
-public class GbifImportState extends SpecimenImportStateBase<GbifImportConfigurator<GbifImportState, InputStream>, GbifImportState> {
+public class GbifImportState
+        extends SpecimenImportStateBase<GbifImportConfigurator<GbifImportState, InputStream>, GbifImportState> {
 
     private TransactionStatus tx;
-
-
-
 
     private ICdmRepository cdmRepository;
 
@@ -49,66 +46,49 @@ public class GbifImportState extends SpecimenImportStateBase<GbifImportConfigura
     private GbifDataHolder dataHolder;
     private DerivedUnit derivedUnitBase;
 
-    private List<OriginalSourceBase> associationRefs = new ArrayList<OriginalSourceBase>();
+    private List<OriginalSourceBase> associationRefs = new ArrayList<>();
     private boolean associationSourcesSet=false;
-    private List<OriginalSourceBase> descriptionRefs = new ArrayList<OriginalSourceBase>();
+    private List<OriginalSourceBase> descriptionRefs = new ArrayList<>();
     private boolean descriptionSourcesSet=false;
-    private List<OriginalSourceBase> derivedUnitSources = new ArrayList<OriginalSourceBase>();
+    private List<OriginalSourceBase> derivedUnitSources = new ArrayList<>();
     private boolean derivedUnitSourcesSet=false;
     private boolean descriptionGroupSet = false;
     private TaxonDescription descriptionGroup = null;
 
-
-
-
     public GbifImportState newInstance(GbifImportConfigurator config){
         GbifImportState result = new GbifImportState(config);
         return result;
-
     }
 
-
-    /* ------Getter/Setter -----*/
-
-    /**
-     * @param config
-     * @return
-     */
     private GbifImportState(GbifImportConfigurator config) {
         super(config);
         setReport(new SpecimenImportReport());
         setTransformer(new AbcdTransformer());
     }
 
+    /* ------Getter/Setter -----*/
+
     @Override
     public TransactionStatus getTx() {
         return tx;
     }
-
-
     @Override
     public void setTx(TransactionStatus tx) {
         this.tx = tx;
     }
 
-
     @Override
     public ICdmRepository getCdmRepository() {
         return cdmRepository;
     }
-
-
     @Override
     public void setCdmRepository(ICdmRepository cdmRepository) {
         this.cdmRepository = cdmRepository;
     }
 
-
     public String getPrefix() {
         return prefix;
     }
-
-
     public void setPrefix(String prefix) {
         this.prefix = prefix;
     }
@@ -118,167 +98,125 @@ public class GbifImportState extends SpecimenImportStateBase<GbifImportConfigura
     public SpecimenImportReport getReport() {
         return report;
     }
-
-
     public void setReport(GbifImportReport report) {
         this.report = report;
     }
-
 
     @Override
     public Classification getClassification() {
         return classification;
     }
-
-
     @Override
     public void setClassification(Classification classification) {
         this.classification = classification;
     }
 
-
     @Override
     public Classification getDefaultClassification(boolean createIfNotExist) {
         return defaultClassification;
     }
-
-
     @Override
     public void setDefaultClassification(Classification defaultClassification) {
         this.defaultClassification = defaultClassification;
     }
 
-
     @Override
     public Reference getRef() {
         return ref;
     }
-
-
     @Override
     public void setRef(Reference ref) {
         this.ref = ref;
     }
 
-
     @Override
     public GbifDataHolder getDataHolder() {
         return dataHolder;
     }
-
-
     public void setDataHolder(GbifDataHolder dataHolder) {
         this.dataHolder = dataHolder;
     }
-
 
     @Override
     public DerivedUnit getDerivedUnitBase() {
         return derivedUnitBase;
     }
-
-
     @Override
     public void setDerivedUnitBase(DerivedUnit derivedUnitBase) {
         this.derivedUnitBase = derivedUnitBase;
     }
 
-
     @Override
     public List<OriginalSourceBase> getAssociationRefs() {
         return associationRefs;
     }
-
-
     @Override
     public void setAssociationRefs(List<OriginalSourceBase> associationRefs) {
         this.associationRefs = associationRefs;
     }
 
-
     @Override
     public boolean isAssociationSourcesSet() {
         return associationSourcesSet;
     }
-
-
     @Override
     public void setAssociationSourcesSet(boolean associationSourcesSet) {
         this.associationSourcesSet = associationSourcesSet;
     }
 
-
     @Override
     public List<OriginalSourceBase> getDescriptionRefs() {
         return descriptionRefs;
     }
-
-
     @Override
     public void setDescriptionRefs(List<OriginalSourceBase> descriptionRefs) {
         this.descriptionRefs = descriptionRefs;
     }
 
-
     @Override
     public boolean isDescriptionSourcesSet() {
         return descriptionSourcesSet;
     }
-
-
     @Override
     public void setDescriptionSourcesSet(boolean descriptionSourcesSet) {
         this.descriptionSourcesSet = descriptionSourcesSet;
     }
-
 
     @Override
     public List<OriginalSourceBase> getDerivedUnitSources() {
         return derivedUnitSources;
     }
 
-
     @Override
     public void setDerivedUnitSources(List<OriginalSourceBase> derivedUnitSources) {
         this.derivedUnitSources = derivedUnitSources;
     }
 
-
     @Override
     public boolean isDerivedUnitSourcesSet() {
         return derivedUnitSourcesSet;
     }
-
-
     @Override
     public void setDerivedUnitSourcesSet(boolean derivedUnitSourcesSet) {
         this.derivedUnitSourcesSet = derivedUnitSourcesSet;
     }
 
-
     @Override
     public boolean isDescriptionGroupSet() {
         return descriptionGroupSet;
     }
-
-
     @Override
     public void setDescriptionGroupSet(boolean descriptionGroupSet) {
         this.descriptionGroupSet = descriptionGroupSet;
     }
-
 
 //    @Override
 //    public TaxonDescription getDescriptionGroup() {
 //        return descriptionGroup;
 //    }
 
-
     @Override
-    public void setDescriptionGroup(TaxonDescription descriptionGroup) {
+    public void setIndividualsAssociationDescriptionPerTaxon(TaxonDescription descriptionGroup) {
         this.descriptionGroup = descriptionGroup;
     }
-
-
-
 }
