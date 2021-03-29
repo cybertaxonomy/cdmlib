5fd823745b9a8604ba1739b5d2414b1182fe2fa6
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2021-03-29 11:53:05 +0200

    ref #9555 IEnumTerms with uuid in json

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/json/processor/value/KeyTermValueProcessor.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/json/processor/value/KeyTermValueProcessor.java
@@ -12,6 +12,7 @@ import org.apache.log4j.Logger;
 
 import eu.etaxonomy.cdm.api.service.l10n.KeyTerm_L10n;
 import eu.etaxonomy.cdm.model.taxon.TaxonNodeStatus;
+import eu.etaxonomy.cdm.model.term.IEnumTerm;
 import eu.etaxonomy.cdm.model.term.IKeyTerm;
 import net.sf.json.JSONNull;
 import net.sf.json.JSONObject;
@@ -45,6 +46,9 @@ public class KeyTermValueProcessor implements JsonValueProcessor  {
         }
         KeyTerm_L10n<?> keyTerm_L10n = new KeyTerm_L10n<>(term);
         json.element("representation_L10n", keyTerm_L10n.localizedLabel());
+        if(IEnumTerm.class.isAssignableFrom(term.getClass())) {
+            json.element("uuid", ((IEnumTerm)term).getUuid().toString());
+        }
         if(term instanceof TaxonNodeStatus) {
             json.element("symbol", ((TaxonNodeStatus)term).getSymbol());
         }
