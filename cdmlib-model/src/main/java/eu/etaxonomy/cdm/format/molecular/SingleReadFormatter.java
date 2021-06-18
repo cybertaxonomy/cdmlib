c4b1960b9100af8c84e36f838d19e4a78133437c
Andreas Müller <a.mueller@bgbm.org>
2021-06-18 16:13:21 +0200

    move SingleReadFormatter and SequenceFormatter to molecular package

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/format/molecular/SingleReadFormatter.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/format/molecular/SingleReadFormatter.java
@@ -6,7 +6,7 @@
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
-package eu.etaxonomy.cdm.format.occurrences;
+package eu.etaxonomy.cdm.format.molecular;
 
 import eu.etaxonomy.cdm.format.AbstractCdmFormatter;
 import eu.etaxonomy.cdm.model.molecular.SingleRead;
