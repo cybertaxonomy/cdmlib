a4df60bc3532d91b32a11654d10b01328a3fcd50
Katja Luther <k.luther@bgbm.org>
2021-07-01 09:52:18 +0200

    ref #9680: adapt searches to search also for collector title cache - add TeamOrPersonUuidAndTitleCache

---
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dto/TeamOrPersonUuidAndTitleCache.java
@@ -0,0 +1,47 @@
+/**
+* Copyright (C) 2021 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.persistence.dto;
+
+import java.util.UUID;
+
+import eu.etaxonomy.cdm.model.common.ICdmBase;
+
+/**
+ * @author k.luther
+ * @since Jun 30, 2021
+ */
+public class TeamOrPersonUuidAndTitleCache<T extends ICdmBase> extends UuidAndTitleCache<T> {
+
+    private static final long serialVersionUID = 3083330169541901724L;
+
+    private String collectorTitleCache;
+
+    /**
+     * @param uuid
+     * @param id
+     * @param titleCache
+     * @param abbrevTitleCache
+     */
+    public TeamOrPersonUuidAndTitleCache(UUID uuid, Integer id, String titleCache, String abbrevTitleCache, String collectorTitleCache) {
+        super(uuid, id, titleCache, abbrevTitleCache);
+        this.setCollectorTitleCache(collectorTitleCache);
+    }
+
+    public String getCollectorTitleCache() {
+        return collectorTitleCache;
+    }
+
+    public void setCollectorTitleCache(String collectorTitleCache) {
+        this.collectorTitleCache = collectorTitleCache;
+    }
+
+
+
+
+}
