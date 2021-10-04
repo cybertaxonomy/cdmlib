19732d4ed294695306dce5d3df1a7be219c62b59
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2021-10-04 13:46:44 +0200

    ref #9797 disabling and deprecating TaxonRelationshipBeanProcessor

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/json/processor/bean/TaxonRelationshipBeanProcessor.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/json/processor/bean/TaxonRelationshipBeanProcessor.java
@@ -1,36 +1,75 @@
 /**
+
 * Copyright (C) 2009 EDIT
+
 * European Distributed Institute of Taxonomy
+
 * http://www.e-taxonomy.eu
+
 *
+
 * The contents of this file are subject to the Mozilla Public License Version 1.1
+
 * See LICENSE.TXT at the top of this package for the full license terms.
+
 */
+
 package eu.etaxonomy.cdm.remote.json.processor.bean;
+
+
 
 import java.util.List;
 
+
+
 import eu.etaxonomy.cdm.model.taxon.TaxonRelationship;
+
 import net.sf.json.JSONObject;
+
 import net.sf.json.JsonConfig;
 
+
+
 /**
+
  * @author a.kohlbecker
+
  * @since 09.06.2009
+
+ * @deprecated unused
+
  */
+
+@Deprecated
+
 public class TaxonRelationshipBeanProcessor extends AbstractCdmBeanProcessor<TaxonRelationship> {
 
-    @Override
-    public List<String> getIgnorePropNames() {
-        return null;
-    }
+
 
     @Override
+
+    public List<String> getIgnorePropNames() {
+
+        return null;
+
+    }
+
+
+
+    @Override
+
     public JSONObject processBeanSecondStep(TaxonRelationship bean, JSONObject json, JsonConfig jsonConfig) {
 
+
+
         json.element("fromTaxon", bean.getFromTaxon(), jsonConfig);
+
         json.element("toTaxon", bean.getToTaxon(), jsonConfig);
 
+
+
         return json;
+
     }
+
 }
