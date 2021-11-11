e9d64907af6dc39a00cead4e6fc0bb3d786c1224
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2021-11-11 13:46:55 +0100

    ref #6161 PasswordReset message templates in separate class - 2.

---
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/security/PasswordResetTemplates.java
@@ -0,0 +1,30 @@
+/**
+* Copyright (C) 2021 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.api.service.security;
+
+/**
+ * @author a.kohlbecker
+ * @since Nov 11, 2021
+ */
+public class PasswordResetTemplates {
+
+    public static final String RESET_REQUEST_EMAIL_SUBJECT_TEMPLATE = "Your password reset request for ${userName}";
+    public static final String RESET_REQUEST_EMAIL_BODY_TEMPLATE = "You are receiving this email because a password reset was requested for your account at the ${dataBase}"
+            + " data base. If this was not initiated by you, please ignore this message."
+            + "\n\nPlease click ${linkUrl} to reset your password";
+
+    public static final String RESET_SUCCESS_EMAIL_SUBJECT_TEMPLATE = "Your password for ${userName} has been changed";
+    public static final String RESET_SUCCESS_EMAIL_BODY_TEMPLATE = "The password of your account at the ${dataBase} data base has just been changed."
+            + "\n\nIf this was not initiated by you, please contact the adminitrator as soon as possible.";
+
+    public static final String RESET_FAILED_EMAIL_SUBJECT_TEMPLATE = "Changing your password for ${userName} has failed";
+    public static final String RESET_FAILED_EMAIL_BODY_TEMPLATE = "The attempt to change the password of your account at the ${dataBase} data base has failed."
+            + "\n\nIf this was not initiated by you, please contact the adminitrator as soon as possible.";
+
+}
