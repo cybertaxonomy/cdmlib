5296291744901a5628418b59c93514417f40be38
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2021-11-18 10:57:42 +0100

    ref #9497 AccountCreationRequest and TokenStore

---
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/security/AccountCreationRequestTokenStore.java
@@ -0,0 +1,34 @@
+/**
+* Copyright (C) 2021 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.api.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import eu.etaxonomy.cdm.api.service.IUserService;
+import eu.etaxonomy.cdm.model.permission.User;
+
+/**
+ * @author a.kohlbecker
+ * @since Nov 3, 2021
+ */
+@Component
+public class AccountCreationRequestTokenStore extends AbstractRequestTokenStore<AccountCreationRequest> {
+
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    public AccountCreationRequest createNewToken(User user, String randomToken, int tokenLifetimeMinutes) {
+        userService.encodeUserPassword(user, user.getPassword());
+        AccountCreationRequest token = new AccountCreationRequest(user.getUsername(), user.getEmailAddress(), user.getPassword(), randomToken, tokenLifetimeMinutes);
+        return token;
+    }
+
+}
