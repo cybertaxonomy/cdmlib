e07545e6b8af70b6156c958092a1e91496cd34eb
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2022-01-21 13:53:38 +0100

    fix #9925 harmonizing env variable key and template placeholder

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/security/AccountSelfManagementService.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/security/AccountSelfManagementService.java
@@ -83,7 +83,7 @@ public abstract class AccountSelfManagementService implements IRateLimitedServic
             additionalValuesMap = new HashedMap<>();
         }
         if(supportEmailAddress != null) {
-            additionalValuesMap.put("supportEmailAddress", supportEmailAddress);
+            additionalValuesMap.put(CdmConfigurationKeys.MAIL_ADDRESS_SUPPORT, supportEmailAddress);
         }
         if(userName != null) {
             additionalValuesMap.put("userName", userName);
