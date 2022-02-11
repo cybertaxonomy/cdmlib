94a6d9b8ca321de16645f0d0d1fcb3b38fb3d2b7
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2022-02-11 11:49:35 +0100

    -m ref

---
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/json/processor/bean/TimePeriodBeanProcessor.java
@@ -0,0 +1,26 @@
+/**
+* Copyright (C) 2022 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.remote.json.processor.bean;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
+
+/**
+ * @author a.kohlbecker
+ * @since Feb 11, 2022
+ */
+public class TimePeriodBeanProcessor extends AbstractModifiableThingBeanProcessor {
+
+    @Override
+    public JSONObject processBeanSecondStep(Object bean, JSONObject json, JsonConfig jsonConfig) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
