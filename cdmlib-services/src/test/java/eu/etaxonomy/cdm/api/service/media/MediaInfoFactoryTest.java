c77a9a7fe36610a33703e7b150e1b27e2f617bfc
Andreas Müller <a.mueller@bgbm.org>
2022-04-04 19:38:06 +0200

    fix #10020 add mediahub to metadataservice transformation including tests

--- cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/media/MediaInfoFactoryTest.java
+++ cdmlib-services/src/test/java/eu/etaxonomy/cdm/api/service/media/MediaInfoFactoryTest.java
@@ -15,12 +15,17 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.util.List;
 
 import org.apache.http.HttpException;
+import org.cybertaxonomy.media.info.model.MediaInfo;
 import org.junit.Before;
 import org.junit.Test;
 import org.unitils.spring.annotation.SpringBeanByType;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import eu.etaxonomy.cdm.common.URI;
 import eu.etaxonomy.cdm.common.UriUtils;
@@ -34,23 +39,26 @@ import eu.etaxonomy.cdm.test.integration.CdmIntegrationTest;
 public class MediaInfoFactoryTest extends CdmIntegrationTest {
 
     @SpringBeanByType
-    private IMediaInfoFactory mediaInfoFactory;
+    private MediaInfoFactory mediaInfoFactory;
 
     private URI transformableURI_ScalerAPI;
     private URI transformableURI_IIIF_API;
 
     private URI notTransformableURI;
 
+    private URI mediahubUri;
+
     @Before
     public void initUris() throws URISyntaxException {
         transformableURI_ScalerAPI = new URI("https://pictures.bgbm.org/digilib/Scaler?fn=Cyprus/Sisymbrium_aegyptiacum_C1.jpg&mo=file");
         transformableURI_IIIF_API = new URI("https://pictures.bgbm.org/digilib/Scaler/IIIF/Cichorieae!Lactuca_serriola_Bc_08.jpg/full/full/0/default.jpg");
         notTransformableURI = new URI(MediaInfoFactoryTest.class.getResource("./images/OregonScientificDS6639-DSC_0307-small.jpg"));
+        mediahubUri = new URI("https://mediahub.bo.berlin/api/File/Original/1adb62d8-0b67-4128-927b-b713d164f98e/Sisymbrium_aegyptiacum_E1.JPG");
     }
 
     @Test
     public void readMediaInfoViaFileReader() throws IOException, HttpException {
-        assertTrue(((MediaInfoFactory)mediaInfoFactory).applyURITransformations(notTransformableURI).isEmpty());
+        assertTrue(mediaInfoFactory.applyURITransformations(notTransformableURI).isEmpty());
         CdmImageInfo cdmImageInfo = mediaInfoFactory.cdmImageInfo(notTransformableURI, true);
         assertNotNull(cdmImageInfo);
         assertEquals(225, cdmImageInfo.getHeight());
@@ -59,7 +67,7 @@ public class MediaInfoFactoryTest extends CdmIntegrationTest {
 
     @Test
     public void readMediaInfoViaServiceReaderScalerAPI() throws IOException, HttpException, URISyntaxException {
-        assertEquals(1, ((MediaInfoFactory)mediaInfoFactory).applyURITransformations(transformableURI_ScalerAPI).size());
+        assertEquals(1, mediaInfoFactory.applyURITransformations(transformableURI_ScalerAPI).size());
         if(UriUtils.isInternetAvailable(new URI("https://image.bgbm.org/"))){
             CdmImageInfo cdmImageInfo = mediaInfoFactory.cdmImageInfo(transformableURI_ScalerAPI, true);
             assertNotNull(cdmImageInfo);
@@ -81,7 +89,7 @@ public class MediaInfoFactoryTest extends CdmIntegrationTest {
 
     @Test
     public void readMediaInfoViaServiceReaderIIIF_API() throws IOException, HttpException, URISyntaxException {
-        assertEquals(1, ((MediaInfoFactory)mediaInfoFactory).applyURITransformations(transformableURI_IIIF_API).size());
+        assertEquals(1, mediaInfoFactory.applyURITransformations(transformableURI_IIIF_API).size());
         if(UriUtils.isInternetAvailable(new URI("https://image.bgbm.org/"))){
             CdmImageInfo cdmImageInfo = mediaInfoFactory.cdmImageInfo(transformableURI_IIIF_API, true);
             assertNotNull(cdmImageInfo);
@@ -106,8 +114,22 @@ public class MediaInfoFactoryTest extends CdmIntegrationTest {
         }
     }
 
-    @Override
-    public void createTestDataSet() throws FileNotFoundException {
-        // not needed
+    @Test
+    public void testMediaMetadataServiceTransformation() throws URISyntaxException, IOException, HttpException {
+        List<URI> list = mediaInfoFactory.applyURITransformations(mediahubUri);
+        assertEquals(1, list.size());
+        URI transformedUri = list.get(0);
+        assertEquals("https://image.bgbm.org/metadata/info?file=mediacloud/org1/1adb62d8-0b67-4128-927b-b713d164f98e/original", transformedUri.toString());
+        if(UriUtils.isInternetAvailable(new URI("https://image.bgbm.org/"))){
+            InputStream jsonStream = UriUtils.getInputStream(transformedUri);
+            ObjectMapper mapper = new ObjectMapper();
+            MediaInfo mediaInfo = mapper.readValue(jsonStream, MediaInfo.class);
+            assertEquals(937, mediaInfo.getHeight());
+            assertEquals(1400, mediaInfo.getWidth());
+        }
     }
+
+
+    @Override
+    public void createTestDataSet() throws FileNotFoundException {}
 }
