4df43dd4e362ac7a3311aa4af06e99d6d58dd42b
Katja Luther <k.luther@bgbm.org>
2022-06-10 13:00:42 +0200

    resolve the merge

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/CdmPersistentDataSource.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/CdmPersistentDataSource.java
@@ -21,8 +21,10 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import org.hibernate.cache.spi.RegionFactory;
 import org.jdom.Attribute;
 import org.jdom.Element;
@@ -77,6 +79,16 @@ public class CdmPersistentDataSource
 	}
 
 	/**
+<<<<<<< Updated upstream
+=======
+	 * Returns the default CdmDataSource
+	 */
+	public final static CdmPersistentDataSource NewLocalHsqlInstance() throws DataSourceNotFoundException{
+		return NewInstance("localDefaultHsql");
+	}
+
+	/**
+>>>>>>> Stashed changes
 	 * Returns the CdmDataSource named by strDataSource
 	 * @param strDataSource
 	 * @return
---
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/types/HSqlDbDatabaseType.java
@@ -0,0 +1,97 @@
+/**
+* Copyright (C) 2007 EDIT
+* European Distributed Institute of Taxonomy 
+* http://www.e-taxonomy.eu
+* 
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+
+package eu.etaxonomy.cdm.database.types;
+
+import javax.sql.DataSource;
+
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.HSQLCorrectedDialect;
+
+import eu.etaxonomy.cdm.database.ICdmDataSource;
+import eu.etaxonomy.cdm.database.LocalHsqldb;
+
+
+/**
+ * @author a.mueller
+ *
+ */
+public class HSqlDbDatabaseType extends DatabaseTypeBase {
+
+	//typeName
+	private String typeName = "Hypersonic SQL DB (HSqlDb)";
+   
+	//class
+	private String classString = "org.hsqldb.jdbcDriver";
+    
+	//url
+	private String urlString = "jdbc:hsqldb:hsql://";
+    
+    //port
+    private int defaultPort = 9001;
+    
+    //hibernate dialect
+    private Dialect hibernateDialect = new HSQLCorrectedDialect();
+    
+    //init method
+    private String initMethod = "init";
+    
+    //destroy method
+    private String destroyMethod = "destroy";
+    
+    //connection String
+	public String getConnectionString(ICdmDataSource ds, int port){
+        return urlString + ds.getServer() + ":" + port + dbSeparator + ds.getDatabase();
+    }
+	
+	private static String dbSeparator = "/";
+	
+    /* (non-Javadoc)
+     * @see eu.etaxonomy.cdm.database.types.DatabaseTypeBase#getServerNameByConnectionString(java.lang.String)
+     */
+    public String getDatabaseNameByConnectionString(String connectionString){
+    	String result;
+    	result = getDatabasePartOfConnectionString(connectionString, dbSeparator);
+    	//TODO
+//    	int posParams = result.indexOf("?");
+//    	if (posParams != -1){
+//    		result = result.substring(0, posParams);
+//    	}
+     	return result;
+    }
+	
+    
+    public HSqlDbDatabaseType() {
+		init (typeName, classString, urlString, defaultPort,  hibernateDialect );
+	}
+
+	@Override
+	public Class<? extends DataSource> getDataSourceClass() {
+		return LocalHsqldb.class;
+	}
+
+	/* (non-Javadoc)
+	 * @see eu.etaxonomy.cdm.database.types.IDatabaseType#getInitMethod()
+	 */
+	@Override
+	public String getInitMethod() {
+		return initMethod;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see eu.etaxonomy.cdm.database.types.IDatabaseType#getDestroyMethod()
+	 */
+	@Override
+	public String getDestroyMethod() {
+		return destroyMethod;
+	}
+
+
+}
