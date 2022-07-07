d1ea83168fa754c4bafa61818a14a29ebc6f9d80
Andreas Müller <a.mueller@bgbm.org>
2022-07-07 10:55:31 +0200

    cleanup

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/name/NameRelationshipType.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/name/NameRelationshipType.java
@@ -23,7 +23,8 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
-import org.apache.logging.log4j.LogManager;import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.annotations.Type;
 import org.hibernate.envers.Audited;
 
