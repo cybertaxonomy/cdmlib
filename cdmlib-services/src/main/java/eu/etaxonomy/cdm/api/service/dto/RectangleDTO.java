ed9555a5526f0ac739f31e4b00d50dbb756eb221
Andreas Müller <a.mueller@bgbm.org>
2022-08-23 13:34:49 +0200

    ref #10009, ref #9359 replace hibernate search Rectangle by RectangleDTO

---
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/dto/RectangleDTO.java
@@ -0,0 +1,45 @@
+/**
+* Copyright (C) 2022 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.api.service.dto;
+
+/**
+ * @author a.mueller
+ * @date 23.08.2022
+ */
+public class RectangleDTO {
+
+    private final double lowerLeftLatitude;
+    private final double lowerLeftLongitude;
+    private final double upperRightLatitude;
+    private final double upperRightLongitude;
+
+    public RectangleDTO(double lowerLeftLatitude, double lowerLeftLongitude, double upperRightLatitude,
+            double upperRightLongitude) {
+        this.lowerLeftLatitude = lowerLeftLatitude;
+        this.lowerLeftLongitude = lowerLeftLongitude;
+        this.upperRightLatitude = upperRightLatitude;
+        this.upperRightLongitude = upperRightLongitude;
+    }
+
+    public Double getLowerLeftLatitude() {
+        return lowerLeftLatitude;
+    }
+
+    public double getLowerLeftLongitude() {
+        return lowerLeftLongitude;
+    }
+
+    public double getUpperRightLatitude() {
+        return upperRightLatitude;
+    }
+
+    public double getUpperRightLongitude() {
+        return upperRightLongitude;
+    }
+}
