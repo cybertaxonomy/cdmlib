cbe13549e0a30b0dd4e3553cb48c7a34ead84e3b
Andreas Müller <a.mueller@bgbm.org>
2022-12-27 13:02:33 +0100

    fix #3408, ref #9359 remove springmodules dependency by workaround in AbstractOaiPmhController
    * fix fully after upgrading to spring 5.x and EhCache 3.x

--- cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/json/processor/matcher/CGLibEnhancedBeanProcessorMatcher.java
+++ cdmlib-remote/src/main/java/eu/etaxonomy/cdm/remote/json/processor/matcher/CGLibEnhancedBeanProcessorMatcher.java
@@ -1,33 +1,32 @@
 /**
  * Copyright (C) 2009 EDIT European Distributed Institute of Taxonomy
  * http://www.e-taxonomy.eu
- * 
+ *
  * The contents of this file are subject to the Mozilla Public License Version
  * 1.1 See LICENSE.TXT at the top of this package for the full license terms.
  */
-
 package eu.etaxonomy.cdm.remote.json.processor.matcher;
 
 import java.util.Set;
 
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.json.processors.JsonBeanProcessorMatcher;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.cglib.proxy.Enhancer;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+//import net.sf.cglib.proxy.Enhancer;
+import net.sf.json.processors.JsonBeanProcessorMatcher;
 
 public class CGLibEnhancedBeanProcessorMatcher extends JsonBeanProcessorMatcher {
 
-	private static Log log = LogFactory.getLog(CGLibEnhancedBeanProcessorMatcher.class);
-	
+	private static final Logger logger = LogManager.getLogger();
+
 	@Override
 	public Object getMatch(Class target, Set set) {
-		
+
 		if (Enhancer.isEnhanced(target)) {
-			log.debug("Found enhanced object of class " + target.getClass() + " returning " + target.getSuperclass());
+			logger.debug("Found enhanced object of class " + target.getClass() + " returning " + target.getSuperclass());
 			return DEFAULT.getMatch(target.getSuperclass(), set);
         }
         return DEFAULT.getMatch(target, set);
 	}
-
 }
