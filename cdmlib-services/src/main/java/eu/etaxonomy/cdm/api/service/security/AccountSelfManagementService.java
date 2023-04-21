5b7cd11129e8192e6245904763e7ae83f4fda045
Katja Luther <k.luther@bgbm.org>
2023-04-21 08:31:30 +0200

    ref #10037: trying to send the email with a different from address

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/security/AccountSelfManagementService.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/security/AccountSelfManagementService.java
@@ -106,7 +106,12 @@ public abstract class AccountSelfManagementService implements IRateLimitedServic
         try {
             emailSender.send(message);
         }catch(MailException e) {
+            logger.debug("Sending e-mail from docker instance.");
             ((JavaMailSenderImpl)emailSender).setHost("host.docker.internal");
+
+            //only for testing!!!
+            message.setFrom("root@edit-integration.bgbm.fu-berlin.de");
+
             emailSender.send(message);
         }
     }
