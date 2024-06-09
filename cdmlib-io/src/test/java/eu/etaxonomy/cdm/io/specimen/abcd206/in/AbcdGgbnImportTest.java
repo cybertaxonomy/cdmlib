6b92006f774816e8bec6d6787f2d6237d2f27fc5
Andreas Müller <a.mueller@bgbm.org>
2024-06-09 07:10:15 +0200

    cleanup

--- cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/specimen/abcd206/in/AbcdGgbnImportTest.java
+++ cdmlib-io/src/test/java/eu/etaxonomy/cdm/io/specimen/abcd206/in/AbcdGgbnImportTest.java
@@ -135,11 +135,9 @@ public class AbcdGgbnImportTest extends CdmTransactionalIntegrationTest {
         Pager<Taxon> taxonPager = taxonService.findByTitleWithRestrictions(Taxon.class, "Campanula isaurica", MatchMode.ANYWHERE, null, null, null, null, null);
         assertEquals(1, taxonPager.getRecords().size());
         //test for correct nomenclatural code
-        assertEquals(NomenclaturalCode.ICNAFP, taxonPager.getRecords().get(0).getName().getNomenclaturalCode());
+        assertEquals(NomenclaturalCode.ICNAFP, taxonPager.getRecords().get(0).getName().getNameType());
 
         //test for sortindex=-1
-
-
 	}
 
 	/**
@@ -226,8 +224,10 @@ public class AbcdGgbnImportTest extends CdmTransactionalIntegrationTest {
         //dna sample
         FindOccurrencesConfigurator dnaConfig = new FindOccurrencesConfigurator();
         dnaConfig.setSignificantIdentifier("DB 6");
+        @SuppressWarnings("rawtypes")
         List<SpecimenOrObservationBase> dnaRecords = occurrenceService.findByTitle(dnaConfig).getRecords();
         assertEquals(1, dnaRecords.size());
+        @SuppressWarnings("rawtypes")
         SpecimenOrObservationBase dnaSpecimen = dnaRecords.iterator().next();
         assertEquals(DnaSample.class, dnaSpecimen.getClass());
         DnaSample dnaSample = (DnaSample) dnaSpecimen;
@@ -253,10 +253,9 @@ public class AbcdGgbnImportTest extends CdmTransactionalIntegrationTest {
         assertEquals(1, identifiers.size());
         assertEquals("CAM010", identifiers.iterator().next());
 
-
         //dna quality
         DnaQuality dnaQuality = dnaSample.getDnaQuality();
-        assertNotNull("Dna quality is null", dnaQuality!=null);
+        assertNotNull("Dna quality is null", dnaQuality);
         assertEquals(new Double("0.77"),dnaQuality.getRatioOfAbsorbance260_230());
         assertEquals(new Double("1.38"),dnaQuality.getRatioOfAbsorbance260_280());
         assertEquals(new DateTime(2008, 4, 15, 0, 0),dnaQuality.getQualityCheckDate());
@@ -371,9 +370,11 @@ public class AbcdGgbnImportTest extends CdmTransactionalIntegrationTest {
 	    //associated specimen
 	    FindOccurrencesConfigurator config = new FindOccurrencesConfigurator();
 	    config.setSignificantIdentifier("B 10 0066577");
-	    List<SpecimenOrObservationBase> records = occurrenceService.findByTitle(config).getRecords();
+	    @SuppressWarnings("rawtypes")
+        List<SpecimenOrObservationBase> records = occurrenceService.findByTitle(config).getRecords();
 	    assertEquals(1, records.size());
-	    SpecimenOrObservationBase derivedUnitSpecimen = records.iterator().next();
+	    @SuppressWarnings("rawtypes")
+        SpecimenOrObservationBase derivedUnitSpecimen = records.iterator().next();
 	    assertEquals(DerivedUnit.class, derivedUnitSpecimen.getClass());
 	    DerivedUnit specimen = (DerivedUnit) derivedUnitSpecimen;
 	    assertEquals("Herbarium Berolinense", specimen.getCollection().getCode());
@@ -382,9 +383,11 @@ public class AbcdGgbnImportTest extends CdmTransactionalIntegrationTest {
 	    //dna sample
 	    FindOccurrencesConfigurator dnaConfig = new FindOccurrencesConfigurator();
 	    dnaConfig.setSignificantIdentifier("DB 6");
-	    List<SpecimenOrObservationBase> dnaRecords = occurrenceService.findByTitle(dnaConfig).getRecords();
+	    @SuppressWarnings("rawtypes")
+        List<SpecimenOrObservationBase> dnaRecords = occurrenceService.findByTitle(dnaConfig).getRecords();
 	    assertEquals(1, dnaRecords.size());
-	    SpecimenOrObservationBase dnaSpecimen = dnaRecords.iterator().next();
+	    @SuppressWarnings("rawtypes")
+        SpecimenOrObservationBase dnaSpecimen = dnaRecords.iterator().next();
 	    assertEquals(DnaSample.class, dnaSpecimen.getClass());
 	    DnaSample dnaSample = (DnaSample) dnaSpecimen;
 	    DerivationEvent derivedFrom = dnaSample.getDerivedFrom();
@@ -426,7 +429,8 @@ public class AbcdGgbnImportTest extends CdmTransactionalIntegrationTest {
 	    //associated specimen
 	    FindOccurrencesConfigurator config = new FindOccurrencesConfigurator();
 	    config.setSignificantIdentifier("B 10 0066577");
-	    List<SpecimenOrObservationBase> records = occurrenceService.findByTitle(config).getRecords();
+	    @SuppressWarnings("rawtypes")
+        List<SpecimenOrObservationBase> records = occurrenceService.findByTitle(config).getRecords();
 	    assertEquals(1, records.size());
 	    SpecimenOrObservationBase<?> derivedUnitSpecimen = records.iterator().next();
 	    assertEquals(DerivedUnit.class, derivedUnitSpecimen.getClass());
@@ -437,7 +441,8 @@ public class AbcdGgbnImportTest extends CdmTransactionalIntegrationTest {
 	    //dna sample
 	    FindOccurrencesConfigurator dnaConfig = new FindOccurrencesConfigurator();
 	    dnaConfig.setSignificantIdentifier("DB 6");
-	    List<SpecimenOrObservationBase> dnaRecords = occurrenceService.findByTitle(dnaConfig).getRecords();
+	    @SuppressWarnings("rawtypes")
+        List<SpecimenOrObservationBase> dnaRecords = occurrenceService.findByTitle(dnaConfig).getRecords();
 	    assertEquals(1, dnaRecords.size());
 	    SpecimenOrObservationBase<?> dnaSpecimen = dnaRecords.iterator().next();
 	    assertEquals(DnaSample.class, dnaSpecimen.getClass());
