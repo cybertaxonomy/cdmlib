a57d7664e787da5c4abb4d11d40f8455d56fe7d7
Andreas Müller <a.mueller@bgbm.org>
2024-07-04 11:01:20 +0200

    ref #10492 at notNull constraint to annotation.annotationType

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/common/Annotation.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/model/common/Annotation.java
@@ -16,6 +16,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -77,6 +78,7 @@ public class Annotation extends LanguageStringBase implements IIntextReferencabl
     @XmlIDREF
     @XmlSchemaType(name = "IDREF")
     @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
     private AnnotationType annotationType;
 
     // for external annotations/comments the URI of these can be set.
