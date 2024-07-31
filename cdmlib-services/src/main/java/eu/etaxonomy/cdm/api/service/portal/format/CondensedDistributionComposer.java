75298784bc2a700c4da166d8ca1fa9ffdf15cfcc
Andreas Müller <a.mueller@bgbm.org>
2024-07-31 09:59:34 +0200

    fix potential NPE in CondensedDistributionComposer

--- cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/portal/format/CondensedDistributionComposer.java
+++ cdmlib-services/src/main/java/eu/etaxonomy/cdm/api/service/portal/format/CondensedDistributionComposer.java
@@ -436,7 +436,7 @@ public class CondensedDistributionComposer {
             CondensedDistributionConfiguration config, String parentAreaLabel) {
 
         //TODO config with symbols, not only idInVocabulary
-        String label = getSymbol(config.areaSymbolField, area, langs);
+        String label = CdmUtils.Nz(getSymbol(config.areaSymbolField, area, langs));
         if (config.shortenSubAreaLabelsIfPossible && parentAreaLabel != null && !parentAreaLabel.isEmpty()){
             //TODO make brackets not hardcoded, but also allow [],- etc., but how?
             if (label.startsWith(parentAreaLabel+"(") && label.endsWith(")") ){
