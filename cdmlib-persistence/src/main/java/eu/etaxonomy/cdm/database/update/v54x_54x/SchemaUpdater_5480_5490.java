461c4064002143e485acce6e27d8b733e225b9aa
Andreas Müller <a.mueller@bgbm.org>
2025-01-02 14:30:26 +0100

    create update script for cdm 5.49.0

---
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/database/update/v54x_54x/SchemaUpdater_5480_5490.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2024 EDIT
+ * European Distributed Institute of Taxonomy
+ * http://www.e-taxonomy.eu
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * See LICENSE.TXT at the top of this package for the full license terms.
+ */
+package eu.etaxonomy.cdm.database.update.v54x_54x;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import eu.etaxonomy.cdm.database.update.ISchemaUpdater;
+import eu.etaxonomy.cdm.database.update.ISchemaUpdaterStep;
+import eu.etaxonomy.cdm.database.update.SchemaUpdaterBase;
+import eu.etaxonomy.cdm.model.metadata.CdmMetaData.CdmVersion;
+
+/**
+ * @author a.mueller
+ * @date 2024-10-17
+ */
+public class SchemaUpdater_5480_5490 extends SchemaUpdaterBase {
+
+	@SuppressWarnings("unused")
+	private static final Logger logger = LogManager.getLogger();
+
+	private static final CdmVersion startSchemaVersion = CdmVersion.V_05_48_00;
+	private static final CdmVersion endSchemaVersion = CdmVersion.V_05_49_00;
+
+// ********************** FACTORY METHOD *************************************
+
+    @Override
+    public ISchemaUpdater getPreviousUpdater() {
+        return SchemaUpdater_5461_5480.NewInstance();
+    }
+
+	public static SchemaUpdater_5480_5490 NewInstance() {
+		return new SchemaUpdater_5480_5490();
+	}
+
+	SchemaUpdater_5480_5490() {
+		super(startSchemaVersion.versionString(), endSchemaVersion.versionString());
+	}
+
+    @Override
+	protected List<ISchemaUpdaterStep> getUpdaterList() {
+
+		String stepName;
+
+		List<ISchemaUpdaterStep> stepList = new ArrayList<>();
+
+
+
+        return stepList;
+    }
+}
