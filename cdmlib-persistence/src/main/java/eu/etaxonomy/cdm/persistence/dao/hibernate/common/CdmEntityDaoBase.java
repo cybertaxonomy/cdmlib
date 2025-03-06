ff19ecab203873bc42e3db666f1b812b77a66826
Katja <k.luther@bgbm.org>
2025-03-06 14:23:11 +0100

    ref #10708: logging for merged descriptions on server side

--- cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/hibernate/common/CdmEntityDaoBase.java
+++ cdmlib-persistence/src/main/java/eu/etaxonomy/cdm/persistence/dao/hibernate/common/CdmEntityDaoBase.java
@@ -56,6 +56,7 @@ import org.springframework.util.ReflectionUtils;
 import eu.etaxonomy.cdm.model.common.CdmBase;
 import eu.etaxonomy.cdm.model.common.IPublishable;
 import eu.etaxonomy.cdm.model.common.VersionableEntity;
+import eu.etaxonomy.cdm.model.description.DescriptionBase;
 import eu.etaxonomy.cdm.model.permission.User;
 import eu.etaxonomy.cdm.model.view.AuditEvent;
 import eu.etaxonomy.cdm.persistence.dao.common.ICdmEntityDao;
@@ -283,6 +284,14 @@ public abstract class CdmEntityDaoBase<T extends CdmBase>
                 result = new MergeResult(transientObject, PostMergeEntityListener.getNewEntities(session));
             } else {
                 result = new MergeResult(persistentObject, null);
+            }
+            if (transientObject instanceof DescriptionBase) {
+                logger.info("merged object: "+result.getMergedEntity().getUuid() + " - " + result.getMergedEntity().getUserFriendlyTypeName());
+                for(Object newEntity : result.getNewEntities()) {
+                    if (newEntity instanceof CdmBase) {
+                        logger.info("new object: "+((CdmBase)newEntity).getUuid() + " - " + ((CdmBase)newEntity).getUserFriendlyTypeName());
+                    }
+                }
             }
             return result;
         } finally {
