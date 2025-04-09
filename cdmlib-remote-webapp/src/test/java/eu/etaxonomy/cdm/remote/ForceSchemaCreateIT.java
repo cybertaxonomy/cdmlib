55f96d9a9d59b8ff5b112fe03c523574fc06ebb5
Katja <k.luther@bgbm.org>
2025-04-09 11:41:18 +0200

    try to run swagger tests

--- cdmlib-remote-webapp/src/test/java/eu/etaxonomy/cdm/remote/ForceSchemaCreateIT.java
+++ cdmlib-remote-webapp/src/test/java/eu/etaxonomy/cdm/remote/ForceSchemaCreateIT.java
@@ -10,7 +10,6 @@ package eu.etaxonomy.cdm.remote;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -22,7 +21,7 @@ public class ForceSchemaCreateIT extends WebServiceTestBase {
     private static final Logger logger = LogManager.getLogger();
 
     @Test
-    @Ignore
+    //@Ignore
     public void checkInstanceIsOnline(){
         String response = httpGetJson("metadata", null);
         logger.debug("response: " + response);
