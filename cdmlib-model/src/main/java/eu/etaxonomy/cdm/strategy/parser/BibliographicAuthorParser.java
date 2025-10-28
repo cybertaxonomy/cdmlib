f3601a30bd9275b6257ff20ee8b8c8466af4da22
Andreas Müller <a.mueller@bgbm.org>
2025-10-28 12:21:58 +0100

    ref #10790 adapt initials regex of BibliographicAuthorParser to regex used in CollectorParser

--- cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/parser/BibliographicAuthorParser.java
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/parser/BibliographicAuthorParser.java
@@ -35,7 +35,7 @@ public class BibliographicAuthorParser {
             + UTF8.CAPITAL_O_ACUTE
             + UTF8.CAPITAL_U_ACUTE
             + "]";
-    private static String initialsRe = "("+initialChars+"\\.?(\\s|"+UTF8.ANY_DASH_RE()+")?|(del?|de la|de los|v[ao]n)\\s*){1,5}";
+    private static String initialsRe = "("+initialChars+"\\.?(\\s|"+UTF8.ANY_DASH_RE()+")?|(de(l|\\sla|\\slos)?|v[ao]n)\\s*){1,5}";
     private static String initialsStrictRe = "((?!"+initialsRe+"\\s).)*\\s+("+initialsRe+")";
     private static Pattern pattern = Pattern.compile(initialsStrictRe);
 
@@ -96,7 +96,7 @@ public class BibliographicAuthorParser {
             Matcher matcher = pattern.matcher(str);
             if (matcher.matches()) {
                 //initials not separated by comma
-                String initials = matcher.group(5);
+                String initials = matcher.group(6);
                 String family = str.replaceAll(initials + "$", "").trim();
                 person.setFamilyName(family);
                 person.setInitials(initials.trim());
