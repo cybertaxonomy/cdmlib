312f3f8265bf6f0fd0dd5c489dc63988deb7ec47
Andreas Müller <a.mueller@bgbm.org>
2025-10-28 12:08:28 +0100

    ref #10840. ref #10790 Add ParserResult

---
+++ cdmlib-commons/src/main/java/eu/etaxonomy/cdm/common/ResultBase.java
@@ -0,0 +1,116 @@
+/**
+* Copyright (C) 2025 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.etaxonomy.cdm.common.ResultBase.ResultInfoBase;
+
+/**
+ * @author muellera
+ * @since 27.10.2025
+ */
+public abstract class ResultBase<T extends ResultInfoBase> {
+
+    private List<T> errors = new ArrayList<>();
+    private List<T> warnings = new ArrayList<>();
+    private List<T> exceptions = new ArrayList<>();
+
+    public static abstract class ResultInfoBase{
+        String message;
+        Exception exception;
+        String codeLocation;
+
+        public ResultInfoBase(String msg, Exception e, String codeLocation){
+            this.message = msg;
+            this.exception = e;
+            this.codeLocation = codeLocation;
+        }
+        public String getMessage(){return message;}
+        public Exception getException(){return exception;}
+        public String getCodeLocation(){return codeLocation;}
+    }
+
+    // ************* GETTERS / SETTERS / ADDERS ***********************/
+
+    //errors
+    public List<T> getErrors() {return errors;}
+    public void setErrors(List<T> resultInfos) {this.errors = resultInfos;}
+    public void addError(String message) {
+        addError(message, null, getLocationByException());
+    }
+    public void addError(String message, Exception e) {
+        addError(message, e, null);
+    }
+    public void addError(String message, String codeLocation) {
+        addError(message, null, codeLocation);
+    }
+    public void addError(String message, Exception e, String codeLocation) {
+        errors.add(newResultInfo(message, e, codeLocation));
+    }
+
+    //warnings
+    public List<T> getWarnings() {return warnings;}
+    public void setWarnings(List<T> warnings) {this.warnings = warnings;}
+    public void addWarning(String message) {
+        addWarning(message, getLocationByException());
+    }
+    public void addWarning(String message, String codeLocation) {
+        warnings.add(newResultInfo(message, null, codeLocation));
+    }
+
+    //exceptions
+    public List<T> getExceptions() {return exceptions;}
+    public void setExceptions(List<T> exceptions) {this.exceptions = exceptions;}
+    public void addException(Exception e) {
+        addException(e, null, null);
+    }
+    public void addException(Exception e, String message) {
+        addException(e, message, null);
+    }
+    public void addException(Exception e, String message, String codeLocation) {
+        exceptions.add(newResultInfo(message, e, makeLocation(e, codeLocation)));
+//        setExceptionState();
+    }
+
+    protected abstract T newResultInfo(String message, Exception e, String codeLocation);
+
+    //***************** COMMON **************************+
+
+
+    /**
+     * Computes the location string. If location is not null the location
+     * parameter is returned. If location is <code>null</code> the stacktrace
+     * is examined and tried to retrieve the location from there
+     */
+    private String makeLocation(Throwable e, String location) {
+        if (location == null && e != null){
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            if (stackTrace != null && stackTrace.length > 0){
+                StackTraceElement el = stackTrace[0];
+                location = locByStackTraceElement(el);
+            }
+        }
+        return location;
+    }
+
+    protected String getLocationByException() {
+        try {
+            throw new RuntimeException();
+        } catch (Exception e) {
+            StackTraceElement st = e.getStackTrace()[2];
+            return locByStackTraceElement(st);
+        }
+    }
+
+    private String locByStackTraceElement(StackTraceElement st) {
+        return st.getMethodName() + "(" + st.getClassName()+ ":" + st.getLineNumber() + ")";
+    }
+}
---
+++ cdmlib-model/src/main/java/eu/etaxonomy/cdm/strategy/parser/ParserResult.java
@@ -0,0 +1,67 @@
+/**
+* Copyright (C) 2025 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.strategy.parser;
+
+import eu.etaxonomy.cdm.common.ResultBase;
+
+/**
+ * @see IoResultBase
+ * @author muellera
+ * @since 27.10.2025
+ */
+public class ParserResult<E extends Object> extends ResultBase<ParserResult<E>.ParserInfo> {
+
+    //not yet really used
+//    private ParserResultState state;
+
+    private E entity;
+
+//    public enum ParserResultState{
+//        SUCCESS,               //All data parsed
+//        SUCCESS_WITH_WARNING,   //All data parsed but with some warnings
+//        FINISHED_WITH_ERROR,    //Probably all data parsed but with errors
+//        ABORTED,                //An handled exception occurred that lead to abort the parsing
+//        ;
+//    }
+
+    public class ParserInfo extends ResultBase.ResultInfoBase{
+
+        public ParserInfo(String msg, Exception e, String codeLocation) {
+            super(msg, e, codeLocation);
+        }
+
+    }
+
+ // ************* GETTERS / SETTERS / ADDERS ***********************/
+
+//    public void addException(Exception e, String message, String codeLocation) {
+//        exceptions.add(new ParserInfo(message, e, makeLocation(e, codeLocation)));
+//        setExceptionState();
+//    }
+
+    public E getEntity() {
+        return entity;
+    }
+    public void setEntity(E entity) {
+        this.entity = entity;
+    }
+
+   //**************************************************
+
+    @Override
+    protected ParserInfo newResultInfo(String message, Exception e, String codeLocation) {
+        ParserInfo result = new ParserInfo(message, e, codeLocation);
+        return result;
+    }
+
+    //not yet really used
+//    protected void setExceptionState() {
+//        state = ParserResultState.FINISHED_WITH_ERROR;
+//    }
+}
